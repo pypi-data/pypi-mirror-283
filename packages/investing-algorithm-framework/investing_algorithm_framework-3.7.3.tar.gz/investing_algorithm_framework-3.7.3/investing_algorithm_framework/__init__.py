e08cdd8ea548930934dc3d146c2f9bd429f3d25d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-04 14:02:56 +0000

    Add package investing-algorithm-framework/3.7.3/investing_algorithm_framework-3.7.3.tar.gz

---
+++ packages/investing-algorithm-framework/investing_algorithm_framework-3.7.3.tar.gz/investing_algorithm_framework-3.7.3/investing_algorithm_framework/__init__.py
@@ -0,0 +1,76 @@
+from investing_algorithm_framework.app import App, Algorithm, AppHook
+from investing_algorithm_framework.app import TradingStrategy, \
+    StatelessAction, Task
+from investing_algorithm_framework.domain import ApiException, \
+    TradingDataType, TradingTimeFrame, OrderType, OperationalException, \
+    OrderStatus, OrderSide, Config, TimeUnit, TimeInterval, Order, Portfolio, \
+    Position, TimeFrame, BACKTESTING_INDEX_DATETIME, MarketCredential, \
+    PortfolioConfiguration, RESOURCE_DIRECTORY, pretty_print_backtest, \
+    Trade, OHLCVMarketDataSource, OrderBookMarketDataSource, SYMBOLS, \
+    TickerMarketDataSource, MarketService, BacktestReportsEvaluation, \
+    pretty_print_backtest_reports_evaluation, load_backtest_reports, \
+    RESERVED_BALANCES, APP_MODE, AppMode, DATETIME_FORMAT, \
+    load_backtest_report, BacktestDateRange, create_ema_graph, \
+    create_prices_graph, create_rsi_graph, get_price_efficiency_ratio
+from investing_algorithm_framework.infrastructure import \
+    CCXTOrderBookMarketDataSource, CCXTOHLCVMarketDataSource, \
+    CCXTTickerMarketDataSource, CSVOHLCVMarketDataSource, \
+    CSVTickerMarketDataSource
+from .create_app import create_app
+from investing_algorithm_framework.services import \
+    create_trade_exit_markers_chart, create_trade_entry_markers_chart
+
+__all__ = [
+    "Algorithm",
+    "RESOURCE_DIRECTORY",
+    "App",
+    "AppHook",
+    "create_app",
+    "ApiException",
+    "TradingDataType",
+    "TradingTimeFrame",
+    "OrderType",
+    "OrderStatus",
+    "OrderSide",
+    "Config",
+    "PortfolioConfiguration",
+    "TimeUnit",
+    "TimeInterval",
+    "TradingStrategy",
+    "Order",
+    "Portfolio",
+    "Position",
+    "StatelessAction",
+    "Task",
+    "pretty_print_backtest",
+    "BACKTESTING_INDEX_DATETIME",
+    "Trade",
+    "TimeFrame",
+    "CCXTOrderBookMarketDataSource",
+    "CCXTTickerMarketDataSource",
+    "CCXTOHLCVMarketDataSource",
+    "OHLCVMarketDataSource",
+    "OrderBookMarketDataSource",
+    "TickerMarketDataSource",
+    "CSVOHLCVMarketDataSource",
+    "CSVTickerMarketDataSource",
+    "MarketCredential",
+    "MarketService",
+    "OperationalException",
+    "pretty_print_backtest_reports_evaluation",
+    "BacktestReportsEvaluation",
+    "load_backtest_reports",
+    "SYMBOLS",
+    "RESERVED_BALANCES",
+    "APP_MODE",
+    "AppMode",
+    "DATETIME_FORMAT",
+    "load_backtest_report",
+    "BacktestDateRange",
+    "create_trade_exit_markers_chart",
+    "create_trade_entry_markers_chart",
+    "create_ema_graph",
+    "create_prices_graph",
+    "create_rsi_graph",
+    "get_price_efficiency_ratio"
+]
