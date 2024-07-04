c96f14fdeb7dc2e009816b768f77172cb854c59e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-04 14:02:55 +0000

    Add package VerbalRegex/1.0.2/VerbalRegex-1.0.2.tar.gz

---
+++ packages/VerbalRegex/VerbalRegex-1.0.2.tar.gz/VerbalRegex-1.0.2/setup.py
@@ -0,0 +1,14 @@
+import setuptools
+
+setuptools.setup(
+    name="VerbalRegex",
+    version="1.0.2",
+    license='MIT',
+    author="dongmin",
+    author_email="happydm09@naver.com",
+    description='정규표현식을 쉽게 쓸 수 있게 합니다. Makes it easy to use regular expressions',
+    url="https://github.com/DM-09/Verbal-Regex.py/",
+    python_requires='>=3.1',
+    packages=setuptools.find_packages(),
+    keywords=['Regex', 'VerbalRegex', 'Verbal Regex', 'Regular Expression'],
+)
