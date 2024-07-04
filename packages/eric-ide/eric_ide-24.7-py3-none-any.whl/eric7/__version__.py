9713073bf295174dd11fa998cbe31c251a33f903
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-04 14:03:01 +0000

    Add package eric-ide/24.7/eric_ide-24.7-py3-none-any.whl

---
+++ packages/eric-ide/eric_ide-24.7-py3-none-any.whl/eric7/__version__.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+
+# Copyright (c) 2024 Detlev Offenbach <detlev@die-offenbachs.de>
+#
+
+"""
+Module containing some eric-ide version information.
+"""
+
+Version = "24.7 (rev. cbdafa2fc879)"
+VersionOnly = "24.7"
