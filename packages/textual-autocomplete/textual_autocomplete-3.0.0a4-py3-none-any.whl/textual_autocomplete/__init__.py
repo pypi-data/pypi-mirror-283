ac082c2435611d7045735354760e52a71de3a720
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-04 14:02:52 +0000

    Add package textual-autocomplete/3.0.0a4/textual_autocomplete-3.0.0a4-py3-none-any.whl

---
+++ packages/textual-autocomplete/textual_autocomplete-3.0.0a4-py3-none-any.whl/textual_autocomplete/__init__.py
@@ -0,0 +1,25 @@
+from textual_autocomplete._autocomplete import (
+    CompletionStrategy,
+    Dropdown,
+    InputState,
+)
+
+from textual_autocomplete._autocomplete2 import (
+    AutoComplete,
+    DropdownItem,
+    TargetState,
+    InvalidTarget,
+)
+
+from textual_autocomplete.matcher import Matcher
+
+__all__ = [
+    "AutoComplete",
+    "CompletionStrategy",
+    "Dropdown",
+    "DropdownItem",
+    "InputState",
+    "TargetState",
+    "InvalidTarget",
+    "Matcher",
+]
