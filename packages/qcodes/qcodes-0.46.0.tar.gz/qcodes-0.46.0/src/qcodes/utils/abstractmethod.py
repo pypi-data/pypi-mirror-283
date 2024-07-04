2a69d396232e36230399187e278c4052821e96bd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-04 14:02:59 +0000

    Add package qcodes/0.46.0/qcodes-0.46.0.tar.gz

---
+++ packages/qcodes/qcodes-0.46.0.tar.gz/qcodes-0.46.0/src/qcodes/utils/abstractmethod.py
@@ -0,0 +1,20 @@
+from typing import Callable, TypeVar
+
+from typing_extensions import ParamSpec
+
+input = ParamSpec("input")
+output = TypeVar("output")
+
+
+def qcodes_abstractmethod(funcobj: Callable[input, output]) -> Callable[input, output]:
+    """
+    A decorator indicating abstract methods.
+
+    This is heavily inspired by the decorator of the same name in
+    the ABC standard library. But we make our own version because
+    we actually want to allow the class with the abstract method to be
+    instantiated and we will use this property to detect if the
+    method is abstract and should be overwritten.
+    """
+    funcobj.__qcodes_is_abstract_method__ = True  # type: ignore[attr-defined]
+    return funcobj
