4fad574c8bac6684c42751f85b855d6fa01d7bf7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-05 05:05:08 +0000

    Add package sentry-infra-event-notifier/0.0.3/sentry_infra_event_notifier-0.0.3-py3-none-any.whl

---
+++ packages/sentry-infra-event-notifier/sentry_infra_event_notifier-0.0.3-py3-none-any.whl/infra_event_notifier/backends/jira.py
@@ -0,0 +1,11 @@
+from typing import Mapping
+
+
+def create_issue(
+    title: str,
+    text: str,
+    tags: Mapping[str, str],
+    jira_api_key: str,
+) -> None:
+    # TODO: implement
+    pass
