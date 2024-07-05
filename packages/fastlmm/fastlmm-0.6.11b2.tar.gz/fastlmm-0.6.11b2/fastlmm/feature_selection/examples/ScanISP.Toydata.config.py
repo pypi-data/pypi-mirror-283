2ef0d228dc6ca95ab0f5153cb6699c46b1183838
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-05 05:04:54 +0000

    Add package fastlmm/0.6.11b2/fastlmm-0.6.11b2.tar.gz

---
+++ packages/fastlmm/fastlmm-0.6.11b2.tar.gz/fastlmm-0.6.11b2/fastlmm/feature_selection/examples/ScanISP.Toydata.config.py
@@ -0,0 +1,31 @@
+exactUpdate = False
+topKbyLinReg = 16
+logdelta = np.log(16)
+
+if topKbyLinReg is not None:
+    outFile = "examples/ScanISP.exact%d.nSnps%d.logdelta%.2f.out.txt" % (
+        exactUpdate,
+        topKbyLinReg,
+        logdelta,
+    )
+else:
+    outFile = "examples/ScanISP.exact%d.all.logdelta%.2f.out.txt" % (
+        exactUpdate,
+        logdelta,
+    )
+
+obj = ScanISP(
+    phenoFile="examples/toydataTrain.phe",
+    # phenoTestFile = 'examples/toydataTest.phe',
+    bedFile="examples/toydata",
+    # fastlmmPath = 'C:/Users/t-baraki/Users/t-baraki/adaptivelmm/software/FaSTLMM.205.Win/Cpp_MKL',
+    fastlmmPath=r"C:\Users\lippert\Projects\FaST-LMM_old\FastLmm\CPP\x64\MKL-Release",
+    windowByPosition=1e1,
+    logdelta=logdelta,
+    topKbyLinReg=topKbyLinReg,
+    exactUpdate=exactUpdate,
+    # extractSim = 'examples/LmmGWAS.snps.txt',
+    outFile=outFile,
+    doPlots=False,
+    doDebugging=True,
+)
---
+++ packages/fastlmm/fastlmm-0.6.11b2.tar.gz/fastlmm-0.6.11b2/fastlmm/util/__init__.py
@@ -0,0 +1,3 @@
+from fastlmm.util.compute_auto_pcs import compute_auto_pcs
+from fastlmm.util.util import manhattan_plot
+from fastlmm.util._example_file import example_file
