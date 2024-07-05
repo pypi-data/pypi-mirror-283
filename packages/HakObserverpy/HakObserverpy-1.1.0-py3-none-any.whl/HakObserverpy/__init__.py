021ff35adf245ce8c0a886c238584ad7b510e3b3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-05 04:02:53 +0000

    Add package HakObserverpy/1.1.0/HakObserverpy-1.1.0-py3-none-any.whl

---
+++ packages/HakObserverpy/HakObserverpy-1.1.0-py3-none-any.whl/HakObserverpy/__init__.py
@@ -0,0 +1,10 @@
+# Import key functions and classes
+from .HakObserverpy import InitiateCollecection  # Import the function from the correct module
+
+# Optional: Define a version number for the package
+__version__ = "1.1.0"
+
+# Optional: Define the package's public interface
+__all__ = ["InitiateCollecection"]
+
+# # Import key functions and classes
