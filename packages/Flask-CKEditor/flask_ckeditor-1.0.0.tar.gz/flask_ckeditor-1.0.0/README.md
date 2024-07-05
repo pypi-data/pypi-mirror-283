64ef2e9ad812b47daf96bd6271d71c470d4acae6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-05 04:03:17 +0000

    Add package Flask-CKEditor/1.0.0/flask_ckeditor-1.0.0.tar.gz

---
+++ packages/Flask-CKEditor/flask_ckeditor-1.0.0.tar.gz/flask_ckeditor-1.0.0/README.md
@@ -0,0 +1,25 @@
+# Flask-CKEditor
+
+[![GitHub License](https://img.shields.io/github/license/helloflask/flask-ckeditor%20)](https://github.com/helloflask/flask-ckeditor/blob/master/LICENSE)
+[![PyPI - Python Version](https://img.shields.io/pypi/pyversions/flask-ckeditor)](https://pypi.org/project/Flask-CKEditor/)
+[![PyPI - Version](https://img.shields.io/pypi/v/flask-ckeditor)](https://pypi.org/project/Flask-CKEditor/)
+[![Build Status](https://github.com/helloflask/flask-ckeditor/workflows/build/badge.svg)](https://github.com/helloflask/flask-ckeditor/actions)
+[![codecov](https://codecov.io/gh/helloflask/flask-ckeditor/graph/badge.svg?token=bCkkuXW4dX)](https://codecov.io/gh/helloflask/flask-ckeditor)
+
+CKEditor integration for Flask, including image upload, code syntax highlighting, and more.
+
+> [!IMPORTANT]\
+> Since CKEditor 4's end of life, and we will not support CKEditor 5 (GPL licensed).
+> This project is in maintainance mode now. There will be no new features, only bug fixes.
+> For security reasons, please consider using paid CKEditor 4 LTS version, CKEditor 5, or other alternatives.
+
+## Links
+
+* [Documentation](https://flask-ckeditor.readthedocs.io/en/latest/)
+* [PyPI](https://pypi.org/project/Flask-CKEditor/)
+* [Examples](https://github.com/helloflask/flask-ckeditor/tree/master/examples)
+
+
+## License
+
+This project is licensed under the MIT License (see the `LICENSE` file for details).
---
+++ packages/Flask-CKEditor/flask_ckeditor-1.0.0.tar.gz/flask_ckeditor-1.0.0/setup.py
@@ -0,0 +1,54 @@
+"""
+Flask-CKEditor
+--------------
+
+CKEditor integration for Flask, including image upload, code syntax
+highlighting, and more.
+
+Features:
+
+* Integrate with Flask-WTF/WTForms.
+* Configure CKEditor through Flask's configuration system.
+* Image upload support.
+* Code snippet highlighting.
+* Built-in CKEditor resources.
+"""
+from setuptools import setup
+
+setup(
+    name='Flask-CKEditor',
+    version='1.0.0',
+    url='http://github.com/helloflask/flask-ckeditor',
+    license='MIT',
+    author='Grey Li',
+    author_email='withlihui@gmail.com',
+    description='CKEditor integration for Flask.',
+    long_description=__doc__,
+    long_description_content_type='text/markdown',
+    packages=['flask_ckeditor'],
+    zip_safe=False,
+    include_package_data=True,
+    platforms='any',
+    test_suite='test_flask_ckeditor',
+    install_requires=[
+        'Flask'
+    ],
+    extras_require={
+        'all': ['flask-wtf', 'bleach']
+    },
+    classifiers=[
+        'Environment :: Web Environment',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
+        'Programming Language :: Python :: 3.11',
+        'Programming Language :: Python :: 3.12',
+        'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
+        'Topic :: Software Development :: Libraries :: Python Modules'
+    ]
+)
