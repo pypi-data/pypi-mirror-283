0e031edede010b1cfed6387d927454d1af44248f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-05 15:04:08 +0000

    Add package pyspark-eda/1.6.0/pyspark_eda-1.6.0.tar.gz

---
+++ packages/pyspark-eda/pyspark_eda-1.6.0.tar.gz/pyspark_eda-1.6.0/setup.py
@@ -0,0 +1,20 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='pyspark_eda',  # Updated name to match your package
+    version='1.6.0',
+    packages=find_packages(),
+    description='A Python package for univariate ,bivariate and multivariate data analysis using PySpark',
+    author='Tanya Irani', 
+    author_email='tanyairani22@gmail.com.com',  
+    keywords='data analysis pyspark univariate bivariate mutlivariate statistics',  # Add relevant keywords
+    install_requires=[
+        'pyspark>=3.0.0',
+        'matplotlib>=3.0.0',
+        'scipy>=1.4.0',
+        'seaborn>=0.11.0'
+    ],
+    python_requires='>=3.6',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+)
