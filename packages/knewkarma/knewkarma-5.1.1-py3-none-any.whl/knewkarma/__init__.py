e2ac06745a152710f09ab8a0809be9fcb6aaee1d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-05 15:03:58 +0000

    Add package knewkarma/5.1.1/knewkarma-5.1.1-py3-none-any.whl

---
+++ packages/knewkarma/knewkarma-5.1.1-py3-none-any.whl/knewkarma/__init__.py
@@ -0,0 +1,17 @@
+from ._core import (
+    Post,
+    Posts,
+    Search,
+    Subreddit,
+    Subreddits,
+    User,
+)
+
+__all__ = [
+    "Post",
+    "Posts",
+    "Search",
+    "Subreddit",
+    "Subreddits",
+    "User",
+]
