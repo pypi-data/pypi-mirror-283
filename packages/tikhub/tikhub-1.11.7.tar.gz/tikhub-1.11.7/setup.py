c53b52ac97a8a1b0ce47f1162afae313cb3b639c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-05 03:03:32 +0000

    Add package tikhub/1.11.7/tikhub-1.11.7.tar.gz

---
+++ packages/tikhub/tikhub-1.11.7.tar.gz/tikhub-1.11.7/setup.py
@@ -0,0 +1,36 @@
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
+# RUN Command Line:
+# 1.Build-check dist folder
+# python setup.py sdist bdist_wheel
+# 2.Upload to PyPi
+# twine upload dist/*
+
+from setuptools import setup, find_packages
+
+with open("README.md", "r", encoding='utf8') as fh:
+    long_description = fh.read()
+
+version = "1.11.7"
+
+setup(
+    name="tikhub",
+    version=version,
+    author="TikHub.io",
+    author_email="tikhub.io@proton.me",
+    description="A Python SDK for TikHub RESTful API",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/TikHubIO/TikHub-API-SDK-Python",
+    packages=find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: Apache Software License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+    install_requires=[
+        "httpx>=0.27.0",
+        "rich~=13.7.1",
+    ],
+)
