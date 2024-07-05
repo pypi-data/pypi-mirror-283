6dd06d9a9ff42716c9eb083c39cb40b262231733
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-05 03:03:34 +0000

    Add package mightstone/0.11.0/mightstone-0.11.0.tar.gz

---
+++ packages/mightstone/mightstone-0.11.0.tar.gz/mightstone-0.11.0/src/mightstone/services/scryfall/__init__.py
@@ -0,0 +1,21 @@
+import mightstone.services.scryfall.models
+
+from .api import Scryfall
+from .models import (
+    Card,
+    Catalog,
+    Error,
+    ManaCost,
+    Migration,
+    Ruling,
+    SerializableCard,
+    SerializableCatalog,
+    SerializableMigration,
+    SerializableRuling,
+    SerializableSet,
+    SerializableSymbol,
+    SerializableTag,
+    Set,
+    Symbol,
+    Tag,
+)
