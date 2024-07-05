eea926d9620205881d2c1ad67c124c14ab86c78b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-05 04:02:54 +0000

    Add package council-ai/0.0.20/council_ai-0.0.20-py3-none-any.whl

---
+++ packages/council-ai/council_ai-0.0.20-py3-none-any.whl/council/contexts/_composite_message_collection.py
@@ -0,0 +1,25 @@
+from typing import Iterable, List
+
+from ._chat_message import ChatMessage
+from ._message_collection import MessageCollection
+
+
+class CompositeMessageCollection(MessageCollection):
+    """
+    Wraps multiple :class:`MessageCollection` as one.
+    """
+
+    _collections: List[MessageCollection]
+
+    def __init__(self, collections: List[MessageCollection]) -> None:
+        self._collections = collections
+
+    @property
+    def messages(self) -> Iterable[ChatMessage]:
+        for collection in self._collections:
+            yield from collection.messages
+
+    @property
+    def reversed(self) -> Iterable[ChatMessage]:
+        for collection in reversed(self._collections):
+            yield from collection.reversed
---
+++ packages/council-ai/council_ai-0.0.20-py3-none-any.whl/council/evaluators/evaluator_base.py
@@ -0,0 +1,39 @@
+from abc import ABC, abstractmethod
+from typing import List
+
+from council.contexts import AgentContext, Monitorable, ScoredChatMessage
+
+
+class EvaluatorException(Exception):
+    def __init__(self, message: str):
+        super().__init__(message)
+
+
+class EvaluatorBase(Monitorable, ABC):
+    """
+    Abstract base class for an agent evaluator.
+
+    """
+
+    def __init__(self) -> None:
+        super().__init__("evaluator")
+
+    def execute(self, context: AgentContext) -> List[ScoredChatMessage]:
+        """
+        Executes the evaluator on the agent's context within the given budget.
+
+        Args:
+            context (AgentContext): The context for executing the evaluator.
+
+        Returns:
+            List[ScoredChatMessage]: A list of scored agent messages resulting from the evaluation.
+
+        Raises:
+            None
+        """
+        with context.log_entry:
+            return self._execute(context=context)
+
+    @abstractmethod
+    def _execute(self, context: AgentContext) -> List[ScoredChatMessage]:
+        pass
