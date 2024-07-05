53e2eac2349288d1cc5fa91a077cf4fd20d0642d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-05 15:03:50 +0000

    Add package fakts/0.5.0/fakts-0.5.0-py3-none-any.whl

---
+++ packages/fakts/fakts-0.5.0-py3-none-any.whl/fakts/grants/io/yaml.py
@@ -0,0 +1,29 @@
+import yaml
+from fakts.models import FaktsRequest, FaktValue
+from typing import Dict
+from pydantic import BaseModel
+
+
+class YamlGrant(BaseModel):
+    """
+    Represent a Grant that loads configuration from a YAML file.
+
+    Attributes
+    ----------
+    filepath : str
+        The path of the YAML file.
+    """
+
+    filepath: str
+
+    async def aload(self, request: FaktsRequest) -> Dict[str, FaktValue]:
+        """Loads the YAML file and returns the configuration."""
+        with open(self.filepath, "r") as file:
+            config = yaml.load(file, Loader=yaml.FullLoader)  # type: ignore #TODO: Check why this is not working
+
+        return config
+
+    class Config:
+        """A pydantic config class"""
+
+        arbitrary_types_allowed = True
