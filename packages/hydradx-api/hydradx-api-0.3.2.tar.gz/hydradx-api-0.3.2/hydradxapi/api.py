7c56b6f8451c3d804f33df97e0f732bf9175ac6c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-05 16:03:08 +0000

    Add package hydradx-api/0.3.2/hydradx-api-0.3.2.tar.gz

---
+++ packages/hydradx-api/hydradx-api-0.3.2.tar.gz/hydradx-api-0.3.2/hydradxapi/api.py
@@ -0,0 +1,39 @@
+from hydradxapi.pallets.fees import DynamicFees
+from hydradxapi.pallets.omnipool import Omnipool
+from hydradxapi.pallets.oracle import Oracle
+from hydradxapi.pallets.registry import AssetRegistry
+from hydradxapi.pallets.stableswap import StableSwap
+from hydradxapi.pallets.tokens import Tokens
+
+
+class API:
+    """
+    HydraDX chain API
+    """
+
+    def __init__(self, client):
+        self._client = client
+
+    @property
+    def omnipool(self):
+        return Omnipool(self._client)
+
+    @property
+    def fees(self):
+        return DynamicFees(self._client)
+
+    @property
+    def registry(self):
+        return AssetRegistry(self._client)
+
+    @property
+    def stableswap(self):
+        return StableSwap(self._client)
+
+    @property
+    def tokens(self):
+        return Tokens(self._client)
+
+    @property
+    def oracle(self):
+        return Oracle(self._client)
