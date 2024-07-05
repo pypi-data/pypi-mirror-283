75fe2b197f7c629905398ede8d9df8522796ff64
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-05 15:04:10 +0000

    Add package artemis-py/0.2.0/artemis_py-0.2.0.tar.gz

---
+++ packages/artemis-py/artemis_py-0.2.0.tar.gz/artemis_py-0.2.0/setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setup(
+    name="artemis_py",
+    version="0.2.0",
+    description="A package to interact with the Artemis API. Blame ChatGPT for errors.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    author="Matt Maximo",
+    author_email="matt@pioneerdigital.org",
+    url="https://github.com/MattMaximo/artemis_py",
+    packages=find_packages(),
+    install_requires=[
+        "pandas",
+        "requests",
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
