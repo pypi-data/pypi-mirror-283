d45af6a382e8926fb0abaca6d980d4d61de07d70
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-05 16:03:15 +0000

    Add package arkitekt-next/0.7.8/arkitekt_next-0.7.8.tar.gz

---
+++ packages/arkitekt-next/arkitekt_next-0.7.8.tar.gz/arkitekt_next-0.7.8/arkitekt_next/cli/commands/inspect/definitions.py
@@ -0,0 +1,53 @@
+import rich_click as click
+from importlib import import_module
+from arkitekt_next.cli.vars import get_console, get_manifest
+from arkitekt_next.cli.options import with_builder
+import json
+
+
+async def run_app(app):
+    async with app:
+        await app.rekuest.run()
+
+
+@click.command("prod")
+@click.pass_context
+@click.option(
+    "--pretty",
+    "-p",
+    help="Should we just output json?",
+    is_flag=True,
+    default=False,
+)
+@with_builder
+def definitions(
+    ctx,
+    pretty: bool,
+    builder=None,
+):
+    """Runs the app in production mode
+
+    \n
+    You can specify the builder to use with the --builder flag. By default, the easy builder is used, which is designed to be easy to use and to get started with.
+
+    """
+    from rekuest.definition.registry import get_default_definition_registry
+
+    manifest = get_manifest(ctx)
+    console = get_console(ctx)
+
+    entrypoint = manifest.entrypoint
+
+    with console.status("Loading entrypoint module..."):
+        try:
+            import_module(entrypoint)
+        except ModuleNotFoundError as e:
+            console.print(f"Could not find entrypoint module {entrypoint}")
+            raise e
+
+    definitions = get_default_definition_registry().dump()
+
+    if pretty:
+        console.print(json.dumps(definitions, indent=2))
+    else:
+        print(json.dumps(definitions))
