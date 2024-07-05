d0838b171f6312f05525d44030e1f66a6e9266fb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-05 04:02:58 +0000

    Add package zen-engine/0.22.0/zen_engine-0.22.0.tar.gz

---
+++ packages/zen-engine/zen_engine-0.22.0.tar.gz/zen_engine-0.22.0/local_dependencies/zen-engine/src/loader/closure.rs
@@ -0,0 +1,34 @@
+use crate::loader::{DecisionLoader, LoaderResponse};
+use async_trait::async_trait;
+use std::future::Future;
+
+/// Loads decisions using an async closure
+#[derive(Debug)]
+pub struct ClosureLoader<F>
+where
+    F: Sync + Send,
+{
+    closure: F,
+}
+
+impl<F, O> ClosureLoader<F>
+where
+    F: Fn(String) -> O + Sync + Send,
+    O: Future<Output = LoaderResponse> + Send,
+{
+    pub fn new(closure: F) -> Self {
+        Self { closure }
+    }
+}
+
+#[async_trait]
+impl<F, O> DecisionLoader for ClosureLoader<F>
+where
+    F: Fn(String) -> O + Sync + Send,
+    O: Future<Output = LoaderResponse> + Send,
+{
+    async fn load(&self, key: &str) -> LoaderResponse {
+        let closure = &self.closure;
+        closure(key.to_string()).await
+    }
+}
