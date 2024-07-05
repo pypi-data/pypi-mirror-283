3c2c455964bc54a6bbcabbfc8244720c1384b0ef
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-05 04:03:30 +0000

    Add package zdxtools/0.0.28/zdxtools-0.0.28.tar.gz

---
+++ packages/zdxtools/zdxtools-0.0.28.tar.gz/zdxtools-0.0.28/tests/test1.py
@@ -0,0 +1,22 @@
+from pypi_update.src.zdxtools.dx_tools import decorator
+from pypi_update.src.zdxtools.dx_tools import spider_tools
+import time
+
+
+class a :
+    @decorator.ExceptionD
+    def test(self):
+        data = {
+            'a':1,
+            '222':'2',
+            'getsig':'213123',
+        }
+        int(data)
+    def test1(self):
+
+        print(spider_tools.return_header(choice='Chrome'))
+
+
+if __name__ == '__main__':
+    b = a()
+    b.test1()
