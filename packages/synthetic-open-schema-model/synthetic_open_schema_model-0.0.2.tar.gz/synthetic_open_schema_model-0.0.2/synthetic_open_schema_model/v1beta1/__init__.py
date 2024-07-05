55c1f5d03880b1a0e56233bb10439977a1ca67dd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-05 06:03:13 +0000

    Add package synthetic-open-schema-model/0.0.2/synthetic_open_schema_model-0.0.2.tar.gz

---
+++ packages/synthetic-open-schema-model/synthetic_open_schema_model-0.0.2.tar.gz/synthetic_open_schema_model-0.0.2/synthetic_open_schema_model/v1beta1/__init__.py
@@ -0,0 +1,6 @@
+from .dns import DnsCheck
+from .http import HttpCheck
+from .tcp import TcpCheck
+from .channels import WebhookChannel
+from .playwright import PlaywrightCheck
+from .ssl import SslCheck 
