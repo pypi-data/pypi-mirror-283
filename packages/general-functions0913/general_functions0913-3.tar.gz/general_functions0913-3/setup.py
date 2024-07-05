fd6350b2ab59bbc680c9565b3f5752c2d0da7b37
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-05 15:03:39 +0000

    Add package general-functions0913/3/general_functions0913-3.tar.gz

---
+++ packages/general-functions0913/general_functions0913-3.tar.gz/general_functions0913-3/setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='general_functions0913',
+    version='3',
+    description='this sdk is a working protype with file encryption',
+    author='rishabh_sharma',
+    author_email='rishabhsharmabwr@gmail.com',
+    packages=find_packages(),
+    install_requires=[
+        # Add any other dependencies here
+    ],
+)
+
+#command to run setup.py
+    # -> python3 setup.py sdist
+#command to uplode sdk package to PyPi account
+    # -> twine upload dist/*
