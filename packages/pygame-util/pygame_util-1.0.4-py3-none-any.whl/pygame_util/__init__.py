ca27ac8e955d3a01038f340653028217d8dea278
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-05 14:04:21 +0000

    Add package pygame-util/1.0.4/pygame_util-1.0.4-py3-none-any.whl

---
+++ packages/pygame-util/pygame_util-1.0.4-py3-none-any.whl/pygame_util/__init__.py
@@ -0,0 +1,8 @@
+"A package that simplifies repetitive pygame code."
+
+from .color import Color
+from .image import load_image
+from .position import RelativePosition, position_relative_to
+from .text import Text, TextStyle
+from .timer import Timer
+from .window import Window
