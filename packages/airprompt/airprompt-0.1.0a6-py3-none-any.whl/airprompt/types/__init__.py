c180857782820b916a07cdb69a4203c4eef76312
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-05 04:03:14 +0000

    Add package airprompt/0.1.0a6/airprompt-0.1.0a6-py3-none-any.whl

---
+++ packages/airprompt/airprompt-0.1.0a6-py3-none-any.whl/airprompt/types/__init__.py
@@ -0,0 +1,6 @@
+# File generated from our OpenAPI spec by Stainless. See CONTRIBUTING.md for details.
+
+from __future__ import annotations
+
+from .prompt import Prompt as Prompt
+from .prompt_get_params import PromptGetParams as PromptGetParams
