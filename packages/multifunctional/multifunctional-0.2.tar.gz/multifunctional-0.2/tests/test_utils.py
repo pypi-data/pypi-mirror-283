72f0ebae71ccd901660c34edb6b79145b5864e65
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-05 15:03:46 +0000

    Add package multifunctional/0.2/multifunctional-0.2.tar.gz

---
+++ packages/multifunctional/multifunctional-0.2.tar.gz/multifunctional-0.2/tests/test_utils.py
@@ -0,0 +1,54 @@
+from multifunctional.utils import add_exchange_input_if_missing
+
+
+def test_add_exchange_input_if_missing(caplog):
+    given = {
+        ("db", "code"): {
+            "exchanges": [
+                {"functional": False},
+                {},
+                {"functional": True, "input": ("db", "other")},
+                {"functional": True, "input": ("db", "foo"), "code": "bar"},
+                {"functional": True, "code": "foo"},
+                {"functional": True, "code": "foo", "database": "something"},
+                {"functional": True},
+            ]
+        },
+        ("db", "more"): {
+            "database": "me",
+            "exchanges": [
+                {"functional": True, "code": "foo"},
+            ],
+        },
+    }
+    expected = {
+        ("db", "code"): {
+            "exchanges": [
+                {"functional": False},
+                {},
+                {"functional": True, "input": ("db", "other"), "code": "other"},
+                {"functional": True, "input": ("db", "foo"), "code": "foo"},
+                {"functional": True, "input": ("db", "foo"), "code": "foo"},
+                {
+                    "functional": True,
+                    "input": ("something", "foo"),
+                    "code": "foo",
+                    "database": "something",
+                },
+                {
+                    "functional": True,
+                    "input": ("db", "code"),
+                    "code": "code",
+                    "mf_artificial_code": True,
+                },
+            ]
+        },
+        ("db", "more"): {
+            "database": "me",
+            "exchanges": [
+                {"functional": True, "code": "foo", "input": ("db", "foo")},
+            ],
+        },
+    }
+    assert add_exchange_input_if_missing(given) == expected
+    assert "given 'code' is 'bar' but 'input' code is 'foo'" in caplog.text
