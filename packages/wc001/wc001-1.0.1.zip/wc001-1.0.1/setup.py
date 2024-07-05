53aa45d9a0de6ab7a00abc20629043c5b8277129
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-05 15:03:40 +0000

    Add package wc001/1.0.1/wc001-1.0.1.zip

---
+++ packages/wc001/wc001-1.0.1.zip/wc001-1.0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+# def readme_file():
+#       with open("README.rst",encoding="utf-8") as rf:
+#             return rf.read()
+
+setup(name="wc001", version="1.0.1", description="王聪的第一个自建库",
+      packages=["sztestlib"], py_modules=["Tool"], author="Sz", author_email="wangshu10@chinaunicom.cn",
+      license="MIT")
