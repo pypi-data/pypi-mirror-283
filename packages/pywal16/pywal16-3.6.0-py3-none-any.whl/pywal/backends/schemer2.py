b18a6ae1ff8d7396d022328888889ed0ccd76e48
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-05 15:04:09 +0000

    Add package pywal16/3.6.0/pywal16-3.6.0-py3-none-any.whl

---
+++ packages/pywal16/pywal16-3.6.0-py3-none-any.whl/pywal/backends/schemer2.py
@@ -0,0 +1,36 @@
+"""
+Generate a colorscheme using Schemer2.
+"""
+import logging
+import shutil
+import subprocess
+import sys
+
+from .. import colors
+from .. import util
+
+
+def gen_colors(img):
+    """Generate a colorscheme using Colorz."""
+    cmd = ["schemer2", "-format", "img::colors", "-minBright", "75", "-in"]
+    return subprocess.check_output([*cmd, img]).splitlines()
+
+
+def adjust(cols, light, cols16):
+    """Create palette."""
+    cols.sort(key=util.rgb_to_yiq)
+    raw_colors = [*cols[8:], *cols[8:]]
+    raw_colors[0] = util.darken_color(cols[0], 0.80)
+
+    return colors.generic_adjust(raw_colors, light, cols16)
+
+
+def get(img, light=False, cols16=False):
+    """Get colorscheme."""
+    if not shutil.which("schemer2"):
+        logging.error("Schemer2 wasn't found on your system.")
+        logging.error("Try another backend. (wal --backend)")
+        sys.exit(1)
+
+    cols = [col.decode('UTF-8') for col in gen_colors(img)]
+    return adjust(cols, light, cols16)
