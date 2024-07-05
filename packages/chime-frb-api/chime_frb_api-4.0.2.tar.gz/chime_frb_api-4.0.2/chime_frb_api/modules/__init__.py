cc6d45f892ebfed8374605e3186439c0f4864529
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-05 05:04:47 +0000

    Add package chime-frb-api/4.0.2/chime_frb_api-4.0.2.tar.gz

---
+++ packages/chime-frb-api/chime_frb_api-4.0.2.tar.gz/chime_frb_api-4.0.2/chime_frb_api/modules/__init__.py
@@ -0,0 +1,19 @@
+from workflow.http import buckets  # noqa: F401
+from workflow.http import results  # noqa: F401
+
+from chime_frb_api.modules import (  # noqa: F401
+    bucket,
+    calibration,
+    candidates,
+    catalog,
+    distributor,
+    events,
+    metrics,
+    mimic,
+    parameters,
+    sources,
+    swarm,
+    tns,
+    voe,
+    voe_subscribers,
+)
