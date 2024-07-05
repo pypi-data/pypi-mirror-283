ce789cfb102c70112ee7747cb23d87858afce842
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-05 14:04:47 +0000

    Add package puffpastry/0.0.2/puffpastry-0.0.2-py3-none-any.whl

---
+++ packages/puffpastry/puffpastry-0.0.2-py3-none-any.whl/puffpastry/tests/test_api.py
@@ -0,0 +1,29 @@
+import pytest
+
+from puffpastry import Application, create, provides
+
+
+def test_create():
+    app = create("test_app")
+
+    assert isinstance(app, Application)
+
+
+def test_provides_and_load():
+    cls = type("MyClass")
+
+    @provides("my_component")
+    def create_component(app):
+        obj = cls()
+        return obj
+    
+    app = create("test_app").load("my_component")
+
+    assert isinstance(app, Application)
+    assert isinstance(app.my_component, cls)
+
+
+def test_create_twice_without_force():
+    create("test_app")
+    with pytest.raises(KeyError):
+        create("test_App", force=False)
