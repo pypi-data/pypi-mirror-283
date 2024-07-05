86eee6ac47f3bf3c2017cc64be6abe0afa53d23e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-05 16:03:24 +0000

    Add package Uni-Curses/3.1.2/Uni-Curses-3.1.2.tar.gz

---
+++ packages/Uni-Curses/Uni-Curses-3.1.2.tar.gz/Uni-Curses-3.1.2/setup.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+
+from setuptools import setup 
+
+setup(name='Uni-Curses',
+      version='v3.1.2',
+      description='Unified Curses Wrapper for Python',
+      long_description='A universal Curses wrapper for Python on Windows, FreeBSD, Linux, and\nMac OS X, with syntax close to the original NCurses. In order\nto provide Curses functionality on Windows it utilizes the ctype\nforeign function interface to wrap PDCurses, a free and open-source\nCurses implementation for Windows.\n\nif you have `ModuleNotFoundError` try specifying the python version like use `python3.9 -m pip install uni-curses`',
+      author='Michael Kamensky',
+      author_email='stavdev@mail.ru',
+      maintainer='George Chousos',
+      maintainer_email='gxousos@gmail.com',
+      url='https://github.com/unicurses/unicurses',
+      license='General Public License v3',
+      platforms=['Windows', 'FreeBSD', 'Linux', 'Mac OS X'],
+      packages=['unicurses'],
+      package_data={'unicurses': ['32 bit binaries/*/*.*', '64 bit binaries/*/*.*']},
+      install_requires=[
+          'x256',
+      ],
+      zip_safe=False)
+
+# pip3 install .
+# python setup.py sdist
+# twine upload dist/*
+
