01a130e8890591ab87bc7d5fab1430b095f36f39
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-05 03:03:25 +0000

    Add package kpops/6.0.2/kpops-6.0.2-py3-none-any.whl

---
+++ packages/kpops/kpops-6.0.2-py3-none-any.whl/kpops/api/registry.py
@@ -0,0 +1,76 @@
+from __future__ import annotations
+
+import importlib
+import inspect
+import logging
+import sys
+from dataclasses import dataclass, field
+from pathlib import Path
+from typing import TYPE_CHECKING, TypeVar
+
+from kpops import __name__
+from kpops.api.exception import ClassNotFoundError
+from kpops.components.base_components.pipeline_component import PipelineComponent
+
+if TYPE_CHECKING:
+    from collections.abc import Iterator
+
+KPOPS_MODULE = __name__ + "."
+
+T = TypeVar("T")
+ClassDict = dict[str, type[T]]  # type -> class
+
+sys.path.append(str(Path.cwd()))
+log = logging.getLogger("Registry")
+
+
+@dataclass
+class Registry:
+    """Class Registry to store and retrieve PipelineComponents."""
+
+    _classes: ClassDict[PipelineComponent] = field(default_factory=dict, init=False)
+
+    def find_components(self, module_name: str) -> None:
+        """Find all PipelineComponent subclasses in module.
+
+        :param module_name: name of the python module.
+        """
+        for _class in _find_classes(module_name, PipelineComponent):
+            self._classes[_class.type] = _class
+
+    def __getitem__(self, component_type: str) -> type[PipelineComponent]:
+        try:
+            return self._classes[component_type]
+        except KeyError as ke:
+            msg = f"Could not find a component of type {component_type}"
+            raise ClassNotFoundError(msg) from ke
+
+
+def find_class(module_name: str, baseclass: type[T]) -> type[T]:
+    try:
+        return next(_find_classes(module_name, baseclass))
+    except StopIteration as e:
+        raise ClassNotFoundError from e
+
+
+def _find_classes(module_name: str, baseclass: type[T]) -> Iterator[type[T]]:
+    module = importlib.import_module(module_name)
+    if module.__file__ and not module_name.startswith(KPOPS_MODULE):
+        file_path = Path(module.__file__)
+        try:
+            rel_path = file_path.relative_to(Path.cwd())
+            log.debug(f"Picked up: {rel_path}")
+        except ValueError:
+            log.debug(f"Picked up: {file_path}")
+    for _, _class in inspect.getmembers(module, inspect.isclass):
+        if not __filter_internal_kpops_classes(
+            _class.__module__, module_name
+        ) and issubclass(_class, baseclass):
+            yield _class
+
+
+def __filter_internal_kpops_classes(class_module: str, module_name: str) -> bool:
+    # filter out internal kpops classes and components unless specifically requested
+    return class_module.startswith(KPOPS_MODULE) and not module_name.startswith(
+        KPOPS_MODULE
+    )
---
+++ packages/kpops/kpops-6.0.2-py3-none-any.whl/kpops/components/base_components/kafka_app.py
@@ -0,0 +1,145 @@
+from __future__ import annotations
+
+import logging
+from abc import ABC
+from collections.abc import Callable
+from typing import Any
+
+import pydantic
+from pydantic import AliasChoices, ConfigDict, Field
+from typing_extensions import override
+
+from kpops.components.base_components.cleaner import Cleaner
+from kpops.components.base_components.helm_app import HelmAppValues
+from kpops.components.base_components.models.topic import KafkaTopic, KafkaTopicStr
+from kpops.components.base_components.pipeline_component import PipelineComponent
+from kpops.components.streams_bootstrap import StreamsBootstrap
+from kpops.utils.docstring import describe_attr
+from kpops.utils.pydantic import (
+    CamelCaseConfigModel,
+    DescConfigModel,
+    exclude_by_value,
+    exclude_defaults,
+)
+
+log = logging.getLogger("KafkaApp")
+
+
+class KafkaStreamsConfig(CamelCaseConfigModel, DescConfigModel):
+    """Kafka Streams config.
+
+    :param brokers: Brokers
+    :param schema_registry_url: URL of the schema registry, defaults to None
+    :param extra_output_topics: Extra output topics
+    :param output_topic: Output topic, defaults to None
+    """
+
+    brokers: str = Field(default=..., description=describe_attr("brokers", __doc__))
+    schema_registry_url: str | None = Field(
+        default=None,
+        validation_alias=AliasChoices(
+            "schema_registry_url", "schemaRegistryUrl"
+        ),  # TODO: same for other camelcase fields, avoids duplicates during enrichment
+        description=describe_attr("schema_registry_url", __doc__),
+    )
+    extra_output_topics: dict[str, KafkaTopicStr] = Field(
+        default={}, description=describe_attr("extra_output_topics", __doc__)
+    )
+    output_topic: KafkaTopicStr | None = Field(
+        default=None,
+        description=describe_attr("output_topic", __doc__),
+        json_schema_extra={},
+    )
+
+    model_config = ConfigDict(extra="allow")
+
+    @pydantic.field_validator("extra_output_topics", mode="before")
+    @classmethod
+    def deserialize_extra_output_topics(
+        cls, extra_output_topics: Any
+    ) -> dict[str, KafkaTopic] | Any:
+        if isinstance(extra_output_topics, dict):
+            return {
+                role: KafkaTopic(name=topic_name)
+                for role, topic_name in extra_output_topics.items()
+            }
+        return extra_output_topics
+
+    @pydantic.field_serializer("extra_output_topics")
+    def serialize_extra_output_topics(
+        self, extra_topics: dict[str, KafkaTopic]
+    ) -> dict[str, str]:
+        return {role: topic.name for role, topic in extra_topics.items()}
+
+    # TODO(Ivan Yordanov): Currently hacky and potentially unsafe. Find cleaner solution
+    @pydantic.model_serializer(mode="wrap", when_used="always")
+    def serialize_model(
+        self, handler: Callable, info: pydantic.SerializationInfo
+    ) -> dict[str, Any]:
+        return exclude_defaults(self, exclude_by_value(handler(self), None))
+
+
+class KafkaAppValues(HelmAppValues):
+    """Settings specific to Kafka Apps.
+
+    :param streams: Kafka streams config
+    """
+
+    streams: KafkaStreamsConfig = Field(
+        default=..., description=describe_attr("streams", __doc__)
+    )
+
+
+class KafkaAppCleaner(Cleaner, StreamsBootstrap, ABC):
+    """Helm app for resetting and cleaning a streams-bootstrap app."""
+
+    from_: None = None
+    to: None = None
+
+    @property
+    @override
+    def helm_chart(self) -> str:
+        raise NotImplementedError
+
+    @override
+    async def clean(self, dry_run: bool) -> None:
+        """Clean an app using a cleanup job.
+
+        :param dry_run: Dry run command
+        """
+        log.info(f"Uninstall old cleanup job for {self.helm_release_name}")
+        await self.destroy(dry_run)
+
+        log.info(f"Init cleanup job for {self.helm_release_name}")
+        await self.deploy(dry_run)
+
+        if not self.config.retain_clean_jobs:
+            log.info(f"Uninstall cleanup job for {self.helm_release_name}")
+            await self.destroy(dry_run)
+
+
+class KafkaApp(PipelineComponent, ABC):
+    """Base component for Kafka-based components.
+
+    Producer or streaming apps should inherit from this class.
+
+    :param app: Application-specific settings
+    """
+
+    app: KafkaAppValues = Field(
+        default=...,
+        description=describe_attr("app", __doc__),
+    )
+
+    @override
+    async def deploy(self, dry_run: bool) -> None:
+        if self.to:
+            for topic in self.to.kafka_topics:
+                await self.handlers.topic_handler.create_topic(topic, dry_run=dry_run)
+
+            if self.handlers.schema_handler:
+                await self.handlers.schema_handler.submit_schemas(
+                    to_section=self.to, dry_run=dry_run
+                )
+
+        await super().deploy(dry_run)
