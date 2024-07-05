cc1d7d8664cc856269590c865570c14f0b913221
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-05 03:03:24 +0000

    Add package reqless/1.0.0a1/reqless-1.0.0a1-py3-none-any.whl

---
+++ packages/reqless/reqless-1.0.0a1-py3-none-any.whl/reqless/queue_resolvers/__init__.py
@@ -0,0 +1,16 @@
+from reqless.queue_resolvers.qmore_dynamic_mapping_queue_identifiers_transformer import (  # noqa: E501
+    QmoreDynamicMappingQueueIdentifiersTransformer,
+)
+from reqless.queue_resolvers.qmore_dynamic_priority_queue_identifiers_transformer import (  # noqa: E501
+    QmoreDynamicPriorityQueueIdentifiersTransformer,
+)
+from reqless.queue_resolvers.transforming_queue_resolver import (
+    TransformingQueueResolver,
+)
+
+
+__all__ = [
+    "QmoreDynamicMappingQueueIdentifiersTransformer",
+    "QmoreDynamicPriorityQueueIdentifiersTransformer",
+    "TransformingQueueResolver",
+]
