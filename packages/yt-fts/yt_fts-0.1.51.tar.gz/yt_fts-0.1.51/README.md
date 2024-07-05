63ff6110c65b66a39d86103eabc82cfacfb0a320
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-05 14:04:20 +0000

    Add package yt-fts/0.1.51/yt_fts-0.1.51.tar.gz

---
+++ packages/yt-fts/yt_fts-0.1.51.tar.gz/yt_fts-0.1.51/README.md
@@ -0,0 +1,168 @@
+
+# yt-fts - Youtube Full Text Search 
+`yt-fts` is a command line program that uses [yt-dlp](https://github.com/yt-dlp/yt-dlp) to scrape all of a youtube channels subtitles and load them into an sqlite database that is searchable from the command line. It allows you to query a channel for specific key word or phrase and will generate time stamped youtube urls to
+the video containing the keyword. 
+
+It also supports semantic search via the [OpenAI embeddings API](https://beta.openai.com/docs/api-reference/) using [chromadb](https://github.com/chroma-core/chroma).
+
+- [Blog Post](https://notjoemartinez.com/blog/youtube_full_text_search/)
+- [Semantic Search](#Semantic-Search-via-OpenAI-embeddings-API) 
+- [CHANGELOG](CHANGELOG.md)
+
+https://github.com/NotJoeMartinez/yt-fts/assets/39905973/6ffd8962-d060-490f-9e73-9ab179402f14
+
+## Installation 
+
+pip 
+
+```bash
+pip install yt-fts
+```
+
+## `download`
+Download subtitles for a channel. 
+
+Takes a channel url or id as an argument. Specify the number of jobs to parallelize the download with the `--number-of-jobs` option. 
+
+```bash
+yt-fts download --number-of-jobs 5 "https://www.youtube.com/@3blue1brown"
+```
+
+## `list`
+List saved channels.
+
+The (ss) next to the channel name indicates that the channel has semantic search enabled. 
+
+```bash
+yt-fts list
+```
+
+```
+┏━━━━┳━━━━━━━━━━━━━━━━━━━━━━━┳━━━━━━━┳━━━━━━━━━━━━━━━━━━━━━━━━━━┓
+┃ ID ┃ Name                  ┃ Count ┃ Channel ID               ┃
+┡━━━━╇━━━━━━━━━━━━━━━━━━━━━━━╇━━━━━━━╇━━━━━━━━━━━━━━━━━━━━━━━━━━┩
+│ 1  │ ChessPage1 (ss)       │ 19    │ UCO2QPmnJFjdvJ6ch-pe27dQ │
+│ 2  │ 3Blue1Brown           │ 127   │ UCYO_jab_esuFRV4b17AJtAw │
+│ 3  │ george hotz archive   │ 410   │ UCwgKmJM4ZJQRJ-U5NjvR2dg │
+│ 4  │ The Tim Dillon Show   │ 288   │ UC4woSp8ITBoYDmjkukhEhxg │
+│ 5  │ Academy of Ideas (ss) │ 190   │ UCiRiQGCHGjDLT9FQXFW0I3A │
+└────┴───────────────────────┴───────┴──────────────────────────┘
+
+```
+
+## `search` (Full Text Search)
+Full text search for a string in saved channels.
+
+- The search string does not have to be a word for word and match 
+- Search strings are limited to 40 characters. 
+
+```bash
+# search in all channels
+yt-fts search "[search query]" 
+
+# search in channel 
+yt-fts search "[search query]" --channel "[channel name or id]" 
+
+# search in specific video
+yt-fts search "[search query]" --video "[video id]"
+
+# limit results 
+yt-fts search "[search query]" --limit "[number of results]" --channel "[channel name or id]"
+
+# export results to csv
+yt-fts search "[search query]" --export --channel "[channel name or id]" 
+```
+
+Advanced Search Syntax:
+
+The search string supports sqlite [Enhanced Query Syntax](https://www.sqlite.org/fts3.html#full_text_index_queries).
+which includes things like [prefix queries](https://www.sqlite.org/fts3.html#termprefix) which you can use to match parts of a word.  
+
+```bash
+# AND search
+yt-fts search "knife AND Malibu" --channel "The Tim Dillon Show" 
+
+# OR SEARCH 
+yt-fts search "knife OR Malibu" --channel "The Tim Dillon Show" 
+
+# wild cards
+yt-fts search "rea* kni* Mali*" --channel "The Tim Dillon Show" 
+```
+
+
+# Semantic Search 
+You can enable semantic search for a channel by using the `get-embeddings` command.
+This requires an OpenAI API key set in the environment variable `OPENAI_API_KEY`, or 
+you can pass the key with the `--openai-api-key` flag. 
+
+
+## `get-embedings`
+Fetches OpenAI embeddings for specified channel
+```bash
+
+# make sure openAI key is set
+# export OPENAI_API_KEY="[yourOpenAIKey]"
+
+yt-fts get-embeddings --channel "3Blue1Brown"
+```
+
+After the embeddings are saved you will see a `(ss)` next to the channel name when you 
+list channels and you will be able to use the `vsearch` command for that channel. 
+
+## `vsearch` (Semantic Search)
+`vsearch` is for "Vector search". This requires that you enable semantic 
+search for a channel with `get-embeddings`. It has the same options as 
+`search` but output will be sorted by similarity to the search string and 
+the default return limit is 10. 
+
+```bash
+# search by channel name
+yt-fts vsearch "[search query]" --channel "[channel name or id]"
+
+# search in specific video
+yt-fts vsearch "[search query]" --video "[video id]"
+
+# limit results 
+yt-fts vsearch "[search query]" --limit "[number of results]" --channel "[channel name or id]"
+
+# export results to csv
+yt-fts vsearch "[search query]" --export --channel "[channel name or id]" 
+
+```
+
+
+
+## How To
+
+**Export search results:**
+For both the `search` and `vsearch` commands you can export the results to a csv file with 
+the `--export` flag. and it will save the results to a csv file in the current directory. 
+```bash
+yt-fts search "life in the big city" --export
+yt-fts vsearch "existing in large metropolaten center" --export
+```
+
+**Delete a channel:**
+You can delete a channel with the `delete` command. 
+
+```bash
+yt-fts delete --channel "3Blue1Brown"
+```
+
+
+**Update a channel:**
+The update command currently only works for full text search and will not update the 
+semantic search embeddings. 
+
+```bash
+yt-fts update --channel "3Blue1Brown"
+```
+
+
+**Export all of a channel's transcript:**
+This command will create a directory in current working directory with the youtube 
+channel id of the specified channel.
+```bash
+# Export to vtt
+yt-fts export --channel "[id/name]" --format "[vtt/txt]"
+```
---
+++ packages/yt-fts/yt_fts-0.1.51.tar.gz/yt_fts-0.1.51/yt_fts/utils.py
@@ -0,0 +1,218 @@
+"""
+This is where I'm putting all the functions that don't belong anywhere else
+"""
+import datetime
+import re
+import sqlite3
+
+def show_message(code):
+    error_dict = {
+        "search_too_long": "Error: Search text must be less than 40 characters",
+        "no_matches_found": "No matches found.\n- Try shortening the search text or use wildcards to match partial words.",
+        "channel_not_found": "channel not found.\n- Try using channel id",
+        "multiple_channels_found": "Multiple channels found.\n- Try using id",
+        "channel_url_not_correct": "The given channel URL is not correct, expected pattern : https://www.youtube.com/@TimDillonShow/videos",
+    }
+
+    print(error_dict[code])
+
+
+def time_to_secs(time_str):
+    """
+    converts timestamp to seconds youtube urls. Subtracts 3 seconds to give a buffer. 
+    """
+    # TODO: Figure out why we get syntax warning on first run
+    #  SyntaxWarning: invalid escape sequence '\d'
+    #  time_rex = re.search("^(\d\d):(\d\d):(\d\d)",time_str)
+    time_rex = re.search("^(\d\d):(\d\d):(\d\d)",time_str)
+    hours = int(time_rex.group(1)) * 3600 
+    mins = int(time_rex.group(2)) * 60
+    secs = int(time_rex.group(3)) 
+    total_secs =  hours + mins + secs
+
+    return total_secs - 3
+
+
+def parse_vtt(file_path):
+    """
+    extracts start time and text from vtt file and return a list of dicts
+    """
+    result = []
+
+    time_pattern = "^(.*) align:start position:0%"
+
+    with open(file_path, "r") as f:
+        lines = f.readlines()
+
+    for count, line in enumerate(lines):
+        time_match = re.match(time_pattern, line)
+
+        if time_match:
+            start = re.search("^(.*) -->",time_match.group(1))
+            start_time = start.group(1)
+
+            stop = re.search("--> (.*)",time_match.group(1))
+            stop_time = stop.group(1)
+
+            sub_titles = lines[count + 1]
+
+            # prevent duplicate entries
+            if result and result[-1]['text'] == sub_titles.strip('\n'):
+                # replace the previous entry with the new one
+                result[-1] = {
+                    'start_time': start_time,
+                    'stop_time': stop_time,
+                    'text': sub_titles.strip('\n'),
+                }
+            else:   
+                result.append({
+                    'start_time': start_time,
+                    'stop_time': stop_time,
+                    'text': sub_titles.strip('\n'),
+                })
+
+    return result 
+
+
+def get_api_key():
+    import os
+    api_key = os.environ.get("OPENAI_API_KEY")
+
+    if api_key is None:
+        return None
+    return api_key
+
+
+def get_time_delta(timestamp1, timestamp2):
+    from datetime import datetime
+    format_string = "%H:%M:%S.%f"
+    dt1 = datetime.strptime(timestamp1, format_string)
+    dt2 = datetime.strptime(timestamp2, format_string)
+    diff = dt2 - dt1
+    # convert to string "HH:MM:SS"
+    diff = str(diff).split(".")[0]
+
+    return diff 
+
+
+def get_date(date_string):
+    # Python 3.11 would support datimetime.date.fromisoformat('YYYYMMDD') directly
+    if '-' in date_string:
+        return datetime.date.fromisoformat(date_string)
+    return datetime.datetime.strptime(date_string, '%Y%m%d').date()
+
+
+# check if semantic search has been enabled for channel
+def check_ss_enabled(channel_id=None):
+
+    from yt_fts.config import get_db_path
+
+    con = sqlite3.connect(get_db_path())
+    cur = con.cursor()
+
+    if channel_id is None:
+        cur.execute(""" 
+            SELECT channel_id FROM SemanticSearchEnabled 
+            """)
+    else:
+        cur.execute(""" 
+            SELECT channel_id FROM SemanticSearchEnabled 
+            WHERE channel_id = ?
+            """, [channel_id])
+
+    res = cur.fetchone()
+    if res is None:
+        return False 
+    else:
+        return True 
+
+
+# enable semantic search for channel
+def enable_ss(channel_id):
+    from yt_fts.config import get_db_path
+
+    con = sqlite3.connect(get_db_path())
+    cur = con.cursor()
+
+    cur.execute(""" 
+        INSERT INTO SemanticSearchEnabled (channel_id)
+        VALUES (?)
+        """, [channel_id])
+    con.commit()
+    con.close() 
+
+
+def split_subtitles(video_id):
+
+    from datetime import datetime
+    from .db_utils import get_subs_by_video_id
+
+    def time_to_seconds(time_str):
+        """ Convert time string to total seconds """
+        return datetime.strptime(time_str, '%H:%M:%S.%f').time().hour * 3600 + \
+            datetime.strptime(time_str, '%H:%M:%S.%f').time().minute * 60 + \
+            datetime.strptime(time_str, '%H:%M:%S.%f').time().second + \
+            datetime.strptime(time_str, '%H:%M:%S.%f').time().microsecond / 1e6
+
+
+    subs = get_subs_by_video_id(video_id)
+
+    if len(subs) == 0:
+        print("Video is too short to split")
+        return None
+
+    total_seconds = time_to_secs(subs[-1][1])
+
+    if (total_seconds < 10):
+        print("Video is too short to split")
+    
+
+
+    # Convert times to seconds and store texts
+    converted_data = [(time_to_seconds(start), start, text) for start, end, text in subs]
+
+    interval_texts = {}
+    for start, start_time_str, text in converted_data:
+        interval = int(start // 10) * 10  
+        key = interval_texts.setdefault(interval, {'start_time': start_time_str, 'texts': []})
+        key['texts'].append(text)
+
+
+    result = [(data['start_time'], ' '.join(data['texts']).strip()) for data in interval_texts.values()]
+    return result
+
+
+def bold_query_matches(text, query):
+    """
+    Bold the query in the text, keeping the case the same
+    """
+    query_words = query.lower().split()
+    result_words = []
+
+    for word in text.split():
+        if word.lower() in query_words:
+            result_words.append(f"[bold][bright_magenta]{word}[/bright_magenta][/bold]")
+        else:
+            result_words.append(word)
+
+    return ' '.join(result_words)
+
+
+def handle_reject_consent_cookie(channel_url, s):
+    """
+    Auto rejects the consent cookie if request is redirected to the consent page
+    """
+    r = s.get(channel_url)
+    if "https://consent.youtube.com" in r.url:
+        m = re.search(r"<input type=\"hidden\" name=\"bl\" value=\"([^\"]*)\"", r.text)
+        if m:
+            data = {
+                "gl":"DE",
+                "pc":"yt",
+                "continue":channel_url,
+                "x":"6",
+                "bl":m.group(1),
+                "hl":"de",
+                "set_eom":"true"
+            }
+            s.post("https://consent.youtube.com/save", data=data)
