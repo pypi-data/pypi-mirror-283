6352183de5ea348af7030d25a2a8e28b5de22e7a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-05 04:03:15 +0000

    Add package O-money/0.0.2/o_money-0.0.2.tar.gz

---
+++ packages/O-money/o_money-0.0.2.tar.gz/o_money-0.0.2/setup.py
@@ -0,0 +1,32 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r") as f:
+    long_description = f.read()
+
+setup(
+    name="O_money",
+    version="0.0.2",
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires=[
+        "python-dotenv",
+    ],
+    entry_points={
+        "console_scripts": [
+            "O_money=O_money.webpay:main",
+        ],
+    },
+    author="Ibrahim Bilaly DICKO",
+    author_email="dicko.dev@gmail.com",
+    description="This Package is created with the aim of facilitating the integration of web payment by orange money into python projects",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/ddicko/O_money",
+    licence="MIT",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.6",
+)
