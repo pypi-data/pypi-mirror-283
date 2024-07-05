6d524a474ef9044ea25eb60788c3f4ea5b35d82a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-05 16:03:18 +0000

    Add package diversity/0.1.22/diversity-0.1.22-py3-none-any.whl

---
+++ packages/diversity/diversity-0.1.22-py3-none-any.whl/diversity/__init__.py
@@ -0,0 +1,7 @@
+from .compression import compression_ratio
+from .patterns.token import token_patterns
+from .patterns.part_of_speech import pos_patterns, get_pos
+from .utils.memoize import memoized
+from .homogenization import homogenization_score
+from .ngram_diversity import ngram_diversity_score
+from .functions import extract_patterns, match_patterns
