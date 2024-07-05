6de9e8f1641e96748cc447b2d6f2cec81e7a28bc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-05 03:03:45 +0000

    Add package pyDEMATEL/0.2.1/pydematel-0.2.1.tar.gz

---
+++ packages/pyDEMATEL/pydematel-0.2.1.tar.gz/pydematel-0.2.1/setup.py
@@ -0,0 +1,29 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="pyDEMATEL",
+    version="0.2.1",
+    packages=find_packages(),
+    install_requires=[
+    'numpy>=1.22.0',
+    'matplotlib>=3.4.3',
+    'openpyxl>=3.0.9',
+    ],
+    author="Abderrahman Chekry, Jamal Bakkas, Mohamed Hanin, Elizabeth Caro Montero, Mirtha Silvana Garat de Marin and Imran Ashraf",
+    author_email="a.chekry@uca.ac.ma",
+    description="pyDematel a python-based tool implementing the Dematel and fuzzy Dematel methods for improved decision making",
+    long_description=open('README.md').read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/achekry/-Elsevier-SoftwareX-pyDEMATEL",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: BSD License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+    entry_points={
+        'console_scripts': [
+            'pydematel=pyDEMATEL.DEMATELWindow:main',
+        ],
+    },
+)
