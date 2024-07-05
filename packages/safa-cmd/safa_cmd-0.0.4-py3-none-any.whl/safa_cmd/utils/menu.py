3c52a5c9b5543d5cf51bad937ce95619eb1a94b0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-05 04:03:20 +0000

    Add package safa-cmd/0.0.4/safa_cmd-0.0.4-py3-none-any.whl

---
+++ packages/safa-cmd/safa_cmd-0.0.4-py3-none-any.whl/safa_cmd/utils/menu.py
@@ -0,0 +1,39 @@
+from typing import List
+
+
+def prompt_option(options: List[str], retries=0, max_retries=3, title: str = "Options", allow_many: bool = False):
+    """
+    Prompts user to select an option.
+    :param options: List of options.
+    :param retries: Current retry count.
+    :param max_retries: Maximum number of retries.
+    :param title: Title displayed before options
+    :return: Option selected.
+    """
+    if retries >= max_retries:
+        raise Exception("Max retries has been reached.")
+
+    instructions = "Enter the options as a comma-delimited list (or 'a' for all)" if allow_many else "Enter the option number."
+    print(f"\n{title}:")
+    print(instructions)
+
+    for i, option in enumerate(options):
+        print(f"{i + 1})", option)
+
+    try:
+        option = input(">").lower().strip()
+        if allow_many:
+            if option == "a":
+                return options
+            else:
+                if "," not in option:
+                    print("Please enter comma-separated list.")
+                    return prompt_option(options, retries=1 + retries, max_retries=max_retries, title=title, allow_many=allow_many)
+                selected_options = [options[int(o.strip())] for o in option.split(",")]
+                return selected_options
+        else:
+            option_num = int(option)
+            return options[option_num - 1]
+    except Exception as e:
+        print(e)
+        return prompt_option(options, retries=retries + 1)
