a60945d061a5b8a5d9a6700971b86032473bd803
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-05 05:05:00 +0000

    Add package netupload/0.1.4/netupload-0.1.4.tar.gz

---
+++ packages/netupload/netupload-0.1.4.tar.gz/netupload-0.1.4/setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="netupload",
+    author="Zach Grimaldi",
+    version="0.1.4",
+    description="Simple upload server for local network file transfers.",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/zpg6/netupload",
+    packages=find_packages(),
+    install_requires=[
+        "Flask",
+    ],
+    entry_points={
+        "console_scripts": [
+            "netupload=src.app:run_server",
+        ],
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+    ],
+)
