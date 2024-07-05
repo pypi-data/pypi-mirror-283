0a4243a3eb2e0c37ed2392c076e904ff5b3b1ce7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-05 03:03:37 +0000

    Add package multidaq/0.0.10/multidaq-0.0.10.tar.gz

---
+++ packages/multidaq/multidaq-0.0.10.tar.gz/multidaq-0.0.10/multidaq/__init__.py
@@ -0,0 +1,11 @@
+"""
+python interface for biovision digitizers
+Classes:
+    multiDaqLowLevel()
+    multiDaq()
+    tMsgSlave()
+    tMsgMaster()
+"""
+__version__ = "0.0.10"
+
+from .multidaq import *
