63959baca1aa538bad517910408f87f26f962daa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-05 03:03:43 +0000

    Add package fmfocus/0.1.0/fmfocus-0.1.0.tar.gz

---
+++ packages/fmfocus/fmfocus-0.1.0.tar.gz/fmfocus-0.1.0/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="fmfocus",
+    version="0.1.0",
+    author="Your Name",
+    packages=find_packages(),
+    install_requires=[
+        "requests",
+        "beautifulsoup4",
+        "vidstream"
+    ],
+)
+
