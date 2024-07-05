d53599fe6c0dda00628590e664ffae7454d42721
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-05 06:03:26 +0000

    Add package better-gradient/0.1.0/better_gradient-0.1.0-py3-none-any.whl

---
+++ packages/better-gradient/better_gradient-0.1.0-py3-none-any.whl/better_gradient/__init__.py
@@ -0,0 +1,7 @@
+from .color_functions import (
+    blackwhite, purplepink, greenblue, pinkred,
+    purpleblue, water, fire, brazil, random
+)
+
+# Optionally include download_latest_update if desired, otherwise omit it
+from .download import download_latest_update
