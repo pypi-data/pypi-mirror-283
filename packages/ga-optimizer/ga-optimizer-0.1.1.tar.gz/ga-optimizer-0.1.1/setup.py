bffd2bc424a724ea843cbd0bc5a238da8d95d893
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-05 05:05:57 +0000

    Add package ga-optimizer/0.1.1/ga-optimizer-0.1.1.tar.gz

---
+++ packages/ga-optimizer/ga-optimizer-0.1.1.tar.gz/ga-optimizer-0.1.1/setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="ga-optimizer",
+    version="0.1.1",
+    description="Gradient Accumulation Optimizer Wrapper for Keras/TensorFlow",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    author="Kim Jansheden",
+    author_email="kim.jansheden@gmail.com",
+    url="https://github.com/kimjansheden/ga-optimizer",
+    packages=find_packages(),
+    install_requires=[
+        "tensorflow>=2.0",
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.6",
+)
