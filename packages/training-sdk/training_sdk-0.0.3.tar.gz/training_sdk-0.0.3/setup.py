6fdb73ec8d7e968d3c376278b27a9449c730193a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-05 05:05:23 +0000

    Add package training-sdk/0.0.3/training_sdk-0.0.3.tar.gz

---
+++ packages/training-sdk/training_sdk-0.0.3.tar.gz/training_sdk-0.0.3/setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='training-sdk',
+    version='0.0.3',
+    description='A Python SDK for training service.',
+    author='Luka',
+    author_email='luka@bitdeer.com',
+    packages=find_packages(include=["protoc-gen-openapiv2", "protobuf", "googleapis-common-protos"]),  # Automatically find and include all packages
+    install_requires=[
+        'grpcio',
+        'pydantic'
+        'protoc-gen-openapiv2'
+        'protobuf'
+        'googleapis-common-protos'    
+    ],
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.6',
+)
