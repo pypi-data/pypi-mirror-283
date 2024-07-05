a178b348b63609ad804faad7bab2b371c1a62d1b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-05 06:03:26 +0000

    Add package vinyl/0.0.26/vinyl-0.0.26.tar.gz

---
+++ packages/vinyl/vinyl-0.0.26.tar.gz/vinyl-0.0.26/tests/vinyl/test_lineage.py
@@ -0,0 +1,16 @@
+import pytest
+
+from vinyl.lib.project import Project
+
+
+@pytest.mark.skip(
+    reason="This test only works when vinyl is hooked up to a dbt project. Now using lineage directly from turntable-cli so not necessary."
+)
+def test_dbt_lineage():
+    project = Project.bootstrap()
+    sqlproj = project.get_sql_project(
+        ids=["internal_project.models.dbt.base_posthog__person"], parallel=False
+    )
+    sqlproj.optimize()
+    sqlproj.get_lineage()
+    assert sqlproj.stitch_lineage() != {}
