8ed34e308875465e0707cd58ae4065c677ec7af1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-05 04:03:13 +0000

    Add package reqless/1.0.0a2/reqless-1.0.0a2.tar.gz

---
+++ packages/reqless/reqless-1.0.0a2.tar.gz/reqless-1.0.0a2/reqless_test/test_listener.py
@@ -0,0 +1,47 @@
+import time
+from threading import Thread
+from typing import Dict
+
+from reqless.abstract import AbstractJob
+from reqless.listener import Listener
+from reqless_test.common import TestReqless
+
+
+class TestListener(TestReqless):
+    def setUp(self) -> None:
+        super().setUp()
+        self.client.queues["foo"].put("reqless_test.common.NoopJob", "{}", jid="jid")
+        job = self.client.jobs["jid"]
+        assert job is not None and isinstance(job, AbstractJob)
+        job.track()
+
+    def test_basic(self) -> None:
+        """Ensure we can get a basic message"""
+
+        count = 0
+
+        def func(message: Dict) -> None:
+            nonlocal count
+            count += 1
+            self.assertEqual("ql:popped", message["channel"])
+            self.assertEqual("message", message["type"])
+            self.assertEqual("jid", message["data"])
+
+        listener = Listener(channels=["ql:popped"], database=self.client.database)
+
+        def publish() -> None:
+            for message in listener.listen():
+                func(message)
+
+        thread = Thread(target=publish)
+        thread.start()
+        self.client.queues["foo"].pop()
+
+        # Give the thread a second to process the message
+        attempts = 0
+        while count == 0 and attempts < 10:
+            attempts += 1
+            time.sleep(0.1)
+        listener.unlisten()
+        thread.join()
+        self.assertEqual(count, 1)
