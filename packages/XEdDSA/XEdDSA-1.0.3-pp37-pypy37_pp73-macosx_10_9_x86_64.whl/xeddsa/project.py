45f6858a520952f8879d8674301722f2c4bde0fe
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-05 03:03:20 +0000

    Add package XEdDSA/1.0.3/XEdDSA-1.0.3-pp37-pypy37_pp73-macosx_10_9_x86_64.whl

---
+++ packages/XEdDSA/XEdDSA-1.0.3-pp37-pypy37_pp73-macosx_10_9_x86_64.whl/xeddsa/project.py
@@ -0,0 +1,13 @@
+__all__ = [ "project" ]  # pylint: disable=unused-variable
+
+project = {
+    "name"         : "XEdDSA",
+    "description"  : "Python bindings to libxeddsa.",
+    "url"          : "https://github.com/Syndace/python-xeddsa",
+    "year"         : "2022",
+    "author"       : "Tim Henkes (Syndace)",
+    "author_email" : "me@syndace.dev",
+    "categories"   : [
+        "Topic :: Security :: Cryptography"
+    ]
+}
