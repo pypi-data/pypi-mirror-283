e276d898dc13c252dbaa1a26b1c5864b43d598e0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-05 15:04:09 +0000

    Add package pythonipc/1.2.1/pythonipc-1.2.1.tar.gz

---
+++ packages/pythonipc/pythonipc-1.2.1.tar.gz/pythonipc-1.2.1/setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='pythonipc',
+    version='1.2.1',
+    author='itsmrmonday',
+    author_email='zackary.live8@gmail.com',
+    description='Inter-process communication library for Python3 to interact with JS renderer',
+    url='https://github.com/its-mr-monday/pyipc',
+    packages=find_packages(),
+    classifiers=[
+    'Programming Language :: Python :: 3',
+    'License :: OSI Approved :: MIT License',
+    'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.6',
+    install_requires=[
+        'flask',
+        'flask_cors',
+        'flask_socketio'
+    ]
+)
