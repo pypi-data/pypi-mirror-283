4895796a4fac1f56319d93fba4615a442839deba
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-05 03:03:26 +0000

    Add package holonote/0.2.0rc0/holonote-0.2.0rc0.tar.gz

---
+++ packages/holonote/holonote-0.2.0rc0.tar.gz/holonote-0.2.0rc0/scripts/jupyterlite/build.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+
+set -euxo pipefail
+
+PACKAGE="holonote"
+
+python -m build -w .
+VERSION=$(python -c "import $PACKAGE; print($PACKAGE._version.__version__)")
+export VERSION
+
+# Update lockfiles
+cd "$(dirname "${BASH_SOURCE[0]}")"
+rm -rf node_modules
+npm install .
+node update_lock.js
+python patch_lock.py
+rm node_modules/pyodide/*.whl
+
+jupyter lite build
+
+cp -r node_modules/pyodide/ ../../jupyterlite/pyodide
+mv pyodide-lock.json ../../jupyterlite/pyodide/pyodide-lock.json
+mv ../../dist/* ../../jupyterlite/pyodide/
