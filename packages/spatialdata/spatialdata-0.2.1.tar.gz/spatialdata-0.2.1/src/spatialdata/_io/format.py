4341ed7b3797875e362e92a27fa091862aa701a4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-05 05:04:48 +0000

    Add package spatialdata/0.2.1/spatialdata-0.2.1.tar.gz

---
+++ packages/spatialdata/spatialdata-0.2.1.tar.gz/spatialdata-0.2.1/src/spatialdata/_io/format.py
@@ -0,0 +1,154 @@
+from typing import Any, Optional, Union
+
+from anndata import AnnData
+from ome_zarr.format import CurrentFormat
+from pandas.api.types import CategoricalDtype
+from shapely import GeometryType
+
+from spatialdata.models import PointsModel, ShapesModel
+
+CoordinateTransform_t = list[dict[str, Any]]
+
+Shapes_s = ShapesModel()
+Points_s = PointsModel()
+
+
+class SpatialDataFormatV01(CurrentFormat):
+    """SpatialDataFormat defines the format of the spatialdata package."""
+
+
+class RasterFormatV01(SpatialDataFormatV01):
+    """Formatter for raster data."""
+
+    def generate_coordinate_transformations(self, shapes: list[tuple[Any]]) -> Optional[list[list[dict[str, Any]]]]:
+        data_shape = shapes[0]
+        coordinate_transformations: list[list[dict[str, Any]]] = []
+        # calculate minimal 'scale' transform based on pyramid dims
+        for shape in shapes:
+            assert len(shape) == len(data_shape)
+            scale = [full / level for full, level in zip(data_shape, shape)]
+            from spatialdata.transformations.ngff.ngff_transformations import NgffScale
+
+            coordinate_transformations.append([NgffScale(scale=scale).to_dict()])
+        return coordinate_transformations
+
+    def validate_coordinate_transformations(
+        self,
+        ndim: int,
+        nlevels: int,
+        coordinate_transformations: Optional[list[list[dict[str, Any]]]] = None,
+    ) -> None:
+        """
+        Validate that a list of dicts contains a 'scale' transformation.
+
+        Raises ValueError if no 'scale' found or doesn't match ndim
+        :param ndim:Number of image dimensions.
+        """
+        if coordinate_transformations is None:
+            raise ValueError("coordinate_transformations must be provided")
+        ct_count = len(coordinate_transformations)
+        if ct_count != nlevels:
+            raise ValueError(f"coordinate_transformations count: {ct_count} must match datasets {nlevels}")
+        for transformations in coordinate_transformations:
+            assert isinstance(transformations, list)
+            types = [t.get("type", None) for t in transformations]
+            if any(t is None for t in types):
+                raise ValueError(f"Missing type in: {transformations}")
+
+            # new validation
+            import json
+
+            json0 = [json.dumps(t) for t in transformations]
+            from spatialdata.transformations.ngff.ngff_transformations import (
+                NgffBaseTransformation,
+            )
+
+            parsed = [NgffBaseTransformation.from_dict(t) for t in transformations]
+            json1 = [json.dumps(p.to_dict()) for p in parsed]
+            import numpy as np
+
+            assert np.all([j0 == j1 for j0, j1 in zip(json0, json1)])
+
+
+class ShapesFormatV01(SpatialDataFormatV01):
+    """Formatter for shapes."""
+
+    @property
+    def version(self) -> str:
+        return "0.1"
+
+    def attrs_from_dict(self, metadata: dict[str, Any]) -> GeometryType:
+        if Shapes_s.ATTRS_KEY not in metadata:
+            raise KeyError(f"Missing key {Shapes_s.ATTRS_KEY} in shapes metadata.")
+        metadata_ = metadata[Shapes_s.ATTRS_KEY]
+        if Shapes_s.GEOS_KEY not in metadata_:
+            raise KeyError(f"Missing key {Shapes_s.GEOS_KEY} in shapes metadata.")
+        for k in [Shapes_s.TYPE_KEY, Shapes_s.NAME_KEY]:
+            if k not in metadata_[Shapes_s.GEOS_KEY]:
+                raise KeyError(f"Missing key {k} in shapes metadata.")
+
+        typ = GeometryType(metadata_[Shapes_s.GEOS_KEY][Shapes_s.TYPE_KEY])
+        assert typ.name == metadata_[Shapes_s.GEOS_KEY][Shapes_s.NAME_KEY]
+        assert self.version == metadata_["version"]
+        return typ
+
+    def attrs_to_dict(self, geometry: GeometryType) -> dict[str, Union[str, dict[str, Any]]]:
+        return {Shapes_s.GEOS_KEY: {Shapes_s.NAME_KEY: geometry.name, Shapes_s.TYPE_KEY: geometry.value}}
+
+
+class PointsFormatV01(SpatialDataFormatV01):
+    """Formatter for points."""
+
+    @property
+    def version(self) -> str:
+        return "0.1"
+
+    def attrs_from_dict(self, metadata: dict[str, Any]) -> dict[str, dict[str, Any]]:
+        if Points_s.ATTRS_KEY not in metadata:
+            raise KeyError(f"Missing key {Points_s.ATTRS_KEY} in points metadata.")
+        metadata_ = metadata[Points_s.ATTRS_KEY]
+        assert self.version == metadata_["version"]
+        d = {}
+        if Points_s.FEATURE_KEY in metadata_:
+            d[Points_s.FEATURE_KEY] = metadata_[Points_s.FEATURE_KEY]
+        if Points_s.INSTANCE_KEY in metadata_:
+            d[Points_s.INSTANCE_KEY] = metadata_[Points_s.INSTANCE_KEY]
+        return d
+
+    def attrs_to_dict(self, data: dict[str, Any]) -> dict[str, dict[str, Any]]:
+        d = {}
+        if Points_s.ATTRS_KEY in data:
+            if Points_s.INSTANCE_KEY in data[Points_s.ATTRS_KEY]:
+                d[Points_s.INSTANCE_KEY] = data[Points_s.ATTRS_KEY][Points_s.INSTANCE_KEY]
+            if Points_s.FEATURE_KEY in data[Points_s.ATTRS_KEY]:
+                d[Points_s.FEATURE_KEY] = data[Points_s.ATTRS_KEY][Points_s.FEATURE_KEY]
+        return d
+
+
+class TablesFormatV01(SpatialDataFormatV01):
+    """Formatter for the table."""
+
+    @property
+    def version(self) -> str:
+        return "0.1"
+
+    def validate_table(
+        self,
+        table: AnnData,
+        region_key: Optional[str] = None,
+        instance_key: Optional[str] = None,
+    ) -> None:
+        if not isinstance(table, AnnData):
+            raise TypeError(f"`table` must be `anndata.AnnData`, was {type(table)}.")
+        if region_key is not None and not isinstance(table.obs[region_key].dtype, CategoricalDtype):
+            raise ValueError(
+                f"`table.obs[region_key]` must be of type `categorical`, not `{type(table.obs[region_key])}`."
+            )
+        if instance_key is not None and table.obs[instance_key].isnull().values.any():
+            raise ValueError("`table.obs[instance_key]` must not contain null values, but it does.")
+
+
+CurrentRasterFormat = RasterFormatV01
+CurrentShapesFormat = ShapesFormatV01
+CurrentPointsFormat = PointsFormatV01
+CurrentTablesFormat = TablesFormatV01
---
+++ packages/spatialdata/spatialdata-0.2.1.tar.gz/spatialdata-0.2.1/src/spatialdata/_io/io_points.py
@@ -0,0 +1,85 @@
+import os
+from collections.abc import MutableMapping
+from pathlib import Path
+from typing import Union
+
+import zarr
+from dask.dataframe import DataFrame as DaskDataFrame  # type: ignore[attr-defined]
+from dask.dataframe import read_parquet
+from ome_zarr.format import Format
+
+from spatialdata._io import SpatialDataFormatV01
+from spatialdata._io._utils import (
+    _get_transformations_from_ngff_dict,
+    _write_metadata,
+    overwrite_coordinate_transformations_non_raster,
+)
+from spatialdata._io.format import CurrentPointsFormat
+from spatialdata.models import get_axes_names
+from spatialdata.transformations._utils import (
+    _get_transformations,
+    _set_transformations,
+)
+
+
+def _read_points(
+    store: Union[str, Path, MutableMapping, zarr.Group],  # type: ignore[type-arg]
+    fmt: SpatialDataFormatV01 = CurrentPointsFormat(),
+) -> DaskDataFrame:
+    """Read points from a zarr store."""
+    assert isinstance(store, (str, Path))
+    f = zarr.open(store, mode="r")
+
+    path = os.path.join(f._store.path, f.path, "points.parquet")
+    # cache on remote file needed for parquet reader to work
+    # TODO: allow reading in the metadata without caching all the data
+    table = read_parquet("simplecache::" + path if "http" in path else path)
+    assert isinstance(table, DaskDataFrame)
+
+    transformations = _get_transformations_from_ngff_dict(f.attrs.asdict()["coordinateTransformations"])
+    _set_transformations(table, transformations)
+
+    attrs = fmt.attrs_from_dict(f.attrs.asdict())
+    if len(attrs):
+        table.attrs["spatialdata_attrs"] = attrs
+    return table
+
+
+def write_points(
+    points: DaskDataFrame,
+    group: zarr.Group,
+    name: str,
+    group_type: str = "ngff:points",
+    fmt: Format = CurrentPointsFormat(),
+) -> None:
+    axes = get_axes_names(points)
+    t = _get_transformations(points)
+
+    points_groups = group.require_group(name)
+    path = Path(points_groups._store.path) / points_groups.path / "points.parquet"
+
+    # The following code iterates through all columns in the 'points' DataFrame. If the column's datatype is
+    # 'category', it checks whether the categories of this column are known. If not, it explicitly converts the
+    # categories to known categories using 'c.cat.as_known()' and assigns the transformed Series back to the original
+    # DataFrame. This step is crucial when the number of categories exceeds 127, as pyarrow defaults to int8 for
+    # unknown categories which can only hold values from -128 to 127.
+    for column_name in points.columns:
+        c = points[column_name]
+        if c.dtype == "category" and not c.cat.known:
+            c = c.cat.as_known()
+            points[column_name] = c
+
+    points.to_parquet(path)
+
+    attrs = fmt.attrs_to_dict(points.attrs)
+    attrs["version"] = fmt.version
+
+    _write_metadata(
+        points_groups,
+        group_type=group_type,
+        axes=list(axes),
+        attrs=attrs,
+        fmt=fmt,
+    )
+    assert t is not None
+    overwrite_coordinate_transformations_non_raster(group=points_groups, axes=axes, transformations=t)
