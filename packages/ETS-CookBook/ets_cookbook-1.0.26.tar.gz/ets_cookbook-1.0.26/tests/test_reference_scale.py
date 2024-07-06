5330b879c77fd0440e7f83b06c238ac9b0375c0b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-06 06:03:44 +0000

    Add package ETS-CookBook/1.0.26/ets_cookbook-1.0.26.tar.gz

---
+++ packages/ETS-CookBook/ets_cookbook-1.0.26.tar.gz/ets_cookbook-1.0.26/tests/test_reference_scale.py
@@ -0,0 +1,15 @@
+import typing as ty
+
+import ETS_CookBook as cook
+
+
+def test_basic() -> None:
+    test_list: ty.List[float] = [1, 2, 10.8]
+    test_scale: ty.List[float] = [1, 20]
+    assert cook.reference_scale(test_list) == test_scale
+
+
+def test_zero() -> None:
+    test_list: ty.List[float] = [0, 1, 10.8]
+    test_scale: ty.List[float] = [0, 20]
+    assert cook.reference_scale(test_list) == test_scale
