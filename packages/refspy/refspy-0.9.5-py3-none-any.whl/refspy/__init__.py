e455d09127e89892445b21366aeb03160eeedad8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-06 15:04:03 +0000

    Add package refspy/0.9.5/refspy-0.9.5-py3-none-any.whl

---
+++ packages/refspy/refspy-0.9.5-py3-none-any.whl/refspy/__init__.py
@@ -0,0 +1,17 @@
+"""A Python package for working with biblical references in ordinary text.
+
+See https://github.com/eukras/refspy
+"""
+
+from refspy.init import get_canon, get_language
+from refspy.manager import Manager
+
+
+def refspy(canon_name: str = "protestant", locale_name: str = "en_US") -> Manager:
+    """Create a Manager object to access all common package functions.
+
+    Args:
+        canon_name: A valid key for the `refspy.config.LIBRARIES` dict
+        locale_name: A valid key for the `refspy.config.LANGUAGES` dict
+    """
+    return Manager(get_canon(canon_name, locale_name), get_language(locale_name[:2]))
