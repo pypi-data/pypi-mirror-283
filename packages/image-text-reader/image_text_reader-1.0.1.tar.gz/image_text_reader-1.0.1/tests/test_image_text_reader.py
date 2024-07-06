4d43bbf16ff7bd038d4205d0da26c2ccb3f45e6e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-06 03:03:39 +0000

    Add package image-text-reader/1.0.1/image_text_reader-1.0.1.tar.gz

---
+++ packages/image-text-reader/image_text_reader-1.0.1.tar.gz/image_text_reader-1.0.1/tests/test_image_text_reader.py
@@ -0,0 +1,9 @@
+import unittest
+from image_text_reader import image_text_reader
+
+class TestImageTextReader(unittest.TestCase):
+    def test_example(self):
+        self.assertTrue(True)
+
+if __name__ == '__main__':
+    unittest.main()
