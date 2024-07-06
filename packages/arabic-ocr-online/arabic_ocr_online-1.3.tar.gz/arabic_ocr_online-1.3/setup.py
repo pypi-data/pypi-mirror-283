fbed8529533bfc8e7613f9acc20a08116e7a2360
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-06 04:04:19 +0000

    Add package arabic-ocr-online/1.3/arabic_ocr_online-1.3.tar.gz

---
+++ packages/arabic-ocr-online/arabic_ocr_online-1.3.tar.gz/arabic_ocr_online-1.3/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="arabic_ocr_online",
+    version='1.3',
+    packages= find_packages(),
+    install_requires=[
+        'easyocr >=1.7.1',
+        'numpy >= 1.26.4',
+        'requests >= 2.32.2',
+        'opencv-python >= 4.9.0.80'
+    ]
+)
