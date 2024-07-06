3f751419ea87d74c1a05760d163df6fa1ecf7c7b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-06 04:06:04 +0000

    Add package ColorReliefEditor/0.1.8/colorreliefeditor-0.1.8.tar.gz

---
+++ packages/ColorReliefEditor/colorreliefeditor-0.1.8.tar.gz/colorreliefeditor-0.1.8/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(
+    entry_points={
+        'console_scripts': [
+            'ColorReliefEditor=ColorReliefEditor.ColorReliefEditor:main',
+        ],
+    },
+)
