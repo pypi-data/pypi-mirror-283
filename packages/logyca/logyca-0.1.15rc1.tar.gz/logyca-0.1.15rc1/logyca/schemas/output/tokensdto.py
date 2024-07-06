909905bc11be3f38e58ce18c765961a9377c4452
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-06 06:03:40 +0000

    Add package logyca/0.1.15rc1/logyca-0.1.15rc1.tar.gz

---
+++ packages/logyca/logyca-0.1.15rc1.tar.gz/logyca-0.1.15rc1/logyca/schemas/output/tokensdto.py
@@ -0,0 +1,8 @@
+from pydantic import BaseModel,Field
+
+class TokensDTO(BaseModel):
+    token:str=Field(default='',description="Gets or sets result Token")
+    refreshToken:str=Field(default='',description="Gets or sets Refresh token")
+    result:str=Field(default='',description="Gets or sets result request")
+    emailActiveDirectory:str=Field(default='',description="Gets or sets email Active Directory")
+    message:str=Field(default='',description="Gets or sets user message")
