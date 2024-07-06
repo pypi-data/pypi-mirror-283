84f3d4371227e12619ac458572746004523e7d88
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-06 15:04:03 +0000

    Add package neyro-api/0.1.6/neyro_api-0.1.6.tar.gz

---
+++ packages/neyro-api/neyro_api-0.1.6.tar.gz/neyro_api-0.1.6/async_api.py
@@ -0,0 +1,24 @@
+import aiohttp
+import json
+
+class AsyncNeyroAPI:
+    BASE_URL = "https://api.neyrogen.online"
+
+    def __init__(self, api_key, captcha_key):
+        self.headers = {
+            "Content-Type": "application/json",
+            "Authorization": f"Bearer {api_key}",
+            "x-captcha-token": captcha_key
+        }
+
+    async def text_gpt_new(self, **kwargs):
+        from .text.gpt_new import TextAPI
+        return await TextAPI.async_gpt_new(self, **kwargs)
+
+    async def image_sh(self, **kwargs):
+        from .image.sh import ImageAPI
+        return await ImageAPI.async_sh(self, **kwargs)
+
+    async def audio_tts(self, **kwargs):
+        from .audio.tts import AudioAPI
+        return await AudioAPI.async_tts(self, **kwargs)
