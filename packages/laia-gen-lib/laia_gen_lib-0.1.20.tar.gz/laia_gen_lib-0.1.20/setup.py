f4b860a8573873280889b7ae272ad4c34a09deda
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-06 05:04:09 +0000

    Add package laia-gen-lib/0.1.20/laia_gen_lib-0.1.20.tar.gz

---
+++ packages/laia-gen-lib/laia_gen_lib-0.1.20.tar.gz/laia_gen_lib-0.1.20/setup.py
@@ -0,0 +1,13 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='laia-gen-lib',
+    packages=find_packages(),
+    version='0.1.20',
+    description='An AI application generator engine',
+    author='Me',
+    install_requires=['pymongo', 'pydantic', 'datamodel-code-generator', 'fastapi', 'bcrypt', 'asyncinit', 'pyjwt'],
+    setup_requires=['pytest-runner'],
+    tests_require=['pytest', 'pymongo', 'pytest-asyncio', 'fastapi'],
+    test_suite='tests',
+)
