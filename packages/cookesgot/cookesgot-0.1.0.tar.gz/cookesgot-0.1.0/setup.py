9bcb1e36e93937b0521e306abc7652d34947fa5f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-06 04:04:22 +0000

    Add package cookesgot/0.1.0/cookesgot-0.1.0.tar.gz

---
+++ packages/cookesgot/cookesgot-0.1.0.tar.gz/cookesgot-0.1.0/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="cookesgot",
+    version="0.1.0",
+    author="Your Name",
+    packages=find_packages(),
+    install_requires=[
+        "requests",
+        "pyautogui",
+        "selenium"
+    ],
+)
+
