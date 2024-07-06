3e4bab39a207e2088524d4b6241257c57958cf53
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-06 04:04:30 +0000

    Add package pyappcore/0.0.51/pyappcore-0.0.51-py3-none-any.whl

---
+++ packages/pyappcore/pyappcore-0.0.51-py3-none-any.whl/pyappcore/debug_util.py
@@ -0,0 +1,25 @@
+#------------------------------------------------------------------------
+# 참조 모듈 목록.
+#------------------------------------------------------------------------
+from __future__ import annotations
+import builtins
+import os
+import sys
+import traceback
+
+
+#------------------------------------------------------------------------
+# 익셉션.
+#------------------------------------------------------------------------
+def RaiseException(exception : Exception):
+	traceback.print_exc()
+	tb = exception.__traceback__
+	while tb:
+		filename = tb.tb_frame.f_code.co_filename
+		lineno = tb.tb_lineno
+		funcname = tb.tb_frame.f_code.co_name
+		line = traceback.format_exc().strip().splitlines()[-1]
+		builtins.print(f"Exception in {filename}, line {lineno}, in {funcname}")
+		builtins.print(f"\t{line}")
+		tb = tb.tb_next
+	sys.exit(1)
