bbe2a8a276face4ae27e37ced0f7691aeb41aa27
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-06 04:05:01 +0000

    Add package neyro-api/0.1.3/neyro_api-0.1.3.tar.gz

---
+++ packages/neyro-api/neyro_api-0.1.3.tar.gz/neyro_api-0.1.3/setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup, find_packages
+
+version = '0.1.3'
+
+setup(
+    name='neyro_api',
+    version='0.1.3',
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires=[
+        'requests',
+        'aiohttp'
+    ],
+    author='Chelik23',
+    author_email='workghmail5775@gmail.com',
+    description='A Python library for interacting with the Neyro API',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    url='https://github.com/NeyroTeam/NeyroAPI-Library',
+    download_url='https://github.com/NeyroTeam/NeyroAPI-Library/archive/v{version}.tar.gz',
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: Apache Software License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.11',
+)
