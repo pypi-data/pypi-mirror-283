8925fd2048381e4840af245a0263f4588427a4c9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-06 03:04:39 +0000

    Add package aimmocore-db/0.1.6/aimmocore_db-0.1.6.tar.gz

---
+++ packages/aimmocore-db/aimmocore_db-0.1.6.tar.gz/aimmocore_db-0.1.6/setup.py
@@ -0,0 +1,37 @@
+from setuptools import setup, find_packages
+from setuptools.command.install import install
+from wheel.bdist_wheel import bdist_wheel as _bdist_wheel
+import subprocess
+
+
+class PostInstallCommand(install):
+    """Post-installation for installation mode."""
+
+    def run(self):
+        install.run(self)
+        self._install_mongodb()
+
+    def _install_mongodb(self):
+        subprocess.run(["python", "-m", "aimmocore_db.install_mongodb"], check=True)
+
+
+class bdist_wheel(_bdist_wheel):
+    """Custom bdist_wheel command to run post-installation tasks."""
+
+    def run(self):
+        _bdist_wheel.run(self)
+        self._install_mongodb()
+
+    def _install_mongodb(self):
+        subprocess.run(["python", "-m", "aimmocore_db.install_mongodb"], check=True)
+
+
+setup(
+    name="aimmocore-db",
+    version="0.1.6",
+    packages=find_packages(),
+    cmdclass={
+        "install": PostInstallCommand,
+        "bdist_wheel": bdist_wheel,
+    },
+)
