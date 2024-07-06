2899b47b68d633ec498a78b110032e10851aa2cc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-06 03:04:08 +0000

    Add package myodish-peak-analysis/1.0.2/myodish_peak_analysis-1.0.2.tar.gz

---
+++ packages/myodish-peak-analysis/myodish_peak_analysis-1.0.2.tar.gz/myodish_peak_analysis-1.0.2/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setup(
+    name="myodish-peak-analysis",
+    version="1.0.2",
+    packages=find_packages(),
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+)
