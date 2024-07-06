2d0173e19aee6b0f344ec427c4e9bc25f89f5cf5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-06 05:03:24 +0000

    Add package aeroterra-ds/1.9.3/aeroterra_ds-1.9.3.tar.gz

---
+++ packages/aeroterra-ds/aeroterra_ds-1.9.3.tar.gz/aeroterra_ds-1.9.3/src/geometry/distance.py
@@ -0,0 +1,40 @@
+from haversine import haversine, Unit
+
+from shapely.ops import nearest_points
+from shapely import distance
+
+from .change_crs import change_crs
+
+def distance_geometries(geom_a, geom_b, crs_a=None, crs_b=None, do_haversine=True, units=Unit.KILOMETERS):
+    """
+    Given 2 shapely geometries, it returns the distance between them
+
+    Parameters:
+        - geom_a: First geometry to compare
+        - geom_b: Seconda geometry to compare
+        - crs_a: CRS of the geom_a. If not given and asked for haversine,
+            epsg:4326 will be assumed.
+        - crs_b: CRS of the geom_b. If not given and asked for haversine,
+            epsg:4326 will be assumed.
+        - do_haversine (Optional): If wanted to check the distance in haversine.
+            By default in True.
+        - units (Optional): If using haversine, what unit to return. Must use 
+            Haversine.Units strcuture like element. By default in Kilometers
+    """
+    if not do_haversine and crs_a and crs_b and crs_b != crs_a:
+        raise Exception("Can't Use Different CRS for non haversine distance")
+    
+    if do_haversine and crs_a:
+        geom_a = change_crs(geom_a, crs_a, 4326)
+    
+    if do_haversine and crs_b:
+        geom_b = change_crs(geom_b, crs_b, 4326)
+    
+    point_a, point_b = nearest_points(geom_a, geom_b)
+
+    if not do_haversine:
+        return distance(point_a, point_b)
+    else:
+        return haversine(point_a.xy, point_b.xy, unit=units)
+
+
