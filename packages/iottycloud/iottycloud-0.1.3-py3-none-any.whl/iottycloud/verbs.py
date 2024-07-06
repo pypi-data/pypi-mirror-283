b6a97eae312d7dbb61a8b6d7270617f43d0c6bb3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-06 03:04:40 +0000

    Add package iottycloud/0.1.3/iottycloud-0.1.3-py3-none-any.whl

---
+++ packages/iottycloud/iottycloud-0.1.3-py3-none-any.whl/iottycloud/verbs.py
@@ -0,0 +1,22 @@
+"""Verbs for iotty CloudApi protocol."""
+
+HTTP_HEADER_CLIENT_ID = "Iotty-Client-Id"
+
+DEVICE_TYPE = "device_type"
+DEVICE_ID = "device_id"
+DEVICE_NAME = "device_name"
+SERIAL_NUMBER = "serial_number"
+
+RESULT = "Result"
+"""Root for the results."""
+
+STATUS = "status"
+STATUS_ON = "on"
+STATUS_OFF = "off"
+
+COMMAND_TURNON = "turnon"
+COMMAND_TURNOFF = "turnoff"
+
+LS_DEVICE_TYPE_UID = "com.iotty.lightswitch"
+
+SUPPORTED_DEVICE_TYPES = [LS_DEVICE_TYPE_UID]
