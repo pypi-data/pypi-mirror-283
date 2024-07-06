bd525ecf46e3ea140e3dd67c7622b97b0a66e6c3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-06 03:04:40 +0000

    Add package hezar/0.40.0/hezar-0.40.0-py3-none-any.whl

---
+++ packages/hezar/hezar-0.40.0-py3-none-any.whl/hezar/models/__init__.py
@@ -0,0 +1,13 @@
+from ..registry import register_model  # noqa
+from ..builders import build_model  # noqa
+from .model import Model, ModelConfig  # noqa
+from .model_outputs import *
+from .backbone import *
+from .image2text import *
+from .mask_filling import *
+from .sequence_labeling import *
+from .speech_recognition import *
+from .text_classification import *
+from .text_detection import *
+from .text_embedding import *
+from .text_generation import *
