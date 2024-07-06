dfa8a3a48e9d28b893c7e41e05dce4248be673ff
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-06 15:04:06 +0000

    Add package rlate-env/0.0.11/rlate_env-0.0.11.tar.gz

---
+++ packages/rlate-env/rlate_env-0.0.11.tar.gz/rlate_env-0.0.11/src/gymnasium_rlate/orbit/Orbit.py
@@ -0,0 +1,19 @@
+import gymnasium as gym
+from gymnasium import spaces
+#
+#
+# class Orbit(gym.Env):
+#     metadata = {"render_modes": ["ansi"], "render_fps": 4}
+#
+#     def __init__(self):
+#         self.observation_space = spaces.Box(5, 50) # Defines the gravity
+#         self.action_space = spaces.Box()
+#
+#     def reset(self, seed, options):
+#         super().reset(seed=seed)
+#
+#     def step(self, action):
+#
+#     def render(self):
+#
+#     def close(self):
