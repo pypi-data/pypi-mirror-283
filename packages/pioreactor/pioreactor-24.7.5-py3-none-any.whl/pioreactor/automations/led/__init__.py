85e957c85d62be26562e4fd0e18a0bd201a9be38
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-06 04:05:27 +0000

    Add package pioreactor/24.7.5/pioreactor-24.7.5-py3-none-any.whl

---
+++ packages/pioreactor/pioreactor-24.7.5-py3-none-any.whl/pioreactor/automations/led/__init__.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+from __future__ import annotations
+
+from .light_dark_cycle import LightDarkCycle
+from pioreactor.automations import events
+from pioreactor.automations.led.base import LEDAutomationJob
+
+
+class Silent(LEDAutomationJob):
+    automation_name = "silent"
+    published_settings = {"duration": {"datatype": "float", "settable": True, "unit": "min"}}
+
+    def __init__(self, **kwargs) -> None:
+        super(Silent, self).__init__(**kwargs)
+
+    def execute(self) -> events.AutomationEvent:
+        return events.NoEvent("no changes occur in Silent")
