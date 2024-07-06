7bfe367cb8ea70a3a38d136c7bc4c61de77e51af
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-06 04:05:09 +0000

    Add package tracer-extension/1.0.3/tracer_extension-1.0.3-py3-none-any.whl

---
+++ packages/tracer-extension/tracer_extension-1.0.3-py3-none-any.whl/python_worker_extension/__init__.py
@@ -0,0 +1,33 @@
+import typing
+from logging import Logger
+from time import time
+from azure.functions import AppExtensionBase, Context, HttpResponse
+
+class TracerExtension(AppExtensionBase):
+    """A Python worker extension to start Datadog tracer for Azure Functions
+    """
+
+    @classmethod
+    def init(cls):
+        print("=========== in init NEW1 =============")
+
+    @classmethod
+    def pre_invocation_app_level(
+        cls, logger: Logger, context: Context,
+        func_args: typing.Dict[str, object],
+        *args, **kwargs
+    ) -> None:
+        # spanZero = tracer.trace('span')  # span is started once created
+        # spanZero.finish()
+        # print("span id: ", spanZero)
+        from ddtrace import patch_all, tracer
+        patch_all()
+
+    @classmethod
+    def post_invocation_app_level(
+        cls, logger: Logger, context: Context,
+        func_args: typing.Dict[str, object],
+        func_ret: typing.Optional[object],
+        *args, **kwargs
+    ) -> None:
+        pass
