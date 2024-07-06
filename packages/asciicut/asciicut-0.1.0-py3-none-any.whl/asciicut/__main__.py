ea5ee23c82fc6265bcc75a5d7ad8e2550eae09a1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-06 03:04:27 +0000

    Add package asciicut/0.1.0/asciicut-0.1.0-py3-none-any.whl

---
+++ packages/asciicut/asciicut-0.1.0-py3-none-any.whl/asciicut/__main__.py
@@ -0,0 +1,24 @@
+"""Main typer application."""
+
+import typer
+from .utils import find_casts
+from .schema import AsciiCast
+
+app = typer.Typer(name="Asciicut", short_help="Slice and dice .cast files")
+
+
+@app.command()
+def ls():
+    """Load in and print the file."""
+    find_casts()
+
+
+@app.command()
+def drop(filename: str, time_s: float):
+    """Drop the first n seconds for an ascii cast."""
+    cast = AsciiCast.from_file(filename)
+    out = cast.shave_time_beginning(time_s)
+    out_file = f"{filename[:-5]}_drop_{time_s:0.5}.cast"
+
+    out.write(filename_out=out_file)
+    find_casts()
