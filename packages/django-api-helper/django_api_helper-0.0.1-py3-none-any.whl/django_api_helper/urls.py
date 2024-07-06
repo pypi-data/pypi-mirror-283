71ae9b878c8a7ca14a9503e2268dbaa9cb4d565d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-06 15:03:41 +0000

    Add package django-api-helper/0.0.1/django_api_helper-0.0.1-py3-none-any.whl

---
+++ packages/django-api-helper/django_api_helper-0.0.1-py3-none-any.whl/django_api_helper/urls.py
@@ -0,0 +1,16 @@
+from django.urls                    import path
+from django_api_helper.views        import  *
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+    TokenVerifyView
+)
+
+urlpatterns = [
+    # Developer
+    path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('api/token/verify/', TokenVerifyView.as_view(), name='token_verify'),
+
+    path('get-jwt/', GenerateJWTToken.as_view(), name='get_jwt_token'),
+]
