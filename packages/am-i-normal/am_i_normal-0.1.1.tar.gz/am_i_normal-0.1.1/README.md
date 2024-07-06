7fa5bc4b000f469119bc888f710f38900088b897
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-06 05:04:12 +0000

    Add package am-i-normal/0.1.1/am_i_normal-0.1.1.tar.gz

---
+++ packages/am-i-normal/am_i_normal-0.1.1.tar.gz/am_i_normal-0.1.1/README.md
@@ -0,0 +1,8 @@
+# am_i_normal
+
+A package to randomly decide if you are normal and provide funny motivations.
+
+## Installation
+
+```sh
+pip install am_i_normal
---
+++ packages/am-i-normal/am_i_normal-0.1.1.tar.gz/am_i_normal-0.1.1/setup.py
@@ -0,0 +1,20 @@
+# setup.py
+
+from setuptools import setup, find_packages
+
+setup(
+    name="am_i_normal",
+    version="0.1.1",
+    author="mike-rollout",
+    description="Are you normal? Time to find out",
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    url="https://github.com/mike-rollout/py-is_normal",
+    packages=find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
