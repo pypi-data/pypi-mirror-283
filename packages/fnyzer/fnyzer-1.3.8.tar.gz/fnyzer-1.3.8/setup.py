057eb9f70195c6f398adbcbe4bbc7af2ac27ec97
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-06 05:04:48 +0000

    Add package fnyzer/1.3.8/fnyzer-1.3.8.tar.gz

---
+++ packages/fnyzer/fnyzer-1.3.8.tar.gz/fnyzer-1.3.8/setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup, find_packages
+
+def readme():
+    with open('README.md') as f:
+        return f.read()
+
+setup(
+    name="fnyzer",
+    version="1.3.8",
+    description="Flexible Nets analYZER",
+    url='https://bitbucket.org/Julvez/fnyzer',
+    long_description=readme(),
+    packages=find_packages(),
+    entry_points={
+        'console_scripts': ['fnyzer=fnyzer.fnyzer:main']
+    },
+    install_requires=[
+       'pyomo>=5.5.0',
+       'numpy==1.23.5', # pyomo 6.7 does not seem to be compatible with numpy 2.0
+       'xlwt>=1.3.0',
+       'matplotlib>=3.0.0',
+       'statsmodels>=0.10.1'
+    ],
+    author="Jorge Julvez",
+    author_email="fnyzer@unizar.es",
+    license="GNU GENERAL PUBLIC LICENSE",
+    keywords="flexible nets analyzer",
+)
