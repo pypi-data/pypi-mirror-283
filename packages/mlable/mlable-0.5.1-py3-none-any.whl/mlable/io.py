dc65b5707652211db4babea776bffdb9ae5c1319
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-06 05:04:03 +0000

    Add package mlable/0.5.1/mlable-0.5.1-py3-none-any.whl

---
+++ packages/mlable/mlable-0.5.1-py3-none-any.whl/mlable/io.py
@@ -0,0 +1,15 @@
+# METADATA ####################################################################
+
+def _label(c: str) -> str:
+    return '#{}'.format(c.encode('utf-32-be').hex())
+
+def label(token: str) -> str:
+    return ' '.join(_label(__c) for __c in token)
+
+# SERIALIZE ###################################################################
+
+def write(data: any, path: str, tsv: bool=True) -> None:
+    with open(path, 'w') as __f:
+        for __row in data:
+            __line = '\t'.join(str(__v) for __v in __row) if tsv else repr(__row)[1:-1]
+            __f.write(__line + '\n') # escape special characters
