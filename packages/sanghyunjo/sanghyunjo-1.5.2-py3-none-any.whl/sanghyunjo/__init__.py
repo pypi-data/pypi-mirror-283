b5050ae82a4a7c846358d43f1b7fe579866dfa93
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-06 03:03:47 +0000

    Add package sanghyunjo/1.5.2/sanghyunjo-1.5.2-py3-none-any.whl

---
+++ packages/sanghyunjo/sanghyunjo-1.5.2-py3-none-any.whl/sanghyunjo/__init__.py
@@ -0,0 +1,8 @@
+# Copyright (C) 2024 * Ltd. All rights reserved.
+# author: Sanghyun Jo <shjo.april@gmail.com>
+
+__version__ = '1.5.2'
+
+from .misc import *
+from .cv_utils import *
+from .json_utils import *
