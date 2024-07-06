c97c049592b5a8e59684b5be905c47274768b99f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-06 03:04:38 +0000

    Add package magpylib-force/0.1.10/magpylib_force-0.1.10.tar.gz

---
+++ packages/magpylib-force/magpylib_force-0.1.10.tar.gz/magpylib_force-0.1.10/magpylib_force/__init__.py
@@ -0,0 +1,12 @@
+from magpylib_force.force import getFT
+
+"""
+The magpylib-force package sits on top of magpylib for force computation
+"""
+
+# module level dunders
+__version__ = "0.1.10"
+__author__ = "SAL"
+__all__ = [
+    "getFT",
+]
