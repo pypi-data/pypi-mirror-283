336f959dee075dddd5b1f0bda5ebeb38d6470182
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-06 04:04:31 +0000

    Add package fold-resize-images/1.0.3/fold_resize_images-1.0.3.tar.gz

---
+++ packages/fold-resize-images/fold_resize_images-1.0.3.tar.gz/fold_resize_images-1.0.3/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="fold_resize_images",
+    version='v1.0.3',
+    description="A simple tool to resize images in a directory.",
+    author="Tadiello Sébastien",
+    author_email="sebastientadiello@gmail.com",
+    packages=find_packages(),
+    install_requires=[
+        "Pillow"
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
