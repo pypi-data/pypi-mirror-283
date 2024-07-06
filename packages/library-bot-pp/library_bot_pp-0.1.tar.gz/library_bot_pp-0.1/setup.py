d7e1ff1c73240d66d81251ef559c023a42956c4d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-06 05:03:27 +0000

    Add package library-bot-pp/0.1/library_bot_pp-0.1.tar.gz

---
+++ packages/library-bot-pp/library_bot_pp-0.1.tar.gz/library_bot_pp-0.1/setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="library-bot-pp",
+    version="0.1",
+    packages=find_packages(),
+    install_requires=[],
+    entry_points={
+        'console_scripts': [
+            'p!c=scripts.create_file:create_file',
+        ],
+    },
+    author="P",
+    author_email="parepare2154@gmail.com",
+    description="A library that creates a Python bot",
+    url="https://ppbot.web.app/mylibrary",
+)
