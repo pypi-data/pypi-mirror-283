9c624de95198e32897db173529c67b4371660511
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-06 15:04:27 +0000

    Add package backdoor-io/0.0.13/backdoor-io-0.0.13.tar.gz

---
+++ packages/backdoor-io/backdoor-io-0.0.13.tar.gz/backdoor-io-0.0.13/backdoor/__init__.py
@@ -0,0 +1,9 @@
+# __init__.py
+
+from backdoor.data import *
+from backdoor.command import *
+from backdoor.actions import *
+from backdoor.executor import *
+from backdoor.action import *
+from backdoor.labels import *
+from backdoor.manager import *
