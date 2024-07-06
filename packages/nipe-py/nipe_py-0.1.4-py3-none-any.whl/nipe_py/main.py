b88c8c06cec6ca29d925b863b93aa70add7ba124
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-06 15:04:18 +0000

    Add package nipe-py/0.1.4/nipe_py-0.1.4-py3-none-any.whl

---
+++ packages/nipe-py/nipe_py-0.1.4-py3-none-any.whl/nipe_py/main.py
@@ -0,0 +1,57 @@
+import click
+from nipe_py import Stop, Start, Restart
+from nipe_py import Status, Install
+from rich import print
+from rich.panel import Panel
+from nipe_py import __version__
+
+
+@click.group()
+def cli():
+    pass
+
+
+@cli.command()
+def install():
+    """Install dependencies"""
+    Install()
+
+
+@cli.command()
+def start():
+    """Start routing"""
+    st = Start()
+    st.start()
+    print(Panel("[+] [green]nipe has been started...."))
+
+
+@cli.command()
+def stop():
+    """Stop routing"""
+    stop = Stop()
+    stop.stop()
+    print(Panel("[+] [red]nipe has been stopped...."))
+
+
+@cli.command()
+def restart():
+    """Restart the Nipe circuit"""
+    rst = Restart()
+    rst.restart()
+    print(Panel("[+] [red]nipe has been restated...."))
+
+
+@cli.command()
+def status():
+    """See status"""
+    Status()
+
+
+@cli.command()
+def version():
+    """Show Version"""
+    print(Panel(f"[green]Version: [yellow]{__version__}"))
+
+
+if __name__ == "__main__":
+    cli()
