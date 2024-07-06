6c2b878ef270b7c3ebf43d433986817e4c3f817a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-06 03:04:08 +0000

    Add package oteapi-core/0.7.0.dev2/oteapi_core-0.7.0.dev2.tar.gz

---
+++ packages/oteapi-core/oteapi_core-0.7.0.dev2.tar.gz/oteapi_core-0.7.0.dev2/oteapi/models/functionconfig.py
@@ -0,0 +1,15 @@
+"""Pydantic Function Configuration Data Model."""
+
+from pydantic import Field
+
+from oteapi.models.genericconfig import GenericConfig
+from oteapi.models.secretconfig import SecretConfig
+
+
+class FunctionConfig(GenericConfig, SecretConfig):
+    """Function Strategy Data Configuration."""
+
+    functionType: str = Field(
+        ...,
+        description=("Type of registered function strategy."),
+    )
---
+++ packages/oteapi-core/oteapi_core-0.7.0.dev2.tar.gz/oteapi_core-0.7.0.dev2/oteapi/triplestore/__init__.py
@@ -0,0 +1,8 @@
+"""`oteapi.triplestore` module.
+
+Get the `TripleStore`.
+"""
+
+from .triplestore import TripleStore
+
+__all__ = ("TripleStore",)
