382850450e01018a85a533f82cfb2186477139a3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-06 06:03:38 +0000

    Add package nomad-media-pip/0.0.132/nomad_media_pip-0.0.132.tar.gz

---
+++ packages/nomad-media-pip/nomad_media_pip-0.0.132.tar.gz/nomad_media_pip-0.0.132/nomad_media_pip/src/admin/schedule_event/add_asset_schedule_event.py
@@ -0,0 +1,21 @@
+from nomad_media_pip.src.helpers.send_request import _send_request
+from nomad_media_pip.src.admin.schedule_event.event_types import _EVENT_TYPES
+
+def _add_asset_schedule_event(self, AUTH_TOKEN, URL, CHANNEL_ID, ASSET, IS_LOOP,
+                              DURATION_TIME_CODE, PREVIOUS_ID, DEBUG):
+    API_URL = f"{URL}/api/liveChannel/{CHANNEL_ID}/liveScheduleEvent" 
+    
+    # Build the payload BODY
+    BODY = {
+        "isLoop": IS_LOOP,
+        "channelId": CHANNEL_ID,
+        "durationTimeCode": DURATION_TIME_CODE,
+        "previousId": PREVIOUS_ID,
+        "type": {
+            "id": _EVENT_TYPES["videoAsset"],
+            "description": "Video-Asset"
+        },
+        "asset": ASSET,
+    }
+
+    return _send_request(self, AUTH_TOKEN, "Add Asset Schedule Event", API_URL, "POST\nBODY: {json.dumps(BODY", None, BODY, DEBUG)
