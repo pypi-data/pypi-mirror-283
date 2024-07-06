5cefe7d93bb7fa1632092ebda5607393edf7ed10
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-06 15:03:48 +0000

    Add package github-comment-bot/0.0.1/github-comment-bot-0.0.1.tar.gz

---
+++ packages/github-comment-bot/github-comment-bot-0.0.1.tar.gz/github-comment-bot-0.0.1/setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup, find_packages
+
+
+setup(
+    name='github-comment-bot',
+    version='0.0.1',
+    author='Eugene Evstafev',
+    author_email='chigwel@gmail.com',
+    description='',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    url='https://github.com/chigwell/gitlab-comment-bot',
+    packages=find_packages(),
+    install_requires=[
+    ],
+    classifiers=[
+        'License :: OSI Approved :: MIT License',
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'Programming Language :: Python :: 3',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.6',
+)
