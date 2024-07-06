5de2883208b191bba98b9511c5ee070bb9481eb1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-06 05:03:26 +0000

    Add package streamlined-custom-component/1.0/streamlined_custom_component-1.0.tar.gz

---
+++ packages/streamlined-custom-component/streamlined_custom_component-1.0.tar.gz/streamlined_custom_component-1.0/setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="streamlined_custom_component",
+    version="1.0",
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires=[
+        "streamlit",
+    ],
+    description="A package to create custom Streamlit components easily.",
+    author="Alex Gain",
+    author_email="alexzgain@gmail.com",
+    url="https://github.com/alexgain/streamlined_custom_component",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
