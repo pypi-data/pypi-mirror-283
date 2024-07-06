b54351f3e85e2492153278197225976f4a60f9b2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-06 04:05:21 +0000

    Add package lisa-on-cuda/1.2.4/lisa_on_cuda-1.2.4.tar.gz

---
+++ packages/lisa-on-cuda/lisa_on_cuda-1.2.4.tar.gz/lisa_on_cuda-1.2.4/lisa_on_cuda/app/routes.py
@@ -0,0 +1,19 @@
+import json
+import logging
+from fastapi import APIRouter
+
+from ..utils import session_logger
+
+
+router = APIRouter()
+
+
+@router.get("/health")
+@session_logger.set_uuid_logging
+def health() -> str:
+    try:
+        logging.info("health check")
+        return json.dumps({"msg": "ok"})
+    except Exception as e:
+        logging.error(f"exception:{e}.")
+        return json.dumps({"msg": "request failed"})
