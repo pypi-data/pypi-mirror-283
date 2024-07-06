5ee029b9b6208dcd6ac25595e34e94e9fe562449
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-06 05:03:20 +0000

    Add package xelis-py-sdk/0.3.0/xelis-py-sdk-0.3.0.tar.gz

---
+++ packages/xelis-py-sdk/xelis-py-sdk-0.3.0.tar.gz/xelis-py-sdk-0.3.0/tests/test_daemon_rpc.py
@@ -0,0 +1,233 @@
+import xelis.daemon.classes as classes
+from xelis.daemon.http import DaemonRPC
+from xelis.config.module import LOCAL_NODE_RPC, TESTNET_NODE_RPC, MAINNET_NODE_RPC, XELIS_ASSET
+
+testnetDaemon = DaemonRPC(url=TESTNET_NODE_RPC)
+mainnetDaemon = DaemonRPC(url=MAINNET_NODE_RPC)
+localDaemon = DaemonRPC(url=LOCAL_NODE_RPC)
+
+TESTNET_ADDR = "xet:rsdm79np9eqar7cg5jy9sdhwas74l4ml5enaasmae8jtjcvpr3vqqnlpysy"
+
+def test_noConnect():
+  try:
+    noDaemon = DaemonRPC(url="http://127.0.0.1:57432")
+    data = noDaemon.getInfo()
+  except Exception as err:
+      print(err)
+
+def test_getInfo():
+  data = testnetDaemon.getInfo()
+  print(data)
+  
+def test_getVersion():
+  version = testnetDaemon.getVersion()
+  print(version)
+  
+def test_getHeight():
+  result = testnetDaemon.getHeight()
+  print(result)
+  
+def test_getTopoheight():
+  result = testnetDaemon.getTopoheight()
+  print(result)
+  
+def test_getStableheight():
+  result = testnetDaemon.getStableheight()
+  print(result)
+  
+def test_getBlockTemplate():
+  result = testnetDaemon.getBlockTemplate(address=TESTNET_ADDR)
+  print(result)
+  
+def test_getBlockAtTopoheight():
+  params = classes.GetBlockAtTopoheightParams(topoheight=0, include_txs=False)
+  result = testnetDaemon.getBlockAtTopoheight(params=params)
+  print(result)
+
+def test_getBlocksAtHeight():
+  params = classes.GetBlocksAtHeightParams(height=0, include_txs=False)
+  result = testnetDaemon.getBlocksAtHeight(params=params)
+  print(result)
+
+def test_getBlockByHash():
+  params = classes.GetBlockByHashParams(hash="6d51e50e6f864c844726f92e6d2d7d5d09f6e78921c1269f8796943eec7db98a", include_txs=False)
+  result = testnetDaemon.getBlockByHash(params=params)
+  print(result)
+  
+def test_getTopBlock():
+  params = classes.GetTopBlockParams(include_txs=False)
+  result = testnetDaemon.getTopBlock(params=params)
+  print(result)
+  
+def test_getNonce():
+  result = testnetDaemon.getNonce(address=TESTNET_ADDR)
+  print(result)
+  
+def test_hasNonce():
+  result = testnetDaemon.hasNonce(address=TESTNET_ADDR)
+  print(result)
+  
+def test_getNonceAtTopoheight():
+  params = classes.GetNonceAtTopoheightParams(address=TESTNET_ADDR, topoheight=632)
+  result = testnetDaemon.getNonceAtTopoheight(params=params)
+  print(result)
+  
+def test_getBalance():
+  params = classes.GetBalanceParams(address=TESTNET_ADDR, asset=XELIS_ASSET)
+  result = testnetDaemon.getBalance(params=params)
+  print(result)
+  
+def test_hasBalance():
+  params = classes.GetBalanceParams(address=TESTNET_ADDR, asset=XELIS_ASSET)
+  result = testnetDaemon.hasBalance(params=params)
+  print(result)
+  
+def test_getBalancecAtTopoheight():
+  params = classes.GetBalanceAtTopoheightParams(address=TESTNET_ADDR, asset=XELIS_ASSET, topoheight=632)
+  result = testnetDaemon.getBalanceAtTopoheight(params=params)
+  print(result)
+  
+def test_getAsset():
+  result = testnetDaemon.getAsset(asset=XELIS_ASSET)
+  print(result)
+  
+def test_getAssets():
+  params = classes.GetAssetsParams()
+  result = testnetDaemon.getAssets(params=params)
+  print(result)
+  
+def test_countAssets():
+  result = testnetDaemon.countAssets()
+  print(result)
+
+def test_countTransactions():
+  result = testnetDaemon.countTransactions()
+  print(result)
+
+def test_countAccounts():
+  result = testnetDaemon.countAccounts()
+  print(result)
+  
+def test_getTips():
+  result = testnetDaemon.getTips()
+  print(result)
+  
+def test_p2pStatus():
+  result = testnetDaemon.p2pStatus()
+  print(result)
+  
+def test_getDAGOrder():
+  result = testnetDaemon.getDAGOrder(params=classes.GetTopoheightRangeParams(
+    start_topoheight=0,
+    end_topoheight=1
+  ))
+  print(result)
+  
+def test_getMempool():
+  result = mainnetDaemon.getMempool()
+  print(result)
+  
+def test_getTransaction():
+  result = mainnetDaemon.getTransaction(hash="33b14221e79c0083e90141b22023d053d112f24ffc0d03d676291d19302ed03d")
+  print(result)
+
+def test_checkExtraData():
+  # v2
+  result = localDaemon.getTransaction(hash="389426d5f181f2b7e503052bd19bccbad344262ad8d90c39a87aa6d5dbd86669")
+  size = len(result.data.transfers[0].extra_data)
+  print(size)
+
+  # v1
+  result = localDaemon.getTransaction(hash="6d2478c646e629cd944afd0d6232d359e4b652c3b0104cc15c7af2bba90c8fce")
+  size = len(result.data.transfers[0].extra_data)
+  print(size)
+  
+  # v1
+  result = localDaemon.getTransaction(hash="067f2b8876cf4bf1e96318f0706bb121116d2138b0396c9dc16319ff315648e7")
+  size = len(result.data.transfers[0].extra_data)
+  print(size)
+  
+  # v2
+  result = localDaemon.getTransaction(hash="009247c48a4e4a03f4c69dadaf1279103ed1d7ff2b2c3d98c3a7b2911d8d69fd")
+  size = len(result.data.transfers[0].extra_data)
+  print(size)
+
+def test_getTransactions():
+  params = classes.GetTransactionsParams(tx_hashes=["33b14221e79c0083e90141b22023d053d112f24ffc0d03d676291d19302ed03d"])
+  result = mainnetDaemon.getTransactions(params=params)
+  print(result)
+  
+def test_getBlocksRangeByTopoheight():
+  params = classes.GetTopoheightRangeParams(start_topoheight=0, end_topoheight=10)
+  result = testnetDaemon.getBlocksRangeByTopoheight(params=params)
+  print(result)
+  
+def test_getBlocksRangeByHeight():
+  params = classes.GetHeightRangeParams(start_height=0, end_height=10)
+  result = testnetDaemon.getBlocksRangeByHeight(params=params)
+  print(result)
+  
+def test_getAcounts():
+  params = classes.GetAssetsParams()
+  result = testnetDaemon.getAccounts(params=params)
+  print(result)
+  
+def test_getAccountHistory():
+  #dev fee wallet
+  result = mainnetDaemon.getAccountHistory(address="xel:vs3mfyywt0fjys0rgslue7mm4wr23xdgejsjk0ld7f2kxng4d4nqqnkdufz")
+  print(result)
+  
+  result = mainnetDaemon.getAccountHistory(address="xel:qcd39a5u8cscztamjuyr7hdj6hh2wh9nrmhp86ljx2sz6t99ndjqqm7wxj8")
+  print(result)
+  
+def test_getAccountAssets():
+  result = testnetDaemon.getAccountAssets(address=TESTNET_ADDR)
+  print(result)
+  
+def test_getPeers():
+  result = testnetDaemon.getPeers()
+  print(result)
+  
+def test_getDevFeeThresholds():
+  result = testnetDaemon.getDevFeeThresholds()
+  print(result)
+  
+def test_getSizeOnDisk():
+  result = testnetDaemon.getSizeOnDisk()
+  print(result)
+  
+def test_isTxExecutedInBlock():
+  params = classes.IsTxExecutedInBlockParams(
+    block_hash="f61a3e037b4c8ae8aff2ace3a499a6f552ccd4ddaabd2e2067fec20c0da71d8c", 
+    tx_hash="c743d8d822cd553672f8f534ed71277e8e50cfa37af9826a1ee1e922f059b019",
+  )
+  result = mainnetDaemon.isTxExecutedInBlock(params=params)
+  print(result)
+  
+def test_getAccountRegistrationTopoheight():
+  result = testnetDaemon.getAccountRegistrationTopoheight(address=TESTNET_ADDR)
+  print(result)
+  
+def test_isAccountRegistered():
+  params = classes.IsAccountRegisteredParams(address=TESTNET_ADDR, in_stable_height=True)
+  result = testnetDaemon.isAccountRegistered(params=params)
+  print(result)
+
+def test_getDifficulty():
+  result = testnetDaemon.getDifficulty()
+  print(result)
+  
+def test_validateAddress():
+  params = classes.ValidateAddressParams(address=TESTNET_ADDR, allow_integrated=False)
+  result = testnetDaemon.validateAddress(params=params)
+  print(result)
+  
+def test_extractKeyFromAddress():
+  params = classes.ExtractKeyFromAddressParams(address=TESTNET_ADDR, as_hex=True)
+  result = testnetDaemon.extractKeyFromAddress(params)
+  print(result)
+  
+  params = classes.ExtractKeyFromAddressParams(address=TESTNET_ADDR, as_hex=False)
+  result = testnetDaemon.extractKeyFromAddress(params)
+  print(result)
+  
---
+++ packages/xelis-py-sdk/xelis-py-sdk-0.3.0.tar.gz/xelis-py-sdk-0.3.0/xelis/daemon/classes.py
@@ -0,0 +1,332 @@
+from dataclasses import dataclass, field
+from typing import Optional
+from mashumaro import field_options
+from rpc.classes import BaseDictMixin
+
+@dataclass
+class GetInfoResult(BaseDictMixin):
+  average_block_time: int
+  block_reward: int
+  block_time_target: int
+  circulating_supply: int
+  difficulty: str
+  height: int
+  maximum_supply: int
+  mempool_size: int
+  network: str
+  stableheight: int
+  top_block_hash: str
+  topoheight: int
+  version: str
+  pruned_topoheight: Optional[int] = None
+  
+@dataclass
+class GetBlockTemplateResult(BaseDictMixin):
+  template: str
+  height: int
+  topoheight: int
+  difficulty: str
+  
+@dataclass
+class GetBlockAtTopoheightParams(BaseDictMixin):
+  topoheight: int
+  include_txs: bool
+  
+@dataclass
+class GetBlocksAtHeightParams(BaseDictMixin):
+  height: int
+  include_txs: bool
+  
+@dataclass
+class GetBlockByHashParams(BaseDictMixin):
+  hash: str
+  include_txs: bool
+  
+@dataclass
+class GetTopBlockParams(BaseDictMixin):
+  include_txs: bool
+  
+@dataclass
+class GetNonceResult(BaseDictMixin):
+  nonce: int
+  topoheight: int
+  previous_topoheight: Optional[int] = None
+  
+@dataclass
+class VersionedNonce(BaseDictMixin):
+  nonce: int
+  previous_topoheight: Optional[int] = None
+  
+@dataclass
+class GetNonceAtTopoheightParams(BaseDictMixin):
+  address: str
+  topoheight: int
+  
+@dataclass
+class GetBalanceParams(BaseDictMixin):
+  address: str
+  asset: str
+  
+@dataclass
+class GetBalanceAtTopoheightParams(BaseDictMixin):
+  address: str
+  asset: str
+  topoheight: int
+
+@dataclass
+class EncryptedBalance(BaseDictMixin):
+  commitment: list[int]
+  handle: list[int]
+  
+@dataclass
+class VersionedBalance(BaseDictMixin):
+  balance_type: str # input, output or both
+  final_balance: EncryptedBalance
+  output_balance: Optional[EncryptedBalance] = None
+  previous_topoheight: Optional[int] = None
+
+@dataclass
+class GetBalanceResult(BaseDictMixin):
+  version: VersionedBalance
+  topoheight: int
+
+@dataclass
+class GetAssetsParams(BaseDictMixin):
+  Skip: Optional[int] = None
+  Maximum: Optional[int] = None
+  MinimumTopoheight: Optional[int] = None
+  MaximumTopoheight: Optional[int] = None
+
+@dataclass
+class Asset(BaseDictMixin):
+  topoheight: int
+  decimals: int
+
+@dataclass
+class AssetWithData(BaseDictMixin):
+  asset: str
+  topoheight: int
+  decimals: int
+  
+@dataclass
+class P2PStatusResult(BaseDictMixin):
+  best_topoheight: int
+  max_peers: int
+  our_topoheight: int
+  peer_count: int
+  peer_id: int
+  tag: Optional[str] = None
+  
+@dataclass
+class GetTopoheightRangeParams(BaseDictMixin):
+  start_topoheight: int
+  end_topoheight: int
+
+@dataclass
+class GetHeightRangeParams(BaseDictMixin):
+  start_height: int
+  end_height: int
+
+@dataclass
+class Block(BaseDictMixin):
+  block_type: str
+  cumulative_difficulty: str
+  difficulty: str
+  extra_nonce: str
+  hash: str
+  height: int
+  miner: str
+  nonce: int
+  timestamp: int
+  tips: list[str]
+  total_size_in_bytes: int
+  txs_hashes: list[str]
+  versions: int
+  supply: Optional[int] = None
+  dev_reward: Optional[int] = None
+  miner_reward: Optional[int] = None
+  reward: Optional[int] = None
+  topoheight: Optional[int] = None
+  total_fees: Optional[int] = None
+
+@dataclass
+class SubmitBlockParams(BaseDictMixin):
+  block_template: str
+  miner_work: Optional[str] = None
+
+@dataclass
+class Proof(BaseDictMixin):
+  Y_0: list[int]
+  Y_1: list[int]
+  z_r: list[int]
+  z_x: list[int]
+
+@dataclass
+class Burn(BaseDictMixin):
+  asset: str
+  amount: int
+
+@dataclass
+class Transfer(BaseDictMixin):
+  asset: str
+  destination: str
+  commitment: list[int]
+  sender_handle: list[int]
+  receiver_handle: list[int]
+  ct_validity_proof: Proof
+  extra_data: Optional[list[int]] = None
+
+@dataclass
+class EqProof(BaseDictMixin):
+  Y_0: list[int]
+  Y_1: list[int]
+  Y_2: list[int]
+  z_r: list[int]
+  z_s: list[int]
+  z_x: list[int]
+
+@dataclass
+class SourceCommitment(BaseDictMixin):
+  commitment: list[int]
+  proof: EqProof
+  asset: str
+  
+@dataclass
+class Reference(BaseDictMixin):
+  hash: str
+  topoheight: int
+  
+@dataclass
+class TransactionData(BaseDictMixin):
+  transfers: list[Transfer]
+  burn: Optional[Burn] = None
+
+@dataclass
+class Transaction(BaseDictMixin):
+  hash: str
+  data: TransactionData
+  fee: int
+  nonce: int
+  source: str
+  reference: Reference
+  source_commitments: list[SourceCommitment]
+  range_proof: list[int]
+  signature: str
+  version: int
+  in_mempool: bool
+  blocks: Optional[list[str]] = None
+  executed_in_block: Optional[str] = None
+  first_seen: Optional[int] = None
+  
+@dataclass
+class GetTransactionsParams(BaseDictMixin):
+  tx_hashes: list[str]
+
+@dataclass
+class MiningHistory(BaseDictMixin):
+  reward: int
+  
+@dataclass
+class BurnHistory(BaseDictMixin):
+  amount: int
+  
+@dataclass
+class OutgoingHistory(BaseDictMixin):
+  to: str
+  
+@dataclass()
+class IncomingHistory(BaseDictMixin):
+  sender: str = field(metadata=field_options(alias="from"))
+
+@dataclass
+class DevFeeHistory(BaseDictMixin):
+  reward: int
+
+@dataclass
+class AccountHistory(BaseDictMixin):
+  topoheight: int
+  block_timestamp: int
+  hash: str
+  mining: Optional[MiningHistory] = None
+  burn: Optional[BurnHistory] = None
+  outgoing: Optional[OutgoingHistory] = None
+  incoming: Optional[IncomingHistory] = None
+  dev_fee: Optional[MiningHistory] = None
+
+@dataclass
+class Peer(BaseDictMixin):
+  id: int
+  cumulative_difficulty: str
+  connected_on: int
+  height: int
+  local_port: int
+  top_block_hash: str
+  addr: str
+  last_ping: int
+  topoheight: int
+  peers: dict[str, str] # in, out or both
+  version: str
+  pruned_topoheight: Optional[int] = None
+  tag: Optional[str] = None
+
+@dataclass
+class GetPeersResult(BaseDictMixin):
+  peers: list[Peer]
+  total_peers: int
+  hidden_peers: int
+  
+@dataclass
+class Fee(BaseDictMixin):
+  fee_percentage: int
+  height: int
+  
+@dataclass
+class SizeOnDisk(BaseDictMixin):
+  size_bytes: int
+  size_formatted: str
+  
+@dataclass
+class IsTxExecutedInBlockParams(BaseDictMixin):
+  tx_hash: str
+  block_hash: str
+  
+@dataclass
+class IsAccountRegisteredParams(BaseDictMixin):
+  address: str
+  in_stable_height: bool
+  
+@dataclass
+class GetDifficultyResult(BaseDictMixin):
+  difficulty: str
+  hashrate: str
+  hashrate_formatted: str
+  
+@dataclass
+class ValidateAddressParams(BaseDictMixin):
+  address: str
+  allow_integrated: bool
+  
+@dataclass
+class ValidateAddressResult(BaseDictMixin):
+  is_integrated: bool
+  is_valid: bool
+  
+@dataclass 
+class ExtractKeyFromAddressParams(BaseDictMixin):
+  address: str
+  as_hex: bool
+  
+@dataclass 
+class CreateMinerWorkParams(BaseDictMixin):
+  template: str
+  address: Optional[str] = None
+  
+@dataclass
+class CreateMinerWorkResult(BaseDictMixin):
+  miner_work: str
+  
+@dataclass
+class TransactionExecutedResult(BaseDictMixin):
+  block_hash: str
+  topoheight: int
+  tx_hash: str
