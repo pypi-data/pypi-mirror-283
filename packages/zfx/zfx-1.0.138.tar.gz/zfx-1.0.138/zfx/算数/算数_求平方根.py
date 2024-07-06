eba6a02b2d4f93aafbf3408effa7e20e26e18fbe
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-06 03:04:39 +0000

    Add package zfx/1.0.138/zfx-1.0.138.tar.gz

---
+++ packages/zfx/zfx-1.0.138.tar.gz/zfx-1.0.138/zfx/算数/算数_求平方根.py
@@ -0,0 +1,19 @@
+import math
+
+
+def 算数_求平方根(数字):
+    """
+    计算给定数字的平方根。
+
+    参数:
+    数字: 要计算平方根的数字。
+
+    # 调用示例
+    result = 求平方根(16)
+    print(result)  # 输出结果为 4.0
+
+    返回:
+    数字，给定数字的平方根。
+    """
+    result = math.sqrt(数字)
+    return result
