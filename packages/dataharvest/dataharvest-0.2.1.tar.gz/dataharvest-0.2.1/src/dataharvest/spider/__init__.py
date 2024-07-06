5cd23831ff0f853435e32f1b0a6ab148ba6839d9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-06 04:04:26 +0000

    Add package dataharvest/0.2.1/dataharvest-0.2.1.tar.gz

---
+++ packages/dataharvest/dataharvest-0.2.1.tar.gz/dataharvest-0.2.1/src/dataharvest/spider/__init__.py
@@ -0,0 +1,5 @@
+from dataharvest.spider.common_spider import CommonSpider
+from dataharvest.spider.toutiao_spider import ToutiaoSpider
+from dataharvest.spider.spider import AutoSpider, BaseSpider
+
+__all__ = ["AutoSpider", "CommonSpider", "BaseSpider", "ToutiaoSpider"]
