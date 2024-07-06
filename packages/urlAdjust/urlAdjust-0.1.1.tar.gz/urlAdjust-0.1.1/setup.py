9115992e9f96d68d3bd9fc25e38f354550c7ffc3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-06 15:03:57 +0000

    Add package urlAdjust/0.1.1/urlAdjust-0.1.1.tar.gz

---
+++ packages/urlAdjust/urlAdjust-0.1.1.tar.gz/urlAdjust-0.1.1/setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup, find_packages
+
+# Read the README file
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setup(
+    name='urlAdjust',
+    version='0.1.1',
+    packages=find_packages(),
+    install_requires=[],
+    description='UrlAdjust is a Python package that help developer generate get http request url with template and adjustment info. It aims to simplify the url generation process for get http request url. ',
+    long_description=long_description,
+    long_description_content_type='text/markdown',  # Use 'text/x-rst' if you use reStructuredText
+    author='Yu Xing',
+    author_email='f0x.sideproject@gmail.com',
+    url='https://github.com/F-0-X/UrlAdjust',
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.9',
+)
