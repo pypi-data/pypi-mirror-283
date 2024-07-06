d222723435f45d43a1c806c7aed16028efef5a9d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-06 14:03:58 +0000

    Add package nglory-py/0.0.1/nglory_py-0.0.1-py3-none-any.whl

---
+++ packages/nglory-py/nglory_py-0.0.1-py3-none-any.whl/ngpy/__init__.py
@@ -0,0 +1,5 @@
+from NGApi import NGApi
+from responses.UserResponse import UserResponse
+from responses.CountryResponse import CountryResponse
+from responses.MarketResponse import MarketResponse
+from responses.NotationResponse import NotationResponse
