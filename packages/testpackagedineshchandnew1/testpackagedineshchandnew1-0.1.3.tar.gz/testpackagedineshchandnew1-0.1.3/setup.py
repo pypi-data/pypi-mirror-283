b54d9cf79015071b1cdca7178acc422fbfd00f97
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-06 05:05:02 +0000

    Add package testpackagedineshchandnew1/0.1.3/testpackagedineshchandnew1-0.1.3.tar.gz

---
+++ packages/testpackagedineshchandnew1/testpackagedineshchandnew1-0.1.3.tar.gz/testpackagedineshchandnew1-0.1.3/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_packages
+setup(
+name='testpackagedineshchandnew1',
+version='0.1.3',
+author='Dineshchand',
+author_email='dineshchandgr@gmail.com',
+description='A short description of your package',
+packages=find_packages(),
+classifiers=[
+'Programming Language :: Python :: 3',
+'License :: OSI Approved :: MIT License',
+'Operating System :: OS Independent',
+],
+python_requires='>=3.6',
+)
