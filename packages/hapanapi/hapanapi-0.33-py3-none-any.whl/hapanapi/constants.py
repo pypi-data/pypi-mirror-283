4f8d8e08fdf5945931296d819187dd14a4d93f70
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-06 03:04:40 +0000

    Add package hapanapi/0.33/hapanapi-0.33-py3-none-any.whl

---
+++ packages/hapanapi/hapanapi-0.33-py3-none-any.whl/hapanapi/constants.py
@@ -0,0 +1,4 @@
+# URLS
+CORE_ADD_CLIENT_URL = "https://core.hapana.com/index.php?route=dashboard/clients/addclientnewpayment&pageurl=clients&from=home"
+GROW_ADD_CLIENT_URL = "https://grow-api.hapana.com/widget/form/vIYNBYCfx0mIl8DJr1fL"
+CORE_CLIENT_SUMMARY_URL = "https://core.hapana.com/index.php?route=dashboard/clients"
