555652ff69077169b26e68be566df81eee8d5914
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-06 03:04:38 +0000

    Add package image-text-reader/1.0.0/image_text_reader-1.0.0.tar.gz

---
+++ packages/image-text-reader/image_text_reader-1.0.0.tar.gz/image_text_reader-1.0.0/setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setup(
+    name="image_text_reader",
+    version="1.0.0",
+    author="barmin15",
+    author_email="bokora@lauderalumni.hu",
+    description="A library to read text from images",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/yourusername/image_text_reader",
+    packages=find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+    install_requires=[
+        'pytesseract',
+        'Pillow'
+    ],
+)
