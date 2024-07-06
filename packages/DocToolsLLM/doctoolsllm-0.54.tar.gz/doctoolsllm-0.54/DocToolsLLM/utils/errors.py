145370472bc7ab333aa62653281b26b9210e2e90
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-06 05:03:18 +0000

    Add package DocToolsLLM/0.54/doctoolsllm-0.54.tar.gz

---
+++ packages/DocToolsLLM/doctoolsllm-0.54.tar.gz/doctoolsllm-0.54/DocToolsLLM/utils/errors.py
@@ -0,0 +1,16 @@
+"""
+Exception classes
+"""
+
+class NoDocumentsRetrieved(Exception):
+    def __init__(self, message: str) -> None:
+        super().__init__(message)
+
+class NoDocumentsAfterLLMEvalFiltering(Exception):
+    def __init__(self, message: str) -> None:
+        super().__init__(message)
+
+class InvalidDocEvaluationByLLMEval(Exception):
+    def __init__(self, message: str) -> None:
+        super().__init__(message)
+
