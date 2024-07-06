4f618d2d91339be0b250cc2431f2240cf9d07723
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-06 15:04:07 +0000

    Add package NJUlogin/3.1.5/NJUlogin-3.1.5.tar.gz

---
+++ packages/NJUlogin/NJUlogin-3.1.5.tar.gz/NJUlogin-3.1.5/NJUlogin/__init__.py
@@ -0,0 +1,11 @@
+from .QRlogin import QRlogin
+from .pwdLogin import pwdLogin
+from .__version__ import (
+    __title__,
+    __version__,
+    __description__,
+    __url__,
+    __author__,
+    __author_email__,
+    __license__,
+)
