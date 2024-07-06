cd50d8294d7965cdc5a1604beb2170c3b1bda1ac
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-06 15:03:41 +0000

    Add package vecx/0.12/vecx-0.12-py3-none-any.whl

---
+++ packages/vecx/vecx-0.12-py3-none-any.whl/vecx/vectorx.py
@@ -0,0 +1,56 @@
+import requests
+
+class VectorX:
+    def __init__(self, token:str):
+        self.token = token
+        self.base_url = "https://vx.launchxlabs.ai"
+
+    def __str__(self):
+        return self.token
+
+    def generate_key(self)->str:
+        # Generate a random hex key of length 16
+        import secrets
+        key = secrets.token_hex(8)  # 8 bytes * 2 hex chars/byte = 16 chars
+        print("Store this encryption key in a secure location. Loss of the key will result in the irreversible loss of associated vector data.\nKey: ",key)
+        return key
+
+    def create_index(self, name:str, size:int, key:str, distance:str):
+        distance = distance.upper()
+        if distance not in ["COSINE", "L2", "DOT_PRODUCT"]:
+            raise ValueError(f"Invalid distance metric: {distance}")
+        headers = {
+            'Authorization': f'{self.token}',
+            'Content-Type': 'application/json'
+        }
+        data = {
+            'name': name,
+            'size': size,
+            'algo': distance,
+            'checksum':self.checksum(key),
+        }
+        response = requests.post(f'{self.base_url}/index/create', headers=headers, json=data)
+        print(response)
+        return "Index created successfully"
+
+    def list_indexes(self):
+        headers = {
+            'Authorization': f'{self.token}',
+        }
+        response = requests.get(f'{self.base_url}/index/create', headers=headers)
+        print(response)
+        return response.json()
+
+    def delete_index(self, name:str):
+        headers = {
+            'Authorization': f'{self.token}',
+        }
+        response = requests.get(f'{self.base_url}/index/create', headers=headers)
+        return f'Index {name} deleted successfully'
+
+    def checksum(self, key:str)->int:
+        # Convert last two characters of key to integer
+        return int(key[-2:], 16)
+
+
+
