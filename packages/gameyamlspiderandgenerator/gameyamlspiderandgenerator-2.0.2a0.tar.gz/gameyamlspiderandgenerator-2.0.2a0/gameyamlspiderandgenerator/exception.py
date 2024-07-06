1cef3ce12e7599e69f853330ef829fc89e188f46
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-06 04:06:03 +0000

    Add package gameyamlspiderandgenerator/2.0.2a0/gameyamlspiderandgenerator-2.0.2a0.tar.gz

---
+++ packages/gameyamlspiderandgenerator/gameyamlspiderandgenerator-2.0.2a0.tar.gz/gameyamlspiderandgenerator-2.0.2a0/gameyamlspiderandgenerator/exception.py
@@ -0,0 +1,38 @@
+from http import HTTPStatus
+
+
+class GenerateError(Exception):
+    pass
+
+
+class ApiKeyNotFoundError(GenerateError):
+    def __init__(self, hook: str):
+        super().__init__(f"The {hook} hook API key is required but not configured.")
+
+
+class PluginNotLoadedError(GenerateError):
+    def __init__(self):
+        super().__init__("Did you forget to use config.load()?")
+
+
+class InvalidUrlError(GenerateError):
+    pass
+
+
+class ReadOrWriteConfigFailed(GenerateError):
+    pass
+
+
+class InvalidTargetResourceError(GenerateError):
+    def __init__(self, code: int):
+        super().__init__(f"{code}: {HTTPStatus(code).description}")
+
+
+class ResponseNotInitialized(GenerateError):
+    def __init__(self, url: str):
+        super().__init__(f"Response not initialized, url: {url}")
+
+
+class InvalidResponse(GenerateError):
+    def __init__(self, url: str):
+        super().__init__(f"Invalid response, url: {url}")
