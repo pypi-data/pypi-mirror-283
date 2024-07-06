93f4f45f92729cc55d9c1a831dd009569dccea75
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-06 03:04:39 +0000

    Add package nsjprdev/0.1/nsjprdev-0.1.tar.gz

---
+++ packages/nsjprdev/nsjprdev-0.1.tar.gz/nsjprdev-0.1/setup.py
@@ -0,0 +1,21 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="nsjprdev",
+    version="0.1",
+    description="Library Of @V1HyperBot",
+    long_description="V1HyperBot",
+    long_description_content_type="text/markdown",
+    author="dikin",
+    author_email="dikin03101999@gmail.com",
+    url="https://github.com/HyperDreamX",
+    license="MIT",
+    install_requires=["pymongo", "pyromod", "pyrogram", "tgcrypto"],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    packages=find_packages(),
+    python_requires=">=3.10",
+)
