49a42fbf7e55df4f313d817e73c68b143bf8480b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-06 15:04:06 +0000

    Add package kt-ai-iot/0.4.0/kt_ai_iot-0.4.0.tar.gz

---
+++ packages/kt-ai-iot/kt_ai_iot-0.4.0.tar.gz/kt_ai_iot-0.4.0/setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup, find_packages
+
+with open('README.md', 'r', encoding='utf-8') as fh:
+    long_description = fh.read()
+setup(
+    name='kt_ai_iot',
+    version='0.4.0',
+    author='Rossi',
+    author_email='rizzieang@gmail.com',
+    description='A library for controlling KT AI IOT devices',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    packages=find_packages(),
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.6',
+    install_requires=[
+        'pyserial',
+        'numpy'
+    ],
+)
