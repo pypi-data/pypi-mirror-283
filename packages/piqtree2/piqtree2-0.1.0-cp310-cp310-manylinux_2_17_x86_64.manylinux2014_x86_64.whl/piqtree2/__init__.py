d88452d2519fe13e7174c913bed664a8136f6606
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-06 14:03:52 +0000

    Add package piqtree2/0.1.0/piqtree2-0.1.0-cp310-cp310-manylinux_2_17_x86_64.manylinux2014_x86_64.whl

---
+++ packages/piqtree2/piqtree2-0.1.0-cp310-cp310-manylinux_2_17_x86_64.manylinux2014_x86_64.whl/piqtree2/__init__.py
@@ -0,0 +1,21 @@
+"""piqtree2 - access the power of IQ-TREE within Python."""
+
+from ._iq_wrappers import (
+    TreeGenMode,
+    build_tree,
+    fit_tree,
+    random_trees,
+    robinson_foulds,
+)
+from ._options import available_models
+
+__version__ = "0.1.0"
+
+__all__ = [
+    "build_tree",
+    "fit_tree",
+    "random_trees",
+    "robinson_foulds",
+    "TreeGenMode",
+    "available_models",
+]
