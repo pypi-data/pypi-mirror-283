112bd492ddeec29e0a001141cadac16b6e9b744e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-06 03:04:00 +0000

    Add package helen-django-polls/0.1/helen-django-polls-0.1.tar.gz

---
+++ packages/helen-django-polls/helen-django-polls-0.1.tar.gz/helen-django-polls-0.1/polls/tests.py
@@ -0,0 +1,43 @@
+import datetime
+
+from django.test import TestCase
+from django.utils import timezone
+
+from .models import Question
+
+
+def create_question(question_text, **time_args):
+    """
+    Create a question with the given `question_text` and published the
+    given number of `days` offset to now (negative for questions published
+    in the past, positive for questions that have yet to be published).
+    """
+    time = timezone.now() + datetime.timedelta(**time_args)
+    return Question.objects.create(question_text=question_text, pub_date=time)
+
+
+class QuestionModelTests(TestCase):
+
+    def test_was_published_recently_with_future_question(self):
+        """
+        was_published_recently() returns False for questions whose pub_date
+        is in the future.
+        """
+        future_question = create_question(question_text="Future question", days=30)
+        self.assertIs(future_question.was_published_recently(), False)
+
+    def test_was_published_recently_with_old_question(self):
+        """
+        was_published_recently() returns False for questions whose pub_date
+        is older than 1 day.
+        """
+        old_question = create_question(question_text="Old question", days=-1, seconds=-1)
+        self.assertIs(old_question.was_published_recently(), False)
+
+    def test_was_published_recently_with_recent_question(self):
+        """
+        was_published_recently() returns True for questions whose pub_date
+        is within the last day.
+        """
+        recent_question = create_question(question_text="Recent question", hours=-23, minutes=-59, seconds=-59)
+        self.assertIs(recent_question.was_published_recently(), True)
