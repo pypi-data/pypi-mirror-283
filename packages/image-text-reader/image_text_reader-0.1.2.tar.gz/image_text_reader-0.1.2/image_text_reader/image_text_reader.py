cb0198053cb17597ec16cc98a19abafaa11f0c71
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-06 03:04:03 +0000

    Add package image-text-reader/0.1.2/image_text_reader-0.1.2.tar.gz

---
+++ packages/image-text-reader/image_text_reader-0.1.2.tar.gz/image_text_reader-0.1.2/image_text_reader/image_text_reader.py
@@ -0,0 +1,17 @@
+import pytesseract
+from PIL import Image, ImageEnhance, ImageFilter
+
+def preprocess_image(image_path):
+    image = Image.open(image_path)
+    image = image.convert('L')
+    image = image.filter(ImageFilter.SHARPEN)
+    enhancer = ImageEnhance.Contrast(image)
+    image = enhancer.enhance(2)
+    return image
+
+def ocr_image(image_path, tesseract_cmd=None):
+    if tesseract_cmd:
+        pytesseract.pytesseract.tesseract_cmd = tesseract_cmd
+    image = preprocess_image(image_path)
+    text = pytesseract.image_to_string(image, lang='eng')
+    return text
