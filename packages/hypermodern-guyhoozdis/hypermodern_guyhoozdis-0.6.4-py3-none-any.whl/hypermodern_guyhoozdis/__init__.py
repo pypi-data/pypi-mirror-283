a98e16773dbc8e04fddb9bb2eef815245454e5bf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-06 14:03:50 +0000

    Add package hypermodern-guyhoozdis/0.6.4/hypermodern_guyhoozdis-0.6.4-py3-none-any.whl

---
+++ packages/hypermodern-guyhoozdis/hypermodern_guyhoozdis-0.6.4-py3-none-any.whl/hypermodern_guyhoozdis/__init__.py
@@ -0,0 +1,6 @@
+"""The Hypermodern Python Project."""
+
+# TODO: Change to single source version strategy.
+#   https://packaging.python.org/en/latest/guides/single-sourcing-package-version/#single-sourcing-the-package-version
+#   https://docs.python.org/3.11/library/importlib.metadata.html#module-importlib.metadata
+__version__ = "0.6.4"
