130f36bcd8a1770f77eee8b80e5948be210c8afe
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-06 03:04:02 +0000

    Add package aimmocore-db/0.1.1/aimmocore_db-0.1.1.tar.gz

---
+++ packages/aimmocore-db/aimmocore_db-0.1.1.tar.gz/aimmocore_db-0.1.1/setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup, find_packages
+from setuptools.command.install import install
+import subprocess
+
+
+class PostInstallCommand(install):
+    """Post-installation for installation mode."""
+
+    def run(self):
+        install.run(self)
+        self._install_mongodb()
+
+    def _install_mongodb(self):
+        subprocess.run(["python", "-m", "aimmocore_db.install_mongodb"], check=True)
+
+
+setup(
+    name="aimmocore-db",
+    version="0.1.1",
+    packages=find_packages(),
+    include_package_data=True,
+    cmdclass={
+        "install": PostInstallCommand,
+    },
+)
