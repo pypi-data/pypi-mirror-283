5010bb88f138e3da2dbd07b0ef6064a099609b90
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-06 06:03:47 +0000

    Add package lega4e-library/0.0/lega4e_library-0.0.tar.gz

---
+++ packages/lega4e-library/lega4e_library-0.0.tar.gz/lega4e_library-0.0/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+  name='lega4e_library',
+  version='0.0',
+  description='Lega4e library',
+  packages=['lega4e_library'],
+  author_email='lega4e@lega4e.ru',
+  zip_safe=False,
+)
