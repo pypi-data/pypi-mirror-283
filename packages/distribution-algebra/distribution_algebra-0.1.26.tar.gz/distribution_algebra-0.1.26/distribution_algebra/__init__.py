a08ba7fc899879ecd607ababe202355978dffe00
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-07 04:04:25 +0000

    Add package distribution-algebra/0.1.26/distribution_algebra-0.1.26.tar.gz

---
+++ packages/distribution-algebra/distribution_algebra-0.1.26.tar.gz/distribution_algebra-0.1.26/distribution_algebra/__init__.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+
+import logging
+
+from distribution_algebra.beta import Beta
+from distribution_algebra.distribution import (UnivariateDistribution,
+                                               VectorizedDistribution)
+from distribution_algebra.lognormal import Lognormal
+from distribution_algebra.normal import Normal
+from distribution_algebra.poisson import Poisson
+
+# Declaring this silences mypy warning on `from distribution_algebra import Beta` etc.
+__all__ = [
+    "Normal",
+    "Lognormal",
+    "Beta",
+    "Poisson",
+    "UnivariateDistribution",
+    "VectorizedDistribution",
+]
+
+logging.getLogger("matplotlib").setLevel(logging.ERROR)
---
+++ packages/distribution-algebra/distribution_algebra-0.1.26.tar.gz/distribution_algebra-0.1.26/tests/test_normal.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+
+from hypothesis import assume, given
+from hypothesis import strategies as st
+
+from distribution_algebra.normal import Normal
+
+
+@given(
+    st.from_type(Normal),
+    st.floats(width=16, allow_infinity=False, allow_nan=False),
+    st.floats(width=64, allow_infinity=False, allow_nan=False),
+)
+def test_affine(x: Normal, a: float, b: float) -> None:
+    assume(a)
+    assert a * x + b == Normal(mean=a * x.mean + b, var=a**2 * x.var)  # type: ignore
+
+
+@given(st.from_type(Normal))
+def test_central_measures(x: Normal) -> None:
+    assert x.mean == x.median == x.mode
