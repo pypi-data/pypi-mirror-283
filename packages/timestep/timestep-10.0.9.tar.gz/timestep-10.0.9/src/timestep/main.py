56b5838b69d83972f40102eb667919839d64c9ed
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-07 04:03:49 +0000

    Add package timestep/10.0.9/timestep-10.0.9.tar.gz

---
+++ packages/timestep/timestep-10.0.9.tar.gz/timestep-10.0.9/src/timestep/main.py
@@ -0,0 +1,30 @@
+import typer
+
+
+app = typer.Typer()
+
+
+# @app.
+
+
+@app.callback()
+def callback():
+    """
+    Awesome Portal Gun
+    """
+
+
+@app.command()
+def shoot():
+    """
+    Shoot the portal gun
+    """
+    typer.echo("Shooting portal gun")
+
+
+@app.command()
+def load():
+    """
+    Load the portal gun
+    """
+    typer.echo("Loading portal gun")
