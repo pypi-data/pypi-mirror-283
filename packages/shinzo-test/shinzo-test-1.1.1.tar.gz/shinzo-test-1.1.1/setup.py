77748d8cb4908fcf23a83ac2bfbec50f63cae91b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-07 03:04:06 +0000

    Add package shinzo-test/1.1.1/shinzo-test-1.1.1.tar.gz

---
+++ packages/shinzo-test/shinzo-test-1.1.1.tar.gz/shinzo-test-1.1.1/setup.py
@@ -0,0 +1,45 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="shinzo-test",
+    version="1.1.1",
+    py_modules=["git_cloner"],
+    packages=find_packages(),
+    entry_points={
+        'console_scripts': [
+            'shinzo=git_cloner.git_cloner:clone_repo',
+        ],
+    },
+    install_requires=[  # List of dependencies
+        "alive_progress",
+        "colorama",
+        "black",
+        "djangorestframework",
+        "drf-spectacular",
+        "drf-spectacular-sidecar",
+        "Django",
+        "django-jazzmin",
+        "gunicorn",
+        "pillow",
+        "psycopg2-binary",
+        "python-dotenv",
+        "django-modeltranslation",
+        "django-ckeditor-5",
+        "django-cors-headers",
+        "django-rosetta",
+        "colorama",
+        "PyJWT",
+        "django-unfold"
+    ],
+    author="Jahongir Hakimjonov",
+    author_email="jahongirhakimjonov@gmail.com",
+    description="A Django project structure generator",
+    keywords="django project structure generator",
+    url="https://github.com/JahongirHakimjonov",
+    classifiers=[  # Classifiers for package indexing
+        'Framework :: Django',
+        'Programming Language :: Python :: 3',
+        'Operating System :: OS Independent',
+    ],
+
+)
