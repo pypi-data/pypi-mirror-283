b4351221c5dff07a0df7cd985fb3eb5279199ec3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-07 14:04:11 +0000

    Add package mmp/0.2.1/mmp-0.2.1.tar.gz

---
+++ packages/mmp/mmp-0.2.1.tar.gz/mmp-0.2.1/setup.py
@@ -0,0 +1,31 @@
+from setuptools import find_packages
+from setuptools import setup
+
+setup(
+    name='mmp',
+    version='0.2.1',
+    author='Alfonso Villaobos',
+    author_email='alfonso@codepeat.com',
+    url='https://github.com/alfonsocv12/mmp',
+    license='MIT',
+    description='mmp handle your py_modules easier and faster',
+    packages=find_packages(exclude=['tests.*', 'tests']),
+    long_description=open('readme.md').read(),
+    long_description_content_type='text/markdown',
+    entry_points={
+        'console_scripts': [
+            'mmp=mmp.cli.main:main',
+            'mex=mmp.cli.mex:main'
+        ]
+    },
+    install_requires=[
+        'virtualenv>=20.4.4',
+        'docopt==0.6.2'
+    ],
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Programming Language :: Python :: 3.7",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent"
+    ]
+)
