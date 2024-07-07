d1a1ce6c983d15f4e158cfcbd1a99f480dada8b7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-07 04:03:55 +0000

    Add package fromager/0.13.0/fromager-0.13.0-py3-none-any.whl

---
+++ packages/fromager/fromager-0.13.0-py3-none-any.whl/fromager/commands/download_sequence.py
@@ -0,0 +1,42 @@
+import json
+import logging
+
+import click
+from packaging.requirements import Requirement
+
+from .. import context, sources
+
+logger = logging.getLogger(__name__)
+
+
+@click.command()
+@click.argument("build_order_file")
+@click.argument("sdist_server_url")
+@click.pass_obj
+def download_sequence(
+    wkctx: context.WorkContext, build_order_file: str, sdist_server_url: str
+):
+    """Download a sequence of source distributions in order.
+
+    BUILD_ORDER_FILE is the build-order.json files to build
+
+    SDIST_SERVER_URL is the URL for a PyPI-compatible package index hosting sdists
+
+    Performs the equivalent of the 'step download-source-archive' command for each item in
+    the build order file.
+
+    """
+    with open(build_order_file, "r") as f:
+        for entry in json.load(f):
+            if entry["prebuilt"]:
+                logger.info(f"{entry['dist']} uses a pre-built wheel, skipping")
+                continue
+
+            if entry["source_url_type"] != "sdist":
+                logger.info(
+                    f"{entry['dist']} uses a {entry['source_url_type']} downloader, skipping"
+                )
+                continue
+
+            req = Requirement(f"{entry['dist']}=={entry['version']}")
+            sources.download_source(wkctx, req, [sdist_server_url])
