de6d5fd3530c3e4adc8810036a9ecedd94292950
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-07 03:03:59 +0000

    Add package zenberry/0.1/zenberry-0.1.tar.gz

---
+++ packages/zenberry/zenberry-0.1.tar.gz/zenberry-0.1/README.md
@@ -0,0 +1,17 @@
+# zenberry
+
+`zenberry` is a Python library that acts like `time.sleep` but beeps gently every second ^_^
+
+## Installation
+
+```bash
+pip install zenberry
+```
+
+## Usage
+
+```bash
+import zenberry
+zenberry.sleep(42)
+#beeps 42 times
+```
---
+++ packages/zenberry/zenberry-0.1.tar.gz/zenberry-0.1/setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='zenberry',
+    version='0.1',
+    packages=find_packages(),
+    install_requires=[
+        'numpy',
+        'sounddevice',
+    ],
+    description='A library that acts like time.sleep but beeps gently every second.',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    url='https://zenberry.one',
+    author='Eugene Zenberry',
+    author_email='zenberry.music@gmail.com',
+    license='MIT',
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.6',
+)
