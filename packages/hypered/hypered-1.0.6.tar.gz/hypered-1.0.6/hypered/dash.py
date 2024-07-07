208dc916c8962677bb07c3540f9784bf563960e4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-07 15:03:47 +0000

    Add package hypered/1.0.6/hypered-1.0.6.tar.gz

---
+++ packages/hypered/hypered-1.0.6.tar.gz/hypered-1.0.6/hypered/dash.py
@@ -0,0 +1,54 @@
+import argparse
+import logging
+
+import flask
+
+from .interface import misc
+from .server import watcher
+
+app = flask.Flask(__name__)
+watch = watcher.Watcher(misc.OUTPUT_DIR)
+
+
+@app.route("/")
+def index():
+    return flask.render_template("index.html")
+
+
+@app.route("/experiment_groups", methods=["GET"])
+def get_experiment_groups():
+    return flask.jsonify(list(watch.experiment_data.keys()))
+
+
+@app.route("/experiment_group/<group_name>/names", methods=["GET"])
+def get_experiment_group_names(group_name: str):
+    experiments = watch.experiment_data[group_name]
+    variables = list(experiments["params"].keys())
+    metrics = list(experiments["results"].keys())
+    return flask.jsonify({"variables": variables, "metrics": metrics})
+
+
+@app.route("/experiment_group/<group_name>/data", methods=["GET"])
+def get_experiment_group_data(group_name: str):
+    experiments = watch.experiment_data[group_name]
+    variables = experiments["params"]
+    metrics = experiments["results"]
+    if experiments["best"]:
+        best = experiments["best"]
+        best = {"variables": best["params"], "metrics": best["results"]}
+    else:
+        best = None
+    return flask.jsonify({"variables": variables, "metrics": metrics, "best": best})
+
+
+def main():
+    logging.basicConfig(level=logging.INFO)
+    parser = argparse.ArgumentParser(description="Hyperparameter optimizer dashboard.")
+    parser.add_argument("--host", type=str, default=None, help="HTTP server host.")
+    parser.add_argument("--port", type=int, default=None, help="HTTP server port.")
+    args = parser.parse_args()
+    app.run(host=args.host, port=args.port, debug=True)
+
+
+if __name__ == "__main__":
+    main()
---
+++ packages/hypered/hypered-1.0.6.tar.gz/hypered-1.0.6/hypered/server/watcher.py
@@ -0,0 +1,69 @@
+import json
+import os
+
+from watchdog.events import FileSystemEventHandler
+from watchdog.observers import Observer
+
+from ..utils.dict_utils import join_dicts
+
+
+class Watcher(FileSystemEventHandler):
+    experiment_data = {}
+
+    def __init__(self, directory):
+        super().__init__()
+        self.directory = directory
+        self.load_experiments()
+        self.observer = self.create_observer()
+
+    def create_observer(self):
+        observer = Observer()
+        observer.schedule(self, path=self.directory, recursive=True)
+        observer.start()
+
+    def on_modified(self, event):
+        if event.is_directory:
+            return
+        self.load_experiments()
+
+    def load_experiments(self):
+        self.experiment_data = {}
+        for experiment_group in os.listdir(self.directory):
+            group_data = self.load_experiment_group(experiment_group)
+            if group_data is None:
+                continue
+            self.experiment_data[experiment_group] = group_data
+
+    def load_experiment_group(self, experiment_group):
+        group_path = os.path.join(self.directory, experiment_group)
+        if not os.path.isdir(group_path):
+            return None
+        params_list = []
+        results_list = []
+        for experiment in os.listdir(group_path):
+            experiment_path = os.path.join(group_path, experiment)
+            params, results = self.load_experiment(experiment_path)
+            if params is None or results is None:
+                continue
+            params_list.append(params)
+            results_list.append(results)
+        params = join_dicts(params_list)
+        results = join_dicts(results_list)
+        best_path = os.path.join(group_path, "best.json")
+        if os.path.exists(best_path):
+            with open(best_path) as best_file:
+                best = json.load(best_file)
+        else:
+            best = None
+        return {"params": params, "results": results, "best": best}
+
+    def load_experiment(self, experiment_path: str):
+        params_path = os.path.join(experiment_path, "params.json")
+        results_path = os.path.join(experiment_path, "results.json")
+        if not os.path.exists(params_path) or not os.path.exists(results_path):
+            return None, None
+        with open(params_path) as params_file:
+            params = json.load(params_file)
+        with open(results_path) as results_file:
+            results = json.load(results_file)
+        return params, results
