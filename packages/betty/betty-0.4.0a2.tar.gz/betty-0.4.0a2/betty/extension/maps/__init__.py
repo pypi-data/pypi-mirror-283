022bfb04f9a98d6f5ae45f17c0ee50acb4a0825e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-07 04:04:23 +0000

    Add package betty/0.4.0a2/betty-0.4.0a2.tar.gz

---
+++ packages/betty/betty-0.4.0a2.tar.gz/betty-0.4.0a2/betty/extension/maps/__init__.py
@@ -0,0 +1,57 @@
+"""Integrate Betty with `Leaflet.js <https://leafletjs.com/>`_."""
+
+from __future__ import annotations
+
+from pathlib import Path
+
+from typing_extensions import override
+
+from betty.locale.localizable import _, Localizable
+from betty.project.extension import Extension, UserFacingExtension
+from betty.extension.webpack import Webpack, WebpackEntryPointProvider
+from typing import TYPE_CHECKING
+
+if TYPE_CHECKING:
+    from collections.abc import Sequence
+
+
+class Maps(UserFacingExtension, WebpackEntryPointProvider):
+    """
+    Provide interactive maps for use on web pages.
+    """
+
+    @override
+    @classmethod
+    def name(cls) -> str:
+        return "betty.extension.Maps"
+
+    @override
+    @classmethod
+    def depends_on(cls) -> set[type[Extension]]:
+        return {Webpack}
+
+    @override
+    @classmethod
+    def assets_directory_path(cls) -> Path:
+        return Path(__file__).parent / "assets"
+
+    @override
+    @classmethod
+    def webpack_entry_point_directory_path(cls) -> Path:
+        return Path(__file__).parent / "webpack"
+
+    @override
+    def webpack_entry_point_cache_keys(self) -> Sequence[str]:
+        return ()
+
+    @override
+    @classmethod
+    def label(cls) -> Localizable:
+        return _("Maps")
+
+    @override
+    @classmethod
+    def description(cls) -> Localizable:
+        return _(
+            'Display lists of places as interactive maps using <a href="https://leafletjs.com/">Leaflet</a>.'
+        )
---
+++ packages/betty/betty-0.4.0a2.tar.gz/betty-0.4.0a2/betty/extension/privatizer/__init__.py
@@ -0,0 +1,93 @@
+"""Privatize people and associated data by determining if they are likely to be alive."""
+
+from __future__ import annotations
+
+from collections import defaultdict
+from logging import getLogger
+
+from typing_extensions import override
+
+from betty.locale.localizable import _, Localizable
+from betty.project.extension import UserFacingExtension
+from betty.load import PostLoader
+from betty.model.ancestry import Person, HasPrivacy
+from betty.privatizer import Privatizer as PrivatizerApi
+from typing import TYPE_CHECKING
+
+if TYPE_CHECKING:
+    from betty.model import Entity
+
+
+class Privatizer(UserFacingExtension, PostLoader):
+    """
+    Extend the Betty Application with privatization features.
+    """
+
+    @override
+    @classmethod
+    def name(cls) -> str:
+        return "betty.extension.Privatizer"
+
+    @override
+    async def post_load(self) -> None:
+        self.privatize()
+
+    @override
+    @classmethod
+    def label(cls) -> Localizable:
+        return _("Privatizer")
+
+    @override
+    @classmethod
+    def description(cls) -> Localizable:
+        return _(
+            "Determine if people can be proven to have died. If not, mark them and their associated entities private."
+        )
+
+    def privatize(self) -> None:
+        """
+        Privatize the project's ancestry.
+        """
+        logger = getLogger(__name__)
+        logger.info(self.project.app.localizer._("Privatizing..."))
+
+        privatizer = PrivatizerApi(
+            self.project.configuration.lifetime_threshold,
+            localizer=self.project.app.localizer,
+        )
+
+        newly_privatized: dict[type[HasPrivacy & Entity], int] = defaultdict(lambda: 0)
+        entities: list[HasPrivacy & Entity] = []
+        for entity in self.project.ancestry:
+            if isinstance(entity, HasPrivacy):
+                entities.append(entity)
+                if entity.private:
+                    newly_privatized[entity.type] -= 1  # type: ignore[index]
+
+        for entity in entities:
+            privatizer.privatize(entity)
+
+        for entity in entities:
+            if entity.private:
+                newly_privatized[entity.type] += 1  # type: ignore[index]
+
+        if newly_privatized[Person] > 0:
+            logger.info(
+                self.project.app.localizer._(
+                    "Privatized {count} people because they are likely still alive."
+                ).format(
+                    count=str(newly_privatized[Person]),
+                )
+            )
+        for entity_type in set(newly_privatized) - {Person}:
+            if newly_privatized[entity_type] > 0:
+                logger.info(
+                    self.project.app.localizer._(
+                        "Privatized {count} {entity_type}, because they are associated with private information."
+                    ).format(
+                        count=str(newly_privatized[entity_type]),
+                        entity_type=entity_type.entity_type_label_plural().localize(
+                            self.project.app.localizer
+                        ),
+                    )
+                )
