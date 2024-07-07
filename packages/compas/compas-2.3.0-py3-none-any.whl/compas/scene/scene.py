348f23b07964e0c4066d5188c3306d294c23302d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-07 03:04:19 +0000

    Add package compas/2.3.0/compas-2.3.0-py3-none-any.whl

---
+++ packages/compas/compas-2.3.0-py3-none-any.whl/compas/scene/scene.py
@@ -0,0 +1,144 @@
+import compas.datastructures  # noqa: F401
+import compas.geometry  # noqa: F401
+from compas.datastructures import Tree
+from compas.datastructures import TreeNode
+
+from .context import after_draw
+from .context import before_draw
+from .context import clear
+from .context import detect_current_context
+from .sceneobject import SceneObject
+
+
+class Scene(Tree):
+    """A scene is a container for hierarchical scene objects which are to be visualised in a given context.
+
+    Parameters
+    ----------
+    name : str, optional
+        The name of the scene.
+    context : str, optional
+        The context in which the scene is visualised. Will be automatically detected if not specified.
+
+    Attributes
+    ----------
+    tree : :class:`compas.scene.SceneTree`
+        The underlying tree data structure of the scene.
+    objects : list[:class:`compas.scene.SceneObject`]
+        All scene objects in the scene.
+
+    Examples
+    --------
+    >>> from compas.scene import Scene
+    >>> from compas.geometry import Box
+    >>> scene = Scene()
+    >>> box = Box.from_width_height_depth(1, 1, 1)
+    >>> boxobj = scene.add(box)
+    >>> scene.draw()  # doctest: +SKIP
+
+    """
+
+    @property
+    def __data__(self):
+        # type: () -> dict
+        items = {str(object.item.guid): object.item for object in self.objects}
+        return {
+            "name": self.name,
+            "root": self.root.__data__,  # type: ignore
+            "items": list(items.values()),
+        }
+
+    @classmethod
+    def __from_data__(cls, data):
+        # type: (dict) -> Scene
+        scene = cls(data["name"])
+        items = {str(item.guid): item for item in data["items"]}
+
+        def add(node, parent, items):
+            for child_node in node["children"]:
+                guid = child_node["item"]
+                settings = child_node["settings"]
+                sceneobject = parent.add(items[guid], **settings)
+                add(child_node, sceneobject, items)
+
+        add(data["root"], scene, items)
+
+        return scene
+
+    def __init__(self, name="Scene", context=None):
+        # type: (str, str | None) -> None
+        super(Scene, self).__init__(name=name)
+        super(Scene, self).add(TreeNode(name="ROOT"))
+        self.context = context or detect_current_context()
+
+    @property
+    def objects(self):
+        # type: () -> list[SceneObject]
+        return [node for node in self.nodes if not node.is_root]  # type: ignore
+
+    def add(self, item, parent=None, **kwargs):
+        # type: (compas.geometry.Geometry | compas.datastructures.Datastructure, SceneObject | TreeNode | None, dict) -> SceneObject
+        """Add an item to the scene.
+
+        Parameters
+        ----------
+        item : :class:`compas.data.Data`
+            The item to add.
+        parent : :class:`compas.data.Data`, optional
+            The parent item.
+        **kwargs : dict
+            Additional keyword arguments to create the scene object for the item.
+
+        Returns
+        -------
+        :class:`compas.scene.SceneObject`
+            The scene object associated with the item.
+        """
+
+        parent = parent or self.root
+
+        if isinstance(item, SceneObject):
+            sceneobject = item
+        else:
+            if "context" in kwargs:
+                if kwargs["context"] != self.context:
+                    raise Exception("Object context should be the same as scene context: {} != {}".format(kwargs["context"], self.context))
+                del kwargs["context"]  # otherwist the SceneObject receives "context" twice, which results in an error
+            sceneobject = SceneObject(item=item, context=self.context, **kwargs)  # type: ignore
+        super(Scene, self).add(sceneobject, parent=parent)
+        return sceneobject
+
+    def clear(self):
+        # type: () -> None
+        """Clear everything from the current context of the scene."""
+
+        clear()
+
+    def clear_objects(self):
+        # type: () -> None
+        """Clear all objects inside the scene."""
+
+        guids = []
+        for sceneobject in self.objects:
+            guids += sceneobject.guids
+            sceneobject._guids = None
+        clear(guids=guids)
+
+    def draw(self):
+        """Draw the scene."""
+
+        if not self.context:
+            raise ValueError("No context detected.")
+
+        before_draw()
+
+        self.clear_objects()
+
+        drawn_objects = []
+        for sceneobject in self.objects:
+            if sceneobject.show:
+                drawn_objects += sceneobject.draw()
+
+        after_draw(drawn_objects)
+
+        return drawn_objects
---
+++ packages/compas/compas-2.3.0-py3-none-any.whl/compas_ghpython/scene/meshobject.py
@@ -0,0 +1,151 @@
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
+from compas.scene import MeshObject as BaseMeshObject
+from compas_rhino import conversions
+from compas_rhino.scene.helpers import ngon
+
+from .sceneobject import GHSceneObject
+
+
+class MeshObject(GHSceneObject, BaseMeshObject):
+    """Scene object for drawing mesh data structures.
+
+    Parameters
+    ----------
+    disjoint : bool, optional
+        Draw the mesh as disjoint faces.
+    **kwargs : dict, optional
+        Additional keyword arguments.
+
+    Attributes
+    ----------
+    disjoint : bool
+        Draw the mesh as disjoint faces.
+
+    """
+
+    def __init__(self, disjoint=False, **kwargs):
+        super(MeshObject, self).__init__(**kwargs)
+        self.disjoint = disjoint
+
+    def draw(self):
+        """Draw the mesh.
+
+        Parameters
+        ----------
+        color : tuple[int, int, int] | tuple[float, float, float] | :class:`compas.colors.Color`, optional
+            The color of the mesh.
+
+        Returns
+        -------
+        list[:rhino:`Rhino.Geometry.Mesh`]
+
+        """
+        # the rhino scene object can set an overal color and component colors simultaneously
+        # because it can set an overall color on the mesh object attributes
+        # this is not possible in GH (since there is no such object)
+        # either we set an overall color or we set component colors
+        self._guids = []
+
+        if self.show_faces is True:
+            vertexcolors = []
+            if len(self.vertexcolor):
+                vertexcolors = [self.vertexcolor[vertex] for vertex in self.mesh.vertices()]
+
+            facecolors = []
+            if len(self.facecolor):
+                facecolors = [self.facecolor[face] for face in self.mesh.faces()]
+
+            color = None
+            if not vertexcolors and not facecolors:
+                color = self.color
+
+            vertex_index = self.mesh.vertex_index()
+            vertex_xyz = self.vertex_xyz
+
+            vertices = [vertex_xyz[vertex] for vertex in self.mesh.vertices()]
+            faces = [[vertex_index[vertex] for vertex in self.mesh.face_vertices(face)] for face in self.mesh.faces()]
+
+            geometry = conversions.vertices_and_faces_to_rhino(
+                vertices,
+                faces,
+                color=color,
+                vertexcolors=vertexcolors,
+                facecolors=facecolors,
+                disjoint=self.disjoint,
+            )
+
+            # geometry.Transform(conversions.transformation_to_rhino(self.worldtransformation))
+
+            self._guids.append(geometry)
+
+        elif self.show_faces:
+            self._guids += self.draw_faces()
+
+        if self.show_vertices:
+            self._guids += self.draw_vertices()
+
+        if self.show_edges:
+            self._guids += self.draw_edges()
+
+        return self.guids
+
+    def draw_vertices(self):
+        """Draw a selection of vertices.
+
+        Returns
+        -------
+        list[:rhino:`Rhino.Geometry.Point3d`]
+
+        """
+        points = []
+
+        vertices = list(self.mesh.vertices()) if self.show_vertices is True else self.show_vertices or []
+
+        if vertices:
+            for vertex in vertices:
+                points.append(conversions.point_to_rhino(self.vertex_xyz[vertex]))
+
+        return points
+
+    def draw_edges(self):
+        """Draw a selection of edges.
+
+        Returns
+        -------
+        list[:rhino:`Rhino.Geometry.Line`]
+
+        """
+        lines = []
+
+        edges = list(self.mesh.edges()) if self.show_edges is True else self.show_edges or []
+
+        if edges:
+            for edge in edges:
+                lines.append(conversions.line_to_rhino((self.vertex_xyz[edge[0]], self.vertex_xyz[edge[1]])))
+
+        return lines
+
+    def draw_faces(self):
+        """Draw a selection of faces.
+
+        Returns
+        -------
+        list[:rhino:`Rhino.Geometry.Mesh`]
+
+        """
+        meshes = []
+
+        faces = list(self.mesh.faces()) if self.show_faces is True else self.show_faces or []
+
+        if faces:
+            for face in faces:
+                color = self.facecolor[face]
+                vertices = [self.vertex_xyz[vertex] for vertex in self.mesh.face_vertices(face)]
+                facet = ngon(len(vertices))
+                if facet:
+                    meshes.append(conversions.vertices_and_faces_to_rhino(vertices, [facet], color=color))
+
+        return meshes
