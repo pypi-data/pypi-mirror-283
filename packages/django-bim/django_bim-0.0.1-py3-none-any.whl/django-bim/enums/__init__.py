5280a7b0684c6c57581c2739898b5919d71fe25b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-07 04:04:01 +0000

    Add package django-bim/0.0.1/django_bim-0.0.1-py3-none-any.whl

---
+++ packages/django-bim/django_bim-0.0.1-py3-none-any.whl/django-bim/enums/__init__.py
@@ -0,0 +1,41 @@
+# -*- coding: utf-8 -*-
+
+"""
+Django IFC Enums Module
+=======================
+
+This module provides enumeration classes that align with the IFC (Industry
+Foundation Classes) standards. These enums are used throughout the Django IFC
+application to ensure consistency and compliance with the predefined IFC
+standards.
+
+Currently, the module includes:
+- `IfcAddressTypeEnum`: ...
+- `IfcChangeActionEnum`: ...
+- `IfcRoleEnum`: Enumerations for different roles specified in IFC standards
+    that can be assigned to actors in a building construction project.
+- `IfcStateEnum`: ...
+
+"""
+
+# =============================================================================
+# Imports
+# =============================================================================
+
+# Local enumeration modules
+from .enum_ifc_address_type import IfcAddressTypeEnum
+from .enum_ifc_change_action import IfcChangeActionEnum
+from .enum_ifc_role import IfcRoleEnum
+from .enum_ifc_state import IfcStateEnum
+
+
+# =============================================================================
+# Public Interface
+# =============================================================================
+
+__all__ = [
+    "IfcAddressTypeEnum",
+    "IfcChangeActionEnum",
+    "IfcRoleEnum",
+    "IfcStateEnum",
+]
---
+++ packages/django-bim/django_bim-0.0.1-py3-none-any.whl/django-bim/fields/model/__init__.py
@@ -0,0 +1,45 @@
+# -*- coding: utf-8 -*-
+
+
+# =============================================================================
+# Docstring
+# =============================================================================
+
+"""
+Django IFC Model Fields Module
+==============================
+
+This module provides custom Django model fields tailored to handle specific
+data types used in the IFC (Industry Foundation Classes) standard for
+building and construction data. These fields ensure compliance with IFC data
+specifications, enabling more accurate data management and exchange within
+Django applications.
+
+"""
+
+
+# =============================================================================
+# Imports
+# =============================================================================
+
+# Import | Local Modules
+from .field_model_ifc_guid import IfcGloballyUniqueIdField
+from .field_model_ifc_identifier import IfcIdentifierField
+from .field_model_ifc_label import IfcLabelField
+from .field_model_ifc_role_type import IfcRoleTypeField
+from .field_model_ifc_text import IfcTextField
+from .field_model_ifc_timestamp import IfcTimestampField
+
+
+# =============================================================================
+# Module Level Variables
+# =============================================================================
+
+__all__ = [
+    "IfcGloballyUniqueIdField",
+    "IfcIdentifierField",
+    "IfcLabelField",
+    "IfcRoleTypeField",
+    "IfcTextField",
+    "IfcTimestampField",
+]
