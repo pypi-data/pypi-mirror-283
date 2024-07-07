6f5263323e2bdcebb20e60b98f35bd79675e7c7c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-07 04:04:15 +0000

    Add package suuuuuuuuu-suuuuu/0.0.1/suuuuuuuuu_suuuuu-0.0.1-py3-none-any.whl

---
+++ packages/suuuuuuuuu-suuuuu/suuuuuuuuu_suuuuu-0.0.1-py3-none-any.whl/rdpvps_999/__init__.py
@@ -0,0 +1,4 @@
+def youness(name,impact):
+    print(f"the are a bug in your website the name of bug is {name} and the ilpact of this bug is {impact}")
+    
+youness("sql injacton ","still you data")
