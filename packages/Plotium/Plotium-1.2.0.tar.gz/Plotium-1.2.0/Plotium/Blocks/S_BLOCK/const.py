44fdd597e25b6438bd67c2613efa1a5f1cb43e9b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-07 04:04:11 +0000

    Add package Plotium/1.2.0/Plotium-1.2.0.tar.gz

---
+++ packages/Plotium/Plotium-1.2.0.tar.gz/Plotium-1.2.0/Plotium/Blocks/S_BLOCK/const.py
@@ -0,0 +1,86 @@
+
+sec_name = "S Block Elements"
+
+electronegativity = {
+    "Hydrogen (H)": 2.20,
+    "Lithium (Li)": 0.98,
+    "Sodium (Na)": 0.93,
+    "Potassium (K)": 0.82,
+    "Rubidium (Rb)": 0.82,
+    "Cesium (Cs)": 0.79,
+    "Francium (Fr)": 0.7,
+    "Beryllium (Be)": 1.57,
+    "Magnesium (Mg)": 1.31,
+    "Calcium (Ca)": 1.00,
+    "Strontium (Sr)": 0.95,
+    "Barium (Ba)": 0.89,
+    "Radium (Ra)": 0.9
+}
+
+# atomic radii in Angstroms
+atomic_radii = {
+    "Hydrogen (H)": 0.53,
+    "Lithium (Li)": 1.67,
+    "Sodium (Na)": 1.90,
+    "Potassium (K)": 2.43,
+    "Rubidium (Rb)": 2.65,
+    "Cesium (Cs)": 2.98,
+    "Francium (Fr)": 3.42,
+    "Beryllium (Be)": 1.53,
+    "Magnesium (Mg)": 1.73,
+    "Calcium (Ca)": 2.31,
+    "Strontium (Sr)": 2.49,
+    "Barium (Ba)": 2.68,
+    "Radium (Ra)": 2.83
+}
+
+# density in g/cm^3
+density = {
+    "Hydrogen (H)": 0.00008988,
+    "Lithium (Li)": 0.534,
+    "Sodium (Na)": 0.971,
+    "Potassium (K)": 0.862,
+    "Rubidium (Rb)": 1.532,
+    "Cesium (Cs)": 1.873,
+    "Francium (Fr)": 2.48,  # Estimated value
+    "Beryllium (Be)": 1.85,
+    "Magnesium (Mg)": 1.74,
+    "Calcium (Ca)": 1.54,
+    "Strontium (Sr)": 2.64,
+    "Barium (Ba)": 3.62,
+    "Radium (Ra)": 5
+}
+
+# in Kelvin
+melting_point = {
+    "Hydrogen (H)": 14.01,
+    "Lithium (Li)": 453.69,
+    "Sodium (Na)": 370.87,
+    "Potassium (K)": 336.53,
+    "Rubidium (Rb)": 312.46,
+    "Cesium (Cs)": 301.59,
+    "Francium (Fr)": 300,  # Estimated value
+    "Beryllium (Be)": 1560,
+    "Magnesium (Mg)": 923,
+    "Calcium (Ca)": 1115,
+    "Strontium (Sr)": 1050,
+    "Barium (Ba)": 1000,
+    "Radium (Ra)": 969
+}
+
+# in K
+boiling_point = {
+    "Hydrogen (H)": 20.28,
+    "Lithium (Li)": 1615,
+    "Sodium (Na)": 1156,
+    "Potassium (K)": 1032,
+    "Rubidium (Rb)": 961,
+    "Cesium (Cs)": 944,
+    "Francium (Fr)": 950,  # Estimated value
+    "Beryllium (Be)": 2741,
+    "Magnesium (Mg)": 1363,
+    "Calcium (Ca)": 1757,
+    "Strontium (Sr)": 1650,
+    "Barium (Ba)": 2118,
+    "Radium (Ra)": 1737
+}
