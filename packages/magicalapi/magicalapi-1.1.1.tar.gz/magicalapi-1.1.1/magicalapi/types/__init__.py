d4486054b4f1622ece40956bb76fa2063d846cbf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-07 03:04:14 +0000

    Add package magicalapi/1.1.1/magicalapi-1.1.1.tar.gz

---
+++ packages/magicalapi/magicalapi-1.1.1.tar.gz/magicalapi-1.1.1/magicalapi/types/__init__.py
@@ -0,0 +1,11 @@
+from .base import ErrorResponse
+from .profile_data import Profile, ProfileDataResponse
+from .youtube_top_keywords import KeywordIdea, YoutubeTopKeywordsResponse
+
+__all__ = [
+    "ErrorResponse",
+    "YoutubeTopKeywordsResponse",
+    "KeywordIdea",
+    "ProfileDataResponse",
+    "Profile",
+]
