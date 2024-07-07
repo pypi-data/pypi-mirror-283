f31733f3526b40f698387681b9fb6380da49b94b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-07 15:03:47 +0000

    Add package athina-client/0.1.5/athina_client-0.1.5.tar.gz

---
+++ packages/athina-client/athina_client-0.1.5.tar.gz/athina_client-0.1.5/athina_client/datasets/dataset.py
@@ -0,0 +1,140 @@
+from typing import Any, Dict, List, Optional
+from dataclasses import dataclass, field
+from athina_client.services import AthinaApiService
+
+
+@dataclass
+class Dataset:
+    id: str
+    source: str
+    name: str
+    description: Optional[str] = None
+    language_model_id: Optional[str] = None
+    prompt_template: Optional[Any] = None
+    rows: List[Dict[str, Any]] = field(default_factory=list)
+
+    @staticmethod
+    def create(
+        name: str,
+        description: Optional[str] = None,
+        language_model_id: Optional[str] = None,
+        prompt_template: Optional[Any] = None,
+        rows: List[Dict[str, Any]] = None,
+    ):
+        """
+        Creates a new dataset with the specified properties.
+        Parameters:
+        - name (str): The name of the dataset. This is a required field.
+        - description (Optional[str]): An optional textual description of the dataset, providing additional context.
+        - language_model_id (Optional[str]): An optional identifier for the language model associated with this dataset.
+        - prompt_template (Optional[Any]): An optional template for prompts used in this dataset.
+
+        Returns:
+        The newly created dataset object
+
+        Raises:
+        - Exception: If the dataset could not be created due to an error like invalid parameters, database errors, etc.
+        """
+        dataset_data = {
+            "source": "dev_sdk",
+            "name": name,
+            "description": description,
+            "language_model_id": language_model_id,
+            "prompt_template": prompt_template,
+            "dataset_rows": rows or [],
+        }
+
+        # Remove keys where the value is None
+        dataset_data = {k: v for k, v in dataset_data.items() if v is not None}
+
+        try:
+            created_dataset_data = AthinaApiService.create_dataset(dataset_data)
+        except Exception as e:
+            raise
+        dataset = Dataset(
+            id=created_dataset_data["id"],
+            source=created_dataset_data["source"],
+            name=created_dataset_data["name"],
+            description=created_dataset_data["description"],
+            language_model_id=created_dataset_data["language_model_id"],
+            prompt_template=created_dataset_data["prompt_template"],
+        )
+        return dataset
+
+    @staticmethod
+    def add_rows(dataset_id: str, rows: List[Dict[str, Any]]):
+        """
+        Adds rows to a dataset in batches of 100.
+
+        Parameters:
+        - dataset_id (str): The ID of the dataset to add rows to.
+        - rows (List[DatasetRow]): The rows to add to the dataset.
+
+        Raises:
+        - Exception: If the API returns an error or the limit of 1000 rows is exceeded.
+        """
+        batch_size = 100
+        for i in range(0, len(rows), batch_size):
+            batch = rows[i : i + batch_size]
+            try:
+                AthinaApiService.add_dataset_rows(dataset_id, batch)
+            except Exception as e:
+                raise
+
+    @staticmethod
+    def list_datasets():
+        """
+        Lists all datasets for a given athina api key.
+        """
+        try:
+            datasets = AthinaApiService.list_datasets()
+        except Exception as e:
+            raise
+        return [
+            Dataset(
+                id=dataset["id"],
+                source=dataset["source"],
+                name=dataset["name"],
+                description=dataset["description"],
+                language_model_id=dataset["language_model_id"],
+                prompt_template=dataset["prompt_template"],
+            )
+            for dataset in datasets
+        ]
+
+    @staticmethod
+    def delete_dataset_by_id(dataset_id: str):
+        """
+        Deletes a dataset by its ID.
+        """
+        try:
+            response = AthinaApiService.delete_dataset_by_id(dataset_id)
+            return response
+        except Exception as e:
+            raise
+
+    @staticmethod
+    def get_dataset_by_id(dataset_id: str):
+        """
+        Gets a dataset by its ID.
+        """
+        try:
+            response = AthinaApiService.get_dataset_by_id(dataset_id)
+        except Exception as e:
+            raise
+        return response
+
+    @staticmethod
+    def get_dataset_by_name(name: str):
+        """
+        Gets a dataset by name.
+        """
+        try:
+            response = AthinaApiService.get_dataset_by_name(name)
+        except Exception as e:
+            raise
+        return response
+
+    @staticmethod
+    def dataset_link(dataset_id: str):
+        return f"https://app.athina.ai/develop/{dataset_id}"
