401d523aebdb546bc331de041ff8e05030d65f07
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-07 14:04:11 +0000

    Add package ace-tools/0.0/ace_tools-0.0.tar.gz

---
+++ packages/ace-tools/ace_tools-0.0.tar.gz/ace_tools-0.0/setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='ace_tools',
+    version='0.0',
+    packages=find_packages(),
+    install_requires=[
+    ],
+    author='Paul McMillan',
+    author_email='paul@mcmillan.ws',
+    description='A placeholder empty package',
+    url='',
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+)
