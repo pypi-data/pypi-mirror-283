d227ad80cde76e2d9c5c460ed92de3a4ddf9f396
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-07 03:04:58 +0000

    Add package zealot/0.1.2/zealot-0.1.2.tar.gz

---
+++ packages/zealot/zealot-0.1.2.tar.gz/zealot-0.1.2/tests/test_listeners.py
@@ -0,0 +1,94 @@
+import logging
+import re
+
+import pytest
+from djangoproject.social.models import Post, User
+from zealot.errors import NPlusOneError
+from zealot.listeners import zealot_context
+
+from .factories import PostFactory, UserFactory
+
+pytestmark = pytest.mark.django_db
+
+
+@zealot_context()
+def test_can_log_errors(settings, caplog):
+    settings.ZEALOT_RAISE = False
+
+    [user_1, user_2] = UserFactory.create_batch(2)
+    PostFactory.create(author=user_1)
+    PostFactory.create(author=user_2)
+    with caplog.at_level(logging.WARNING):
+        for user in User.objects.all():
+            _ = list(user.posts.all())
+        assert (
+            re.search(
+                r"N\+1 detected on User\.posts at .*\/test_listeners\.py:23 in test_can_log_errors",
+                caplog.text,
+            )
+            is not None
+        ), f"{caplog.text} does not match regex"
+
+
+@zealot_context()
+def test_errors_include_caller():
+    [user_1, user_2] = UserFactory.create_batch(2)
+    PostFactory.create(author=user_1)
+    PostFactory.create(author=user_2)
+    with pytest.raises(
+        NPlusOneError,
+        match=r"N\+1 detected on User\.posts at .*\/test_listeners\.py:43 in test_errors_include_caller",
+    ):
+        for user in User.objects.all():
+            _ = list(user.posts.all())
+
+
+@zealot_context()
+def test_can_exclude_with_allowlist(settings):
+    settings.ZEALOT_ALLOWLIST = [{"model": "social.User", "field": "posts"}]
+
+    [user_1, user_2] = UserFactory.create_batch(2)
+    PostFactory.create(author=user_1)
+    PostFactory.create(author=user_2)
+
+    # this will not raise, allow-listed
+    for user in User.objects.all():
+        _ = list(user.posts.all())
+
+    with pytest.raises(NPlusOneError):
+        for post in Post.objects.all():
+            _ = post.author
+
+
+@zealot_context()
+def test_can_use_fnmatch_pattern_in_allowlist_model(settings):
+    settings.ZEALOT_ALLOWLIST = [{"model": "social.U*"}]
+
+    [user_1, user_2] = UserFactory.create_batch(2)
+    PostFactory.create(author=user_1)
+    PostFactory.create(author=user_2)
+
+    # this will not raise, allow-listed
+    for user in User.objects.all():
+        _ = list(user.posts.all())
+
+    with pytest.raises(NPlusOneError):
+        for post in Post.objects.all():
+            _ = post.author
+
+
+@zealot_context()
+def test_can_use_fnmatch_pattern_in_allowlist_field(settings):
+    settings.ZEALOT_ALLOWLIST = [{"model": "social.User", "field": "p*st*"}]
+
+    [user_1, user_2] = UserFactory.create_batch(2)
+    PostFactory.create(author=user_1)
+    PostFactory.create(author=user_2)
+
+    # this will not raise, allow-listed
+    for user in User.objects.all():
+        _ = list(user.posts.all())
+
+    with pytest.raises(NPlusOneError):
+        for post in Post.objects.all():
+            _ = post.author
---
+++ packages/zealot/zealot-0.1.2.tar.gz/zealot-0.1.2/tests/test_patch.py
@@ -0,0 +1,29 @@
+import sys
+
+import pytest
+from djangoproject.social.models import User
+from zealot.listeners import zealot_context
+
+from tests.factories import UserFactory
+
+pytestmark = pytest.mark.django_db
+
+
+@zealot_context()
+def test_handles_calling_queryset_many_times():
+    UserFactory.create()
+    user = User.objects.prefetch_related("posts").all()[0]
+    for _ in range(sys.getrecursionlimit() + 1):
+        # this should *not* raise a recursion error
+        list(user.posts.all())
+
+
+@zealot_context()
+def test_handles_empty_querysets():
+    User.objects.none().first()
+
+
+@zealot_context()
+def test_handles_get_with_values():
+    user = UserFactory.create()
+    User.objects.filter(pk=user.pk).values("username").get()
