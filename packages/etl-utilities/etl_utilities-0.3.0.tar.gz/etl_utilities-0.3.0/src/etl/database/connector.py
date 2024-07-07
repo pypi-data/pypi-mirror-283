9e113fe7afbe29474af34c666f9f3fa2312782d5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-07 15:04:07 +0000

    Add package etl-utilities/0.3.0/etl_utilities-0.3.0.tar.gz

---
+++ packages/etl-utilities/etl_utilities-0.3.0.tar.gz/etl_utilities-0.3.0/src/etl/database/connector.py
@@ -0,0 +1,11 @@
+import urllib
+from sqlalchemy import create_engine
+
+
+class Connector:
+    @staticmethod
+    def get_mssql_trusted_connection(host: str, instance: str, database: str):
+        connection_string = f"SERVER={host}\\{instance};DATABASE={database};Trusted_Connection=yes;"
+        quoted = urllib.parse.quote_plus("DRIVER={ODBC DRIVER 17 for SQL SERVER};" + connection_string)
+        engine = create_engine('mssql+pyodbc:////?odbc_connect={}'.format(quoted))
+        return engine.connect().connection
