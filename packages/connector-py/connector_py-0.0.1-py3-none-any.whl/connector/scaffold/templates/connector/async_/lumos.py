a5fed32ccc9d2f4ecd37d45c49fe9c4c5b54dd3e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-07 03:03:58 +0000

    Add package connector-py/0.0.1/connector_py-0.0.1-py3-none-any.whl

---
+++ packages/connector-py/connector_py-0.0.1-py3-none-any.whl/connector/scaffold/templates/connector/async_/lumos.py
@@ -0,0 +1,111 @@
+from connector.async_.lumos import LumosCommandsMixin
+from connector.serializers.lumos import (
+    AssignEntitlementResp,
+    EntitlementType,
+    FindEntitlementAssociationsResp,
+    FoundAccountData,
+    FoundEntitlementData,
+    FoundResourceData,
+    GetAccountResp,
+    GetResourceResp,
+    ListAccountsResp,
+    ListEntitlementsResp,
+    ListResourcesResp,
+    ResourceType,
+    UnassignEntitlementResp,
+    ValidateCredentialsResp,
+)
+
+from {name}.serializers.lumos import (
+    AssignEntitlementArgs,
+    FindEntitlementAssociationsArgs,
+    GetAccountArgs,
+    GetResourceArgs,
+    ListAccountsArgs,
+    ListEntitlementsArgs,
+    ListResourcesArgs,
+    UnassignEntitlementArgs,
+    ValidateCredentialsArgs,
+)
+
+
+class AsyncCommands(LumosCommandsMixin):
+    app_id = "{hyphenated_name}"
+
+    async def validate_credentials(self, args: ValidateCredentialsArgs) -> ValidateCredentialsResp:
+        return ValidateCredentialsResp(
+            response=True,
+            errors=[],
+            raw_data={{}},
+        )
+
+    async def list_accounts(self, args: ListAccountsArgs) -> ListAccountsResp:
+        return ListAccountsResp(
+            response=[FoundAccountData(given_name="{title}", integration_specific_id="100", email="albus@lumos.com")],
+            errors=[],
+            raw_data = {{"localhost:8080/test": {{"test": "test"}}}},
+        )
+
+    async def get_account(self, args: GetAccountArgs) -> GetAccountResp:
+        return GetAccountResp(
+            response=FoundAccountData(
+                integration_specific_id="1234", email="albus@lumos.com"
+            ),
+            errors=[],
+            raw_data={{"localhost:8080/test": {{"test": "test"}}}},
+        )
+
+    async def list_resources(self, args: ListResourcesArgs) -> ListResourcesResp:
+        return ListResourcesResp(
+            response={{
+                ResourceType.MOCK_CONNECTOR_RESOURCE_1: [
+                    FoundResourceData(
+                        integration_specific_id="1234",
+                        label="Starter Resource",
+                        resource_type=ResourceType.MOCK_CONNECTOR_RESOURCE_1,
+                    )
+                ]
+            }},
+            errors=[],
+            raw_data={{}},
+        )
+
+    async def get_resource(self, args: GetResourceArgs) -> GetResourceResp:
+        return GetResourceResp(
+            response=FoundResourceData(
+                integration_specific_id=args.integration_specific_id,
+                label="label",
+                resource_type=ResourceType.MOCK_CONNECTOR_RESOURCE_1,
+            ),
+            errors=[],
+            raw_data={{}},
+        )
+
+    async def list_entitlements(self, args: ListEntitlementsArgs) -> ListEntitlementsResp:
+        return [
+            FoundEntitlementData(
+                integration_specific_id="1234",
+                integration_specific_resource_id="1234",
+                is_assignable=False,
+                label="mock entitlement",
+                entitlement_type=EntitlementType.MOCK_CONNECTOR_ENTITLEMENT_1,
+            )
+        ]
+
+    async def find_entitlement_associations(
+        self,
+        args: FindEntitlementAssociationsArgs,
+    ) -> FindEntitlementAssociationsResp:
+        return []
+
+    async def assign_entitlement(
+        self,
+        args: AssignEntitlementArgs,
+    ) -> AssignEntitlementResp:
+        pass
+
+    async def unassign_entitlement(
+        self,
+        args: UnassignEntitlementArgs,
+    ) -> UnassignEntitlementResp:
+        pass
---
+++ packages/connector-py/connector_py-0.0.1-py3-none-any.whl/connector/scaffold/templates/connector/serializers/lumos.py
@@ -0,0 +1,53 @@
+from connector.serializers.lumos import (
+    AssignEntitlementArgsBase,
+    FindEntitlementAssociationsArgsBase,
+    GetAccountArgsBase,
+    GetResourceArgsBase,
+    ListAccountsArgsBase,
+    ListEntitlementsArgsBase,
+    ListResourcesArgsBase,
+    UnassignEntitlementArgsBase,
+    ValidateCredentialsArgsBase,
+)
+from pydantic import Field
+
+from .auth import Auth
+
+
+class ListAccountsArgs(Auth, ListAccountsArgsBase):
+    page: int
+    size: int = Field(5)
+
+
+class ValidateCredentialsArgs(Auth, ValidateCredentialsArgsBase):
+    pass
+
+
+class GetAccountArgs(Auth, GetAccountArgsBase):
+    account_id: int
+
+
+class ListResourcesArgs(Auth, ListResourcesArgsBase):
+    page: int
+    size: int = Field(5)
+
+
+class GetResourceArgs(Auth, GetResourceArgsBase):
+    pass
+
+
+class ListEntitlementsArgs(Auth, ListEntitlementsArgsBase):
+    page: int
+    size: int = Field(5)
+
+
+class FindEntitlementAssociationsArgs(Auth, FindEntitlementAssociationsArgsBase):
+    pass
+
+
+class AssignEntitlementArgs(Auth, AssignEntitlementArgsBase):
+    pass
+
+
+class UnassignEntitlementArgs(Auth, UnassignEntitlementArgsBase):
+    pass
