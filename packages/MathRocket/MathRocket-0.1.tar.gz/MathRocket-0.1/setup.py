5aa700efa2a02591836192050c848be91812b991
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-07 04:03:48 +0000

    Add package MathRocket/0.1/MathRocket-0.1.tar.gz

---
+++ packages/MathRocket/MathRocket-0.1.tar.gz/MathRocket-0.1/setup.py
@@ -0,0 +1,34 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='MathRocket', 
+    version='0.1',  
+
+    description='Uma biblioteca Python para simulação e cálculo de trajetórias de foguetes, com suporte para calcular massa, empuxo e tempo de queima.',
+    long_description='A biblioteca Python "MathRocket" é projetada para facilitar a simulação e o cálculo de trajetórias de foguetes em ambientes de modelagem física e engenharia. Com "MathRocket", desenvolvedores podem calcular dinamicamente a aceleração, impulso e outras características essenciais de foguetes através de uma interface simples e intuitiva. Esta biblioteca é ideal para projetos que envolvem análise de desempenho de lançamentos, estudos de física aplicada e simulações educacionais, oferecendo uma maneira eficiente e precisa de explorar o comportamento de sistemas de propulsão espacial.',  
+    long_description_content_type='text/markdown',  
+
+    author='Unoxys',
+
+    url='https://github.com/Uno13x/MathRocket',
+
+    license='MIT',
+
+    keywords='python, física, Foguete, Rocket, rocket, physic, Physics, Physic, mass, Mass',
+
+    packages=find_packages(),
+
+    install_requires=[
+        'pacote1',
+        'pacote2',
+    ],
+
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+    ],
+)
