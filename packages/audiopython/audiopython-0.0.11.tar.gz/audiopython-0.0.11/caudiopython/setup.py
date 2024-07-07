f3bd2df48d84e2a9fe351db9b1f9711aeea7aba5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-07 14:04:04 +0000

    Add package audiopython/0.0.11/audiopython-0.0.11.tar.gz

---
+++ packages/audiopython/audiopython-0.0.11.tar.gz/audiopython-0.0.11/caudiopython/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, Extension
+from Cython.Build import cythonize
+import numpy as np
+
+extensions = [
+    Extension("analysis", ["analysis.py"], include_dirs=[np.get_include()]),
+    Extension("granulator", ["granulator.py"], include_dirs=[np.get_include()]),
+    Extension("operations", ["operations.py"], include_dirs=[np.get_include()]),
+    Extension("sampler", ["sampler.py"], include_dirs=[np.get_include()]),
+    Extension("spectrum", ["spectrum.py"], include_dirs=[np.get_include()]),
+]
+
+setup(name="audiopython", ext_modules=cythonize(extensions))
