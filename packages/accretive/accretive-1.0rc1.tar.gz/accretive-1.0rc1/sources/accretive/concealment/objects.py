ca07320330a04cbb2dd5cf3ec8ce9b577b4ea0e0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-07 04:04:17 +0000

    Add package accretive/1.0rc1/accretive-1.0rc1.tar.gz

---
+++ packages/accretive/accretive-1.0rc1.tar.gz/accretive-1.0rc1/sources/accretive/concealment/objects.py
@@ -0,0 +1,35 @@
+# vim: set filetype=python fileencoding=utf-8:
+# -*- coding: utf-8 -*-
+
+#============================================================================#
+#                                                                            #
+#  Licensed under the Apache License, Version 2.0 (the "License");           #
+#  you may not use this file except in compliance with the License.          #
+#  You may obtain a copy of the License at                                   #
+#                                                                            #
+#      http://www.apache.org/licenses/LICENSE-2.0                            #
+#                                                                            #
+#  Unless required by applicable law or agreed to in writing, software       #
+#  distributed under the License is distributed on an "AS IS" BASIS,         #
+#  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.  #
+#  See the License for the specific language governing permissions and       #
+#  limitations under the License.                                            #
+#                                                                            #
+#============================================================================#
+
+
+''' Accretive objects with attribute concealment. '''
+
+
+from .. import __
+from .. import objects as _objects
+
+
+class Object( __.ConcealerExtension, _objects.Object ):
+    ''' Accretive objects. '''
+
+Object.__doc__ = __.generate_docstring(
+    _objects.Object, 'instance attributes concealment' )
+
+
+__all__ = __.discover_public_attributes( globals( ) )
