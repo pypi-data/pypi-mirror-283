b9298a5ee7bee851eac8feace5c17a204bc8c38a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-07 15:03:46 +0000

    Add package pycryptoex/0.3.0/pycryptoex-0.3.0.tar.gz

---
+++ packages/pycryptoex/pycryptoex-0.3.0.tar.gz/pycryptoex-0.3.0/test.py
@@ -0,0 +1,15 @@
+# Websocket / ReconnetingWebsocket
+
+# .subscribe ?
+# .send ?
+# .connect
+# .reconnect
+# .disconnect
+# .start / .run / .process / .receive_loop
+# .stop
+
+# Клиент имеет метод для создания объектов класса вебсокета
+# exchange.create_websocket().start()
+#                sync          async
+
+# WebsocketApi
