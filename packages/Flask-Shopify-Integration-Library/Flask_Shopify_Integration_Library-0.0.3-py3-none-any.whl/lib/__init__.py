d1cdf5fffb137b318d1ba1218170ce349eabb9ac
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-07 15:03:50 +0000

    Add package Flask-Shopify-Integration-Library/0.0.3/Flask_Shopify_Integration_Library-0.0.3-py3-none-any.whl

---
+++ packages/Flask-Shopify-Integration-Library/Flask_Shopify_Integration_Library-0.0.3-py3-none-any.whl/lib/__init__.py
@@ -0,0 +1,3 @@
+from .utils import is_dictionary, verify_webhook
+from .data import SqlServerDatabase
+from .services import ShopifyBaseService
---
+++ packages/Flask-Shopify-Integration-Library/Flask_Shopify_Integration_Library-0.0.3-py3-none-any.whl/lib/utils/__init__.py
@@ -0,0 +1,2 @@
+from .dictionary_helper import is_dictionary
+from .webhook_verify import verify_webhook
