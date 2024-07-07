7616224577d223ea6a103603903698d834161352
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-07 04:04:13 +0000

    Add package orchestralAI/0.0.1/orchestralai-0.0.1.tar.gz

---
+++ packages/orchestralAI/orchestralai-0.0.1.tar.gz/orchestralai-0.0.1/README.md
@@ -0,0 +1,2 @@
+# orchestralAI
+This is a placeholder package for OrchestralAI. Full functionality will be added soon. Stay tuned!
---
+++ packages/orchestralAI/orchestralai-0.0.1.tar.gz/orchestralai-0.0.1/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="orchestralAI",
+    version="0.0.1",
+    author="Jacob Roman",
+    author_email="jakemroman@gmail.com",
+    description="A placeholder for orchestralAI package.",
+    long_description="This is a placeholder package for orchestralAI. Full functionality will be added soon.",
+    long_description_content_type="text/markdown",
+    url="https://github.com/jakeroman/orchestralAI",
+    packages=find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
