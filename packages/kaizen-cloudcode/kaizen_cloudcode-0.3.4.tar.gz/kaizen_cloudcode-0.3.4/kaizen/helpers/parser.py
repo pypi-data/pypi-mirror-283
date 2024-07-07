afa2c1106f2db93a7aa1541b1838e4af8a72785e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-07 15:03:48 +0000

    Add package kaizen-cloudcode/0.3.4/kaizen_cloudcode-0.3.4.tar.gz

---
+++ packages/kaizen-cloudcode/kaizen_cloudcode-0.3.4.tar.gz/kaizen_cloudcode-0.3.4/kaizen/helpers/parser.py
@@ -0,0 +1,39 @@
+import json
+import re
+
+EXCLUDED_FILETYPES = ["json"]
+
+
+def extract_json(text):
+    # Find the start and end positions of the JSON data
+    start_index = text.find("{")
+    end_index = text.rfind("}") + 1
+
+    # Extract the JSON data from the text
+    json_data = text[start_index:end_index]
+    json_data = re.sub(r"\s*\\*\n*\s*{\s*\n*\s*", "{", json_data)
+    json_data = re.sub(r"\s*\\*\n*\s*\[\s*\n*\s*", "[", json_data)
+    json_data = re.sub(r"\s*\\*\n*\s*}\s*\n*\s*", "}", json_data)
+    json_data = re.sub(r"\s*\\*\n\s*\]\s*\n\s*", "]", json_data)
+    json_data = re.sub(r",\s*\\*\n\s*", ",", json_data)
+    json_data = re.sub(r'"\s*\\*\n\s*', '"', json_data)
+    json_data = json_data.replace("\n", "\\n")
+
+    # Parse the JSON data
+    parsed_data = json.loads(json_data)
+    return parsed_data
+
+
+def extract_multi_json(text):
+    start_index = text.find("[")
+    end_index = text.rfind("]") + 1
+    json_data = text[start_index:end_index]
+    parsed_data = json.loads(json_data)
+    return parsed_data
+
+
+def extract_markdown_content(text: str) -> str:
+    match = re.search(r"```([\s\S]*?)```", text)
+    if match:
+        return match.group(1).strip()
+    return ""
