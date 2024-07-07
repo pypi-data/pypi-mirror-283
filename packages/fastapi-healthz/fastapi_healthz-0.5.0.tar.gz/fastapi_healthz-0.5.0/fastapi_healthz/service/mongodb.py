4604d051cecb1aa48a88b7f1908313858623ff2f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-07 03:04:52 +0000

    Add package fastapi-healthz/0.5.0/fastapi_healthz-0.5.0.tar.gz

---
+++ packages/fastapi-healthz/fastapi_healthz-0.5.0.tar.gz/fastapi_healthz-0.5.0/fastapi_healthz/service/mongodb.py
@@ -0,0 +1,44 @@
+from typing import Any
+try:
+    from pymongo import MongoClient
+except ImportError:
+    raise ImportError("You must install pymongo to use MongoDB health check")
+
+from .abstract import HealthCheckAbstract
+from ..models import HealthCheckStatusEnum
+
+
+class HealthCheckMongoDb(HealthCheckAbstract):
+    def __init__(
+        self,
+        host: str | None,
+        port: str | None,
+        service: str | None = None,
+        tags: list[str] | None = None,
+        **kwargs: Any
+    ):
+        super().__init__(service=service, tags=tags)
+
+        self.__host = host
+        self.__port = port
+        self.__kwargs = kwargs
+
+    @property
+    def service(self) -> str:
+        return self._service if self._service is not None else "mongodb"
+
+    @property
+    def connection_uri(self) -> str | None:
+        return f"mongodb://{self.__host}:{self.__port}" if self.__host and self.__port else None
+
+    def check_health(self) -> HealthCheckStatusEnum:
+        res: HealthCheckStatusEnum = HealthCheckStatusEnum.UNHEALTHY
+
+        try:
+            client = MongoClient(host=self.__host, port=self.__port, **self.__kwargs)
+            client.server_info()
+            res = HealthCheckStatusEnum.HEALTHY
+        except Exception:
+            pass
+
+        return res
