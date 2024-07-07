5ca6a83a296d17f47a5e23e10c2a798575066c29
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-07 04:03:50 +0000

    Add package flexiai/0.8.39/flexiai-0.8.39.tar.gz

---
+++ packages/flexiai/flexiai-0.8.39.tar.gz/flexiai-0.8.39/flexiai/config/config.py
@@ -0,0 +1,30 @@
+# config/config.py 
+import logging
+from pydantic_settings import BaseSettings
+from pydantic import ValidationError
+from dotenv import load_dotenv
+
+# Load environment variables from .env file
+load_dotenv()
+
+class Config(BaseSettings):
+    OPENAI_API_KEY: str
+    AZURE_OPENAI_API_KEY: str
+    AZURE_OPENAI_ENDPOINT: str
+    AZURE_OPENAI_API_VERSION: str
+    CREDENTIAL_TYPE: str
+
+    class Config:
+        env_file = ".env"
+
+# Initialize logger
+logger = logging.getLogger(__name__)
+
+try:
+    # Attempt to load the configuration
+    config = Config()
+    logger.info("Configuration loaded successfully.")
+except ValidationError as e:
+    # Log and raise an error if configuration validation fails
+    logger.error(f"Configuration validation error: {str(e)}", exc_info=True)
+    raise RuntimeError("Environment variable validation failed. Please check your .env file and ensure all variables are set correctly.") from e
