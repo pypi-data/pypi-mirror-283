844bef7f95e9ca8384b43803256b038ed5800607
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-07 14:03:59 +0000

    Add package algopy/0.7.1/algopy-0.7.1.tar.gz

---
+++ packages/algopy/algopy-0.7.1.tar.gz/algopy-0.7.1/algopy/version.py
@@ -0,0 +1,8 @@
+
+# THIS FILE IS GENERATED FROM ALGOPY SETUP.PY
+short_version='0.7.1'
+version='0.7.1'
+release=True
+
+if not release:
+    version += '.devf903c708ce5a5a76801239963380ca6e2f57772d'
