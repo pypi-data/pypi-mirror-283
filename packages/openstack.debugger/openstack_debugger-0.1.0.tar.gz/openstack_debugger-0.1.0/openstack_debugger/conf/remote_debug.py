e58981858d527f7627b131b5b2ece74da3c20b02
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-08 14:05:16 +0000

    Add package openstack.debugger/0.1.0/openstack_debugger-0.1.0.tar.gz

---
+++ packages/openstack.debugger/openstack_debugger-0.1.0.tar.gz/openstack_debugger-0.1.0/openstack_debugger/conf/remote_debug.py
@@ -0,0 +1,35 @@
+"""
+Nova 프로젝트와 유사한 원격 디버깅 옵션을 지원하기 위한 코드
+
+Ref: <https://opendev.org/openstack/nova/src/commit/8e4a7290f8467f1e915f3bb494ce61ade5aa511c/nova/conf/remote_debug.py>
+"""
+from oslo_config import cfg
+
+debugger_group = cfg.OptGroup('remote_debug', title='debugger options')
+
+ALL_OPTS = [
+    cfg.HostAddressOpt(
+        'host',
+        help="""
+디버깅 클라이언트를 연결할 서버 IP 혹은 이름
+"""),
+    cfg.PortOpt('port',
+        help="""
+디버깅 클라이언트를 연결할 서버의 포트
+"""),
+    cfg.BoolOpt('wait_for_client',
+        default=False,
+        help="""
+(선택옵션) 디버깅 클라이언트의 연결이 될 때까지 실행을 기다린다. 코드의 초기화 과정을 기다릴 때 유용
+Ref: <https://github.com/microsoft/debugpy/wiki/Command-Line-Reference#--wait-for-client>
+"""),
+]
+
+
+def register_opts(conf):
+    """'remote_debug' 그룹 옵션을 구성에 등록"""
+    conf.register_opts(ALL_OPTS, group=debugger_group)
+
+def list_opts():
+    """'remote_debug' 그룹 옵션 내용"""
+    return {debugger_group: ALL_OPTS}
