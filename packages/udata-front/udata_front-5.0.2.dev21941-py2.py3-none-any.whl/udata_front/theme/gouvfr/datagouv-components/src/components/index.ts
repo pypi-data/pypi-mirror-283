60a465007418dcf11f23c96d568353540c483757
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-08 15:05:00 +0000

    Add package udata-front/5.0.2.dev21941/udata_front-5.0.2.dev21941-py2.py3-none-any.whl

---
+++ packages/udata-front/udata_front-5.0.2.dev21941-py2.py3-none-any.whl/udata_front/theme/gouvfr/datagouv-components/src/components/index.ts
@@ -0,0 +1,17 @@
+import AppLink from "./AppLink/AppLink.vue";
+import { CopyButton } from "./CopyButton";
+import { DatasetCard } from "./DatasetCard";
+import InformationPanel from "./InformationPanel";
+import OrganizationNameWithCertificate from "./Organization";
+import Pagination from "./Pagination";
+import { ResourceAccordion, Loader as ResourceAccordionLoader } from "./ResourceAccordion";
+import { QualityItem } from "./QualityItem";
+import { QualityItemWarning } from "./QualityItemWarning";
+import { QualityScore } from "./QualityScore";
+import { Toggletip } from "./Toggletip";
+import { QualityComponent } from "./QualityComponent"
+import { QualityComponentInline } from "./QualityComponentInline"
+import { ReadMore } from "./ReadMore"
+import Well from "./Well";
+
+export { AppLink, CopyButton, DatasetCard, InformationPanel, OrganizationNameWithCertificate, Pagination, QualityComponent, QualityComponentInline, QualityItem, QualityItemWarning, QualityScore, ReadMore, ResourceAccordion, ResourceAccordionLoader, Toggletip, Well };
