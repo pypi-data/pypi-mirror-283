555bfcafef7eddb487b6481bc98bf1513e60d65b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-08 04:04:21 +0000

    Add package django-umsebenzi/1.2.0/django_umsebenzi-1.2.0-py3-none-any.whl

---
+++ packages/django-umsebenzi/django_umsebenzi-1.2.0-py3-none-any.whl/umsebenzi/admin.py
@@ -0,0 +1,18 @@
+from django.contrib import admin
+
+from umsebenzi.models import Project, Task
+from umsebenzi.forms import TaskForm
+
+
+@admin.register(Project)
+class ProjectAdmin(admin.ModelAdmin):
+    list_display = ('title', 'description', 'code', 'created_by', 'created_at')
+
+
+@admin.register(Task)
+class TaskAdmin(admin.ModelAdmin):
+    list_display = (
+        'project', 'code', 'title', 'description', 'status',
+        'created_by', 'assigned_to', 'created_at'
+    )
+    form = TaskForm
