8203047ef1c3ddb824cdb6e0f460faaa58ba59a5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-08 15:04:17 +0000

    Add package alooyupd/1.0.6/alooyupd-1.0.6.tar.gz

---
+++ packages/alooyupd/alooyupd-1.0.6.tar.gz/alooyupd-1.0.6/alooyupd/check_update.py
@@ -0,0 +1,9 @@
+def latest_vers():
+  return "1.0.2"
+
+current_version = "1.0.1"
+def check_update():
+    if latest_vers() != current_version:
+        return "You are using old version. Please Update"
+    else:
+        pass
