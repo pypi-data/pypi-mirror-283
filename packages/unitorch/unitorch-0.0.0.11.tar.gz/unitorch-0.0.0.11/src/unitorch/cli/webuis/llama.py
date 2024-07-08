6f62998b18d5b3f88b3d625d37172f12aa4ff2ee
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-08 15:04:49 +0000

    Add package unitorch/0.0.0.11/unitorch-0.0.0.11.tar.gz

---
+++ packages/unitorch/unitorch-0.0.0.11.tar.gz/unitorch-0.0.0.11/src/unitorch/cli/webuis/llama.py
@@ -0,0 +1,108 @@
+# Copyright (c) FULIUCANSHENG.
+# Licensed under the MIT License.
+
+import io
+import torch
+import gc
+import gradio as gr
+from PIL import Image
+from unitorch.cli import CoreConfigureParser, GenericWebUI
+from unitorch.cli import register_webui
+from unitorch.cli.models.llama import pretrained_llama_infos
+from unitorch.cli.pipelines.llama import LlamaForGenerationPipeline
+from unitorch.cli.webuis import (
+    matched_pretrained_names,
+    create_element,
+    create_accordion,
+    create_row,
+    create_column,
+    create_group,
+    create_tab,
+    create_tabs,
+    create_blocks,
+    create_pretrain_layout,
+    create_freeu_layout,
+)
+from unitorch.cli.webuis import SimpleWebUI
+
+
+@register_webui("core/webui/llama")
+class LlamaGenerationWebUI(SimpleWebUI):
+    pretrained_names = list(pretrained_llama_infos.keys())
+    supported_pretrained_names = matched_pretrained_names(
+        pretrained_names,
+        ["^llama-", "^llama2-", "^open-llama-", "^llama3-"],
+    )
+
+    def __init__(self, config: CoreConfigureParser):
+        self._config = config
+        self._pipe = None if not hasattr(self, "_pipe") else self._pipe
+        self._status = "Stopped" if self._pipe is None else "Running"
+        if len(self.supported_pretrained_names) == 0:
+            raise ValueError("No supported pretrained models found.")
+        self._name = self.supported_pretrained_names[0]
+
+        # Create the elements
+        pretrain_layout_group = create_pretrain_layout(
+            self.supported_pretrained_names, self._name
+        )
+        name, status, start, stop, pretrain_layout = (
+            pretrain_layout_group.name,
+            pretrain_layout_group.status,
+            pretrain_layout_group.start,
+            pretrain_layout_group.stop,
+            pretrain_layout_group.layout,
+        )
+
+        prompt = create_element("text", "Input Prompt", lines=3)
+        generate = create_element("button", "Generate")
+        result = create_element("text", "Output Result", lines=3)
+
+        # Create the blocks
+        left = create_column(prompt, generate)
+        right = create_column(result)
+        iface = create_blocks(pretrain_layout, create_row(left, right))
+
+        # Create the events
+        iface.__enter__()
+
+        start.click(self.start, inputs=[name], outputs=[status])
+        stop.click(self.stop, outputs=[status])
+        generate.click(self.serve, inputs=[prompt], outputs=[result])
+
+        iface.load(
+            fn=lambda: [gr.update(value=self._name), gr.update(value=self._status)],
+            outputs=[name, status],
+        )
+
+        iface.__exit__()
+
+        super().__init__(config, iname="LLAMA", iface=iface)
+
+    def start(self, pretrained_name, **kwargs):
+        if self._status == "Running":
+            self.stop()
+        self._name = pretrained_name
+        self._pipe = LlamaForGenerationPipeline.from_core_configure(
+            self._config,
+            pretrained_name=pretrained_name,
+        )
+        self._status = "Running"
+        return self._status
+
+    def stop(self, **kwargs):
+        self._pipe.to("cpu")
+        del self._pipe
+        gc.collect()
+        torch.cuda.empty_cache()
+        self._pipe = None if not hasattr(self, "_pipe") else self._pipe
+        self._status = "Stopped" if self._pipe is None else "Running"
+        return self._status
+
+    def serve(
+        self,
+        text: str,
+    ):
+        assert self._pipe is not None
+        result = self._pipe(text)
+        return result
