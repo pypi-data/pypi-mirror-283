6ccbab7c20225dd3aa76362092003184037213e8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-08 14:04:58 +0000

    Add package onnx-weekly/1.17.0.dev20240708/onnx_weekly-1.17.0.dev20240708-cp310-cp310-manylinux_2_17_aarch64.manylinux2014_aarch64.whl

---
+++ packages/onnx-weekly/onnx_weekly-1.17.0.dev20240708-cp310-cp310-manylinux_2_17_aarch64.manylinux2014_aarch64.whl/onnx/test/version_utils.py
@@ -0,0 +1,13 @@
+# Copyright (c) ONNX Project Contributors
+
+# SPDX-License-Identifier: Apache-2.0
+from __future__ import annotations
+
+from packaging.version import parse as version
+
+
+def numpy_older_than(ver: str) -> bool:
+    """Returns True if the numpy version is older than the given version."""
+    import numpy  # pylint: disable=import-outside-toplevel
+
+    return version(numpy.__version__) < version(ver)
