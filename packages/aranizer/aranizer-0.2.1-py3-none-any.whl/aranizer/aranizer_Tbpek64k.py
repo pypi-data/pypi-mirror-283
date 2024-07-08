ea3dd870736946807bb17e9c88bba0ced84f6062
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-08 04:04:41 +0000

    Add package aranizer/0.2.1/aranizer-0.2.1-py3-none-any.whl

---
+++ packages/aranizer/aranizer-0.2.1-py3-none-any.whl/aranizer/aranizer_Tbpek64k.py
@@ -0,0 +1,23 @@
+import pkg_resources
+from transformers import PreTrainedTokenizerFast
+
+def get_tokenizer():
+    """
+    Initializes and returns a custom tokenizer for Arabic language processing.
+
+    This function loads a tokenizer from a predefined file and enriches its
+    vocabulary with a set of Arabic diacritics as special tokens. The tokenizer
+    is based on the PreTrainedTokenizerFast class from the transformers library.
+
+    Returns:
+        PreTrainedTokenizerFast: A tokenizer with extended vocabulary to
+                                 include Arabic diacritics.
+    """
+    # Locate the tokenizer file using pkg_resources
+    tokenizer_path = pkg_resources.resource_filename(__name__, 'BPE_Moshakal_Tokenizer/TBPE_tokenizer_64.0K.json')
+
+    # Initialize the tokenizer
+    tokenizer_fast = PreTrainedTokenizerFast(tokenizer_file=tokenizer_path)
+
+
+    return tokenizer_fast
