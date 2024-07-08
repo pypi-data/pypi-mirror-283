8424235bcf503c96bc5000bbcf1c91c12bcb93f4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-08 04:04:32 +0000

    Add package Medyator/0.2.0/medyator-0.2.0.tar.gz

---
+++ packages/Medyator/medyator-0.2.0.tar.gz/medyator-0.2.0/setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="Medyator",
+    version="0.2.0",
+    author="Misdirection",
+    author_email="misdirection@live.de",
+    description="A command and query mediator for Python.",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/misdirection/medyator",
+    packages=find_packages(where="src"),
+    package_dir={"": "src"},
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.12",
+    install_requires=["typing-extensions", "kink"],
+    tests_require=[
+        "pytest",
+    ],
+    setup_requires=["pytest-runner"],
+)
---
+++ packages/Medyator/medyator-0.2.0.tar.gz/medyator-0.2.0/tests/test_medyator.py
@@ -0,0 +1,58 @@
+import os
+import sys
+
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
+import pytest
+from src.medyator import Medyator
+from src.contracts import Query, Command
+from src.request_handler import QueryHandler, CommandHandler
+
+from kink import di
+import src.kink  # for extension of Container
+
+
+@pytest.fixture
+def test_query():
+    class TestQuery(Query):
+        def __init__(self, value: int) -> None:
+            self.value = value
+
+    class TestQueryHandler(QueryHandler[TestQuery, int]):
+        def __call__(self, request: TestQuery) -> int:
+            return request.value + 9000
+
+    return TestQuery, TestQueryHandler()
+
+
+@pytest.fixture
+def test_command():
+    class TestCommand(Command):
+        def __init__(self, value: str) -> None:
+            self.value = value
+
+    class TestCommandHandler(CommandHandler[TestCommand]):
+        def __init__(self) -> None:
+            self.value = None
+
+        def __call__(self, request: TestCommand) -> None:
+            self.value = request.value
+
+    return TestCommand, TestCommandHandler()
+
+
+def test_query_handler(test_command, test_query):
+    di.add_medyator()
+    query, query_handler = test_query
+    di[query] = query_handler
+    # service_provider.register_handler(query, query_handler)
+    # command, command_handler = test_command
+    # service_provider.register_handler(command, command_handler)
+    medyator = di[Medyator]
+    result = medyator.sendQuery(query(1))
+    assert result == 9001
+
+    result = medyator.sendQuery(query(1))
+    assert result == 9001
+
+    # medyator.sendCommand(command('World'))
+    # assert command_handler.value == 'World'
