66dd9868240a240e3b1018b745e8fcb1f4e35f7d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-08 03:04:40 +0000

    Add package tianhutools/0.0.1/tianhutools-0.0.1.tar.gz

---
+++ packages/tianhutools/tianhutools-0.0.1.tar.gz/tianhutools-0.0.1/setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup
+with open("./README.md",mode='r',encoding='utf-8') as f:
+    des = f.read()
+setup(
+    name="tianhutools",      # 包名，用于安装和调用该包
+    version="0.0.1",               # 版本号
+    author="Sen",
+    description="a math lib developed by Sen",
+    long_description=des,
+    long_description_content_type='text/markdown',
+    author_email="tianhuzong@qq.com",
+    url="https://github.com/tianhuzong/thztnumpy",
+    license="MIT",
+    packages=["thznumpy"],     
+    install_requires=[
+        #依赖
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent"
+    ],
+)
---
+++ packages/tianhutools/tianhutools-0.0.1.tar.gz/tianhutools-0.0.1/thznumpy/__init__.py
@@ -0,0 +1,3 @@
+version = 0.0.1
+author = "Sen"
+email = "tianhuzong@qq.com"
