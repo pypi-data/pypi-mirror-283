be15ea0ee923cdc5119da0a175eaa393fda322fb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-08 15:04:53 +0000

    Add package sbsha69/0.2/sbsha69-0.2.tar.gz

---
+++ packages/sbsha69/sbsha69-0.2.tar.gz/sbsha69-0.2/setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='sbsha69',
+    version='0.02',
+    packages=find_packages(),
+    description='Custom hashing algorithm (SHA-69) implementation',
+    author='PythonScratcher',
+    author_email='me@psr.tf',
+    license='MIT',
+    keywords='hashing sbsha-69',
+    url='https://github.com/PythonScratcher/SHA-69',
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+)
