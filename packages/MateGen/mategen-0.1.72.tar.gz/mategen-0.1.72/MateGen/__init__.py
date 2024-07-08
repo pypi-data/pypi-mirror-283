e04b83e686b4df0cd8650c747457bc23d79fafd4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-08 03:05:15 +0000

    Add package MateGen/0.1.72/mategen-0.1.72.tar.gz

---
+++ packages/MateGen/mategen-0.1.72.tar.gz/mategen-0.1.72/MateGen/__init__.py
@@ -0,0 +1,7 @@
+# __init__.py
+
+__version__ = "0.1.72"
+
+from .mateGenClass import MateGenClass, export_variables, internet_config_assistant, check_network_environment
+
+__all__ = ['MateGenClass', 'export_variables', 'internet_config_assistant', 'check_network_environment']
