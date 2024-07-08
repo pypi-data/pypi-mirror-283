b33e6116fac9691f228ddd5f8ba6704212ec69ef
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-08 14:05:24 +0000

    Add package hello-world-th/0.0.2/hello_world_th-0.0.2.tar.gz

---
+++ packages/hello-world-th/hello_world_th-0.0.2.tar.gz/hello_world_th-0.0.2/setup.py
@@ -0,0 +1,16 @@
+from setuptools import find_packages, setup
+
+PACKAGE_NAME = "hello_world_th"
+
+setup(
+    name=PACKAGE_NAME,
+    version="0.0.2",
+    description="This is my tools package",
+    packages=find_packages(),
+    entry_points={
+        "package_tools": [
+            "hello_world_tool = hello_world.tools.utils:list_package_tools"
+        ],
+    },
+    include_package_data=True,  # This line tells setuptools to include files from MANIFEST.in
+)
