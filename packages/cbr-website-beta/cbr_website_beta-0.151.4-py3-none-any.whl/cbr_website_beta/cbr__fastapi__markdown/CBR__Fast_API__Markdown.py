f883383ce570a7d6aad6aed4075dd46c1234ad1e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-08 04:04:40 +0000

    Add package cbr-website-beta/0.151.4/cbr_website_beta-0.151.4-py3-none-any.whl

---
+++ packages/cbr-website-beta/cbr_website_beta-0.151.4-py3-none-any.whl/cbr_website_beta/cbr__fastapi__markdown/CBR__Fast_API__Markdown.py
@@ -0,0 +1,11 @@
+from osbot_fast_api.api.Fast_API import Fast_API
+
+
+class CBR__Fast_API__Markdown(Fast_API):
+
+    pass
+    # def setup(self):
+    #     #self.setup_server()
+    #     super().setup()
+    #     return self
+
