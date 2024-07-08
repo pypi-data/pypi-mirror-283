1a76cdf4ca06144190db9da1d16ef1f8179ed819
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-08 15:04:58 +0000

    Add package ciphers-module/2.0.1/ciphers_module-2.0.1.tar.gz

---
+++ packages/ciphers-module/ciphers_module-2.0.1.tar.gz/ciphers_module-2.0.1/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup, find_packages
+
+VERSION = '2.0.1'
+
+with open("README.md", "r") as f:
+    long_description = f.read()
+
+# Setting up
+setup(
+    name="ciphers_module",
+    version=VERSION,
+    author="Avyukt27",
+    author_email="<avyukt.aggarwal007@gmail.com>",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    packages=find_packages(),
+    install_requires=[],
+    keywords=['python', 'cipher', 'encode', 'encrypt', 'decrypt', 'decode'],
+)
