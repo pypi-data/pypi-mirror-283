c94dc7b373771f728a767204d11f86ecc557106d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-08 14:04:18 +0000

    Add package cbr-website-beta/0.151.9/cbr_website_beta-0.151.9.tar.gz

---
+++ packages/cbr-website-beta/cbr_website_beta-0.151.9.tar.gz/cbr_website_beta-0.151.9/cbr_website_beta/apps/docs/docs_routes.py
@@ -0,0 +1,51 @@
+from flask import render_template
+
+from cbr_website_beta.apps.docs                                 import blueprint
+from cbr_website_beta.cbr__flask.decorators.allow_annonymous    import allow_anonymous
+from cbr_website_beta.content.CBR__Content__Static              import cbr_content_static
+
+EXPECTED_ROUTES__DOCS = [ '/docs', '/markdown_edit']
+
+@blueprint.route('/markdown_edit')
+@allow_anonymous
+def markdown_edit(path='/'):
+    content_view  = 'docs/markdown_edit.html'
+    template_name = '/pages/page_with_view.html'
+    title         = 'Markdown Edit'
+    content       = '# Markdown Edit\n\nThis page is used to edit markdown files'
+    return render_template(template_name_or_list = template_name ,
+                           title                 = title         ,
+                           content_view          = content_view  ,
+                           path                  = path          ,
+                           content               = content       ,
+                           markdown_examples     = markdown_examples())
+
+@blueprint.route('')
+#@blueprint.route('/')                  # this is not working with the route ''
+@blueprint.route('/<path:path>')
+@allow_anonymous
+def minerva_root(path='/'):
+    content_view  = 'docs/index.html'
+    template_name = '/pages/page_with_view.html'
+    title         = 'Documentation'
+    if path == '' or path == '/':
+        path = 'index'
+    content       = cbr_content_static.file_contents__for__web_page(path)
+    base_folder   = cbr_content_static.base_folder                 (path)
+    parent_folder = cbr_content_static.parent_folder               (path)
+    folders       = cbr_content_static.folders                     (path)
+    files         = cbr_content_static.files                       (path)
+    return render_template(template_name_or_list = template_name,
+                           title                 =  title        ,
+                           content_view          = content_view  ,
+                           path                  = path          ,
+                           content               = content       ,
+                           files                 = files         ,
+                           folders               = folders       ,
+                           base_folder           = base_folder   ,
+                           parent_folder         = parent_folder )
+
+def markdown_examples():
+    return {'text'   : {'headers'  : '## Headers\n\n# Header 1\n## Header 2\n### Header 3\n#### Header 4\n##### Header 5\n###### Header 6\n\n'},
+            'mermaid': {'graph' : '```mermaid\ngraph TD;\n    A-->B;\n    A-->C;\n    B-->D;\n    C-->D;\n```\n'}
+            }
---
+++ packages/cbr-website-beta/cbr_website_beta-0.151.9.tar.gz/cbr_website_beta-0.151.9/cbr_website_beta/cbr__fastapi__markdown/markdown/Markdown__Ex__Mermaid.py
@@ -0,0 +1,38 @@
+import re
+import xml.etree.ElementTree as etree
+
+from markdown                   import Extension
+from markdown.blockprocessors   import BlockProcessor
+
+MERMAID_BLOCK_RE = re.compile(r'^\s*```mermaid\s*\n(.*?)\n\s*```\s*$', re.DOTALL | re.MULTILINE)
+
+class Markdown__Ex__Mermaid(Extension):
+    def extendMarkdown(self, md):
+        md.parser.blockprocessors.register(Block_Processor__Mermaid(md.parser), 'mermaid', 175)
+
+    def __repr__(self):
+        return f'Markdown__Ex__Mermaid'
+
+class Block_Processor__Mermaid(BlockProcessor):
+
+    def test(self, parent, block):
+        return bool(MERMAID_BLOCK_RE.search(block))
+
+    def run(self, parent, blocks):
+        block = blocks.pop(0)
+        m = MERMAID_BLOCK_RE.search(block)
+        if m:
+            code = m.group(1)
+            pre = etree.SubElement(parent, 'pre')
+            pre.set('class', 'mermaid')
+            pre.text = code
+
+def makeExtension(**kwargs):
+    return Markdown__Ex__Mermaid(**kwargs)
+
+# script = etree.SubElement(parent, 'script')
+# script.set('type', 'module')
+# script.text = """
+#     import mermaid from 'https://cdn.jsdelivr.net/npm/mermaid@10.9.1/dist/mermaid.esm.min.mjs';
+#     mermaid.initialize({ startOnLoad: true });
+# """
