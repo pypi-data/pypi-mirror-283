2ad4e7914358088c0c830ba505ea8f891d86726a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-08 05:03:44 +0000

    Add package coerced/0.0.1/coerced-0.0.1.tar.gz

---
+++ packages/coerced/coerced-0.0.1.tar.gz/coerced-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name='coerced',
+    version='0.0.1',
+    description='',
+    url='https://github.com/kszucs',
+    author='kszucs',
+    author_email='kszucs@kszucs.me',
+    packages=[],
+)
