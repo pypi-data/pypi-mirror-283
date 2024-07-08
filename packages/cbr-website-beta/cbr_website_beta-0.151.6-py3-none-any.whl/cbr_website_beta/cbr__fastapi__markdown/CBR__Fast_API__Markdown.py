3653aed2e9cb529a12ac256c574939d5af3817aa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-08 05:03:28 +0000

    Add package cbr-website-beta/0.151.6/cbr_website_beta-0.151.6-py3-none-any.whl

---
+++ packages/cbr-website-beta/cbr_website_beta-0.151.6-py3-none-any.whl/cbr_website_beta/cbr__fastapi__markdown/CBR__Fast_API__Markdown.py
@@ -0,0 +1,8 @@
+from cbr_website_beta.cbr__fastapi__markdown.routes.Routes__Markdown import Routes__Markdown
+from osbot_fast_api.api.Fast_API import Fast_API
+
+
+class CBR__Fast_API__Markdown(Fast_API):
+
+    def setup_routes(self):
+        self.add_routes(Routes__Markdown)
