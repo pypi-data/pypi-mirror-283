551105754df577d65eeb65b53f06f771eae176bc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-08 14:04:24 +0000

    Add package anotify/0.1.8/anotify-0.1.8.tar.gz

---
+++ packages/anotify/anotify-0.1.8.tar.gz/anotify-0.1.8/setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='anotify',
+    version='0.1.8',
+    packages=find_packages(),
+    install_requires=[
+        'requests>=2.15.1',
+    ],
+    author='7ommy',
+    author_email='tommymerlin0920@gmail.com',
+    description='Send notifications by multiple channels',
+    long_description=open('README.md', encoding='utf-8').read(),
+    long_description_content_type='text/markdown',
+    url='https://github.com/TommyMerlin/ANotify',
+    license='MIT',
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+    ],
+    python_requires='>=3.6',
+)
