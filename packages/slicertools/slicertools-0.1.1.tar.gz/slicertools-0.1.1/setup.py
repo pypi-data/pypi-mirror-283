0d9cf4595b53431a07e3c28da3749ccb48eeed41
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-08 14:04:14 +0000

    Add package slicertools/0.1.1/slicertools-0.1.1.tar.gz

---
+++ packages/slicertools/slicertools-0.1.1.tar.gz/slicertools-0.1.1/setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='slicertools',
+    version='0.1.1',
+    description='A library for slicing 3D models for 3D printing.',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    author='Alexander Ibragimov',
+    author_email='sasha.2000ibr@example.com',
+    url='https://github.com/pysashapy/slicertools',
+    packages=find_packages(),
+    install_requires=[
+        'trimesh[easy]'
+    ],
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
+        'Operating System :: Microsoft :: Windows',
+        'Operating System :: POSIX :: Linux',
+    ],
+    python_requires='>=3.9',
+    include_package_data=True,
+)
---
+++ packages/slicertools/slicertools-0.1.1.tar.gz/slicertools-0.1.1/slicertools/printers/__init__.py
@@ -0,0 +1,6 @@
+from slicertools.path import BASE_DIR
+
+
+class Printers:
+    CUSTOM = BASE_DIR / 'printers' / 'custom.def.json'
+    ENDER_S1 = BASE_DIR / 'printers' / 'creality_ender3s1pro.def.json'
