903b57089c5129fc59b38b279dda48744076f2ee
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-08 04:04:32 +0000

    Add package homgarapi/0.0.2/homgarapi-0.0.2.tar.gz

---
+++ packages/homgarapi/homgarapi-0.0.2.tar.gz/homgarapi-0.0.2/setup.py
@@ -0,0 +1,29 @@
+from setuptools import setup
+
+setup(
+    name='homgarapi',
+    version='0.0.2',
+    description='HomGar API client',
+    keywords=['homgar'],
+    url='https://github.com/Remboooo/homgarapi',
+    download_url='https://github.com/Remboooo/homgarapi/archive/refs/tags/v0.0.2.tar.gz',
+    author='Rembrand van Lakwijk',
+    author_email='rem@lakwijk.com',
+    license='MIT',
+    packages=['homgarapi'],
+    install_requires=[
+        'requests>=2.0.0',
+        'PyYAML>=5.0.0',
+        'platformdirs>=4.2.2',
+    ],
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+
+        'License :: OSI Approved :: MIT License',
+
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.10',
+        'Programming Language :: Python :: 3.11',
+    ],
+)
