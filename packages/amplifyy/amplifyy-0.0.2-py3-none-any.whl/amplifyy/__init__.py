ae7730185adde61fa18bfee6eb608ecee0ccc893
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-08 03:04:26 +0000

    Add package amplifyy/0.0.2/amplifyy-0.0.2-py3-none-any.whl

---
+++ packages/amplifyy/amplifyy-0.0.2-py3-none-any.whl/amplifyy/__init__.py
@@ -0,0 +1,9 @@
+from .main import (
+    get_custom_augmented_images,
+    get_augmentation_descriptions,
+    apply_all_augmentations,
+    create_zip,
+    create_tar_gz,
+    create_tar_gz_with_timestamp,
+    welcome
+)
