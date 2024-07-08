260419735d5c9758b05e6e54ce6653bf1f1cfbdb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-08 14:05:31 +0000

    Add package ChatRoom-jianjun/3.2.3/ChatRoom_jianjun-3.2.3-cp38-cp38-win_amd64.whl

---
+++ packages/ChatRoom-jianjun/ChatRoom_jianjun-3.2.3-cp38-cp38-win_amd64.whl/ChatRoom/__init__.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+import sys
+
+__version__ = "3.2.3"
+
+import cprint
+from ChatRoom.main import Room, User
+# from ChatRoom.net import Server, Client
+from ChatRoom.tools import hash_encryption
+from ChatRoom.launcher import Launcher
+# from ChatRoom.gui import RunRoom as _RunRoom
+
+log = encrypt = main = net = sys = gui = sysinfo = cprint = launcher = package = tools = None
+del log, encrypt, main, net, sys, gui, sysinfo, cprint, launcher, package, tools
