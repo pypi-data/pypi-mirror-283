268c18ae72da49e86fe9ee84b9380f7a8f4f94a8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-08 03:05:18 +0000

    Add package LightDB/2.0/LightDB-2.0.tar.gz

---
+++ packages/LightDB/LightDB-2.0.tar.gz/LightDB-2.0/setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup
+
+
+setup(
+      name="LightDB",
+      version="2.0",
+      description="Lightweight JSON Database for Python",
+      long_description=open("README.rst").read(),
+
+      url="https://github.com/Fl1yd/LightDB",
+      download_url="https://github.com/Fl1yd/LightDB/releases",
+
+      author="Fl1yd",
+      author_email="projects@fl1yd.su",
+      packages=["lightdb"],
+      license="MIT",
+)
