e9ba148614524c229a2558b17d399ec845716425
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-08 03:04:26 +0000

    Add package amplifyy/0.0.2/amplifyy-0.0.2.tar.gz

---
+++ packages/amplifyy/amplifyy-0.0.2.tar.gz/amplifyy-0.0.2/setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup, find_packages
+
+VERSION = '0.0.2'
+DESCRIPTION = 'Image Augmentation Tool'
+
+with open('requirements.txt') as f:
+    requirements = f.read().splitlines()
+
+with open('README.md', 'r', encoding='utf-8') as fh:
+    long_description = fh.read()
+
+setup(
+    name="amplifyy",
+    version=VERSION,
+    author="Adarsh Kesharwani",
+    author_email="<akesherwani900@gamil.com>",
+    description=DESCRIPTION,
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    packages=find_packages(),
+    install_requires=requirements,
+    keywords=['python','image augement','augmentation'],
+    entry_points={
+        "console_scripts":[
+            "amplifyy = amplifyy:welcome",
+        ],
+    },
+)
