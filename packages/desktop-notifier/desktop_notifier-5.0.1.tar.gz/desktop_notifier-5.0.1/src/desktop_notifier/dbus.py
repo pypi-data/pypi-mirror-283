bef571365df4b51341ded9f540cedf87b2b10f6f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-08 03:04:56 +0000

    Add package desktop-notifier/5.0.1/desktop_notifier-5.0.1.tar.gz

---
+++ packages/desktop-notifier/desktop_notifier-5.0.1.tar.gz/desktop_notifier-5.0.1/src/desktop_notifier/dbus.py
@@ -0,0 +1,324 @@
+# -*- coding: utf-8 -*-
+"""
+Notification backend for Linux
+
+Includes an implementation to send desktop notifications over Dbus. Responding to user
+interaction with a notification requires a running asyncio event loop.
+"""
+from __future__ import annotations
+
+# system imports
+import logging
+from typing import TypeVar
+
+# external imports
+from dbus_next.signature import Variant
+from dbus_next.aio.message_bus import MessageBus
+from dbus_next.aio.proxy_object import ProxyInterface
+
+# local imports
+from .base import Notification, DesktopNotifierBase, Urgency, Capability
+
+
+__all__ = ["DBusDesktopNotifier"]
+
+logger = logging.getLogger(__name__)
+
+T = TypeVar("T")
+
+NOTIFICATION_CLOSED_EXPIRED = 1
+NOTIFICATION_CLOSED_DISMISSED = 2
+NOTIFICATION_CLOSED_PROGRAMMATICALLY = 3
+NOTIFICATION_CLOSED_UNDEFINED = 4
+
+
+def identifier_from_dbus(nid: int) -> str:
+    if nid == 0:
+        return ""
+    return str(nid)
+
+
+def identifier_to_dbus(nid: str) -> int:
+    if nid == "":
+        return 0
+    return int(nid)
+
+
+class DBusDesktopNotifier(DesktopNotifierBase):
+    """DBus notification backend for Linux
+
+    This implements the org.freedesktop.Notifications standard. The DBUS connection is
+    created in a thread with a running asyncio loop to handle clicked notifications.
+
+    :param app_name: The name of the app. If it matches the application name in an
+        existing desktop entry, the icon from that entry will be used by default.
+    :param notification_limit: Maximum number of notifications to keep in the system's
+        notification center.
+    """
+
+    to_native_urgency = {
+        Urgency.Low: Variant("y", 0),
+        Urgency.Normal: Variant("y", 1),
+        Urgency.Critical: Variant("y", 2),
+    }
+
+    supported_hint_signatures = {"a{sv}", "a{ss}"}
+
+    def __init__(
+        self,
+        app_name: str = "Python",
+        notification_limit: int | None = None,
+    ) -> None:
+        super().__init__(app_name, notification_limit)
+        self.interface: ProxyInterface | None = None
+
+    async def request_authorisation(self) -> bool:
+        """
+        Request authorisation to send notifications.
+
+        :returns: Whether authorisation has been granted.
+        """
+        return True
+
+    async def has_authorisation(self) -> bool:
+        """
+        Whether we have authorisation to send notifications.
+        """
+        return True
+
+    async def _init_dbus(self) -> ProxyInterface:
+        self.bus = await MessageBus().connect()
+        introspection = await self.bus.introspect(
+            "org.freedesktop.Notifications", "/org/freedesktop/Notifications"
+        )
+        self.proxy_object = self.bus.get_proxy_object(
+            "org.freedesktop.Notifications",
+            "/org/freedesktop/Notifications",
+            introspection,
+        )
+        self.interface = self.proxy_object.get_interface(
+            "org.freedesktop.Notifications"
+        )
+
+        # Some older interfaces may not support notification actions.
+        if hasattr(self.interface, "on_notification_closed"):
+            self.interface.on_notification_closed(self._on_closed)
+
+        if hasattr(self.interface, "on_action_invoked"):
+            self.interface.on_action_invoked(self._on_action)
+
+        return self.interface
+
+    async def _send(
+        self,
+        notification: Notification,
+        notification_to_replace: Notification | None,
+    ) -> None:
+        """
+        Asynchronously sends a notification via the Dbus interface.
+
+        :param notification: Notification to send.
+        :param notification_to_replace: Notification to replace, if any.
+        """
+        if not self.interface:
+            self.interface = await self._init_dbus()
+
+        if notification_to_replace:
+            replaces_nid = identifier_to_dbus(notification_to_replace.identifier)
+        else:
+            replaces_nid = 0
+
+        actions: list[str] = []
+
+        if notification.on_clicked:
+            # The "default" action is typically invoked when clicking on the
+            # notification body itself, see
+            # https://specifications.freedesktop.org/notification-spec. There are some
+            # exceptions though, such as XFCE, where this will result in a separate
+            # button. If no label name is provided in XFCE, it will result in a default
+            # symbol being used. We therefore don't specify a label name.
+            actions = ["default", ""]
+
+        for n, button in enumerate(notification.buttons):
+            actions += [str(n), button.title]
+
+        hints_v: dict[str, Variant] = dict()
+        hints_v["urgency"] = self.to_native_urgency[notification.urgency]
+
+        if notification.sound:
+            if notification.sound.is_named():
+                hints_v["sound-name"] = Variant("s", "message-new-instant")
+            else:
+                hints_v["sound-file"] = Variant("s", notification.sound.as_uri())
+
+        if notification.attachment:
+            hints_v["image-path"] = Variant("s", notification.attachment.as_uri())
+
+        # The current notification spec defines hints as a Dbus dictionary type 'a{sv}',
+        # represented in Python as dict[str, Variant]. However, some older notification
+        # servers expect 'a{ss}' (Python dict[str, str]). We therefore check the
+        # expected argument type at runtime and cast arguments accordingly.
+        # See https://github.com/samschott/desktop-notifier/issues/143.
+        hints_signature = get_hints_signature(self.interface)
+
+        if hints_signature == "":
+            logger.warning("Notification server not supported")
+            return
+
+        hints: dict[str, str] | dict[str, Variant]
+
+        if hints_signature == "a{sv}":
+            hints = hints_v
+        elif hints_signature == "a{ss}":
+            hints = {k: str(v.value) for k, v in hints_v.items()}
+        else:
+            hints = {}
+
+        timeout = notification.timeout * 1000 if notification.timeout != -1 else -1
+        if notification.icon:
+            if notification.icon.is_named():
+                icon = notification.icon.name
+            else:
+                icon = notification.icon.as_uri()
+        else:
+            icon = ""
+
+        # dbus_next proxy APIs are generated at runtime. Silence the type checker but
+        # raise an AttributeError if required.
+        platform_nid = await self.interface.call_notify(  # type:ignore[attr-defined]
+            self.app_name,
+            replaces_nid,
+            icon,
+            notification.title,
+            notification.message,
+            actions,
+            hints,
+            timeout,
+        )
+
+        notification.identifier = identifier_from_dbus(platform_nid)
+
+    async def _clear(self, notification: Notification) -> None:
+        """
+        Asynchronously removes a notification from the notification center
+        """
+        if not self.interface:
+            return
+
+        # dbus_next proxy APIs are generated at runtime. Silence the type checker but
+        # raise an AttributeError if required.
+        await self.interface.call_close_notification(  # type:ignore[attr-defined]
+            identifier_to_dbus(notification.identifier)
+        )
+
+    async def _clear_all(self) -> None:
+        """
+        Asynchronously clears all notifications from notification center
+        """
+        if not self.interface:
+            return
+
+        for notification in self.current_notifications:
+            # dbus_next proxy APIs are generated at runtime. Silence the type checker
+            # but raise an AttributeError if required.
+            await self.interface.call_close_notification(  # type:ignore[attr-defined]
+                identifier_to_dbus(notification.identifier)
+            )
+
+    # Note that _on_action and _on_closed might be called for the same notification
+    # with some notification servers. This is not a problem because the _on_action
+    # call will come first, in which case we are no longer interested in calling the
+    # _on_closed callback.
+
+    def _on_action(self, nid: int, action_key: str) -> None:
+        """
+        Called when the user performs a notification action. This will invoke the
+        handler callback.
+
+        :param nid: The platform's notification ID as an integer.
+        :param action_key: A string identifying the action to take. We choose those keys
+            ourselves when scheduling the notification.
+        """
+        notification = self._notification_for_nid.get(identifier_from_dbus(nid))
+
+        if notification:
+            self._clear_notification_from_cache(notification)
+
+            button_number: int | None
+
+            try:
+                button_number = int(action_key)
+            except ValueError:
+                button_number = None
+
+            if action_key == "default" and notification.on_clicked:
+                notification.on_clicked()
+
+            elif button_number is not None:
+                button = notification.buttons[button_number]
+
+                if button.on_pressed:
+                    button.on_pressed()
+
+    def _on_closed(self, nid: int, reason: int) -> None:
+        """
+        Called when the user closes a notification. This will invoke the registered
+        callback.
+
+        :param nid: The platform's notification ID as an integer.
+        :param reason: An integer describing the reason why the notification was closed.
+        """
+        notification = self._notification_for_nid.get(identifier_from_dbus(nid))
+
+        if notification:
+            self._clear_notification_from_cache(notification)
+
+            if reason == NOTIFICATION_CLOSED_DISMISSED and notification.on_dismissed:
+                notification.on_dismissed()
+
+    async def get_capabilities(self) -> frozenset[Capability]:
+        if not self.interface:
+            self.interface = await self._init_dbus()
+
+        capabilities = {
+            Capability.APP_NAME,
+            Capability.ICON,
+            Capability.TITLE,
+            Capability.TIMEOUT,
+            Capability.URGENCY,
+        }
+
+        # Capabilities supported by some notification servers.
+        # See https://specifications.freedesktop.org/notification-spec/notification-spec-latest.html#protocol.
+        if hasattr(self.interface, "on_notification_closed"):
+            capabilities.add(Capability.ON_CLICKED)
+            capabilities.add(Capability.ON_DISMISSED)
+
+        cps = await self.interface.call_get_capabilities()  # type:ignore[attr-defined]
+        if "actions" in cps:
+            capabilities.add(Capability.BUTTONS)
+        if "body" in cps:
+            capabilities.add(Capability.MESSAGE)
+        if "sound" in cps:
+            capabilities.add(Capability.SOUND)
+            capabilities.add(Capability.SOUND_NAME)
+
+        hints_signature = get_hints_signature(self.interface)
+        if hints_signature not in self.supported_hint_signatures:
+            # Any hint-based capabilities are not supported because we got an unexpected
+            # DBus interface.
+            capabilities.discard(Capability.SOUND)
+            capabilities.discard(Capability.SOUND_NAME)
+            capabilities.discard(Capability.URGENCY)
+
+        return frozenset(capabilities)
+
+
+def get_hints_signature(interface: ProxyInterface) -> str:
+    """Returns the dbus type signature for the hints argument"""
+    methods = interface.introspection.methods
+    notify_method = next(m for m in methods if m.name == "Notify")
+    try:
+        return str(notify_method.in_args[6].signature)
+    except IndexError:
+        return ""
---
+++ packages/desktop-notifier/desktop_notifier-5.0.1.tar.gz/desktop_notifier-5.0.1/src/desktop_notifier/winrt.py
@@ -0,0 +1,336 @@
+# -*- coding: utf-8 -*-
+"""
+Notification backend for Windows
+
+Unlike other platforms, sending rich "toast" notifications cannot be done via FFI /
+ctypes because the C winapi only supports basic notifications with a title and message.
+This backend therefore requires interaction with the Windows Runtime and uses the winrt
+package with compiled components.
+"""
+from __future__ import annotations
+
+# system imports
+import sys
+import uuid
+import logging
+from xml.etree.ElementTree import Element, SubElement, tostring
+from typing import TypeVar
+
+# external imports
+import winreg
+from winrt.windows.foundation.interop import unbox
+from winrt.windows.ui.notifications import (
+    ToastNotificationManager,
+    ToastNotificationPriority,
+    NotificationSetting,
+    ToastNotification,
+    ToastActivatedEventArgs,
+    ToastDismissalReason,
+    ToastDismissedEventArgs,
+    ToastFailedEventArgs,
+)
+from winrt.windows.data.xml.dom import XmlDocument
+from winrt.windows.applicationmodel.core import CoreApplication
+from winrt.system import Object as WinRTObject
+
+# local imports
+from .base import Notification, DesktopNotifierBase, Urgency, Capability, DEFAULT_SOUND
+
+
+__all__ = ["WinRTDesktopNotifier"]
+
+logger = logging.getLogger(__name__)
+
+T = TypeVar("T")
+
+
+def register_hkey(app_id: str, app_name: str) -> None:
+    # mypy type guard
+    if not sys.platform == "win32":
+        return
+
+    winreg.ConnectRegistry(None, winreg.HKEY_CURRENT_USER)
+    key_path = f"SOFTWARE\\Classes\\AppUserModelId\\{app_id}"
+    with winreg.CreateKeyEx(winreg.HKEY_CURRENT_USER, key_path) as master_key:
+        winreg.SetValueEx(master_key, "DisplayName", 0, winreg.REG_SZ, app_name)
+
+
+class WinRTDesktopNotifier(DesktopNotifierBase):
+    """Notification backend for the Windows Runtime
+
+    :param app_name: The name of the app. This has no effect since the app name will be
+        automatically determined.
+    :param notification_limit: Maximum number of notifications to keep in the system's
+        notification center.
+    """
+
+    _to_native_urgency = {
+        Urgency.Low: ToastNotificationPriority.DEFAULT,
+        Urgency.Normal: ToastNotificationPriority.DEFAULT,
+        Urgency.Critical: ToastNotificationPriority.HIGH,
+    }
+
+    DEFAULT_ACTION = "default"
+    REPLY_ACTION = "action=reply&amp"
+    BUTTON_ACTION_PREFIX = "action=button&amp;id="
+    REPLY_TEXTBOX_NAME = "textBox"
+
+    def __init__(
+        self,
+        app_name: str = "Python",
+        notification_limit: int | None = None,
+    ) -> None:
+        super().__init__(app_name, notification_limit)
+
+        manager = ToastNotificationManager.get_default()
+
+        if not manager:
+            raise RuntimeError("Could not get ToastNotificationManagerForUser")
+
+        self.manager = manager
+
+        # Prefer using the real App ID if detected, fall back to user-provided name
+        # and icon otherwise.
+        if CoreApplication.id != "":
+            self.app_id = CoreApplication.id
+        else:
+            self.app_id = app_name
+            register_hkey(app_id=app_name, app_name=app_name)
+
+        notifier = self.manager.create_toast_notifier(self.app_id)
+
+        if not notifier:
+            raise RuntimeError(f"Could not get ToastNotifier for app_id: {self.app_id}")
+
+        self.notifier = notifier
+
+    async def request_authorisation(self) -> bool:
+        """
+        Request authorisation to send notifications.
+
+        :returns: Whether authorisation has been granted.
+        """
+        return await self.has_authorisation()
+
+    async def has_authorisation(self) -> bool:
+        """
+        Whether we have authorisation to send notifications.
+        """
+        try:
+            return bool(self.notifier.setting == NotificationSetting.ENABLED)
+        except OSError:
+            # See https://github.com/samschott/desktop-notifier/issues/95.
+            return True
+
+    async def _send(
+        self,
+        notification: Notification,
+        notification_to_replace: Notification | None,
+    ) -> None:
+        """
+        Asynchronously sends a notification.
+
+        :param notification: Notification to send.
+        :param notification_to_replace: Notification to replace, if any.
+        """
+        if notification_to_replace:
+            platform_nid = notification_to_replace.identifier
+        else:
+            platform_nid = str(uuid.uuid4())
+
+        # Create notification XML.
+        toast_xml = Element("toast", {"launch": WinRTDesktopNotifier.DEFAULT_ACTION})
+        visual_xml = SubElement(toast_xml, "visual")
+        actions_xml = SubElement(toast_xml, "actions")
+
+        if notification.thread:
+            SubElement(
+                toast_xml,
+                "header",
+                {
+                    "id": notification.thread,
+                    "title": notification.thread,
+                    "arguments": WinRTDesktopNotifier.DEFAULT_ACTION,
+                    "activationType": "background",
+                },
+            )
+
+        binding = SubElement(visual_xml, "binding", {"template": "ToastGeneric"})
+
+        title_xml = SubElement(binding, "text")
+        title_xml.text = notification.title
+
+        message_xml = SubElement(binding, "text")
+        message_xml.text = notification.message
+
+        if notification.icon and notification.icon.is_file():
+            SubElement(
+                binding,
+                "image",
+                {
+                    "placement": "appLogoOverride",
+                    "src": notification.icon.as_uri(),
+                },
+            )
+
+        if notification.attachment:
+            SubElement(
+                binding,
+                "image",
+                {"placement": "hero", "src": notification.attachment.as_uri()},
+            )
+
+        if notification.reply_field:
+            SubElement(
+                actions_xml,
+                "input",
+                {"id": WinRTDesktopNotifier.REPLY_TEXTBOX_NAME, "type": "text"},
+            )
+            reply_button_xml = SubElement(
+                actions_xml,
+                "action",
+                {
+                    "content": notification.reply_field.button_title,
+                    "activationType": "background",
+                    "arguments": "action=reply&amp",
+                },
+            )
+
+            # If there are no other buttons, show the
+            # reply buttons next to the text field.
+            if not notification.buttons:
+                reply_button_xml.set(
+                    "hint-inputId", WinRTDesktopNotifier.REPLY_TEXTBOX_NAME
+                )
+
+        for n, button in enumerate(notification.buttons):
+            SubElement(
+                actions_xml,
+                "action",
+                {
+                    "content": button.title,
+                    "activationType": "background",
+                    "arguments": WinRTDesktopNotifier.BUTTON_ACTION_PREFIX + str(n),
+                },
+            )
+
+        if notification.sound:
+            if notification.sound == DEFAULT_SOUND:
+                sound_attr = {"src": "ms-winsoundevent:Notification.Default"}
+            elif notification.sound.name:
+                sound_attr = {"src": notification.sound.name}
+            else:
+                sound_attr = {"src": notification.sound.as_uri()}
+        else:
+            sound_attr = {"silent": "true"}
+
+        SubElement(toast_xml, "audio", sound_attr)
+
+        xml_document = XmlDocument()
+        xml_document.load_xml(tostring(toast_xml, encoding="unicode"))
+
+        native = ToastNotification(xml_document)
+        native.tag = platform_nid
+        native.priority = self._to_native_urgency[notification.urgency]
+
+        def on_activated(
+            sender: ToastNotification | None, boxed_activated_args: WinRTObject | None
+        ) -> None:
+            if not sender or not boxed_activated_args:
+                return
+
+            try:
+                activated_args = ToastActivatedEventArgs._from(boxed_activated_args)
+            except Exception:
+                return
+
+            action_id = activated_args.arguments
+
+            if action_id == WinRTDesktopNotifier.DEFAULT_ACTION:
+                if notification.on_clicked:
+                    notification.on_clicked()
+            elif action_id == WinRTDesktopNotifier.REPLY_ACTION:
+                if (
+                    notification.reply_field
+                    and notification.reply_field.on_replied
+                    and activated_args.user_input
+                ):
+                    boxed_text = activated_args.user_input[
+                        WinRTDesktopNotifier.REPLY_TEXTBOX_NAME
+                    ]
+                    text = unbox(boxed_text)
+                    notification.reply_field.on_replied(text)
+            elif action_id.startswith(WinRTDesktopNotifier.BUTTON_ACTION_PREFIX):
+                action_number_str = action_id.replace(
+                    WinRTDesktopNotifier.BUTTON_ACTION_PREFIX, ""
+                )
+                action_number = int(action_number_str)
+                callback = notification.buttons[action_number].on_pressed
+                if callback:
+                    callback()
+
+        def on_dismissed(
+            sender: ToastNotification | None,
+            dismissed_args: ToastDismissedEventArgs | None,
+        ) -> None:
+            self._clear_notification_from_cache(notification)
+
+            if (
+                dismissed_args
+                and dismissed_args.reason == ToastDismissalReason.USER_CANCELED
+            ):
+                if notification.on_dismissed:
+                    notification.on_dismissed()
+
+        def on_failed(
+            sender: ToastNotification | None, failed_args: ToastFailedEventArgs | None
+        ) -> None:
+            if failed_args:
+                logger.warning(
+                    f"Notification failed (error code {failed_args.error_code.value})"
+                )
+            else:
+                logger.warning("Notification failed (unknown error code)")
+
+        native.add_activated(on_activated)
+        native.add_dismissed(on_dismissed)
+        native.add_failed(on_failed)
+
+        self.notifier.show(native)
+
+        notification.identifier = platform_nid
+
+    async def _clear(self, notification: Notification) -> None:
+        """
+        Asynchronously removes a notification from the notification center.
+        """
+        if self.manager.history:
+            self.manager.history.remove(notification.identifier)
+
+    async def _clear_all(self) -> None:
+        """
+        Asynchronously clears all notifications from notification center.
+        """
+        if self.manager.history:
+            self.manager.history.clear(self.app_id)
+
+    async def get_capabilities(self) -> frozenset[Capability]:
+        capabilities = {
+            Capability.TITLE,
+            Capability.MESSAGE,
+            Capability.ICON,
+            Capability.BUTTONS,
+            Capability.REPLY_FIELD,
+            Capability.ON_CLICKED,
+            Capability.ON_DISMISSED,
+            Capability.THREAD,
+            Capability.ATTACHMENT,
+            Capability.SOUND,
+            Capability.SOUND_NAME,
+        }
+        # Custom audio is support only starting with the Windows 10 Anniversary update.
+        # See https://learn.microsoft.com/en-us/windows/apps/design/shell/tiles-and-notifications/custom-audio-on-toasts#add-the-custom-audio.
+        if sys.getwindowsversion().build >= 1607:  # type:ignore[attr-defined]
+            capabilities.add(Capability.SOUND_FILE)
+
+        return frozenset(capabilities)
