6ce579097e0e8119dedcabab748271bcb3f96d01
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-08 15:04:00 +0000

    Add package recommendation-api/0.0.2/recommendation_api-0.0.2-py3-none-any.whl

---
+++ packages/recommendation-api/recommendation_api-0.0.2-py3-none-any.whl/tests/test_recommendation_api_tool.py
@@ -0,0 +1,32 @@
+import pytest
+import unittest
+
+from promptflow.connections import CustomConnection
+
+# from hello_world.tools.hello_world_tool import get_greeting_messagename
+from recommendation.tools.recommendation_api_tool import get_recommendation_api
+
+
+@pytest.fixture
+def my_custom_connection() -> CustomConnection:
+    my_custom_connection = CustomConnection(
+        {
+            "api-key": "my-api-key",
+            "api-secret": "my-api-secret",
+            "api-url": "my-api-url",
+        }
+    )
+    return my_custom_connection
+
+
+class TestTool:
+    def test_get_greeting_messagename(self):
+        result = get_recommendation_api(
+            recommendation_api_url="http://k8s-eksrecommendergro-5887642276-2114908543.ap-northeast-2.elb.amazonaws.com/prodrec/recommendation"
+        )
+        assert result
+
+
+# Run the unit tests
+if __name__ == "__main__":
+    unittest.main()
