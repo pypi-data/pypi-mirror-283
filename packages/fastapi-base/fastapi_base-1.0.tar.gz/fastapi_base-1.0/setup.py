f9423f357c69fad90044e4e70200ea4e6766dc5a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-08 04:04:40 +0000

    Add package fastapi-base/1.0/fastapi_base-1.0.tar.gz

---
+++ packages/fastapi-base/fastapi_base-1.0.tar.gz/fastapi_base-1.0/setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='fastapi-base',
+    version='1.0',
+    entry_points={
+        'console_scripts': [
+            'fastapi-base = fastapi_base.main:main',
+        ],
+    },
+    install_requires=[
+        # 'pkg_resources',
+    ],
+    author='Dementev Maksim',
+    author_email='i@dmaksim.ru',
+    description='FastAPI project structure generator',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    url='https://github.com/idmaksim/fastapi-base',
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.6',
+)
