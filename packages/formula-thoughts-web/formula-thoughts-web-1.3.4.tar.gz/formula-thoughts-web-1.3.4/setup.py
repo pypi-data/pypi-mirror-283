b591611a5d144a01ca7b48e71ca6c0eb27f0e5a6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-08 03:05:02 +0000

    Add package formula-thoughts-web/1.3.4/formula-thoughts-web-1.3.4.tar.gz

---
+++ packages/formula-thoughts-web/formula-thoughts-web-1.3.4.tar.gz/formula-thoughts-web-1.3.4/setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup, find_packages
+
+DESCRIPTION = 'A web library for AWS lambda, gateway and SQS event handling'
+LONG_DESCRIPTION = 'Allows for handling http api gateway requests, and SQS events'
+REQUIREMENTS = [i.strip() for i in open("requirements.txt").readlines()]
+
+# Setting up
+setup(
+    name="formula-thoughts-web",
+    version="1.3.4",
+    author="GanTheMan",
+    author_email="aidanwilliamgannon@gmail.com",
+    description=DESCRIPTION,
+    long_description_content_type="text/markdown",
+    long_description=LONG_DESCRIPTION,
+    packages=find_packages(),
+    install_requires=REQUIREMENTS,
+    keywords=['python', 'lambda', 'api gateway', 'sqs'],
+    classifiers=[
+        "Development Status :: 1 - Planning",
+        "Intended Audience :: Developers",
+        "Programming Language :: Python :: 3",
+        "Operating System :: Unix"
+    ]
+)
