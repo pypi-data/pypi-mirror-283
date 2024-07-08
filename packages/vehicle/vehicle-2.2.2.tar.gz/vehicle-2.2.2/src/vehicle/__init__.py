953065c10417fddd416abbc4dbff9a22250bf3b1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-08 03:05:02 +0000

    Add package vehicle/2.2.2/vehicle-2.2.2.tar.gz

---
+++ packages/vehicle/vehicle-2.2.2.tar.gz/vehicle-2.2.2/src/vehicle/__init__.py
@@ -0,0 +1,17 @@
+"""Asynchronous Python client providing RDW vehicle information."""
+
+from .const import VehicleInterior, VehicleOdometerJudgement, VehicleType
+from .exceptions import RDWConnectionError, RDWError, RDWUnknownLicensePlateError
+from .models import Vehicle
+from .rdw import RDW
+
+__all__ = [
+    "RDW",
+    "RDWConnectionError",
+    "RDWError",
+    "RDWUnknownLicensePlateError",
+    "Vehicle",
+    "VehicleInterior",
+    "VehicleOdometerJudgement",
+    "VehicleType",
+]
