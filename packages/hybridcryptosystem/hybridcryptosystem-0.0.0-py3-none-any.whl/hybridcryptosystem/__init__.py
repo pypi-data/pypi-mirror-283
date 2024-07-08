631cda58aaf52d44eb8e7ff44189b6ca54a34824
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-08 15:04:08 +0000

    Add package hybridcryptosystem/0.0.0/hybridcryptosystem-0.0.0-py3-none-any.whl

---
+++ packages/hybridcryptosystem/hybridcryptosystem-0.0.0-py3-none-any.whl/hybridcryptosystem/__init__.py
@@ -0,0 +1,13 @@
+'''
+hybridcryptosystem/__init__.py
+Created on Monday, 8th July 2024 1:32:08 pm
+Author: Sira Pornsiriprasert <code@psira.me>
+
+Last modified on Monday, 8th July 2024 1:33:00 pm
+By Sira Pornsiriprasert <code@psira.me>
+
+The 3-Clause BSD License
+hybridcrypto Copyright 2024 Sira Pornsiriprasert
+'''
+
+import hybridcryptosystem
---
+++ packages/hybridcryptosystem/hybridcryptosystem-0.0.0-py3-none-any.whl/hybridcryptosystem/hybridcryptosystem.py
@@ -0,0 +1,15 @@
+'''
+hybridcryptosystem/hybridcryptosystem.py
+Created on Monday, 8th July 2024 1:25:37 pm
+Author: Sira Pornsiriprasert <code@psira.me>
+
+Last modified on Monday, 8th July 2024 1:36:39 pm
+By Sira Pornsiriprasert <code@psira.me>
+
+The 3-Clause BSD License
+hybridcrypto Copyright 2024 Sira Pornsiriprasert
+'''
+
+import cryptography
+
+__version__ = '0'
