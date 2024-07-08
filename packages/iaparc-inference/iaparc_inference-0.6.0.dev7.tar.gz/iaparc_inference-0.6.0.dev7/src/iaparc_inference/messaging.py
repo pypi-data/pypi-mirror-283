19e1ee39b2fc1faf46963049559093ccbfe269f8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-08 05:03:26 +0000

    Add package iaparc-inference/0.6.0.dev7/iaparc_inference-0.6.0.dev7.tar.gz

---
+++ packages/iaparc-inference/iaparc_inference-0.6.0.dev7.tar.gz/iaparc_inference-0.6.0.dev7/src/iaparc_inference/messaging.py
@@ -0,0 +1,39 @@
+from typing import List
+
+import iaparc_inference.messenger.messenger as messenger
+
+
+# type DataMsg struct {
+#     Uid         string
+#     Source      string
+#     Data[]byte
+#     Params      map[string]string
+#     ContentType string
+# }
+
+def Init(uid: str, out_links: List[str], in_links: List[str], nats_url: str, queue: str, batch: int):
+    _outputs = messenger.Map_string_string(out_links)
+    _inputs = messenger.Map_string_string(in_links)
+    messenger.MessengerInit(_outputs, _inputs, nats_url, queue, batch)
+    
+def GetDatas(timeout: int):
+    msgs = messenger.MessengerGetMsgs(timeout)
+    input_names = []
+    uids = []
+    sources = []
+    datas = []
+    params = []
+    content_types = []
+    for msg in msgs:
+        input_names.append(msg.InputName)
+        uids.append(msg.Uid)
+        sources.append(msg.Source)
+        datas.append(msg.Data)
+        params.append(msg.Params)
+        content_types.append(msg.ContentType)
+    
+    return input_names, uids, sources, datas, params, content_types
+
+def SendData(dest: str, uid: str, source: str, data: bytes, params: dict, content_type: str, err_str: str):
+    messenger.MessengerSendReply(dest, uid, source, data, params, content_type, err_str)
+    
