0641ccf4136fb8a9dacf81b6d8cc3be607eff106
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-08 05:03:51 +0000

    Add package koerce/0.0.1/koerce-0.0.1.tar.gz

---
+++ packages/koerce/koerce-0.0.1.tar.gz/koerce-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name='koerce',
+    version='0.0.1',
+    description='',
+    url='https://github.com/kszucs',
+    author='kszucs',
+    author_email='kszucs@kszucs.me',
+    packages=[],
+)
