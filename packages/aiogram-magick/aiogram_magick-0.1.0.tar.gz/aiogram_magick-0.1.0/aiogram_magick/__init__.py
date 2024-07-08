96d6cb11964e7b9443153c4ded87c33fc239c476
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-08 05:03:26 +0000

    Add package aiogram-magick/0.1.0/aiogram_magick-0.1.0.tar.gz

---
+++ packages/aiogram-magick/aiogram_magick-0.1.0.tar.gz/aiogram_magick-0.1.0/aiogram_magick/__init__.py
@@ -0,0 +1,9 @@
+"""## Aiogram Magick - Magick for Aiogram 3.x-based Telegram bots.
+
+### Available submodules
+* [sqlite.py](sqlite.py) - SQLite-based storage with caching & automatic commits
+"""
+
+from aiogram_magick import sqlite
+
+__all__ = ('sqlite',)
