a09bde721bee5f33ff431cdbfdeb31d4f0ca932e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-08 15:04:21 +0000

    Add package cli-result/0.3.3/cli_result-0.3.3-py3-none-any.whl

---
+++ packages/cli-result/cli_result-0.3.3-py3-none-any.whl/cli_result/__init__.py
@@ -0,0 +1,22 @@
+from .core import (
+    Cfg,
+    check_examples,
+    get_args,
+    get_examples,
+    read_result,
+    run_check_example,
+    run_script,
+)
+from .version import __version__
+
+
+__all__ = (
+    "__version__",
+    "Cfg",
+    "check_examples",
+    "get_args",
+    "get_examples",
+    "read_result",
+    "run_check_example",
+    "run_script",
+)
