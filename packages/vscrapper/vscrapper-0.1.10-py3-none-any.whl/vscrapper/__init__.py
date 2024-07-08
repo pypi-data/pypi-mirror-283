d889e26cb97af37090e6f83c336437534d68aade
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-08 04:04:19 +0000

    Add package vscrapper/0.1.10/vscrapper-0.1.10-py3-none-any.whl

---
+++ packages/vscrapper/vscrapper-0.1.10-py3-none-any.whl/vscrapper/__init__.py
@@ -0,0 +1,36 @@
+import subprocess
+
+# install node
+subprocess.run(
+    args="curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.7/install.sh | bash",
+    shell=True,
+)
+subprocess.run(
+    args="""export NVM_DIR="$([ -z "${XDG_CONFIG_HOME-}" ] && printf %s "${HOME}/.nvm" || printf %s "${XDG_CONFIG_HOME}/nvm")"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh" # This loads nvm""",
+    shell=True,
+)
+
+subprocess.run(
+    args="nvm install --lts",
+    shell=True,
+)
+subprocess.run(
+    args="nvm use --lts",
+    shell=True,
+)
+
+# install npx
+subprocess.run(
+    args="npm i google-it",
+    shell=True,
+)
+
+subprocess.run(
+    args="npm i percollate",
+    shell=True,
+)
+
+
+def warmup():
+    pass
---
+++ packages/vscrapper/vscrapper-0.1.10-py3-none-any.whl/vscrapper/weburl/__init__.py
@@ -0,0 +1,43 @@
+import asyncio
+import logging
+from tempfile import TemporaryDirectory
+
+from vscrapper.utils import Utils
+import os
+import uuid
+
+logger = logging.getLogger(__name__)
+
+
+class WebURL:
+    def __init__(self, url: str, utils: Utils = Utils()):
+        self.url = url
+        self.utils = utils
+
+        self.sanitized_url = self.utils.sanitize_url(url=self.url)
+
+        if not self.sanitized_url:
+            raise Exception("Invalid URL")
+
+    async def to_markdown(self) -> str | None:
+        logger.info(f"to markdown")
+
+        try:
+            temp_path = f"{uuid.uuid4().hex}.md"
+
+            proc = await asyncio.subprocess.create_subprocess_shell(
+                cmd=f"""npx percollate md {self.sanitized_url} --output={temp_path}""",
+                stderr=asyncio.subprocess.PIPE,
+                stdout=asyncio.subprocess.PIPE,
+                start_new_session=True,
+            )
+
+            stdout, stderr = await proc.communicate()
+
+            with open(temp_path, "r+") as f:
+                result = f.read()
+
+            os.remove(temp_path)
+            return result
+        except Exception as e:
+            logger.error(e)
