a761faaca6f20b2c1efbe0d9b5f677652be5b399
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-08 04:04:23 +0000

    Add package spark-on-k8s/0.8.0/spark_on_k8s-0.8.0-py3-none-any.whl

---
+++ packages/spark-on-k8s/spark_on_k8s-0.8.0-py3-none-any.whl/spark_on_k8s/api/apps.py
@@ -0,0 +1,48 @@
+from __future__ import annotations
+
+from fastapi import APIRouter
+from kubernetes_asyncio.client import CoreV1Api
+from pydantic import BaseModel
+
+from spark_on_k8s.api import KubernetesClientSingleton
+from spark_on_k8s.api.configuration import APIConfiguration
+from spark_on_k8s.utils.app_manager import SparkAppStatus, get_app_status
+
+router = APIRouter(
+    prefix="/apps",
+    tags=["spark-apps"],
+)
+
+
+class SparkApp(BaseModel):
+    """App status."""
+
+    app_id: str
+    status: SparkAppStatus
+    driver_logs: bool = False
+    spark_ui_proxy: bool = False
+    spark_history_proxy: bool = False
+
+
+@router.get("/list_apps")
+async def list_apps_default_namespace() -> list[SparkApp]:
+    """List spark apps in the default namespace."""
+    return await list_apps(namespace=APIConfiguration.SPARK_ON_K8S_API_DEFAULT_NAMESPACE)
+
+
+@router.get("/list_apps/{namespace}")
+async def list_apps(namespace: str) -> list[SparkApp]:
+    """List spark apps in a namespace."""
+    core_client = CoreV1Api(await KubernetesClientSingleton.client())
+    driver_pods = await core_client.list_namespaced_pod(
+        namespace=namespace, label_selector="spark-role=driver"
+    )
+    return [
+        SparkApp(
+            app_id=pod.metadata.labels.get("spark-app-id", pod.metadata.name),
+            status=get_app_status(pod),
+            driver_logs=True,
+            spark_ui_proxy=pod.metadata.labels.get("spark-ui-proxy", False),
+        )
+        for pod in driver_pods.items
+    ]
