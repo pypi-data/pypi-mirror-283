c2114572897315a1a7aaaff392ab05a561b08053
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-08 14:04:24 +0000

    Add package base64Compressor/0.2.0/base64Compressor-0.2.0.tar.gz

---
+++ packages/base64Compressor/base64Compressor-0.2.0.tar.gz/base64Compressor-0.2.0/setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="base64Compressor",
+    version="0.2.0",
+    packages=find_packages(),
+    install_requires=[
+        "Pillow"
+    ],
+    entry_points={
+        "console_scripts": [
+            "base64Compressor=base64Compressor.__main__:main",
+        ],
+    },
+    author="Aditya Rachman",
+    author_email="adityarachman24.ar@gmail.com",
+    description="A library to compress base64 encoded images.",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/adityarach/base64Compressor",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.9',
+)
