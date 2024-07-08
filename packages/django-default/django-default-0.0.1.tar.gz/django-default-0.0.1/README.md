4933d519bbd70b01837d914509c33f54a3aabc83
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-08 04:04:20 +0000

    Add package django-default/0.0.1/django-default-0.0.1.tar.gz

---
+++ packages/django-default/django-default-0.0.1.tar.gz/django-default-0.0.1/README.md
@@ -0,0 +1,40 @@
+# Shinzo Test
+
+Django Default is a Django project structure generator that simplifies the process of setting up new Django projects. It automates the cloning of a predefined Django project structure from a GitHub repository, enabling developers to start their projects with a solid foundation.
+
+## Features
+
+- Automates the cloning of a Django project structure.
+- Customizable to fit specific project needs.
+- Streamlines the initial setup process for Django projects.
+
+## Installation
+
+Install Shinzo Test using pip:
+
+```bash
+pip install django-default
+```
+
+## Usage
+
+To generate a new Django project structure, execute the following command:
+
+```bash
+shinzo
+```
+
+This command clones the predefined Django project structure into your current working directory.
+
+## Contributing
+
+Contributions are welcome! If you have suggestions for improving Shinzo Test, please open an issue or submit a pull request.
+
+## License
+
+Shinzo Test is released under the MIT License. See the LICENSE file in the project repository for more information.
+
+## Acknowledgments
+
+- Thanks to all contributors who have helped to improve Shinzo Test.
+- Special thanks to Jahongir Hakimjonov for creating and maintaining this project.
---
+++ packages/django-default/django-default-0.0.1.tar.gz/django-default-0.0.1/setup.py
@@ -0,0 +1,54 @@
+import os
+
+from setuptools import setup, find_packages
+
+
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
+
+setup(
+    name="django-default",
+    version="0.0.1",
+    py_modules=["git_cloner"],
+    packages=find_packages(),
+    entry_points={
+        "console_scripts": [
+            "shinzo=git_cloner.git_cloner:clone_repo",
+        ],
+    },
+    install_requires=[
+        "alive_progress",
+        "colorama",
+        "black",
+        "djangorestframework",
+        "djangorestframework-simplejwt",
+        "drf-spectacular",
+        "drf-spectacular-sidecar",
+        "Django",
+        "django-jazzmin",
+        "gunicorn",
+        "pillow",
+        "psycopg2-binary",
+        "python-dotenv",
+        "django-modeltranslation",
+        "django-ckeditor-5",
+        "django-cors-headers",
+        "django-rosetta",
+        "colorama",
+        "PyJWT",
+        "django-unfold",
+    ],
+    author="Jahongir Hakimjonov",
+    author_email="jahongirhakimjonov@gmail.com",
+    description="A Django project structure generator",
+    keywords="django project structure generator",
+    url="https://github.com/JahongirHakimjonov",
+    classifiers=[
+        "Framework :: Django",
+        "Programming Language :: Python :: 3",
+        "Operating System :: OS Independent",
+    ],
+    long_description=read('README.md'),
+    long_description_content_type='text/markdown',
+)
