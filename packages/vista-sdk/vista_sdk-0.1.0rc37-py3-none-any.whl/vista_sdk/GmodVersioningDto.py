6fe4025fb1b7a1198a75cca161402eb926a4a0f5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-08 15:04:06 +0000

    Add package vista-sdk/0.1.0rc37/vista_sdk-0.1.0rc37-py3-none-any.whl

---
+++ packages/vista-sdk/vista_sdk-0.1.0rc37-py3-none-any.whl/vista_sdk/GmodVersioningDto.py
@@ -0,0 +1,11 @@
+from pydantic import BaseModel, Field
+from typing import Optional, Dict
+
+class GmodVersioningNodeChangesDto(BaseModel):
+    next_vis_version: Optional[str] = Field(None, alias="nextVisVersion")
+    next_code: Optional[str] = Field(None, alias="nextCode")
+    previous_vis_version: Optional[str] = Field(None, alias="previousVisVersion")
+    previous_code: Optional[str] = Field(None, alias="previousCode")
+
+class GmodVersioningDto(BaseModel):
+    items: Dict[str, Dict[str, GmodVersioningNodeChangesDto]] = Field(..., alias="items")
