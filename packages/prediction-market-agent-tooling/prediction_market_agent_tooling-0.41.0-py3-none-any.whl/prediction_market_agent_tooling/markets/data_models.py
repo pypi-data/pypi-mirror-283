b189a8b03b67d30e8d8d86564d7b84ee64262d0d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-08 15:04:43 +0000

    Add package prediction-market-agent-tooling/0.41.0/prediction_market_agent_tooling-0.41.0-py3-none-any.whl

---
+++ packages/prediction-market-agent-tooling/prediction_market_agent_tooling-0.41.0-py3-none-any.whl/prediction_market_agent_tooling/markets/data_models.py
@@ -0,0 +1,66 @@
+from datetime import datetime
+from enum import Enum
+from typing import TypeAlias
+
+from pydantic import BaseModel, computed_field
+
+from prediction_market_agent_tooling.gtypes import OutcomeStr
+
+
+class Currency(str, Enum):
+    xDai = "xDai"
+    Mana = "Mana"
+    USDC = "USDC"
+
+
+class Resolution(str, Enum):
+    YES = "YES"
+    NO = "NO"
+    CANCEL = "CANCEL"
+    MKT = "MKT"
+
+
+class TokenAmount(BaseModel):
+    amount: float
+    currency: Currency
+
+
+BetAmount: TypeAlias = TokenAmount
+ProfitAmount: TypeAlias = TokenAmount
+
+
+class Bet(BaseModel):
+    amount: BetAmount
+    outcome: bool
+    created_time: datetime
+    market_question: str
+
+
+class ResolvedBet(Bet):
+    market_outcome: bool
+    resolved_time: datetime
+    profit: ProfitAmount
+
+    @computed_field  # type: ignore[misc]
+    @property
+    def is_correct(self) -> bool:
+        return self.outcome == self.market_outcome
+
+
+class Position(BaseModel):
+    market_id: str
+    amounts: dict[OutcomeStr, TokenAmount]
+
+    @property
+    def total_amount(self) -> TokenAmount:
+        return TokenAmount(
+            amount=sum(amount.amount for amount in self.amounts.values()),
+            currency=self.amounts[next(iter(self.amounts.keys()))].currency,
+        )
+
+    def __str__(self) -> str:
+        amounts_str = ", ".join(
+            f"{amount.amount} '{outcome}' tokens"
+            for outcome, amount in self.amounts.items()
+        )
+        return f"Position for market id {self.market_id}: {amounts_str}"
