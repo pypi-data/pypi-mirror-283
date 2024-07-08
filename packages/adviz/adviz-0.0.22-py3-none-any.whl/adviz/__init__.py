c7f94bce7cdd92111ba02782b29baab0719bc37a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-08 05:03:22 +0000

    Add package adviz/0.0.22/adviz-0.0.22-py3-none-any.whl

---
+++ packages/adviz/adviz-0.0.22-py3-none-any.whl/adviz/__init__.py
@@ -0,0 +1,11 @@
+__version__ = "0.0.22"
+
+from .value_counts_plus import value_counts_plus
+from .value_counts import value_counts
+from .status_codes import status_codes
+from .url_structure import url_structure
+from .racing_chart import racing_chart
+from .flag import flag, country_code_flag
+from .style_table import style_table
+from .serp_heatmap import serp_heatmap
+from .ecdf import ecdf
