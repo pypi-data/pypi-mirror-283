94ce1e70833fb2aaa446f6dc39115c5433345849
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-08 03:05:19 +0000

    Add package pygcloud/0.0.4/pygcloud-0.0.4-py3-none-any.whl

---
+++ packages/pygcloud/pygcloud-0.0.4-py3-none-any.whl/pygcloud/utils.py
@@ -0,0 +1,62 @@
+"""
+@author: jldupont
+"""
+from typing import List, Any, Tuple, Union
+
+
+def flatten(*liste: Union[List[Any], Tuple[Any]]):
+    """
+    Flatten a list of lists
+    """
+    assert isinstance(liste, tuple), \
+        f"Expected list, got: {type(liste)}"
+
+    result = []
+    for item in liste:
+        if isinstance(item, list):
+            result.extend(flatten(*item))
+        else:
+            result.append(item)
+    return result
+
+
+def split_head_tail(liste) -> Tuple[List[Any], List[Any]]:
+    """
+    Cases:
+    1) head ... tail ---> normal case
+    2) ... tail      ---> degenerate
+    3) tail          ---> normal case
+    4) ...           ---> degenerate
+    """
+    head = []
+    tail = []
+
+    current = head
+
+    for item in liste:
+
+        if item is ...:
+            current = tail
+            continue
+
+        current.append(item)
+
+    return (head, tail)
+
+
+def prepare_params(params: Union[List[Any], List[Tuple[str, str]]]) \
+        -> List[Any]:
+    """
+    Prepare a list of parameters for a command line invocation
+    """
+    liste = flatten(params)
+    new_liste = []
+
+    for item in liste:
+        if isinstance(item, tuple):
+            new_item = f"{item[0]}={item[1]}"
+            new_liste.append(new_item)
+            continue
+        new_liste.append(item)
+
+    return new_liste
