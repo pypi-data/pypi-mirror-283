417eb72ac3b5866a0ffe86fc49c366b0e22aafe2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-08 14:04:48 +0000

    Add package PySimons/0.0.1/PySimons-0.0.1.tar.gz

---
+++ packages/PySimons/PySimons-0.0.1.tar.gz/PySimons-0.0.1/setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='PySimons',
+    version='0.0.1',
+    packages=find_packages(),
+    install_requires=[
+        'numpy', 'pandas','pyfinance'
+    ],
+    entry_points={
+        'console_scripts': [
+            # Add command line scripts if needed
+        ],
+    },
+    author='Zachary Fagnou',
+    author_email='zfagnou3@gmail.com',
+    description='A package which expands on the tools provided by pyfinance',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    url='https://github.com/zfagnou/pySimons',
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.6',
+)
