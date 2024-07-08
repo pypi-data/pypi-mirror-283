36d295b0ec75da368f1fc47837fd6185b84f58da
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-08 14:04:59 +0000

    Add package utils-zihang/0.0.6/utils_zihang-0.0.6.tar.gz

---
+++ packages/utils-zihang/utils_zihang-0.0.6.tar.gz/utils_zihang-0.0.6/setup.py
@@ -0,0 +1,35 @@
+# -*- coding:utf-8 -*-
+"""
+Created on 2023/7/26 17:45
+@File: setup.py
+---------
+@summary:
+---------
+@Author: luzihang
+@Contact: https://github.com/luzihang123
+"""
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="utils_zihang",
+    version="0.0.6",
+    author="zihang",
+    author_email="clark1203@foxmail.com",
+    description="工具方法",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    install_requires=[
+        "loguru==0.6.0",
+        "pymongo==4.8.0"
+    ],
+    python_requires='>=3',
+)
