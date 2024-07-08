c5d26c53c4090aebc24287bcd803a372524c1eae
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-08 14:05:10 +0000

    Add package user-envs/0.0.1/user_envs-0.0.1.tar.gz

---
+++ packages/user-envs/user_envs-0.0.1.tar.gz/user_envs-0.0.1/setup.py
@@ -0,0 +1,13 @@
+import setuptools
+from pathlib import Path
+
+
+setuptools.setup(
+    name='user_envs',
+    version='0.0.1',
+    description='An openAI gym environment from user',
+    long_description=Path('README.md').read_text(),
+    long_description_content_type='text/markdown',
+    packages=setuptools.find_packages(include='user_envs*'),
+    install_requires=["gymnasium>=0.26.0", "pygame>=2.1.0"]
+)
