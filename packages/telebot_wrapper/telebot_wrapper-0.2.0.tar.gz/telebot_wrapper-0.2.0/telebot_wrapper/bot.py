66751f819ac5db2b0d01f057b87c0b0058abaa41
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-08 15:04:00 +0000

    Add package telebot_wrapper/0.2.0/telebot_wrapper-0.2.0.tar.gz

---
+++ packages/telebot_wrapper/telebot_wrapper-0.2.0.tar.gz/telebot_wrapper-0.2.0/telebot_wrapper/bot.py
@@ -0,0 +1,31 @@
+from functools import wraps
+from typing import Callable
+from telebot import TeleBot
+from telebot.types import CallbackQuery
+
+from .utils import decompress_string
+
+
+__all__ = ["Bot"]
+
+
+class Bot(TeleBot):
+    def __init__(self, token: str, *args, **kwargs):
+        super().__init__(token, *args, **kwargs)
+
+    def callback_query_handler(
+        self, *filters: Callable[[CallbackQuery], bool], **kwargs
+    ):
+        def decorator(func: Callable[[CallbackQuery, list[str]], None]):
+            @wraps(func)
+            def wrapper(call: CallbackQuery):
+                call.data = decompress_string(call.data)
+                data_list = call.data.split(";")
+                return func(call, data_list)
+
+            handler = super(Bot, self).callback_query_handler(*filters, **kwargs)(
+                wrapper
+            )
+            return handler
+
+        return decorator
