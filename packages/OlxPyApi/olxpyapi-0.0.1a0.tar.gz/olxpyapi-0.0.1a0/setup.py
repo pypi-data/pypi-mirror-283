42667b67a14c8dc31089af86e79dc4628f180539
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-08 03:04:31 +0000

    Add package OlxPyApi/0.0.1a0/olxpyapi-0.0.1a0.tar.gz

---
+++ packages/OlxPyApi/olxpyapi-0.0.1a0.tar.gz/olxpyapi-0.0.1a0/setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup, find_packages
+
+__version__ = "0.0.1alpha"
+
+setup(
+    name='OlxPyApi',
+    version=__version__,
+
+    url='https://github.com/Vladislavus1/OlxPyApi',
+    author='Vladislavus1',
+    author_email='vlydgames@gmail.com',
+
+    packages=find_packages(),
+    install_requires=[
+        'beautifulsoup4==4.12.3',
+        'requests==2.31.0'
+    ],
+)
