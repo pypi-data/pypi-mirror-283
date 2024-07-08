827ff1f08a4c3f3777cfa9dc337568cf5ef82547
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-08 03:05:16 +0000

    Add package vscrapper/0.1.6/vscrapper-0.1.6-py3-none-any.whl

---
+++ packages/vscrapper/vscrapper-0.1.6-py3-none-any.whl/vscrapper/__init__.py
@@ -0,0 +1,39 @@
+from nest_asyncio import apply
+
+apply()
+import subprocess
+
+# install node
+subprocess.run(
+    args="curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.7/install.sh | bash",
+    shell=True,
+)
+subprocess.run(
+    args="""export NVM_DIR="$([ -z "${XDG_CONFIG_HOME-}" ] && printf %s "${HOME}/.nvm" || printf %s "${XDG_CONFIG_HOME}/nvm")"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh" # This loads nvm""",
+    shell=True,
+)
+
+subprocess.run(
+    args="nvm install --lts",
+    shell=True,
+)
+subprocess.run(
+    args="nvm use --lts",
+    shell=True,
+)
+
+# install npx
+subprocess.run(
+    args="npx google-it -y",
+    shell=True,
+)
+
+subprocess.run(
+    args="npx percollate -y",
+    shell=True,
+)
+
+
+def warmup():
+    pass
---
+++ packages/vscrapper/vscrapper-0.1.6-py3-none-any.whl/vscrapper/search/__init__.py
@@ -0,0 +1,77 @@
+import asyncio
+import json
+from vscrapper.types import DetailedSearchResult, SearchResult
+from vscrapper.utils import Utils
+from vscrapper.weburl import WebURL, logger
+from nest_asyncio import apply
+from tempfile import TemporaryDirectory
+
+apply()
+
+from urllib.parse import quote_plus
+
+
+class SearchQuery:
+
+    def __init__(self, query: str, utils: Utils = Utils()):
+        self.query = query
+        self.url = None
+        self.sanitized_url = None
+        self.utils = utils
+
+    async def google_search(self) -> list[SearchResult]:
+        logger.info(f"google search")
+
+        encoded_query = quote_plus(self.query)
+
+        # sanitize url
+        self.url = f"https://www.google.com/search?q={encoded_query}"
+
+        self.sanitized_url = self.utils.sanitize_url(url=self.url)
+
+        if not self.sanitized_url:
+            raise Exception("Invalid URL")
+
+        with TemporaryDirectory(prefix="temp") as tempdir:
+            temp_path = f"{tempdir}/results.json"
+
+            proc = await asyncio.subprocess.create_subprocess_shell(
+                cmd=f"""npx google-it --query="{self.query}" -o {temp_path}""",
+                stderr=asyncio.subprocess.PIPE,
+                stdout=asyncio.subprocess.PIPE,
+            )
+
+            stdout, stderr = await proc.communicate()
+
+            with open(temp_path, "r") as f:
+                results = f.read()
+                results_dict = json.loads(results)
+
+            parsed_results = []
+            for res in results_dict:
+                parsed_results.append(SearchResult.model_validate(res))
+            return parsed_results
+
+    async def detailed_google_search(self) -> list[DetailedSearchResult]:
+        logger.info(f"detailed google search")
+        search_results = await self.google_search()
+
+        tasks = []
+
+        for result in search_results:
+            tasks.append(asyncio.create_task(WebURL(result.link).to_markdown()))
+
+        results = await asyncio.gather(*tasks)
+
+        output: list[DetailedSearchResult] = []
+        for search_result, detail in zip(search_results, results):
+            output.append(
+                DetailedSearchResult(
+                    title=search_result.title,
+                    link=search_result.link,
+                    snippet=search_result.snippet,
+                    detail=detail,
+                )
+            )
+
+        return output
