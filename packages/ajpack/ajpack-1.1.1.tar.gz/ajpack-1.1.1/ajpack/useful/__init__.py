5030a54d92201f44b65806dd718f0f3567b73db3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-08 04:04:33 +0000

    Add package ajpack/1.1.1/ajpack-1.1.1.tar.gz

---
+++ packages/ajpack/ajpack-1.1.1.tar.gz/ajpack-1.1.1/ajpack/useful/__init__.py
@@ -0,0 +1,7 @@
+from .convert import remove_duplicates
+from .wait import waiter
+from .shortcut import create_shortcut
+from .notifications import desktop_msg
+from .format import table
+
+__all__ = ["remove_duplicates", "waiter", "create_shortcut", "desktop_msg", "table"]
