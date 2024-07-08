7636602a7b5b17278c58c9068ae2c7c26fea9936
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-08 03:05:02 +0000

    Add package formula-thoughts-web/1.3.3/formula-thoughts-web-1.3.3.tar.gz

---
+++ packages/formula-thoughts-web/formula-thoughts-web-1.3.3.tar.gz/formula-thoughts-web-1.3.3/formula_thoughts_web/exceptions.py
@@ -0,0 +1,14 @@
+class EventSchemaInvalidException(Exception):
+    pass
+
+
+class EventNotFoundException(Exception):
+    pass
+
+
+class MappingException(Exception):
+    pass
+
+
+class StrategyNotFoundException(Exception):
+    pass
