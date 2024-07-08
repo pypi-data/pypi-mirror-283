3a0967f26c626feab59b71d981bfa08faaf8b887
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-08 04:04:32 +0000

    Add package memelator/0.2/memelator-0.2.tar.gz

---
+++ packages/memelator/memelator-0.2.tar.gz/memelator-0.2/setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='memelator',
+    version='0.2',
+    packages=find_packages(),
+    description='A simple module to calculate two numbers using eval',
+    author='Your Name',
+    author_email='your_email@example.com',
+    url='https://github.com/your_username/memelator',
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.6',
+)
