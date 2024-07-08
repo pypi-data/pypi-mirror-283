f01648fe852f986e4c38f76a43d5a1df5d442477
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-08 03:05:07 +0000

    Add package framecat/0.1.9/framecat-0.1.9-py3-none-any.whl

---
+++ packages/framecat/framecat-0.1.9-py3-none-any.whl/framecat/cli.py
@@ -0,0 +1,13 @@
+import sys
+from framecat.command import (
+    parse_args, 
+    execute_command,
+)
+
+def main():
+    args = parse_args(sys.argv[1:])
+    execute_command(args)
+
+if __name__ == "__main__":
+    main()
+
