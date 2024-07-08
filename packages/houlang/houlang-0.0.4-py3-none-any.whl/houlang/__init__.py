295df710f2e6dce8079390d2b299c7f2925e6a03
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-08 14:04:19 +0000

    Add package houlang/0.0.4/houlang-0.0.4-py3-none-any.whl

---
+++ packages/houlang/houlang-0.0.4-py3-none-any.whl/houlang/__init__.py
@@ -0,0 +1,9 @@
+# from houlang.segmentation.yolo_segmenter import YOLOSegmenter as YOLOSegmenter
+# from houlang.lib.data.images import DocumentImage as DocumentImage
+# from houlang.binarization.unet_binarizer import UnetBinarizer as UNetBinarizer
+# from houlang.recognition.kraken_recognizer import KrakenRecognizer as KrakenRecognizer
+# from houlang.reading_order.heuristic_reading_order_classifier import HeuristicReadingOrderClassifier as HeuristicReadingOrderClassifier
+
+# __all__ = [YOLOSegmenter, DocumentImage, UNetBinarizer, KrakenRecognizer, HeuristicReadingOrderClassifier]
+
+__version__ = '0.0.4'
