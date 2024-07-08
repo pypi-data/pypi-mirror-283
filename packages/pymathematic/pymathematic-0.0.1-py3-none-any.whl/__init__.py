0371a1bb743d69d6eb4841632e6509b1a2a323ae
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-08 14:05:16 +0000

    Add package pymathematic/0.0.1/pymathematic-0.0.1-py3-none-any.whl

---
+++ packages/pymathematic/pymathematic-0.0.1-py3-none-any.whl/__init__.py
@@ -0,0 +1,9 @@
+"""pymathematic, A better Python package for math.
+
+pymathematic is a Python package designed to make mathematical operations easier, faster, and more powerful. Its goal is to provide a clean and intuitive package for a wide range of mathematical functions.
+"""
+
+from .errors import *
+from .constants import *
+from .functions import *
+from ._types import *
