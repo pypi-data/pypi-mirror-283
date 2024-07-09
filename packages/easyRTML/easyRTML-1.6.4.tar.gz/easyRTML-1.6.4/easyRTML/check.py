af8eadaeb12bbe1785d16417757b3cc534512c92
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-09 07:04:31 +0000

    Add package easyRTML/1.6.4/easyRTML-1.6.4.tar.gz

---
+++ packages/easyRTML/easyRTML-1.6.4.tar.gz/easyRTML-1.6.4/easyRTML/check.py
@@ -0,0 +1,25 @@
+import os
+import gspread
+from google.oauth2.service_account import Credentials
+
+def is_access_granted(email, key):
+    # Define the scope and authorize the credentials
+    scopes = ["https://www.googleapis.com/auth/spreadsheets"]
+    
+    # Get the path to the credentials.json file
+    creds_path = os.path.join(os.path.dirname(__file__), 'credentials.json')
+    creds = Credentials.from_service_account_file(creds_path, scopes=scopes)
+    client = gspread.authorize(creds)
+
+    # Open the Google Sheet using the sheet ID
+    sheet_id = "1T2GQT5l_OhSR2g2ZkuOUHLcpK7MeXdRov50ETnmOCn4"
+    sheet = client.open_by_key(sheet_id)
+
+    # Get all records from the Google Sheet
+    records = sheet.sheet1.get_all_records()
+
+    # Verify email and key
+    for record in records:
+        if record['email'] == email and record['keys'] == key and record['Status'] == "Active":
+            return True
+    return False
