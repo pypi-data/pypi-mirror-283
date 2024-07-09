9743fd86a54dfd46445e646bee24baa47c84e6e1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-09 04:05:25 +0000

    Add package nomenclature-iamc/0.18.0/nomenclature_iamc-0.18.0.tar.gz

---
+++ packages/nomenclature-iamc/nomenclature_iamc-0.18.0.tar.gz/nomenclature_iamc-0.18.0/nomenclature/processor/__init__.py
@@ -0,0 +1,6 @@
+from nomenclature.processor.processor import Processor  # noqa
+from nomenclature.processor.region import (  # noqa
+    RegionAggregationMapping,
+    RegionProcessor,
+)
+from nomenclature.processor.required_data import RequiredDataValidator  # noqa
