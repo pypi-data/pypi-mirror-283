006a40ceccdab3cf1a0146907ad4cc93e61a0651
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-09 18:04:52 +0000

    Add package aleph-message/0.4.8/aleph_message-0.4.8-py3-none-any.whl

---
+++ packages/aleph-message/aleph_message-0.4.8-py3-none-any.whl/aleph_message/models/base.py
@@ -0,0 +1,33 @@
+from enum import Enum
+
+
+class Chain(str, Enum):
+    """Supported chains"""
+
+    AVAX = "AVAX"
+    BSC = "BSC"
+    CSDK = "CSDK"
+    DOT = "DOT"
+    ETH = "ETH"
+    NEO = "NEO"
+    NULS = "NULS"
+    NULS2 = "NULS2"
+    SOL = "SOL"
+    TEZOS = "TEZOS"
+
+
+class HashType(str, Enum):
+    """Supported hash functions"""
+
+    sha256 = "sha256"
+
+
+class MessageType(str, Enum):
+    """Message types supported by Aleph"""
+
+    post = "POST"
+    aggregate = "AGGREGATE"
+    store = "STORE"
+    program = "PROGRAM"
+    instance = "INSTANCE"
+    forget = "FORGET"
