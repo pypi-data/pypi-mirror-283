2d69832d2be069a3d5151309a87a14631e037267
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-09 04:06:04 +0000

    Add package django-rahavard-robots/0.0.2/django_rahavard_robots-0.0.2-py3-none-any.whl

---
+++ packages/django-rahavard-robots/django_rahavard_robots-0.0.2-py3-none-any.whl/django_rahavard_robots/apps.py
@@ -0,0 +1,7 @@
+from django.apps import AppConfig
+
+
+class DjangoRahavardRobotsConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'django_rahavard_robots'
+    label = 'django_rahavard_robots'
