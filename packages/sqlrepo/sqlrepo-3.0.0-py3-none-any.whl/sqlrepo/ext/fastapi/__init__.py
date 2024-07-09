a90c0a82732a06c8f8b91e3969937902f239cb3b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-09 03:05:30 +0000

    Add package sqlrepo/3.0.0/sqlrepo-3.0.0-py3-none-any.whl

---
+++ packages/sqlrepo/sqlrepo-3.0.0-py3-none-any.whl/sqlrepo/ext/fastapi/__init__.py
@@ -0,0 +1,6 @@
+from .containers import BaseAsyncContainer as BaseAsyncContainer
+from .containers import BaseSyncContainer as BaseSyncContainer
+from .containers import add_container_overrides as add_container_overrides
+from .services import BaseAsyncService as BaseAsyncService
+from .services import BaseService as BaseService
+from .services import BaseSyncService as BaseSyncService
