e01fae89e4baccf09dcbe5905d2c7c0a8ed6c862
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-09 03:04:30 +0000

    Add package inoopa_utils/2.56.0/inoopa_utils-2.56.0.tar.gz

---
+++ packages/inoopa_utils/inoopa_utils-2.56.0.tar.gz/inoopa_utils-2.56.0/inoopa_utils/custom_types/phone_operators.py
@@ -0,0 +1,16 @@
+from dataclasses import dataclass, field
+from datetime import datetime
+
+
+@dataclass(slots=True)
+class PhoneOperator:
+    """
+    Store the phone operator data.
+    This is used to cache the data we scrape from crdc.be which gives us the operator of a phone number.
+
+    This is stored into the `phone_operators_cache` collection.
+    """
+
+    _id: str
+    operator: str
+    last_update: datetime = field(default_factory=lambda: datetime.now())
