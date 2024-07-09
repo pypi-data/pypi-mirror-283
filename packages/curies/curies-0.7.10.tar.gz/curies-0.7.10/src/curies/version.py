9aa289009094acc7273bc6ca0bf8eb0b4dc7f8d5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-09 17:04:58 +0000

    Add package curies/0.7.10/curies-0.7.10.tar.gz

---
+++ packages/curies/curies-0.7.10.tar.gz/curies-0.7.10/src/curies/version.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+
+"""Version information for :mod:`curies`."""
+
+__all__ = [
+    "VERSION",
+    "get_version",
+]
+
+VERSION = "0.7.10"
+
+
+def get_version() -> str:
+    """Get the :mod:`curies` version string."""
+    return VERSION
