e2fc96c8db5ae5c5cad2766c2fceb578f67dcb92
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-09 05:04:21 +0000

    Add package explainable/0.2.1/explainable-0.2.1-py3-none-any.whl

---
+++ packages/explainable/explainable-0.2.1-py3-none-any.whl/explainable/base_entities.py
@@ -0,0 +1,13 @@
+class BaseStructure:
+    """ Structures define raw data """
+    pass
+
+
+class BaseWidget:
+    """ Widgets are visual representations for structures """
+    pass
+
+
+class BaseSource:
+    """ Base class for sources of structures """
+    pass
