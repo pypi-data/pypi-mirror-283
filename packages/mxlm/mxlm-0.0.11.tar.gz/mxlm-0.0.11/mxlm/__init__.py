67b769b688da04b7a613987924b027ed1d4f9eac
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-09 15:05:27 +0000

    Add package mxlm/0.0.11/mxlm-0.0.11.tar.gz

---
+++ packages/mxlm/mxlm-0.0.11.tar.gz/mxlm-0.0.11/mxlm/__init__.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+
+from .__info__ import __version__, __description__
+from .chat_api import ChatAPI
+from .chatmd_utils import messages_to_chatmd, chatmd_to_messages
+from .mxlm_utils import df_to_html, markdown_escape
+from .random_utils import shuffle_loop_with_seed
+
+# Not imported by default
+# from .richtext import *
---
+++ packages/mxlm/mxlm-0.0.11.tar.gz/mxlm-0.0.11/mxlm/mxlm_utils.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Mar 29 16:15:58 2024
+
+@author: yl
+"""
+
+
+def df_to_html(df, *args, max_width=400, HTML_WIDTH_PER_CHAR=8, **argkws):
+    """
+    Pretty print DataFrame to html
+    """
+    import html
+    import pprint
+
+    if hasattr(df, "to_frame"):
+        df = df.to_frame()
+
+    argkws.setdefault(
+        "formatters",
+        {
+            col: lambda x: f'<div style="max-width:{max_width}px;"><span style="white-space: pre-wrap; font-family: Monospace;">%s</span></div>'
+            % html.escape(
+                pprint.pformat(x, indent=0, width=max_width // HTML_WIDTH_PER_CHAR)
+            )
+            for col in df.columns
+        },
+    )
+    argkws.setdefault("escape", False)
+    return df.to_html(*args, **argkws)
+
+
+def markdown_escape(text):
+    return text.replace("\n", "↳").replace("|", "\|").replace("$", "\$").strip()
