3d6034a328f6da340198824ab6edf6ef5af42ecd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-09 06:04:37 +0000

    Add package sharkey-crawler/0.0.1/sharkey_crawler-0.0.1-py3-none-any.whl

---
+++ packages/sharkey-crawler/sharkey_crawler-0.0.1-py3-none-any.whl/sharkey_crawler/__init__.py
@@ -0,0 +1,11 @@
+#   -------------------------------------------------------------
+#   Copyright (c) Hexafuchs. All rights reserved.
+#   Licensed under the MIT License. See LICENSE in project root for information.
+#   -------------------------------------------------------------
+"""Python library to crawl user data from sharkey instances"""
+from __future__ import annotations
+
+__version__ = "0.0.1"
+
+from .main import *
+from .types import *
