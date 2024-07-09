a0beb908eca5d3d51ded07ae14270d1839ff84f3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-09 05:04:15 +0000

    Add package mvf1/1.1.4/mvf1-1.1.4.tar.gz

---
+++ packages/mvf1/mvf1-1.1.4.tar.gz/mvf1-1.1.4/setup.py
@@ -0,0 +1,34 @@
+scripts = ['bin/mvf1-cli']
+
+setup_args = {
+    "name": "mvf1",
+    "version": "1.1.4",
+    "url": "https://github.com/RobSpectre/mvf1",
+    "description": "A Python package to control video players for "
+                   "MultiViewer for F1, the best way to watch "
+                   "Formula 1.",
+    "long_description": open('README.rst').read(),
+    "author": "Rob Spectre",
+    "author_email": "rob@brooklynhacker.com",
+    "license": "MIT",
+    "packages": ["mvf1", "tests"],
+    "scripts": ["bin/mvf1-cli"],
+    "include_package_data": True,
+    "install_requires": ["click", "sgqlc"],
+    "classifiers": [
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Developers',
+        'Natural Language :: English',
+        'Environment :: Console',
+    ]
+}
+
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+setup(**setup_args)
