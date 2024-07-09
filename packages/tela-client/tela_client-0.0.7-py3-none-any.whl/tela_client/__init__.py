61f5b9f64f49b19593adcbdfea5088cb556ae84e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-09 14:04:28 +0000

    Add package tela-client/0.0.7/tela_client-0.0.7-py3-none-any.whl

---
+++ packages/tela-client/tela_client-0.0.7-py3-none-any.whl/tela_client/__init__.py
@@ -0,0 +1,6 @@
+from .client import TelaClient, Canvas, file
+from .folder_watcher import watch_folder, run_canvas_on_folder_update
+
+
+__all__ = ['TelaClient', 'Canvas', 'file', 'watch_folder', 'run_canvas_on_folder_update']
+
