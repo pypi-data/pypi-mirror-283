9924f49ed3dd1c712a224e8b1bda5944e7dae74a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-09 17:05:42 +0000

    Add package DrissionPage/4.1.0.0b2/DrissionPage-4.1.0.0b2.tar.gz

---
+++ packages/DrissionPage/DrissionPage-4.1.0.0b2.tar.gz/DrissionPage-4.1.0.0b2/DrissionPage/_units/cookies_setter.pyi
@@ -0,0 +1,57 @@
+# -*- coding:utf-8 -*-
+"""
+@Author   : g1879
+@Contact  : g1879@qq.com
+@Copyright: (c) 2024 by g1879, Inc. All Rights Reserved.
+@License  : BSD 3-Clause.
+"""
+from http.cookiejar import Cookie, CookieJar
+from typing import Union
+
+from .._base.browser import Chromium
+from .._pages.chromium_base import ChromiumBase
+from .._pages.tabs import MixTab
+from .._pages.session_page import SessionPage
+from .._pages.mix_page import MixPage
+
+
+class CookiesSetter(object):
+    _owner: ChromiumBase
+
+    def __init__(self, page: ChromiumBase): ...
+
+    def __call__(self, cookies: Union[CookieJar, Cookie, list, tuple, str, dict]) -> None: ...
+
+    def remove(self, name: str, url: str = None, domain: str = None, path: str = None) -> None: ...
+
+    def clear(self) -> None: ...
+
+
+class BrowserCookiesSetter(CookiesSetter):
+    _owner: Chromium = ...
+
+    def __init__(self, page: Chromium): ...
+
+
+class SessionCookiesSetter(object):
+    _owner: SessionPage = ...
+
+    def __init__(self, page: SessionPage): ...
+
+    def __call__(self, cookies: Union[CookieJar, Cookie, list, tuple, str, dict]) -> None: ...
+
+    def remove(self, name: str) -> None: ...
+
+    def clear(self) -> None: ...
+
+
+class MixPageCookiesSetter(CookiesSetter, SessionCookiesSetter):
+    _owner: Union[MixPage, MixTab] = ...
+
+    def __init__(self, page: SessionPage): ...
+
+    def __call__(self, cookies: Union[CookieJar, Cookie, list, tuple, str, dict]) -> None: ...
+
+    def remove(self, name: str, url: str = None, domain: str = None, path: str = None) -> None: ...
+
+    def clear(self) -> None: ...
