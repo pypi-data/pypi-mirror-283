ccd5565d0931d4a5f53cba2d3fa35365b93fec3b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-09 14:04:33 +0000

    Add package langflow-base/0.0.83/langflow_base-0.0.83.tar.gz

---
+++ packages/langflow-base/langflow_base-0.0.83.tar.gz/langflow_base-0.0.83/langflow/components/toolkits/VectorStoreInfo.py
@@ -0,0 +1,25 @@
+from langchain.agents.agent_toolkits.vectorstore.toolkit import VectorStoreInfo
+from langchain_core.vectorstores import VectorStore
+
+from langflow.custom import CustomComponent
+
+
+class VectorStoreInfoComponent(CustomComponent):
+    display_name = "VectorStoreInfo"
+    description = "Information about a VectorStore"
+    name = "VectorStoreInfo"
+
+    def build_config(self):
+        return {
+            "vectorstore": {"display_name": "VectorStore"},
+            "description": {"display_name": "Description", "multiline": True},
+            "name": {"display_name": "Name"},
+        }
+
+    def build(
+        self,
+        vectorstore: VectorStore,
+        description: str,
+        name: str,
+    ) -> VectorStoreInfo:
+        return VectorStoreInfo(vectorstore=vectorstore, description=description, name=name)
