c6b24763b39a28a8e719b8b9c834bc3c758df98c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-09 04:04:25 +0000

    Add package netbox-swupdate/0.0.1.2/netbox_swupdate-0.0.1.2.tar.gz

---
+++ packages/netbox-swupdate/netbox_swupdate-0.0.1.2.tar.gz/netbox_swupdate-0.0.1.2/netbox_swupdate/views/view_repository.py
@@ -0,0 +1,39 @@
+from netbox.views import generic
+
+from .models import Repository
+
+__all__ = (
+    "RepositoryListView",
+    "RepositoryCreateView",
+    "RepositoryDetailView",
+    "RepositoryUpdateView",
+    "RepositoryDeleteView",
+)
+
+
+class RepositoryListView(generic.ObjectListView):
+    model = Repository
+    template_name = "netbox_swupdate/repository_list.html"
+
+
+class RepositoryCreateView(generic.ObjectView):
+    model = Repository
+    template_name = "netbox_swupdate/repository_form.html"
+    fields = "__all__"
+
+
+class RepositoryDetailView(generic.ObjectView):
+    model = Repository
+    template_name = "netbox_swupdate/repository_detail.html"
+
+
+class RepositoryUpdateView(generic.ObjectEditView):
+    model = Repository
+    template_name = "netbox_swupdate/repository_form.html"
+    fields = "__all__"
+
+
+class RepositoryDeleteView(generic.ObjectDeleteView):
+    model = Repository
+    template_name = "netbox_swupdate/repository_confirm_delete.html"
+    success_url = "/swupdate/repository/"
