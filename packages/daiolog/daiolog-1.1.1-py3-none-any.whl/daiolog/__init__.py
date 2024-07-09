2c9e982dad7453086c3919ea5eeea18e50cdb383
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-09 15:04:53 +0000

    Add package daiolog/1.1.1/daiolog-1.1.1-py3-none-any.whl

---
+++ packages/daiolog/daiolog-1.1.1-py3-none-any.whl/daiolog/__init__.py
@@ -0,0 +1,5 @@
+from .handler import QueueHandler
+from .json_encoder import UniversalJSONEncoder
+from .formatters import JsonFormatter
+from .listener import QueueListener
+from .decorators import entrypoint
