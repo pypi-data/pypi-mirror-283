2ed2170f0721697c5fa779f97d28c3d23cd6a358
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-09 15:05:35 +0000

    Add package qusim/1.0.3/qusim-1.0.3.tar.gz

---
+++ packages/qusim/qusim-1.0.3.tar.gz/qusim-1.0.3/README.md
@@ -0,0 +1,59 @@
+# QuSim, a transmon system simulator
+
+<img src="https://github.com/RunawayFancy/QuSim/blob/main/QuSim_logo.png" alt="Alt text" width="330" height="300">
+
+## Table of Contents
+
+1. [About](#about)
+2. [User_Guide](#user_guide)
+
+## About <a name="about"></a>
+
+A simulator aim at simulating and solving Transmon based qubit-coupler-resonator system.
+
+This program is directed by Dr. Fei Yan, BAQIS. The original code structe is adoppted from Mr. Pan Shi, Wuhan University & BAQIS, and contributed by Mr. Jiheng Duan and Dr. Ruixia Wang, and Dr. Zhen Chen.
+
+Look up the PyPI page: [https://pypi.org/project/qusim/](https://pypi.org/project/qusim/)
+
+## User_Guide <a name = "user_guide"></a>
+
+### Install (use pip)
+
+```
+pip install qusim
+```
+
+### Requirement
+
+You are required to install all these python packages in the system for running this simulator. See `requirements.txt`.
+>* `Python`, version >= 3.10
+>* `qutip`, version >= 4.6.3
+>>* `qutip 4.6.3` is strongly recommanded. As we use `qutip 4.7.5`, the `propagator` solver will crash your terminal.
+>>* Nocie that `qutip 4.6.3` requires `scipy 1.10.1` exactly. Otherwise it will raise up error in function `fast_crs_matrix`. (https://stackoverflow.com/questions/76560698/python-3-10-qutip-attributeerror-cant-set-attribute-format)
+>* `matplotlib`
+>* `pickle`, any version
+>* `jupyter lab` or `jupyter notebook`, optional
+
+### Download
+
+Just downloading the entire folder. 
+
+There is also a release.
+
+### Usage
+> We have two version: the `transmon_system.py` and `arb_qubit_system.py`
+
+The detailed tutorial are shown in folder `tutorial/~`, including required imports, define a system, what inside a system, scan zz-coupling, scan energy level, driven system dynamics, single qubit gate calibration, and DRAG calibration.
+
+### Features
+
+Our simulator includes the following features:
+
+* Support multiple qubits
+* Multiple initial state simulation
+* Internal plot function
+* DRAG
+* Generalized for any qubit
+* Good data saving and loading functions
+* Tunable coupler helper
+* Aviod crossing helper
---
+++ packages/qusim/qusim-1.0.3.tar.gz/qusim-1.0.3/qusim/Utils/qsave.py
@@ -0,0 +1,154 @@
+# -*- coding: utf-8 -*-
+"""
+@author: Jiheng Duan
+"""
+import os
+import pickle
+from tkinter import Tk, filedialog
+
+class QSave:
+    def __init__(self, path):
+        self.path = path
+        self.init_directory
+        self.init
+
+    @property
+    def init_directory(self):
+        if not os.path.exists(self.path):
+            try:
+                os.makedirs(self.path)
+                print(f'Directory {self.path} created successfully.')
+            except Exception as e:
+                print(f"An error occurred while creating directory {self.path}: {e}")
+        else:
+            print(f'Directory {self.path} exists.')
+
+    @property
+    def init(self):
+        switch = 0
+        try:
+            counts = pickle.load(open(self.path+'counts.pkl', 'rb'))
+            print("Current counts.pickle exist.")
+        except FileNotFoundError:
+            switch = 1
+        if switch == 1:
+            counts = [0, '']
+            pickle.dump(counts, open(self.path+'counts.pkl', 'wb'))
+            print(f'Successfully create counts.pickle file under directory {self.path}')
+
+    @property
+    def reinit(self):
+        try:
+            counts = pickle.load(open(self.path+'counts.pkl', 'rb'))
+            response = input("Current counts.pickle exist, do you want to reinitialize the counts.pkl file? [y/n]: ")
+            if response.lower() == 'y':
+                switch = 1
+            elif response.lower() == 'n': 
+                switch = 0
+                print("No action taken.")
+            else: print("Invalid input. Please enter 'y' or 'n'.")
+        except FileNotFoundError:
+            switch = 1
+        if switch == 1:
+            counts = [0, '']
+            pickle.dump(counts, open(self.path+'counts.pkl', 'wb'))
+            print(f'Successfully create counts.pickle file under directory {self.path}')
+
+    @property
+    def check(self):
+        try:
+            counts = pickle.load(open(self.path+'counts.pkl', 'rb'))
+            print(f"Current counts.pickle exist, with value: {counts}")
+        except FileNotFoundError:
+            print(f"Current counts.pickle missing, please initial a new one using `QSave.init`")
+
+    @property
+    def delete(self):
+        response = input("Do you want to delete the counts.pkl file? [y/n]: ")
+        if response.lower() == 'y':
+            try:
+                os.remove(self.path + 'counts.pkl')
+                print(f'Successfully deleted counts.pkl file under directory {self.path}')
+            except FileNotFoundError:
+                print(f"The file {self.path}counts.pkl does not exist.")
+            except PermissionError:
+                print(f"Permission denied: unable to delete {self.path}counts.pkl.")
+            except Exception as e:
+                print(f"An error occurred: {e}")
+        elif response.lower() == 'n':
+            print("No action taken.")
+        else:
+            print("Invalid input. Please enter 'y' or 'n'.")
+
+    @property
+    def undo(self):
+        count, filename = pickle.load(open(f"{self.path}counts.pkl", "rb"))
+        # print("Undoing the process...")
+        fpath = self.path+filename
+        response = input(f"Do you want to undo the {filename}? [y/n]: ")
+        # Check the user's input and respond
+        if response.lower() == 'y':
+            try:
+                os.remove(fpath)
+                print(f"Successfully undo {filename}")
+            # Add code here to undo the process
+            except FileNotFoundError:
+                print(f"The file {fpath} does not exist.")
+            except PermissionError:
+                print(f"Permission denied: unable to delete {fpath}.")
+            except Exception as e:
+                print(f"An error occurred: {e}")
+            if count > 0:
+                count -= 1
+        elif response.lower() == 'n':
+            print("No action taken.")
+        else:
+            print("Invalid input. Please enter 'y' or 'n'.")
+        pickle.dump([count, filename], open(f"{self.path}counts.pkl", "wb"))
+
+    def save(self, filename: str ,data=None, scan=None):
+        count, _ = pickle.load(open(f"{self.path}counts.pkl", "rb"))
+        if filename.endswith('.pkl'):
+            pass
+        else:
+            filename = filename + f'_{count}.pkl'
+        # scan save, need a formate scan dict.
+        if scan is not None:
+            exit(0)
+        # custom data save
+        if data is not None:
+            pickle.dump(data, open(f"{self.path}{filename}", "wb"))
+            pickle.dump([count+1, filename], open(f"{self.path}counts.pkl", "wb"))
+            print(f"Save as file {filename}")
+
+    @property
+    def sfile(self):
+        root = Tk()
+        root.withdraw()
+        root.call('wm', 'attributes', '.', '-topmost', True)
+        filepaths = filedialog.askopenfilenames(initialdir=self.path)
+        root.destroy()
+        filenames = [os.path.basename(filepath) for filepath in filepaths]
+        print(f"Selected files: {filenames}")
+        return filenames  # Return the list of filenames for further use
+    
+    def load(self, *filenames):
+        print(filenames)
+        if not filenames:
+            filenames = self.sfile
+        else:
+            filenames = list(filenames)
+            filenames = [fn if fn.endswith('.pkl') else fn + '.pkl' for fn in filenames]
+        data = []
+        for fn in filenames:
+            full_path = os.path.join(self.path, fn)
+            try:
+                with open(full_path, 'rb') as f:
+                    data.append(pickle.load(f))
+                print(f'Successfully loaded {full_path}')
+            except FileNotFoundError:
+                print(f"The file {full_path} does not exist.")
+            except Exception as e:
+                print(f"An error occurred while loading {full_path}: {e}")
+        
+        return data
