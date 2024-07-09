802ee6597912c8a836208e454ebbf0f341213778
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-09 06:05:12 +0000

    Add package humanloop/0.7.30/humanloop-0.7.30-py3-none-any.whl

---
+++ packages/humanloop/humanloop-0.7.30-py3-none-any.whl/humanloop/apis/tags/__init__.py
@@ -0,0 +1,21 @@
+# do not import all endpoints into this module because that uses a lot of memory and stack frames
+# if you need the ability to import all endpoints from this module, import them with
+# from humanloop.apis.tag_to_api import tag_to_api
+
+import enum
+
+
+class TagValues(str, enum.Enum):
+    PROJECTS = "Projects"
+    EVALUATIONS = "Evaluations"
+    DATASETS = "Datasets"
+    LOGS = "Logs"
+    EVALUATORS = "Evaluators"
+    EXPERIMENTS = "Experiments"
+    MODEL_CONFIGS = "Model Configs"
+    CHATS = "Chats"
+    COMPLETIONS = "Completions"
+    DATAPOINTS = "Datapoints"
+    SESSIONS = "Sessions"
+    FEEDBACK = "Feedback"
+    AUTHENTICATION = "Authentication"
