681766a59f77f8727bf658df639be624cdf57b56
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-09 15:05:18 +0000

    Add package iacele/0.0.1/iacele-0.0.1-py3-none-any.whl

---
+++ packages/iacele/iacele-0.0.1-py3-none-any.whl/iacele/__init__.py
@@ -0,0 +1,7 @@
+"""
+## IACele
+Librería para crear BI en la administración de la empresa LA CASA DEL CARPINTERO.
+"""
+
+from .algoritmo_ventas_mes import ventas_mes_func
+from .algoritmo_cobranza_mes import cobranza_mes_func
