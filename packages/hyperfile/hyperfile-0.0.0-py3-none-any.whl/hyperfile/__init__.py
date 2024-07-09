3791b01f043aa54e2aa84a00363cf1ce369b6565
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-09 17:04:55 +0000

    Add package hyperfile/0.0.0/hyperfile-0.0.0-py3-none-any.whl

---
+++ packages/hyperfile/hyperfile-0.0.0-py3-none-any.whl/hyperfile/__init__.py
@@ -0,0 +1,7 @@
+"""hyperfile - A hyper convenient terminal file manager."""
+from __future__ import annotations
+
+
+def greet(name: str) -> str:
+    """Returns a greeting."""
+    return f"Hello {name}!"
