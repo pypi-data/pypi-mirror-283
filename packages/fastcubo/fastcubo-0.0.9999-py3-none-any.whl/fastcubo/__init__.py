fac722b43c1d89ca9293912d37296b0cc6aceeff
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-09 17:04:52 +0000

    Add package fastcubo/0.0.9999/fastcubo-0.0.9999-py3-none-any.whl

---
+++ packages/fastcubo/fastcubo-0.0.9999-py3-none-any.whl/fastcubo/__init__.py
@@ -0,0 +1,3 @@
+from fastcubo.main import (computePixels, getPixels, query_computePixels_image,
+                           query_getPixels_image,
+                           query_getPixels_imagecollection)
