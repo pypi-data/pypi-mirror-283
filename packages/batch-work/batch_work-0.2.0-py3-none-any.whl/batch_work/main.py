8e0532b70e67c655c418ad510884e612610de393
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-09 03:05:55 +0000

    Add package batch-work/0.2.0/batch_work-0.2.0-py3-none-any.whl

---
+++ packages/batch-work/batch_work-0.2.0-py3-none-any.whl/batch_work/main.py
@@ -0,0 +1,33 @@
+# Copyright (c) 2024 Ugyen Norbu
+# 
+# This source code is licensed under the MIT license found in the
+# LICENSE file in the root directory of this source tree.
+
+import os
+import sys
+
+from .bw_sort import bw_sort
+from .bw_rename import bw_rename
+
+def main():
+    if len(sys.argv) < 2:
+        print("Error: Please follow the following usage")
+        print("Usage: bw_rename <prefix> <directory_path>")
+        print("Usage: bw_sort <directory_path>")
+
+    elif 'bw_sort' in sys.argv[0] and len(sys.argv) == 2:
+        directory_path = sys.argv[1]
+        bw_sort(directory_path)
+        
+    elif 'bw_rename' in sys.argv[0] and len(sys.argv) == 3:
+        prefix = sys.argv[1]
+        directory_path = sys.argv[2]
+        bw_rename(directory_path, prefix)
+
+    else:
+        print("Error: Please follow the following usage")
+        print("Usage: bw_rename <prefix> <directory_path>")
+        print("Usage: bw_sort <directory_path>")
+
+if __name__ == "__main__":
+    main()
