74fec9c34879c44d5429469e60a5820ce987f7e5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-09 17:05:49 +0000

    Add package project-utils-config/1.1.9/project_utils_config-1.1.9-py3-none-any.whl

---
+++ packages/project-utils-config/project_utils_config-1.1.9-py3-none-any.whl/project_utils/conf/addr_config/kafka_config.py
@@ -0,0 +1,24 @@
+﻿from typing import List, Optional, Dict, Union
+
+from .base_config import BaseConfig
+
+
+class KafkaConfig(BaseConfig):
+    bootstrap_servers: List[str]
+    topic: str
+    group: Optional[str]
+
+    def __init__(self, bootstrap_servers: str, topic: str, group: Optional[str] = None):
+        super().__init__(port="0")
+        self.bootstrap_servers = bootstrap_servers.split(";")
+        self.topic = topic
+        if group: self.group = group
+
+    def to_dict(self) -> Dict[str, Union[str, int, List[str]]]:
+        result: Dict[str, Union[str, int, List[str]]] = {
+            "bootstrap_servers": self.bootstrap_servers,
+            "topic": self.topic,
+        }
+        if self.group:
+            result['group'] = self.group
+        return result
