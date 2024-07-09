94c839b6ad30b50ef32ad80d61b293e2f31b21c1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-09 14:04:38 +0000

    Add package blurhash-pyside/0.2.3/blurhash_pyside-0.2.3-cp311-cp311-win_amd64.whl

---
+++ packages/blurhash-pyside/blurhash_pyside-0.2.3-cp311-cp311-win_amd64.whl/blurhash_pyside/__init__.py
@@ -0,0 +1,24 @@
+# SPDX-FileCopyrightText: 2024-present Leonardo Covarrubias <leo@leocov.com>
+#
+# SPDX-License-Identifier: MIT
+from . import errors
+from ._wrapper import (
+    Components,
+    decode_to_qimage,
+    decode_to_qpixmap,
+    encode_qimage,
+    encode_qpixmap,
+)
+
+__version__ = "0.2.3"
+
+
+__all__ = [
+    "__version__",
+    "decode_to_qimage",
+    "decode_to_qpixmap",
+    "Components",
+    "encode_qimage",
+    "encode_qpixmap",
+    "errors",
+]
