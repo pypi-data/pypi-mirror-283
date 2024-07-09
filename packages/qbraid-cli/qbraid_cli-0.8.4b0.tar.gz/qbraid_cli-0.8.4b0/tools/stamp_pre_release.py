6168ae171a76ff62feba5d88d1faa1457802d9d8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-09 14:04:41 +0000

    Add package qbraid-cli/0.8.4b0/qbraid_cli-0.8.4b0.tar.gz

---
+++ packages/qbraid-cli/qbraid_cli-0.8.4b0.tar.gz/qbraid_cli-0.8.4b0/tools/stamp_pre_release.py
@@ -0,0 +1,18 @@
+# Copyright (c) 2024, qBraid Development Team
+# All rights reserved.
+
+"""
+Script for getting/bumping the next pre-release version.
+
+"""
+
+import pathlib
+
+from qbraid_core.system.versions import get_prelease_version
+
+if __name__ == "__main__":
+
+    PACKAGE = "qbraid_cli"
+    root = pathlib.Path(__file__).parent.parent.resolve()
+    version = get_prelease_version(root, PACKAGE)
+    print(version)
