e4164e4cb81bb7e6a186ba8498af0958c6dee1c3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-09 06:04:41 +0000

    Add package ugrc-palletjack/4.4.2/ugrc_palletjack-4.4.2-py3-none-any.whl

---
+++ packages/ugrc-palletjack/ugrc_palletjack-4.4.2-py3-none-any.whl/palletjack/errors.py
@@ -0,0 +1,10 @@
+"""Errors specific to palletjack
+"""
+
+
+class IntFieldAsFloatError(Exception):
+    """A field expected to be integer is float instead"""
+
+
+class TimezoneAwareDatetimeError(Exception):
+    """A datetime field uses a timezone-aware dtype"""
