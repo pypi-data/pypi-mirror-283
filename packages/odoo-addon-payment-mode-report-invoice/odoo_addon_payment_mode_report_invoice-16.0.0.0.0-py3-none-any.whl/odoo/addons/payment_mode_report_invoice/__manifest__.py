e8adb14aa8e699a9b175e34022bc85466461d204
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-09 05:04:50 +0000

    Add package odoo-addon-payment-mode-report-invoice/16.0.0.0.0/odoo_addon_payment_mode_report_invoice-16.0.0.0.0-py3-none-any.whl

---
+++ packages/odoo-addon-payment-mode-report-invoice/odoo_addon_payment_mode_report_invoice-16.0.0.0.0-py3-none-any.whl/odoo/addons/payment_mode_report_invoice/__manifest__.py
@@ -0,0 +1,16 @@
+# Copyright 2023-Coopdevs Treball SCCL (<https://coopdevs.org>)
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
+{
+    "name": "Payment mode report invoice",
+    "version": "16.0.0.0.0",
+    "depends": ["account"],
+    "author": "Coopdevs Treball SCCL",
+    "category": "Account invoice",
+    "website": "https://coopdevs.org",
+    "license": "AGPL-3",
+    "summary": """
+        Adapts payment information by payment mode
+    """,
+    "data": ["views/report_invoice.xml"],
+    "installable": True,
+}
