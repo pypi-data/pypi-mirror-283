6b790f079f1fe777314c1783dd68a3584b1e6645
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-09 03:05:43 +0000

    Add package sc.translation/1.0.0/sc_translation-1.0.0.tar.gz

---
+++ packages/sc.translation/sc_translation-1.0.0.tar.gz/sc_translation-1.0.0/setup.py
@@ -0,0 +1,10 @@
+# setup.py 示例
+from setuptools import setup, find_packages
+ 
+setup(
+    name='sc.translation',
+    version='1.0.0',
+    description='A Translate Agent Python package',
+    packages=find_packages(),
+    python_requires='>=3.8',
+)
