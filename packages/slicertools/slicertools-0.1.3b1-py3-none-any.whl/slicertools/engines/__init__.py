2ecc448b8b0834ff0580c2c117a40ccd4bbd896f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-09 07:04:24 +0000

    Add package slicertools/0.1.3b1/slicertools-0.1.3b1-py3-none-any.whl

---
+++ packages/slicertools/slicertools-0.1.3b1-py3-none-any.whl/slicertools/engines/__init__.py
@@ -0,0 +1,1 @@
+from .cura import CuraEngineSlicer
---
+++ packages/slicertools/slicertools-0.1.3b1-py3-none-any.whl/slicertools/materials.py
@@ -0,0 +1,38 @@
+import dataclasses
+
+
+@dataclasses.dataclass
+class BaseMaterial:
+    material: str = None
+    printing_temperature: str = None
+    density: float = 1.0
+
+
+class PLA(BaseMaterial):
+    material = "PLA"
+    printing_temperature = "190-220°C"
+    density = 1.24
+
+
+class ABS(BaseMaterial):
+    material = "ABS"
+    printing_temperature = "220-250°C"
+    density = 1.04
+
+
+class PETG(BaseMaterial):
+    material = "PETG"
+    printing_temperature = "230-250°C"
+    density = 1.27
+
+
+class Nylon(BaseMaterial):
+    material = "Nylon"
+    printing_temperature = "230-260°C"
+    density = 1.15
+
+
+class TPU(BaseMaterial):
+    material = "TPU"
+    printing_temperature = "220-250°C"
+    density = 1.2
