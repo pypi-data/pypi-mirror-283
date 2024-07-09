880607d5f9b8c4fe524f527d0ed5f4b946b145ed
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-09 14:04:29 +0000

    Add package constants-importer-3/4.0.0/constants_importer_3-4.0.0.tar.gz

---
+++ packages/constants-importer-3/constants_importer_3-4.0.0.tar.gz/constants_importer_3-4.0.0/setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup, find_packages
+
+setup(
+
+    name='constants-importer-3',
+
+    version='4.0.0',
+
+    author='J J',
+
+    author_email='jai.jalah@krakenflex.com',
+
+    description='A package for initialising the path to the Constants.py file in a root repo, and then importing said constants into the current directory',
+
+    long_description_content_type="text/markdown",
+
+    packages=find_packages(),
+)
