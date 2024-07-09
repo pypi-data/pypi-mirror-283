202b279c9e4a3ea753258dc6ec282b9861c53c0c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-09 05:04:09 +0000

    Add package launch-cli/0.8.0/launch_cli-0.8.0-py3-none-any.whl

---
+++ packages/launch-cli/launch_cli-0.8.0-py3-none-any.whl/launch/constants/common.py
@@ -0,0 +1,15 @@
+SERVICE_SKELETON = "https://github.com/launchbynttdata/lcaf-template-terragrunt.git"
+SKELETON_BRANCH = "main"
+MAIN_BRANCH = "main"
+INIT_BRANCH = "feature/init"
+BUILD_DEPENDENCIES_DIR = ".launch"
+CODE_GENERATION_DIR_SUFFIX = "-singleRun"
+DISCOVERY_FORBIDDEN_DIRECTORIES = [
+    ".git",
+    "components",
+    ".repo",
+    "__pycache__",
+    ".venv",
+    ".terraform",
+    ".terragrunt-cache",
+]
