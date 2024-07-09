b59f9249db4af5a73b7a001c3eed2588fddd6d50
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-09 04:06:29 +0000

    Add package netbox-swupdate/0.0.1.3/netbox_swupdate-0.0.1.3.tar.gz

---
+++ packages/netbox-swupdate/netbox_swupdate-0.0.1.3.tar.gz/netbox_swupdate-0.0.1.3/netbox_swupdate/views/__init__.py
@@ -0,0 +1,15 @@
+from .view_repository import (
+    RepositoryCreateView,
+    RepositoryDeleteView,
+    RepositoryDetailView,
+    RepositoryListView,
+    RepositoryUpdateView,
+)
+
+__all__ = (
+    "RepositoryListView",
+    "RepositoryCreateView",
+    "RepositoryDetailView",
+    "RepositoryUpdateView",
+    "RepositoryDeleteView",
+)
