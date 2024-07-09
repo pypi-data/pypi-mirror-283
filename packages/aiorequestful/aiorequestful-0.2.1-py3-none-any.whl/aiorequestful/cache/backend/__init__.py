8f528656f933a6c0fad54bba46b6d41cfe4c85f3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-09 14:04:34 +0000

    Add package aiorequestful/0.2.1/aiorequestful-0.2.1-py3-none-any.whl

---
+++ packages/aiorequestful/aiorequestful-0.2.1-py3-none-any.whl/aiorequestful/cache/backend/__init__.py
@@ -0,0 +1,5 @@
+from aiorequestful.cache.backend.base import ResponseCache
+from aiorequestful.cache.backend.sqlite import SQLiteCache
+
+CACHE_CLASSES: frozenset[type[ResponseCache]] = frozenset({SQLiteCache})
+CACHE_TYPES = frozenset(cls.type for cls in CACHE_CLASSES)
