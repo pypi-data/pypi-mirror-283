034ff35e86451e15fc6165d839b0b00bbf963ed9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-09 06:04:38 +0000

    Add package package-hello-mmeiler-dev/0.1/package_hello_mmeiler-dev-0.1.tar.gz

---
+++ packages/package-hello-mmeiler-dev/package_hello_mmeiler-dev-0.1.tar.gz/package_hello_mmeiler-dev-0.1/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+        name="package_hello_mmeiler-dev",
+        version="0.1",
+        pacakges=find_packages(),
+        install_requires=[
+            
+        ],
+    )
