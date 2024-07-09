0d1a4c94b2f39cede10ea2a769a0627b0af72b5c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-09 03:05:23 +0000

    Add package sampo/0.1.1.342/sampo-0.1.1.342-py3-none-any.whl

---
+++ packages/sampo/sampo-0.1.1.342-py3-none-any.whl/sampo/native/cmake-build-debug/native/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: C:/Users/stasb/PycharmProjects/sampo/sampo/native/native
+# Build directory: C:/Users/stasb/PycharmProjects/sampo/sampo/native/cmake-build-debug/native
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
