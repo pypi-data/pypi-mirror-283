47e8b37303b425f77256562c88f952192ad0f85a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-09 07:04:31 +0000

    Add package reds-simple-logger/2.0.7/reds_simple_logger-2.0.7.tar.gz

---
+++ packages/reds-simple-logger/reds_simple_logger-2.0.7.tar.gz/reds_simple_logger-2.0.7/reds_simple_logger/__init__.py
@@ -0,0 +1,15 @@
+from termcolor import colored
+from datetime import datetime
+
+class Logger:
+    def info(self, msg: str):
+        print(f'[{datetime.now().strftime("%H:%M:%S")}] [{colored("INFO",  "light_blue")}]\t\t: {msg}')
+    
+    def success(self, msg: str):
+        print(f'[{datetime.now().strftime("%H:%M:%S")}] [{colored("SUCCESS",  "light_green")}\t\t: {msg}')
+
+    def error(self, msg: str):
+        print(f'[{datetime.now().strftime("%H:%M:%S")}] [{colored("ERROR",  "light_red")}]\t\t: {msg}')
+
+    def warn(self, msg: str):
+        print(f'[{datetime.now().strftime("%H:%M:%S")}] [{colored("WARN",  "yellow")}]\t\t: {msg}')
