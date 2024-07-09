4092fea070e5fc752f965d33c096b3670d59c1a9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-09 03:04:51 +0000

    Add package cs-models/0.0.693/cs_models-0.0.693-py3-none-any.whl

---
+++ packages/cs-models/cs_models-0.0.693-py3-none-any.whl/cs_models/resources/SmartGrid/models.py
@@ -0,0 +1,28 @@
+from datetime import datetime
+
+from sqlalchemy import Column, DateTime, Boolean, ForeignKey, Integer, Text, String
+from sqlalchemy.orm import relationship
+
+from ...database import Base
+from ..SmartGridCell.models import SmartGridCellModel
+
+
+class SmartGridModel(Base):
+    __tablename__ = "smart_grids"
+
+    id = Column(Integer, primary_key=True)
+    user_id = Column(String(128), nullable=False)
+    is_deleted = Column(Boolean, nullable=True)
+    updated_at = Column(
+        DateTime,
+        nullable=False,
+        # https://stackoverflow.com/questions/58776476/why-doesnt-freezegun-work-with-sqlalchemy-default-values
+        default=lambda: datetime.utcnow(),
+        onupdate=lambda: datetime.utcnow(),
+    )
+
+    cells = relationship(
+        "SmartGridCellModel",
+        order_by=SmartGridCellModel.row,
+        back_populates="smart_grid",
+    )
