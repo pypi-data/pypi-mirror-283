1d760e0a0e0b84be4bbafc36de4de8320981c182
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-09 14:04:43 +0000

    Add package marimo/0.7.1/marimo-0.7.1.tar.gz

---
+++ packages/marimo/marimo-0.7.1.tar.gz/marimo-0.7.1/marimo/_server/models/home.py
@@ -0,0 +1,42 @@
+# Copyright 2024 Marimo. All rights reserved.
+from __future__ import annotations
+
+from dataclasses import dataclass
+from typing import List, Optional
+
+from marimo._server.ids import SessionId
+
+
+@dataclass
+class MarimoFile:
+    # Name of the file
+    name: str
+    # Absolute path to the file
+    path: str
+    # Last modified time of the file
+    last_modified: float
+    # Session id
+    session_id: Optional[SessionId] = None
+    # Session initialization id
+    # This is the ID for when the session was initialized
+    initialization_id: Optional[str] = None
+
+
+@dataclass
+class RecentFilesResponse:
+    files: List[MarimoFile]
+
+
+@dataclass
+class WorkspaceFilesRequest:
+    include_markdown: bool = False
+
+
+@dataclass
+class WorkspaceFilesResponse:
+    files: List[MarimoFile]
+
+
+@dataclass
+class ShutdownSessionRequest:
+    session_id: SessionId
