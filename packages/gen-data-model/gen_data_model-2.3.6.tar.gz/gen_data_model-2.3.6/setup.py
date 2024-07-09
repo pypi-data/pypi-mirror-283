aaeb8d66d9c1cc5d3bbf2bb3d8c322e91f713da3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-09 05:04:46 +0000

    Add package gen-data-model/2.3.6/gen_data_model-2.3.6.tar.gz

---
+++ packages/gen-data-model/gen_data_model-2.3.6.tar.gz/gen_data_model-2.3.6/setup.py
@@ -0,0 +1,99 @@
+#!/usr/bin/env python
+# -*- coding: UTF-8 -*-
+
+'''
+Module
+    setup.py
+Copyright
+    Copyright (C) 2017 - 2024 Vladimir Roncevic <elektron.ronca@gmail.com>
+    gen_data_model is free software: you can redistribute it and/or modify it
+    under the terms of the GNU General Public License as published by the
+    Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+    gen_data_model is distributed in the hope that it will be useful, but
+    WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+    See the GNU General Public License for more details.
+    You should have received a copy of the GNU General Public License along
+    with this program. If not, see <http://www.gnu.org/licenses/>.
+Info
+    Defines setup for tool gen_data_model.
+'''
+
+from __future__ import print_function
+from typing import List, Optional
+from os.path import abspath, dirname, join
+from setuptools import setup
+
+__author__ = 'Vladimir Roncevic'
+__copyright__ = '(C) 2024, https://vroncevic.github.io/gen_data_model'
+__credits__: List[str] = ['Vladimir Roncevic', 'Python Software Foundation']
+__license__ = 'https://github.com/vroncevic/gen_data_model/blob/dev/LICENSE'
+__version__ = '2.3.6'
+__maintainer__ = 'Vladimir Roncevic'
+__email__ = 'elektron.ronca@gmail.com'
+__status__ = 'Updated'
+
+TOOL_DIR: str = 'gen_data_model/'
+CONF: str = 'conf'
+TEMPLATE: str = 'conf/template'
+LOG: str = 'log'
+THIS_DIR: str = abspath(dirname(__file__))
+long_description: Optional[str] = None
+with open(join(THIS_DIR, 'README.md'), encoding='utf-8') as readme:
+    long_description = readme.read()
+PROGRAMMING_LANG: str = 'Programming Language :: Python ::'
+VERSIONS: List[str] = ['3.10', '3.11']
+SUPPORTED_PY_VERSIONS: List[str] = [
+    f'{PROGRAMMING_LANG} {VERSION}' for VERSION in VERSIONS
+]
+LICENSE_PREFIX: str = 'License :: OSI Approved ::'
+LICENSES: List[str] = [
+    'GNU Lesser General Public License v2 (LGPLv2)',
+    'GNU Lesser General Public License v2 or later (LGPLv2+)',
+    'GNU Lesser General Public License v3 (LGPLv3)',
+    'GNU Lesser General Public License v3 or later (LGPLv3+)',
+    'GNU Library or Lesser General Public License (LGPL)'
+]
+APPROVED_LICENSES: List[str] = [
+    f'{LICENSE_PREFIX} {LICENSE}' for LICENSE in LICENSES
+]
+PYP_CLASSIFIERS: List[str] = SUPPORTED_PY_VERSIONS + APPROVED_LICENSES
+
+setup(
+    name='gen_data_model',
+    version='2.3.6',
+    description='Python App/Tool/Script Utilities',
+    author='Vladimir Roncevic',
+    author_email='elektron.ronca@gmail.com',
+    url='https://vroncevic.github.io/gen_data_model/',
+    license='GPL 2017 - 2024 Free software to use and distributed it.',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    keywords='data model, django, flask, sqlalchemy',
+    platforms='POSIX',
+    classifiers=PYP_CLASSIFIERS,
+    packages=['gen_data_model', 'gen_data_model.pro'],
+    install_requires=['ats-utilities'],
+    package_data={
+        'gen_data_model': [
+            'py.typed',
+            f'{CONF}/gen_data_model.logo',
+            f'{CONF}/gen_data_model.cfg',
+            f'{CONF}/gen_data_model_util.cfg',
+            f'{CONF}/model_types.yaml',
+            f'{TEMPLATE}/django.template',
+            f'{TEMPLATE}/flask.template',
+            f'{TEMPLATE}/sqlalchemy.template',
+            f'{TEMPLATE}/django_base_model.template',
+            f'{TEMPLATE}/flask_base_model.template',
+            f'{TEMPLATE}/sqlalchemy_base_model.template',
+            f'{LOG}/gen_data_model.log'
+        ]
+    },
+    data_files=[(
+        '/usr/local/bin/', [
+            f'{TOOL_DIR}run/gen_data_model_run.py'
+        ]
+    )]
+)
