d1be1043965abbee90542a817e6b35e1bf6b3f62
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-09 05:04:14 +0000

    Add package nullval/0.0.1/nullval-0.0.1-py3-none-any.whl

---
+++ packages/nullval/nullval-0.0.1-py3-none-any.whl/nullval/__init__.py
@@ -0,0 +1,9 @@
+__version__ = '0.0.1'
+
+
+from .linear_interpolation import *
+from .polynomial_interpolation import *
+from .spline_interpolation import *
+from .trigonometric_interpolation import *
+from .loader import *
+
