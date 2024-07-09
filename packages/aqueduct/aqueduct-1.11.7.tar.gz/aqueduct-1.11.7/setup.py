3327f8de7b7f9ee4952bdbaefc12a83dee36d29a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-09 18:04:41 +0000

    Add package aqueduct/1.11.7/aqueduct-1.11.7.tar.gz

---
+++ packages/aqueduct/aqueduct-1.11.7.tar.gz/aqueduct-1.11.7/setup.py
@@ -0,0 +1,57 @@
+from setuptools import find_packages, setup
+
+with open('README.rst') as f:
+    long_description = f.read()
+
+packages = ['aqueduct']
+
+required = [
+    'cffi==1.16.0',
+    'psutil==5.9.4',
+    'setuptools==70.2.0',
+]
+
+extras = {
+    'aiohttp': [
+        'aiohttp',
+    ],
+    'numpy': [
+        'numpy'
+    ]
+}
+
+setup(
+    name='aqueduct',
+    packages=find_packages(),
+    version='1.11.7',
+    license='MIT',
+    license_files='LICENSE.txt',
+    author='Data Science SWAT',
+    author_email='UnitDataScienceSwat@avito.ru',
+    description='Builder for performance-efficient prediction.',
+    url='https://github.com/avito-tech/aqueduct',
+    download_url='https://github.com/avito-tech/aqueduct/archive/refs/heads/main.zip',
+    keywords=['datascience', 'learning', ],
+    python_requires='>=3.8',
+    include_package_data=True,
+    long_description=long_description,
+    long_description_content_type='text/x-rst',
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'Topic :: Scientific/Engineering',
+        'Topic :: Software Development :: Build Tools',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3.8',
+        'Operating System :: MacOS',
+        'Operating System :: Microsoft :: Windows',
+        'Operating System :: POSIX',
+        'Operating System :: Unix',
+        'Operating System :: OS Independent',
+    ],
+    setup_requires=required,
+    install_requires=required,
+    extras_require=extras,
+)
