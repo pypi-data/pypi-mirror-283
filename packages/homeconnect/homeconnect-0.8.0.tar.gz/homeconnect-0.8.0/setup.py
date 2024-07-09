badd24f5430fcf0ff24c97ca224d142a2cfff5e9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-09 06:04:45 +0000

    Add package homeconnect/0.8.0/homeconnect-0.8.0.tar.gz

---
+++ packages/homeconnect/homeconnect-0.8.0.tar.gz/homeconnect-0.8.0/setup.py
@@ -0,0 +1,23 @@
+from setuptools import find_packages, setup
+
+with open("README.md") as f:
+    LONG_DESCRIPTION = f.read()
+
+setup(
+    name="homeconnect",
+    version="0.8.0",
+    author="David M. Straub",
+    author_email="straub@protonmail.com",
+    url="https://github.com/DavidMStraub/homeconnect",
+    description="Python client for the BSH Home Connect REST API",
+    long_description=LONG_DESCRIPTION,
+    long_description_content_type="text/markdown",
+    license="MIT",
+    packages=find_packages(),
+    install_requires=["requests", "requests_oauthlib"],
+    extras_require={
+        "testing": [
+            "nose",
+        ],
+    },
+)
