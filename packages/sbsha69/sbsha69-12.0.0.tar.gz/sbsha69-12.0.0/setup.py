d9da5b82698830c5162bf1aab69e361bafc2ae50
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-09 03:04:32 +0000

    Add package sbsha69/12.0.0/sbsha69-12.0.0.tar.gz

---
+++ packages/sbsha69/sbsha69-12.0.0.tar.gz/sbsha69-12.0.0/setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup
+
+setup(
+    name='sbsha69',
+    version='12.0.0',
+    py_modules=['sbsha69'],
+    author='PythonScratcher',
+    author_email='me@psr.tf',
+    description='Basic encryption algorithm',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    url='https://github.com/yourusername/sbsha69',
+    license='MIT',
+    classifiers=[
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
+    ],
+)
