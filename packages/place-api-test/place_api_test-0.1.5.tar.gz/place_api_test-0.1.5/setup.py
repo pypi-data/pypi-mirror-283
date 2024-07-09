df0c5cb1207a9e3ffade7767adc310ea839fb2b1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-09 17:05:36 +0000

    Add package place-api-test/0.1.5/place_api_test-0.1.5.tar.gz

---
+++ packages/place-api-test/place_api_test-0.1.5.tar.gz/place_api_test-0.1.5/setup.py
@@ -0,0 +1,14 @@
+from setuptools import find_packages, setup
+
+PACKAGE_NAME = "place_api_test"
+
+setup(
+    name=PACKAGE_NAME,
+    version="0.1.5",
+    description="This is my tools package",
+    packages=find_packages(),
+    entry_points={
+        "package_tools": ["place_api_tool = place.tools.utils:list_package_tools"],
+    },
+    include_package_data=True,  # This line tells setuptools to include files from MANIFEST.in
+)
