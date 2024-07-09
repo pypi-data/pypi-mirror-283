aa0975418b36c3ec073976d7e735338367650295
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-09 03:06:31 +0000

    Add package ckanext-nhm/6.9.1/ckanext_nhm-6.9.1-py3-none-any.whl

---
+++ packages/ckanext-nhm/ckanext_nhm-6.9.1-py3-none-any.whl/ckanext/nhm/lib/utils.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+# encoding: utf-8
+#
+# This file is part of ckanext-nhm
+# Created by the Natural History Museum in London, UK
+
+from ckan.plugins import toolkit
+import requests
+
+
+def get_iiif_status():
+    health = {}
+
+    url = toolkit.config.get('ckanext.iiif.image_server_url')
+    r = requests.get(url + '/status')
+    if r.ok:
+        health['ping'] = True
+        response_json = r.json()
+    else:
+        response_json = {}
+
+    health['status'] = response_json.get('status')
+    mss = response_json.get('profiles', {}).get('mss', {})
+    health['specimens'] = (
+        mss.get('source_cache', {}).get('mss_status', {}).get('status', ':(')
+    )
+    health['es'] = mss.get('es', {'status': 'red', 'response_time': None})
+
+    return health
