0af801b50b98dd6601e9baf58e407333a14d6072
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-09 04:04:25 +0000

    Add package unique-print-lib/0.2/unique_print_lib-0.2.tar.gz

---
+++ packages/unique-print-lib/unique_print_lib-0.2.tar.gz/unique_print_lib-0.2/README.md
@@ -0,0 +1,8 @@
+# unique_print_lib
+
+A simple Python library that prints different messages.
+
+## Installation
+
+```bash
+pip install unique_print_lib
---
+++ packages/unique-print-lib/unique_print_lib-0.2.tar.gz/unique_print_lib-0.2/unique_print_lib/__init__.py
@@ -0,0 +1,8 @@
+def print_hello_world():
+    print("Hello, World!")
+
+def print_goodbye_world():
+    print("Goodbye, World!")
+
+def print_custom_message(message):
+    print(message)
