cf8a6df75c84647d949b68822ea969db5db6ea3b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-09 18:05:00 +0000

    Add package django-gateway-interface/0.1.3/django_gateway_interface-0.1.3.tar.gz

---
+++ packages/django-gateway-interface/django_gateway_interface-0.1.3.tar.gz/django_gateway_interface-0.1.3/setup.py
@@ -0,0 +1,31 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='django-gateway-interface',
+    version='0.1.3',
+    description='A Django application for configuring gateway settings with a web-based interface',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    author='Ankit Kumar',
+    author_email='bantiyadav16095@gmail.com',
+    url='https://github.com/ankit3388/BlockerGateway',
+    packages=find_packages(include=['gatewayApplication', 'gatewayApplication.*', 'gateapp', 'gateapp.*']),
+    include_package_data=True,
+    install_requires=[
+        'Django>=3.0,<5.0',
+        'gunicorn',
+    ],
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3',
+        'Framework :: Django',
+    ],
+    python_requires='>=3.8',
+    keywords='django gateway interface web configuration',
+    project_urls={
+        'Bug Reports': 'https://github.com/ankit3388/BlockerGateway/issues',
+        'Source': 'https://github.com/ankit3388/BlockerGateway',
+    },
+)
