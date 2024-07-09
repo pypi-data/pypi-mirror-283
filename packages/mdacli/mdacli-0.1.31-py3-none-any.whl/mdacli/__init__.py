4abae8ecf2678ee738f1b15bf4b0cda1f0df7101
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-09 04:06:24 +0000

    Add package mdacli/0.1.31/mdacli-0.1.31-py3-none-any.whl

---
+++ packages/mdacli/mdacli-0.1.31-py3-none-any.whl/mdacli/__init__.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+# -*- Mode: python; tab-width: 4; indent-tabs-mode:nil; coding:utf-8 -*-
+#
+# Copyright (c) 2021 Authors and contributors
+#
+# Released under the GNU Public Licence, v2 or any higher version
+# SPDX-License-Identifier: GPL-2.0-or-later
+"""Command line interfaces for MDAnalysis analysis classes."""
+
+from mdacli.cli import cli
+
+
+__all__ = ["cli"]
+__version__ = '0.1.31'
+__authors__ = 'MDAnalysis Development Team and contributors'
