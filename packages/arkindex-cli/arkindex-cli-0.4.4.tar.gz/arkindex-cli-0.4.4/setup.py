c56387ab34e6f8c53d7f5c03b5c0b5b01a446f01
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-09 03:05:42 +0000

    Add package arkindex-cli/0.4.4/arkindex-cli-0.4.4.tar.gz

---
+++ packages/arkindex-cli/arkindex-cli-0.4.4.tar.gz/arkindex-cli-0.4.4/setup.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+import os.path
+
+from setuptools import find_packages, setup
+
+
+def requirements(path):
+    assert os.path.exists(path), "Missing requirements {}".format(path)
+    with open(path) as f:
+        return list(map(str.strip, f.read().splitlines()))
+
+
+with open("VERSION") as f:
+    VERSION = f.read()
+
+with open("README.md") as f:
+    README = f.read()
+
+install_requires = requirements("requirements.txt")
+
+setup(
+    name="arkindex-cli",
+    version=VERSION,
+    description="Arkindex CLI client easy and sexy to use",
+    author="Teklia",
+    author_email="contact@teklia.com",
+    url="https://arkindex.teklia.com",
+    long_description=README,
+    long_description_content_type="text/markdown",
+    python_requires=">=3.8",
+    install_requires=install_requires,
+    tests_require=[],
+    extras_require={"export": requirements("requirements-export.txt")},
+    packages=find_packages(),
+    entry_points={"console_scripts": ["arkindex = arkindex_cli.cli:main"]},
+)
