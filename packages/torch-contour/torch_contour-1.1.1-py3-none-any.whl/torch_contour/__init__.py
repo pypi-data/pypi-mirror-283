cf23e3ab984526d9519bb6224e0245af79cb8e6f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-09 18:04:44 +0000

    Add package torch-contour/1.1.1/torch_contour-1.1.1-py3-none-any.whl

---
+++ packages/torch-contour/torch_contour-1.1.1-py3-none-any.whl/torch_contour/__init__.py
@@ -0,0 +1,4 @@
+from .torch_contour import Contour_to_mask, Contour_to_distance_map, Draw_contour, area, perimeter, hausdorff_distance, Sobel
+
+__all__ = ["Contour_to_mask","Contour_to_distance_map","Draw_contour" "area", "perimeter", "haussdorff_distance","Sobel"]
+__version__ = "1.1.1"
