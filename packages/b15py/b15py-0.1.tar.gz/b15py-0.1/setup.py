df437786c295495bcfedacfa8fa70aaec86cb6c8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-09 06:04:40 +0000

    Add package b15py/0.1/b15py-0.1.tar.gz

---
+++ packages/b15py/b15py-0.1.tar.gz/b15py-0.1/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+        name="b15py",
+        version="0.1",
+        pacakges=find_packages(),
+        install_requires=[
+            
+        ],
+    )
