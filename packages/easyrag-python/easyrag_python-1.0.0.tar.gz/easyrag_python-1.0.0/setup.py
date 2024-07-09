145dc89069f174baf5661beec3ecc1b3dd0e1d09
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-09 15:05:19 +0000

    Add package easyrag-python/1.0.0/easyrag_python-1.0.0.tar.gz

---
+++ packages/easyrag-python/easyrag_python-1.0.0.tar.gz/easyrag_python-1.0.0/setup.py
@@ -0,0 +1,30 @@
+from setuptools import setup, find_packages
+
+VERSION = "1.0.0"
+DESCRIPTION = 'EasyRAG Python Package'
+
+with open("README.md", "r", encoding="utf-8") as f:
+    long_description = f.read()
+
+setup(
+    name="easyrag-python",
+    author="Sayed Shaun",
+    version=VERSION,
+    packages=find_packages(),
+    description=DESCRIPTION,
+    install_requires=[
+        "langchain",
+        "langchain-community",
+        "langchain_google_genai",
+        "pypdf",
+        "torch",
+        "transformers",
+        "faiss-cpu",
+        "sentence-transformers",
+        "bitsandbytes",
+        "accelerate"
+    ],
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    python_requires=">=3.8",
+)
