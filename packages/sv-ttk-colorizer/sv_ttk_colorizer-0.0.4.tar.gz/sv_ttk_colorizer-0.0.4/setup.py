3e95afdce0d16375f281a303f84d201120bd907a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-09 06:04:52 +0000

    Add package sv-ttk-colorizer/0.0.4/sv_ttk_colorizer-0.0.4.tar.gz

---
+++ packages/sv-ttk-colorizer/sv_ttk_colorizer-0.0.4.tar.gz/sv_ttk_colorizer-0.0.4/setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup, find_packages
+
+setup(
+    name = "sv_ttk_colorizer",
+    version = "0.0.4",
+    packages = find_packages(),
+    install_requires = [
+        "darkdetect",
+        "Pillow",
+        "requests",
+        "pywinstyles",
+        "sv_ttk",
+        "appdirs"
+    ],
+    entry_points = {
+        "console_scripts": [
+            "sv_ttk_colorizer = sv_ttk_colorizer:main",
+            "sv_ttk_c = sv_ttk_colorizer:main"
+        ],
+    },
+)
