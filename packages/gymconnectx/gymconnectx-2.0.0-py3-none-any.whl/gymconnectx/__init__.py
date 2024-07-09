cccd2c8aac4021c9ced66ad81e37fa7351c405fe
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-09 18:04:37 +0000

    Add package gymconnectx/2.0.0/gymconnectx-2.0.0-py3-none-any.whl

---
+++ packages/gymconnectx/gymconnectx-2.0.0-py3-none-any.whl/gymconnectx/__init__.py
@@ -0,0 +1,27 @@
+# gymconnectx/__init__.py
+
+from gymnasium.envs.registration import register
+
+# Register the custom environment
+register(
+    id='gymconnectx/ConnectGameEnv',
+    entry_point='gymconnectx.envs:ConnectGameEnv',
+    kwargs={
+        'connect': 4,
+        'width': 7,
+        'height': 7,
+        'reward_winner': 1,
+        'reward_loser': -1,
+        'reward_living': 0,
+        'reward_draw': 0.5,
+        'reward_hell': -0.5,
+        'max_steps': 100,
+        'delay': 100,
+        'square_size': 100,
+        'avatar_player_1': None,
+        'avatar_player_2': None
+    }
+)
+
+from . import envs
+
