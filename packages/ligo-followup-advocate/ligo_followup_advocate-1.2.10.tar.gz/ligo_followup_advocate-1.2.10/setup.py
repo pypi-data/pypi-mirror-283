5e6849d2a3c35885272f0d2fc368cfecb3194ff8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-09 07:04:37 +0000

    Add package ligo-followup-advocate/1.2.10/ligo_followup_advocate-1.2.10.tar.gz

---
+++ packages/ligo-followup-advocate/ligo_followup_advocate-1.2.10.tar.gz/ligo_followup_advocate-1.2.10/setup.py
@@ -0,0 +1,19 @@
+import sys
+
+import versioneer
+from setuptools import setup
+
+
+def get_requirements(filename):
+    with open(filename, 'r') as f:
+        return [str(r) for r in f.readlines()]
+
+
+setup_requires = ['setuptools >= 30.3.0']
+if {'pytest', 'test', 'ptr'}.intersection(sys.argv):
+    setup_requires.append('pytest-runner')
+
+setup(install_requires=get_requirements('requirements.txt'),
+      setup_requires=setup_requires,
+      version=versioneer.get_version(),
+      cmdclass=versioneer.get_cmdclass())
