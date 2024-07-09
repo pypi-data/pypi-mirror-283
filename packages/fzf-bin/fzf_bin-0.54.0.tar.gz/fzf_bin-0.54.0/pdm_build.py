8db6d0618351ee3da1cd5cbaff37a4d94436ea72
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-09 04:05:26 +0000

    Add package fzf-bin/0.54.0/fzf_bin-0.54.0.tar.gz

---
+++ packages/fzf-bin/fzf_bin-0.54.0.tar.gz/fzf_bin-0.54.0/pdm_build.py
@@ -0,0 +1,61 @@
+from __future__ import annotations
+
+import os
+import shutil
+import subprocess
+import sys
+from pathlib import Path
+from typing import TYPE_CHECKING
+
+from wheel.cli.tags import tags
+
+if TYPE_CHECKING:
+    from pdm.backend.hooks import Context
+
+NAME = "fzf"
+VERSION = "0.54.0"
+
+
+def is_windows():
+    if "GOOS" in os.environ:
+        return os.environ["GOOS"] == "windows"
+    return sys.platform == "win32"
+
+
+def build(output: str) -> None:
+    go = shutil.which("go")
+    if go is None:
+        msg = "golang is required and 'go' should be in $PATH"
+        raise RuntimeError(msg)
+
+    args = [
+        go,
+        "build",
+        "-o",
+        output,
+        "-trimpath",
+        "-ldflags",
+        f"-s -w -X main.Version={VERSION}",
+        ".",
+    ]
+
+    submodule = Path(__file__).parent.joinpath(NAME)
+    subprocess.run(args, check=True, cwd=submodule)
+    Path(output).chmod(0o777)
+
+
+def pdm_build_hook_enabled(context: Context):
+    return context.target != "sdist"
+
+
+def pdm_build_initialize(context: Context) -> None:
+    context.ensure_build_dir()
+    output_path = Path(context.build_dir, "bin", NAME)
+    if is_windows():
+        output_path = output_path.with_suffix(".exe")
+    build(str(output_path))
+
+
+def pdm_build_finalize(context: Context, artifact: Path) -> None:
+    renamed = tags(str(artifact), python_tags="py3", abi_tags="none", remove=True)
+    print(renamed)
