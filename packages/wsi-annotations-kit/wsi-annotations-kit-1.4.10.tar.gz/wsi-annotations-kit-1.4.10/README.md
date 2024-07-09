01d6129aff1d3d5f4a5981479643b78f04c9a322
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-09 04:04:27 +0000

    Add package wsi-annotations-kit/1.4.10/wsi-annotations-kit-1.4.10.tar.gz

---
+++ packages/wsi-annotations-kit/wsi-annotations-kit-1.4.10.tar.gz/wsi-annotations-kit-1.4.10/README.md
@@ -0,0 +1,10 @@
+# wsi_annotations_kit
+Utility functions for generation, conversion, and saving annotation files for various whole slide image viewers
+
+Version 1.0.0: Initial release, annotations from shapely Polygons
+
+Version 1.0.1: Annotations generated from masks
+
+Version 1.0.2-->1.3.0: Various bug fixes, fixing conversions to other formats
+
+Version 1.4.0: Added AnnotationPatches object for merging annotations from tiles with varying levels of overlap
---
+++ packages/wsi-annotations-kit/wsi-annotations-kit-1.4.10.tar.gz/wsi-annotations-kit-1.4.10/setup.py
@@ -0,0 +1,30 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="wsi-annotations-kit",
+    version="1.4.10",
+    author="Sam Border",
+    author_email="sam.border2256@gmail.com",
+    description="Utility functions for generating, saving, and converting annotation files",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/spborder/wsi_annotations_kit",
+    install_requires=[
+        "lxml>=4.9.2",
+        "geojson>=3.0.1",
+        "shapely>=2.0.1",
+        "tqdm",
+        "numpy>=1.20.0",
+        "uuid",
+        "scikit-image"
+    ],
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
