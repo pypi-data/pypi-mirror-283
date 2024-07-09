cca1eaf5d8d14d67abb313f9e243b54fc7422331
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-09 06:04:52 +0000

    Add package gpt4all-cli/0.0.1/gpt4all_cli-0.0.1.tar.gz

---
+++ packages/gpt4all-cli/gpt4all_cli-0.0.1.tar.gz/gpt4all_cli-0.0.1/setup.py
@@ -0,0 +1,27 @@
+from os import environ
+from os.path import join
+from setuptools import setup, find_packages
+
+gpt4all = 'gpt4all-bindings' if 'NIX_PATH' in environ else 'gpt4all >= 2.7.0'
+
+setup(
+  name="gpt4all-cli",
+  zip_safe=False, # https://mypy.readthedocs.io/en/latest/installed_packages.html
+  version="0.0.1",
+  package_dir={'':'.'},
+  packages=find_packages(where='.'),
+  install_requires=[gpt4all, 'gnureadline', 'lark'],
+  scripts=[join('.','python','gpt4all-cli')],
+  python_requires='>=3.6',
+  author="Sergei Mironov",
+  author_email="sergei.v.mironov@proton.me",
+  description="Command-line interface using GPT4ALL bindings",
+  classifiers=[
+    "Programming Language :: Python :: 3",
+    "License :: OSI Approved :: Apache Software License",
+    "Operating System :: POSIX :: Linux",
+    "Topic :: Software Development :: Build Tools",
+    "Intended Audience :: Developers",
+    "Development Status :: 3 - Alpha",
+  ],
+)
