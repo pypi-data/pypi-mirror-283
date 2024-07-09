93c984c308797fc73e757cf30d694c28a0c4a786
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-09 14:04:43 +0000

    Add package vectorgebra/3.4.0/vectorgebra-3.4.0.tar.gz

---
+++ packages/vectorgebra/vectorgebra-3.4.0.tar.gz/vectorgebra-3.4.0/setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="vectorgebra",
+    version="3.4.0",
+    author="Ahmet Erdem",
+    description="A numerical methods tool for python, in python.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent"
+    ],
+    python_requires='>=3.9',
+    py_modules=["vectorgebra"],
+    install_requires=[]
+)
