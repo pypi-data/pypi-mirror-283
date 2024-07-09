bb1d7d2d6be1b95ac06dbb598d63709705090473
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-09 05:04:31 +0000

    Add package rfproto/0.0.10/rfproto-0.0.10.tar.gz

---
+++ packages/rfproto/rfproto-0.0.10.tar.gz/rfproto-0.0.10/rfproto/hello.py
@@ -0,0 +1,19 @@
+""" Example of simple math
+
+The module contains:
+- `add(a, b)` - Returns the sum of two numbers
+"""
+
+def add(a: float, b: float) -> float:
+    """ Compute the sum ($\Sigma$) of two numbers
+
+    $$ y = a + b $$
+
+    Args:
+        a: First number to add
+        b: Second number to add
+
+    Returns:
+        Arithmetic sum of `a` and `b`
+    """
+    return a + b
