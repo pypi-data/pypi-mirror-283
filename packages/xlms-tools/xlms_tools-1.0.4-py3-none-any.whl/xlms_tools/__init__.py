edb16b3f2e0f7506fef62dfaa88150f807508426
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-09 03:04:45 +0000

    Add package xlms-tools/1.0.4/xlms_tools-1.0.4-py3-none-any.whl

---
+++ packages/xlms-tools/xlms_tools-1.0.4-py3-none-any.whl/xlms_tools/__init__.py
@@ -0,0 +1,7 @@
+# This file is part of the xlms-tools package
+#
+# Copyright (c) 2023 - Topf Lab, Centre for Structural Systems Biology
+# Hamburg, Germany.
+#
+# This module was developed by:
+#   Karen Manalastas-Cantos    <karen.manalastas-cantos AT cssb-hamburg.de>
