f6b1f764793e04466fe6b6920ace1ebc6a850cb0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-09 05:04:31 +0000

    Add package superduperdb/0.2.1/superduperdb-0.2.1.tar.gz

---
+++ packages/superduperdb/superduperdb-0.2.1.tar.gz/superduperdb-0.2.1/superduperdb/ext/vllm/__init__.py
@@ -0,0 +1,8 @@
+from superduperdb.ext.vllm.model import VllmAPI, VllmModel
+from superduperdb.misc.annotations import requires_packages
+
+__all__ = ["VllmAPI", "VllmModel"]
+
+_, requirements = requires_packages(
+    ['vllm', None, None], ['ray', None, None], warn=True
+)
