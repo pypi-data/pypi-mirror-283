8a7352941d29894319bb9d21b749f14f26ba3724
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-09 04:06:40 +0000

    Add package pyspedas/1.5.16/pyspedas-1.5.16.tar.gz

---
+++ packages/pyspedas/pyspedas-1.5.16.tar.gz/pyspedas-1.5.16/pyspedas/goes/__init__.py
@@ -0,0 +1,17 @@
+from .load import load, loadr
+from .load_orbit import load_orbit
+from functools import partial
+
+# Define alias to the load function
+orbit = load_orbit
+fgm = partial(load, instrument="fgm")
+eps = partial(load, instrument="eps")
+epead = partial(load, instrument="epead")
+maged = partial(load, instrument="maged")
+magpd = partial(load, instrument="magpd")
+hepad = partial(load, instrument="hepad")
+xrs = partial(load, instrument="xrs")
+euvs = partial(load, instrument="euvs")
+mag = partial(load, instrument="mag")
+mpsh = partial(load, instrument="mpsh")
+sgps = partial(load, instrument="sgps")
