a3cbbec30d7968408566e418b3b8eaf673f9fc4e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-09 17:05:40 +0000

    Add package libpipe/0.4/libpipe-0.4-py3-none-any.whl

---
+++ packages/libpipe/libpipe-0.4-py3-none-any.whl/libpipe/msutils.py
@@ -0,0 +1,6 @@
+#
+# libpipe.msutils has been moved to nenucal.msutils
+#
+
+raise DeprecationWarning('The libpipe.msutils module is deprecated and replaced by nenucal.msutils'
+              'Please install nenucal-cd: https://gitlab.com/flomertens/nenucal-cd')
