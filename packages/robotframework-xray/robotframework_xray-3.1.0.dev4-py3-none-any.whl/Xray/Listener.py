e75dd6e3872bb7fba7427bde9b0f3fd8cd0774a8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-09 15:05:17 +0000

    Add package robotframework-xray/3.1.0.dev4/robotframework_xray-3.1.0.dev4-py3-none-any.whl

---
+++ packages/robotframework-xray/robotframework_xray-3.1.0.dev4-py3-none-any.whl/Xray/Listener.py
@@ -0,0 +1,50 @@
+import logging
+from robot.libraries.BuiltIn import BuiltIn
+from xray import Xray
+
+logger = logging.getLogger(__name__)
+
+
+class Listener:
+
+    ROBOT_LIBRARY_SCOPE = "GLOBAL"
+    ROBOT_LISTENER_API_VERSION = 3
+    XRAY = Xray()
+
+    def __init__(self):
+        self.ROBOT_LIBRARY_LISTENER = self
+
+    def start_suite(self, suite, result):
+        logger.warning(f"Suite '{suite.name}' starting with status {result.status}.")
+        self.XRAY.createTestExecution()
+
+    def start_test(self, test, result):
+        pass
+
+    def end_test(self, test, result):
+        pass
+
+    def end_suite(self, suite, result):
+        logger.warning(f"Suite '{suite.name}' ending with status {result.status}.")
+        pass
+
+    def log_message(self, message):
+        pass
+
+    def message(self, message):
+        pass
+
+    def debug_file(self, path):
+        pass
+
+    def output_file(self, path):
+        pass
+
+    def log_file(self, path):
+        pass
+
+    def report_file(self, path):
+        pass
+
+    def close(self):
+        print("Fim.")
