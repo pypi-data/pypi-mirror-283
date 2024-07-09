c9ed15d19aea4672dfc0dfceb1906fefb465666e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-09 15:05:05 +0000

    Add package aphyt/0.1.22/aphyt-0.1.22-py3-none-any.whl

---
+++ packages/aphyt/aphyt-0.1.22-py3-none-any.whl/aphyt/eip/interface_objects.py
@@ -0,0 +1,9 @@
+__author__ = 'Joseph Ryan'
+__license__ = "GPLv2"
+__maintainer__ = "Joseph Ryan"
+__email__ = "jr@aphyt.com"
+
+
+class TCPInterfaceObject(CIPDispatcher):
+    def __init__(self):
+        super().__init__()
---
+++ packages/aphyt/aphyt-0.1.22-py3-none-any.whl/aphyt/omron/omron_cip_objects.py
@@ -0,0 +1,11 @@
+__author__ = 'Joseph Ryan'
+__license__ = "GPLv2"
+__maintainer__ = "Joseph Ryan"
+__email__ = "jr@aphyt.com"
+
+
+class TCPIPInterfaceObject:
+    class_id = b'\xf5'
+
+    def __init__(self):
+        pass
