ab24916e6fbd1f2d52dfbc66461a771d6746b733
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-10 05:05:38 +0000

    Add package shipyard-coalesce/0.2.3a1/shipyard_coalesce-0.2.3a1.tar.gz

---
+++ packages/shipyard-coalesce/shipyard_coalesce-0.2.3a1.tar.gz/shipyard_coalesce-0.2.3a1/shipyard_coalesce/cli/authtest.py
@@ -0,0 +1,15 @@
+import os
+import sys
+from shipyard_coalesce import CoalesceClient
+
+
+def main():
+    client = CoalesceClient(
+        os.environ.get("COALESCE_ACCESS_TOKEN"), 
+        os.environ.get("COALESCE_REGION"))
+    
+    sys.exit(client.connect())
+
+
+if __name__ == "__main__":
+    main()
