1830143a228b04d8d828959f141fa0b8173c455d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-10 05:05:26 +0000

    Add package nb-sdk-test-v1/0.1.3/nb_sdk_test_v1-0.1.3.tar.gz

---
+++ packages/nb-sdk-test-v1/nb_sdk_test_v1-0.1.3.tar.gz/nb_sdk_test_v1-0.1.3/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup, find_packages
+
+setup(
+  name="nb_sdk_test_v1",
+  version="0.1.3",
+  packages=find_packages(),
+  install_requires=[
+    # List your package dependencies here
+  ],
+  author='Neural Bridge',
+  description="test",
+  license="Apache License 2.0",
+  classifiers=[
+    "Programming Language :: Python :: 3",
+    "License :: OSI Approved :: Apache Software License",
+    "Operating System :: OS Independent",
+  ],
+  python_requires=">=3.9",
+)
