7fb0c53b30609e390b4805da7328ce7b6f61f188
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-10 03:06:51 +0000

    Add package dyca/0.1.1/dyca-0.1.1-py3-none-any.whl

---
+++ packages/dyca/dyca-0.1.1-py3-none-any.whl/dyca/__init__.py
@@ -0,0 +1,10 @@
+"""
+dyca.
+
+Dynamical Component Analysis - A method to decompose multivariate signals
+"""
+
+__version__ = '0.1.0'
+__author__ = 'Annika Stiehl'
+__credits__ = 'University of Applied Sciences Ansbach'
+__license__ = 'GPL-3.0'
