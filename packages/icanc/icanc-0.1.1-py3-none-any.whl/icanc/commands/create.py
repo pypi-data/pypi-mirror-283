011a855a95a690d4b170b40cccb8a46e1c8bfd20
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-10 14:04:34 +0000

    Add package icanc/0.1.1/icanc-0.1.1-py3-none-any.whl

---
+++ packages/icanc/icanc-0.1.1-py3-none-any.whl/icanc/commands/create.py
@@ -0,0 +1,83 @@
+import click
+import os
+import shutil
+import subprocess
+import tomllib
+
+@click.group()
+def create():
+    """Create solutions or testcases."""
+
+    pass
+
+@create.command()
+@click.argument("judge", type=str)
+@click.argument("problem", type=str)
+@click.option("--template", type=click.Path(exists=True), required=True)
+@click.option("--solution", "solution_dst", default="solution", help="Name for the solution file.")
+@click.option("--open", "open_editor", is_flag=True, help="Open solution file on text editor.")
+def solution(**kwargs):
+    create_solution(**kwargs)
+
+def create_solution(judge, problem, template, solution_dst, open_editor):
+    with open(os.path.join(os.getcwd(), "icancrc.toml"), "rb") as f:
+        cfg = tomllib.load(f)
+    
+    dir = os.path.join(os.getcwd(), "problems", judge, problem)
+    os.makedirs(dir, exist_ok=True)
+
+    solution_path = os.path.join(dir, "{}.c".format(solution_dst))
+    shutil.copy2(template, solution_path)
+    
+    click.echo("Created blank solution: ./problems/{}/{}/{}.c".format(judge, problem, solution_dst))
+
+    if open_editor:
+        subprocess.run([cfg["editor"], solution_path])
+
+@create.command()
+@click.argument("judge", type=str)
+@click.argument("problem", type=str)
+@click.option("--open", "open_editor", is_flag=True, help="Open testcases file on text editor.")
+@click.option("--testcases", "testcases_dst", default="testcases", help="Name for the testcases file.")
+def testcases(**kwargs):
+    create_testcases(**kwargs)
+
+def create_testcases(judge, problem, testcases_dst, open_editor):
+    with open(os.path.join(os.getcwd(), "icancrc.toml"), "rb") as f:
+        cfg = tomllib.load(f)
+    
+    dir = os.path.join(os.getcwd(), "problems", judge, problem)
+    if not os.path.exists(dir):
+        click.echo("Problem {}/{} does not exist.".format(judge, problem), err=True)
+        exit(1)
+    os.makedirs(dir, exist_ok=True)
+
+    testcases_path = os.path.join(dir, "{}.toml".format(testcases_dst))
+    
+    with open(testcases_path, "w") as f:
+        f.writelines([
+            "# {}/{}\n\n".format(judge, problem),
+            "[case1]\n",
+            "in = \"\"\"\"\"\"\n",
+            "out = \"\"\"\"\"\"\n"
+        ])
+
+    click.echo("Created blank testcases: ./problems/{}/{}/{}.toml".format(judge, problem, testcases_dst))
+
+    if open_editor:
+        subprocess.run([cfg["editor"], testcases_path])
+
+@click.command()
+@click.argument("judge", type=str)
+@click.argument("problem", type=str)
+@click.option("--template", type=click.Path(exists=True), required=True)
+@click.option("--solution", "solution_dst", default="solution", help="Name for the solution file.")
+@click.option("--testcases", "testcases_dst", default="testcases", help="Name for the testcases file.")
+@click.option("--open", "open_editor", is_flag=True, help="Open solution file on text editor.")
+def scaffold(judge, problem, template, solution_dst, testcases_dst, open_editor):
+    """Create a solution and testcase files."""
+
+    click.echo("Scaffolding {}/{}\n".format(judge, problem))
+
+    create_solution(judge, problem, template, solution_dst, open_editor)
+    create_testcases(judge, problem, testcases_dst, False)
---
+++ packages/icanc/icanc-0.1.1-py3-none-any.whl/icanc/commands/test.py
@@ -0,0 +1,97 @@
+import click
+import os
+from .submit import handle_submit
+import time
+from .tools.builder import build
+from .tools.runner import run
+from .tools.writer import present_cases
+from watchdog.events import FileSystemEventHandler
+from watchdog.observers import Observer
+
+@click.command()
+@click.argument("judge", type=str)
+@click.argument("problem", type=str)
+@click.option("--solution", "solution_src", default="solution", help="Which solution to test.")
+@click.option("--testcases", "testcases_src", multiple=True, help="Which testcases to test against.")
+@click.option("--watch", is_flag=True, help="Rerun on changes to the problem or include directories.")
+def test(**kwargs):
+    """Test a solution against given testcases."""
+
+    handle_test(**kwargs)
+
+def handle_test(judge, problem, solution_src, testcases_src, watch):
+    solution_dir = os.path.join(os.getcwd(), "problems", judge, problem)
+    solution_path = os.path.join(solution_dir, f"{solution_src}.c")
+    submission_dir = os.path.join(os.getcwd(), "submissions", judge, problem)
+    submission_path = os.path.join(submission_dir, "{}.c".format(solution_src))
+    
+    include_dir = os.path.join(os.getcwd(), "include")
+    binary_dir = os.path.join(os.getcwd(), "binaries", judge, problem)
+    binary_path = os.path.join(binary_dir, solution_src)
+    os.makedirs(binary_dir, exist_ok=True)
+
+    testcases_dir = os.path.join(os.getcwd(), "problems", judge, problem) 
+
+    # click.clear() leaves uncleared lines if files change too fast.
+    def clear():
+        if os.name == "nt":
+            os.system("cls")
+        else:
+            os.system("clear")
+
+    def test():
+        clear()
+        if watch:
+            click.secho(" WATCH ", bg="cyan", fg="white", nl=False)
+        else:
+            click.secho(" RUN ", bg="cyan", fg="white", nl=False)
+        click.echo(f" ./problems/{judge}/{problem}/", nl=False)
+        click.secho(f"{solution_src}.c", bold=True)
+
+        # TODO Only rebuild the list of testcases when a file is created or deleted.
+        testcases = []
+        if testcases_src:
+            testcases.extend([f"{t}.toml" for t in testcases_src])
+        else:
+            files = os.listdir(testcases_dir)
+            testcases.extend([f for f in files if f.endswith(".toml")])
+
+        for testcase in testcases:
+            click.echo(f"\n< ./problems/{judge}/{problem}/{testcase}")
+
+            handle_submit(judge, problem, solution_src, False, False)
+            
+            result = build(solution_path, submission_path, binary_path)
+            if result != 0:
+                return
+
+            testcase_path = os.path.join(testcases_dir, testcase)
+            runs = run(binary_path, testcase_path)
+            present_cases(runs)
+
+        if watch:
+            click.echo("\nWatching for changes. ^C to stop.")
+
+    class FileWatcher(FileSystemEventHandler):
+        def __init__(self):
+            FileSystemEventHandler.__init__(self)
+
+        def on_modified(self, event):
+            clear()
+            test()
+
+    if watch:
+        watcher = FileWatcher()
+        observer = Observer()
+        observer.schedule(watcher, path=solution_dir, recursive=True)
+        observer.schedule(watcher, path=include_dir, recursive=True)
+        observer.start()
+
+        try:
+            test()
+            while True:
+                time.sleep(1)
+        except KeyboardInterrupt:
+            observer.stop()
+    else:
+        test()
