c77db8b1be1b9464fbdc363b7b83977101738012
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-10 07:04:57 +0000

    Add package recompi/2.0.20/recompi-2.0.20.tar.gz

---
+++ packages/recompi/recompi-2.0.20.tar.gz/recompi-2.0.20/recompi/__init__.py
@@ -0,0 +1,23 @@
+from .recompi import (
+    RecomPIFieldTypeError,
+    RecomPIException,
+    Tag,
+    Profile,
+    SecureProfile,
+    Location,
+    Geo,
+    RecomPIResponse,
+    RecomPI,
+)
+
+__all__ = [
+    "RecomPIFieldTypeError",
+    "RecomPIException",
+    "Tag",
+    "Profile",
+    "SecureProfile",
+    "Location",
+    "Geo",
+    "RecomPIResponse",
+    "RecomPI",
+]
