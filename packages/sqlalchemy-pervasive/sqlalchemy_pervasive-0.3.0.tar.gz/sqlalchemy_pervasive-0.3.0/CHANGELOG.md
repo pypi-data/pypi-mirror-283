eb004492174ff34c0dfe147021dd9fd9b76270d8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-10 07:04:59 +0000

    Add package sqlalchemy-pervasive/0.3.0/sqlalchemy_pervasive-0.3.0.tar.gz

---
+++ packages/sqlalchemy-pervasive/sqlalchemy_pervasive-0.3.0.tar.gz/sqlalchemy_pervasive-0.3.0/CHANGELOG.md
@@ -0,0 +1,46 @@
+
+# Changelog
+All notable changes to sqlalchemy-pervasive will be documented in this file.
+
+The format is based on [Keep a Changelog](http://keepachangelog.com/en/1.0.0/)
+and this project adheres to [Semantic Versioning](http://semver.org/spec/v2.0.0.html).
+
+## v0.3.0 (2024-07-09)
+
+### Feat
+
+- switch from setup.cfg to pyproject.toml + hatchling
+
+## 0.2.2 (2023-06-01)
+
+* Replace ``setup.py`` contents with ``setup.cfg``.
+
+
+## 0.2.1 (2021-03-03)
+
+* Update author / homepage metadata.
+
+
+## 0.2.0 (2021-03-03)
+
+* Officially declare "Python 3 only".
+
+
+## 0.1.3 (2018-02-24)
+
+* Replace ``execfile()`` usage per python 3.
+
+
+## 0.1.2
+
+* Misc. housekeeping.  Added manifest, transferred copyright, etc.
+
+
+## 0.1.1
+
+* Fixed boolean conditions in rendered SQL.
+
+
+## 0.1.0
+
+* Initial version.
---
+++ packages/sqlalchemy-pervasive/sqlalchemy_pervasive-0.3.0.tar.gz/sqlalchemy_pervasive-0.3.0/tasks.py
@@ -0,0 +1,42 @@
+# -*- coding: utf-8; -*-
+################################################################################
+#
+#  sqlalchemy-pervasive -- SQLAlchemy Dialect for Pervasive PSQL
+#  Copyright © 2013-2024 Sacramento Natural Foods Co-op, Inc
+#
+#  This file is part of sqlalchemy-pervasive.
+#
+#  sqlalchemy-pervasive is free software: you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by the
+#  Free Software Foundation, either version 3 of the License, or (at your
+#  option) any later version.
+#
+#  sqlalchemy-pervasive is distributed in the hope that it will be useful, but
+#  WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+#  or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for
+#  more details.
+#
+#  You should have received a copy of the GNU General Public License along with
+#  sqlalchemy-pervasive.  If not, see <http://www.gnu.org/licenses/>.
+#
+################################################################################
+
+import os
+import shutil
+
+from invoke import task
+
+
+@task
+def release(c):
+    """
+    Release a new version of 'sqlalchemy-pervasive'.
+    """
+    if os.path.exists('dist'):
+        shutil.rmtree('dist')
+    if os.path.exists('sqlalchemy_pervasive.egg-info'):
+        shutil.rmtree('sqlalchemy_pervasive.egg-info')
+
+    c.run('python -m build --sdist')
+
+    c.run('twine upload dist/*')
