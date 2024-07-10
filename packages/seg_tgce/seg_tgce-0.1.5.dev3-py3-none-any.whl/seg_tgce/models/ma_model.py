2fe7786fc50e12eec00ce382f8a7bace2b9c67f1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-10 03:05:28 +0000

    Add package seg_tgce/0.1.5.dev3/seg_tgce-0.1.5.dev3-py3-none-any.whl

---
+++ packages/seg_tgce/seg_tgce-0.1.5.dev3-py3-none-any.whl/seg_tgce/models/ma_model.py
@@ -0,0 +1,23 @@
+from keras.models import Model
+from tensorflow import GradientTape
+
+
+class ModelMultipleAnnotators(
+    Model
+):  # pylint: disable=abstract-method, too-few-public-methods
+    def train_step(self, data):
+        x, y = data
+
+        with GradientTape() as tape:
+            y_pred = self(x, training=True)
+            loss = self.compute_loss(y=y, y_pred=y_pred)
+
+        trainable_vars = self.trainable_variables
+        gradients = tape.gradient(loss, trainable_vars)
+        self.optimizer.apply_gradients(zip(gradients, trainable_vars))
+        for metric in self.metrics:
+            if metric.name == "loss":
+                metric.update_state(loss)
+            else:
+                metric.update_state(y, y_pred)
+        return {m.name: m.result() for m in self.metrics}
