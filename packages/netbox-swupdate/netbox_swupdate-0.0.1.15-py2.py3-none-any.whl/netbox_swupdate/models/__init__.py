fc28d2030460f831aa0dfa4848f22c8dfacaded8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-10 14:04:25 +0000

    Add package netbox-swupdate/0.0.1.15/netbox_swupdate-0.0.1.15-py2.py3-none-any.whl

---
+++ packages/netbox-swupdate/netbox_swupdate-0.0.1.15-py2.py3-none-any.whl/netbox_swupdate/models/__init__.py
@@ -0,0 +1,6 @@
+from .deploy import Deploy
+from .monitoring import Monitoring
+from .repository import Repository
+from .software import Software
+
+__all__ = ["Deploy", "Repository", "Software", "Monitoring"]
