e7fdddc4f6302cc5f8e5c211be4867dad0c063cd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-10 04:06:11 +0000

    Add package telempack/1.1.0/telempack-1.1.0-py3-none-any.whl

---
+++ packages/telempack/telempack-1.1.0-py3-none-any.whl/telempack/__init__.py
@@ -0,0 +1,10 @@
+# src/package_mchalitsios/__init__.py
+'''
+Package-level initialization so users can:
+from package_mchalitsios import Observer, Resource
+'''
+from .telemetry_kit import Observer, Resource
+
+__all__ = ["Observer", "Resource"]
+
+#TODO: Initialize logger, etc here.
