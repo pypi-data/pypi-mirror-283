41719a10d54298b64da245ef011f8c5dd73e4424
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-10 05:05:32 +0000

    Add package notification-schedule-library/0.2/notification_schedule_library-0.2.tar.gz

---
+++ packages/notification-schedule-library/notification_schedule_library-0.2.tar.gz/notification_schedule_library-0.2/notification_schedule_library/__init__.py
@@ -0,0 +1,38 @@
+import os
+import requests
+import json
+
+# Fetch API URLs and tokens from environment variables
+API_URL = os.getenv('NOTIFICATION_API_URL')
+ACCESS_TOKEN = os.getenv('NOTIFICATION_ACCESS_TOKEN')
+WEBHOOK_TOKEN = os.getenv('NOTIFICATION_WEBHOOK_TOKEN')
+
+
+def print_variables():
+    print(f"{API_URL}\n{ACCESS_TOKEN}\n{WEBHOOK_TOKEN}")
+
+def create_rule(data):
+    url = f"{API_URL}"
+    headers = {
+        'Content-Type': 'application/json',
+        'Authorization': f'Bearer {ACCESS_TOKEN}',
+    }
+    response = requests.post(url, data=json.dumps(data), headers=headers)
+    return response.json(), response.status_code
+
+def delete_rule(rule_id):
+    url = f"{API_URL}{rule_id}"
+    headers = {
+        'Authorization': f'Bearer {ACCESS_TOKEN}',
+    }
+    response = requests.delete(url, headers=headers)
+    return response.status_code
+
+def update_rule(rule_id, data):
+    url = f"{API_URL}{rule_id}"
+    headers = {
+        'Content-Type': 'application/json',
+        'Authorization': f'Bearer {ACCESS_TOKEN}',
+    }
+    response = requests.put(url, data=json.dumps(data), headers=headers)
+    return response.json(), response.status_code
