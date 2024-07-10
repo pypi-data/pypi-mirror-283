cb799b9783fdc39ce90697ad6d53f6fff1f7fe7d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-10 07:04:53 +0000

    Add package lingualens/0.7.0/lingualens-0.7.0-py3-none-any.whl

---
+++ packages/lingualens/lingualens-0.7.0-py3-none-any.whl/lingualens/__init__.py
@@ -0,0 +1,7 @@
+from .analyzer import TextAnalyzer
+from .metrics import Metrics
+from .pipelines import check_summary, check_paraphrase, check_similarity
+from .utils import ensure_model
+
+__version__ = "0.7.0"
+__all__ = ["TextAnalyzer", "Metrics", "check_summary", "check_paraphrase", "check_similarity", "ensure_model"]
