54de571bc6668121b021b591c4d3f83b5da59a9e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-10 04:06:23 +0000

    Add package DoubleRatchet/1.0.4/DoubleRatchet-1.0.4-py3-none-any.whl

---
+++ packages/DoubleRatchet/DoubleRatchet-1.0.4-py3-none-any.whl/doubleratchet/project.py
@@ -0,0 +1,13 @@
+__all__ = [ "project" ]  # pylint: disable=unused-variable
+
+project = {
+    "name"         : "DoubleRatchet",
+    "description"  : "A Python implementation of the Double Ratchet algorithm.",
+    "url"          : "https://github.com/Syndace/python-doubleratchet",
+    "year"         : "2022",
+    "author"       : "Tim Henkes (Syndace)",
+    "author_email" : "me@syndace.dev",
+    "categories"   : [
+        "Topic :: Security :: Cryptography"
+    ]
+}
---
+++ packages/DoubleRatchet/DoubleRatchet-1.0.4-py3-none-any.whl/doubleratchet/recommended/__init__.py
@@ -0,0 +1,10 @@
+from .crypto_provider import HashFunction
+
+
+# Fun:
+# https://github.com/PyCQA/pylint/issues/6006
+# https://github.com/python/mypy/issues/10198
+__all__ = [  # pylint: disable=unused-variable
+    # .crypto_provider
+    "HashFunction"
+]
