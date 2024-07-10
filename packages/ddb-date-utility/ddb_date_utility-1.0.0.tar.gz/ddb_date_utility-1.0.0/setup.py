166a52c4a5b6f5671bf680d424afcc54a4bcfceb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-10 14:04:45 +0000

    Add package ddb-date-utility/1.0.0/ddb_date_utility-1.0.0.tar.gz

---
+++ packages/ddb-date-utility/ddb_date_utility-1.0.0.tar.gz/ddb_date_utility-1.0.0/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="ddb_date_utility",
+    version="1.0.0",
+    description="Utility untuk menghitung perbedaan tanggal, tanggal hari ini, dan tanggal sebelum n hari",
+    author="Nama Anda",
+    packages=find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
