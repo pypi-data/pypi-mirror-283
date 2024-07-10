97ff12992161752308068a4dcb07dfdecaa81bec
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-10 06:05:09 +0000

    Add package HelloPyPI-Project/0.0.1/HelloPyPI_Project-0.0.1-py3-none-any.whl

---
+++ packages/HelloPyPI-Project/HelloPyPI_Project-0.0.1-py3-none-any.whl/TestLibrary/hello.py
@@ -0,0 +1,11 @@
+def introduce():
+    print("This is an example program to demonstrate to publish our own Python Package.")
+
+
+def main():
+    introduce()
+
+
+if __name__ == "__main__":
+    main()
+
