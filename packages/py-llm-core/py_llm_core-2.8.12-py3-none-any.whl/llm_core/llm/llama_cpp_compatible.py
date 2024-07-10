c39ed077588c2610f558f4523d479218b16dc53e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-10 03:06:03 +0000

    Add package py-llm-core/2.8.12/py_llm_core-2.8.12-py3-none-any.whl

---
+++ packages/py-llm-core/py_llm_core-2.8.12-py3-none-any.whl/llm_core/llm/llama_cpp_compatible.py
@@ -0,0 +1,186 @@
+# -*- coding: utf-8 -*-
+from pathlib import Path
+import platform
+import llama_cpp
+
+from dataclasses import dataclass
+
+from .base import (
+    LLMBase,
+    ChatCompletion,
+)
+from ..schema import to_grammar
+from ..settings import MODELS_CACHE_DIR
+
+
+@dataclass
+class LLaMACPPModel(LLMBase):
+    """
+    Model wrapper using the https://github.com/ggerganov/llama.cpp library
+
+    Not limited to LLaMA models. For the complete list of supported models,
+
+    see https://github.com/ggerganov/llama.cpp#readme
+    """
+
+    name: str = "mistral"
+    system_prompt: str = "You are a helpful assistant"
+    ctx_size: int = 8000
+    verbose: bool = False
+    llama_cpp_kwargs: dict = None
+
+    def __enter__(self):
+        self.load_model()
+        return self
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        self.release_model()
+
+    def release_model(self):
+        del self.model
+
+    def load_model(self):
+        if self.llama_cpp_kwargs is None:
+            self.llama_cpp_kwargs = {
+                "n_ctx": self.ctx_size,
+                "verbose": self.verbose,
+            }
+
+            if platform.system() == "Darwin" and platform.machine() == "arm64":
+                # Offload everything onto the GPU on MacOS
+                self.llama_cpp_kwargs["n_gpu_layers"] = 100
+                self.llama_cpp_kwargs["n_threads"] = 1
+
+        model_path = str(Path(MODELS_CACHE_DIR) / self.name)
+
+        self.model = llama_cpp.Llama(model_path, **self.llama_cpp_kwargs)
+
+    def ask(
+        self,
+        prompt,
+        history=None,
+        schema=None,
+        temperature=0,
+        **llama_cpp_kwargs,
+    ):
+        self.sanitize_prompt(prompt=prompt, history=history, schema=schema)
+
+        messages = [
+            {
+                "role": "system",
+                "content": self.system_prompt,
+            },
+        ]
+        if history:
+            messages += history
+
+        messages.append(
+            {
+                "role": "user",
+                "content": prompt,
+            },
+        )
+
+        kwargs = {}
+        if schema:
+            grammar = to_grammar(schema)
+            kwargs = {"grammar": grammar}
+
+        # Allow to call `ask` and free up memory immediately
+        model = getattr(self, "model", None)
+
+        if model:
+            completion = model.create_chat_completion(
+                messages=messages,
+                temperature=temperature,
+                max_tokens=-1,
+                **kwargs,
+            )
+        else:
+            with self as wrapper:
+                completion = wrapper.model.create_chat_completion(
+                    messages=messages,
+                    temperature=temperature,
+                    **kwargs,
+                )
+
+        return ChatCompletion.parse(completion)
+
+
+@dataclass
+class LLaVACPPModel(LLaMACPPModel):
+    def load_model(self):
+        if self.llama_cpp_kwargs is None:
+            self.llama_cpp_kwargs = {
+                "n_ctx": self.ctx_size,
+                "verbose": self.verbose,
+            }
+
+            if platform.system() == "Darwin" and platform.machine() == "arm64":
+                # Offload everything onto the GPU on MacOS
+                self.llama_cpp_kwargs["n_gpu_layers"] = 100
+                self.llama_cpp_kwargs["n_threads"] = 1
+
+        model_path = str(Path(MODELS_CACHE_DIR) / self.name)
+        chat_format = "llava-1-5"
+        chat_handler = llama_cpp.llama_chat_format.Llava15ChatHandler(
+            clip_model_path=self.llama_cpp_kwargs["clip_model_path"]
+        )
+
+        self.model = llama_cpp.Llama(
+            model_path,
+            chat_format=chat_format,
+            chat_handler=chat_handler,
+            **self.llama_cpp_kwargs,
+        )
+
+    def ask(
+        self,
+        prompt,
+        history=None,
+        schema=None,
+        temperature=0,
+        **llama_cpp_kwargs,
+    ):
+        self.sanitize_prompt(prompt=prompt, history=history, schema=schema)
+
+        messages = [
+            {
+                "role": "system",
+                "content": self.system_prompt,
+            },
+        ]
+        if history:
+            messages += history
+
+        messages.append(
+            {
+                "role": "user",
+                "content": [{"type": "text", "text": prompt}],
+            },
+        )
+
+        kwargs = {}
+        if schema:
+            grammar = to_grammar(schema)
+            kwargs = {"grammar": grammar}
+
+        # Allow to call `ask` and free up memory immediately
+        model = getattr(self, "model", None)
+
+        if model:
+            completion = model.create_chat_completion(
+                messages=messages,
+                temperature=temperature,
+                max_tokens=-1,
+                **kwargs,
+            )
+        else:
+            with self as wrapper:
+                completion = wrapper.model.create_chat_completion(
+                    messages=messages,
+                    temperature=temperature,
+                    **kwargs,
+                )
+
+        return ChatCompletion.parse(completion)
---
+++ packages/py-llm-core/py_llm_core-2.8.12-py3-none-any.whl/llm_core/llm/openai.py
@@ -0,0 +1,140 @@
+# -*- coding: utf-8 -*-
+import httpx
+from openai import AzureOpenAI, OpenAI
+
+from dataclasses import dataclass
+
+from .base import (
+    LLMBase,
+    ChatCompletion,
+)
+
+from ..settings import (
+    USE_AZURE_OPENAI,
+    AZURE_OPENAI_ENDPOINT,
+    DEFAULT_TIMEOUT,
+)
+
+
+def create_chat_completion(
+    model, messages, temperature, max_tokens=1000, **kwargs
+):
+    default_timeout = httpx.Timeout(DEFAULT_TIMEOUT, write=10.0, connect=2.0)
+
+    if USE_AZURE_OPENAI:
+        # gets the API Key from environment variable AZURE_OPENAI_API_KEY
+        client = AzureOpenAI(
+            api_version="2024-02-01",
+            azure_endpoint=AZURE_OPENAI_ENDPOINT,
+            timeout=default_timeout,
+        )
+
+        # Here we are using the following convention:
+        # Every model name is mapped to an Azure deployment where we remove the
+        # dot sign.
+        model_name = model.replace(".", "")
+    else:
+        client = OpenAI(timeout=default_timeout)
+        model_name = model
+
+    completion = client.chat.completions.create(
+        model=model_name,
+        messages=messages,
+        temperature=temperature,
+        max_tokens=max_tokens,
+        **kwargs,
+    )
+
+    return completion
+
+
+@dataclass
+class OpenAIChatModel(LLMBase):
+    name: str = "gpt-3.5-turbo"
+    system_prompt: str = "You are a helpful assistant"
+
+    def __enter__(self):
+        # No special initialization required as we are using API
+        return self
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        # No special cleanup required as we are using API
+        pass
+
+    @property
+    def ctx_size(self):
+        if self.name in ("gpt-3.5-turbo", "gpt-3.5-turbo-0613"):
+            return 4_000
+        elif self.name in ("gpt-3.5-turbo-16k", "gpt-3.5-turbo-16k-0613"):
+            return 16_000
+        elif self.name in ("gpt-4", "gpt-4-0613"):
+            return 8_000
+        elif self.name == "gpt-4-32k":
+            return 32_000
+        elif self.name in ("gpt-4-1106-preview", "gpt-4o-2024-05-13"):
+            return 128_000
+        else:
+            raise KeyError("Unsupported model")
+
+    def ask(
+        self,
+        prompt,
+        history=None,
+        schema=None,
+        temperature=0,
+    ):
+        max_tokens = self.sanitize_prompt(
+            prompt=prompt,
+            history=history,
+            schema=schema,
+        )
+
+        #: Reduce by 10 percent the maximum tokens to be generated to take into
+        #: account inaccuracies of sanitize_prompt (especially the schema token
+        #: consumption)
+
+        #: this needs refactoring
+        if self.name == "gpt-4o-2024-05-13":
+            max_tokens = 4_000
+        else:
+            max_tokens = int(0.9 * max_tokens)
+
+        messages = [
+            {
+                "role": "system",
+                "content": self.system_prompt,
+            },
+        ]
+        if history:
+            messages += history
+
+        messages.append(
+            {
+                "role": "user",
+                "content": prompt,
+            },
+        )
+
+        kwargs = {}
+        if schema:
+            functions = {
+                "name": "PublishAnswer",
+                "description": "Publish the answer",
+                "parameters": schema,
+            }
+            function_call = {"name": "PublishAnswer"}
+
+            kwargs = {
+                "functions": [functions],
+                "function_call": function_call,
+            }
+
+        completion = create_chat_completion(
+            model=self.name,
+            messages=messages,
+            temperature=temperature,
+            max_tokens=max_tokens,
+            **kwargs,
+        )
+
+        return ChatCompletion.parse(completion.dict())
