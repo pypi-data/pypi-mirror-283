cd0981c27614ae6ca98364920386f980b6759592
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-10 04:06:26 +0000

    Add package senpi/0.1.1/senpi-0.1.1.tar.gz

---
+++ packages/senpi/senpi-0.1.1.tar.gz/senpi-0.1.1/setup.py
@@ -0,0 +1,40 @@
+# Always prefer setuptools over distutils
+from setuptools import setup, find_packages
+
+# To use a consistent encoding
+from codecs import open
+from os import path
+
+# The directory containing this file
+HERE = path.abspath(path.dirname(__file__))
+
+# Get the long description from the README file
+with open(path.join(HERE, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+
+# This call to setup() does all the work
+setup(
+    name="senpi",
+    version="0.1.1",
+    description="Library to facilitate the processing of and deep learning with event-based computer vision data.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://senpi.readthedocs.io/",
+    author="Adrish Kar",
+    author_email="example@email.com",
+    license="MIT",
+    classifiers=[
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Operating System :: OS Independent"
+    ],
+    packages=["senpi"],
+    include_package_data=True,
+    install_requires=["kornia", "matplotlib", "numpy", "opencv_python", "pandas", "Pillow", "seaborn", "torch", "tqdm"]
+)
