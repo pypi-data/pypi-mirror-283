375c021214839a1ea86377d250a1b167061eba06
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-10 06:04:47 +0000

    Add package agentsim/0.0.2/agentsim-0.0.2.tar.gz

---
+++ packages/agentsim/agentsim-0.0.2.tar.gz/agentsim-0.0.2/src/agentsim/evaluators/toxicity_score_llm.py
@@ -0,0 +1,13 @@
+from agentsim.utils import llm_eval_call, check_guardrail
+
+def toxicity_score_llm(messages):
+    
+    # get the toxicity score
+    score = llm_eval_call({
+        'messages': str(messages)
+    })['toxicity_score']
+    
+    # check if the score is in the target range
+    result = check_guardrail(score)
+    
+    return score, result
