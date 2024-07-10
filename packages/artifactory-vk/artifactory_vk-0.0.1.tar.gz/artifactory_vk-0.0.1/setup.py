d016e87e9cf83d79d30c67be23cbfda73df4f9c3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-10 04:06:20 +0000

    Add package artifactory-vk/0.0.1/artifactory_vk-0.0.1.tar.gz

---
+++ packages/artifactory-vk/artifactory_vk-0.0.1.tar.gz/artifactory_vk-0.0.1/setup.py
@@ -0,0 +1,13 @@
+from setuptools import find_packages, setup
+
+
+setup(
+    name='artifactory-vk',
+    version='0.0.1',
+    packages=find_packages(),
+    install_requires=[
+        'pydantic~=2.8.2',
+        'requests~=2.32.3',
+        'tuspy~=1.0.3',
+    ],
+)
