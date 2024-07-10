cbaef8103dcc847dae7f3c074e373c03eb29d42e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-10 06:04:45 +0000

    Add package TikTokLive/6.0.7/TikTokLive-6.0.7-py3-none-any.whl

---
+++ packages/TikTokLive/TikTokLive-6.0.7-py3-none-any.whl/TikTokLive/client/web/routes/__init__.py
@@ -0,0 +1,7 @@
+from .fetch_gift_list import GiftListRoute
+from .fetch_image import ImageFetchRoute
+from .fetch_room_id_html import RoomIdHTMLRoute
+from .fetch_room_info import FetchRoomInfoRoute
+from .fetch_sign import SignFetchRoute
+from .fetch_video import VideoFetchRoute
+from .fetch_is_live import FetchIsLiveRoute
