3377210d94c923d4fb76f9a1d6e416f37ea7ad99
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-10 14:04:29 +0000

    Add package Conqace/0.2.0/conqace-0.2.0-py3-none-any.whl

---
+++ packages/Conqace/conqace-0.2.0-py3-none-any.whl/source/Conqace/__init__.py
@@ -0,0 +1,14 @@
+"""
+Easily update your computer.
+"""
+
+
+__title__ = "Conqace"
+__description__ = "Easily update your computer."
+__url__ = "https://tabbyslimeking.xyz/conqace"
+
+__author__ = "Kasen Engel"
+__email__ = "kasen.engel@gmail.com"
+
+__license__ = "GPL 3.0"
+__copyright__ = f"Copyright (c) 2024 {__author__}"
