98dca1494fbf6b2f6f8f44a4ce1d65781c8dcae4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-10 14:04:28 +0000

    Add package pysyncrosim/1.1.1/pysyncrosim-1.1.1-py3-none-any.whl

---
+++ packages/pysyncrosim/pysyncrosim-1.1.1-py3-none-any.whl/pysyncrosim/__init__.py
@@ -0,0 +1,17 @@
+from pysyncrosim.session import Session
+from pysyncrosim.library import Library
+from pysyncrosim.project import Project
+from pysyncrosim.scenario import Scenario
+from pysyncrosim.raster import Raster
+from pysyncrosim.folder import Folder
+from pysyncrosim.environment import runtime_input_folder
+from pysyncrosim.environment import runtime_output_folder
+from pysyncrosim.environment import runtime_temp_folder
+from pysyncrosim.environment import progress_bar
+from pysyncrosim.helper import library
+
+__version__ = "unknown"
+try:  
+    from pysyncrosim._version import __version__
+except ImportError:
+    pass
