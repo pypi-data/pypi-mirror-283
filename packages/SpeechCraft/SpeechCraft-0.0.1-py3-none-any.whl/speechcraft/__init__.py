d3cdf39f254d393e0cc7a06393287f694ebd4609
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-10 04:06:29 +0000

    Add package SpeechCraft/0.0.1/SpeechCraft-0.0.1-py3-none-any.whl

---
+++ packages/SpeechCraft/SpeechCraft-0.0.1-py3-none-any.whl/speechcraft/__init__.py
@@ -0,0 +1,5 @@
+from speechcraft.text2voice import text2voice, text2voice_advanced
+from speechcraft.voice2voice import voice2voice
+from speechcraft.voice_cloning.voice2embedding import voice2embedding
+from speechcraft.core.voice_embedding import VoiceEmbedding
+from speechcraft.server import start_server
