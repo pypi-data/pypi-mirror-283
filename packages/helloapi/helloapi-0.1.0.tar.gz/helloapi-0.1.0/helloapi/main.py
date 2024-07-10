6b5e8899efd436450856bdb65be73ab974739b56
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-10 06:04:54 +0000

    Add package helloapi/0.1.0/helloapi-0.1.0.tar.gz

---
+++ packages/helloapi/helloapi-0.1.0.tar.gz/helloapi-0.1.0/helloapi/main.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+# helloapi/main.py
+from fastapi import FastAPI
+
+app = FastAPI()
+
+@app.get("/")
+def read_root():
+    return {"Hello": "World"}
+
+if __name__ == "__main__":
+    import uvicorn
+    uvicorn.run(app, host="0.0.0.0", port=8000)
