0b89fc4d1645556ff7cb3e85bcb60f15495140bc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-10 03:05:25 +0000

    Add package linkahead/0.15.0/linkahead-0.15.0-py3-none-any.whl

---
+++ packages/linkahead/linkahead-0.15.0-py3-none-any.whl/linkahead/version.py
@@ -0,0 +1,11 @@
+
+# THIS FILE IS GENERATED FROM linkahead SETUP.PY
+#
+short_version = '0.15.0'
+version = '0.15.0'
+full_version = '0.15.0'
+git_revision = '0124d2ce899d6b9870b65639c6dbe71ab7cfc39d'
+release = True
+
+if not release:
+    version = full_version
