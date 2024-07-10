cc2c273c1fed3fb3ba32cf9c0be11db99a08c89d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-10 03:05:55 +0000

    Add package bfcm/0.1/bfcm-0.1.tar.gz

---
+++ packages/bfcm/bfcm-0.1.tar.gz/bfcm-0.1/setup.py
@@ -0,0 +1,12 @@
+import setuptools
+
+setuptools.setup(
+    name='bfcm',
+    version='0.1',
+    author='Isel Grau, Gonzalo Nápoles, Leonardo Concepción, Fabian Hoitsma, Lisa Koutsoviti, João Paulo Papa, Koen Vanhoof',
+    author_email='lisa.koutsoviti@uhasselt.be',
+    description='A library that quantifies implicit bias in tabular data with Fuzzy Cognitive Maps',
+    long_description='This python package includes the functions related to the papers "Modeling implicit bias with fuzzy cognitive maps" (Nápoles et al., 2022) and "Measuring Implicit Bias Using SHAP Feature Importance and Fuzzy Cognitive Maps" (Grau et al., 2023). An additional method included is using clustering to automatically discover the groups describing a numeric feature, which allows computing the association between numeric and categorical features using Cramer\'s V coefficient. \nThe following people were involved in creating this package: Isel Grau, Gonzalo Nápoles, Leonardo Concepción, Fabian Hoitsma, Lisa Koutsoviti, João Paulo Papa, Koen Vanhoof',
+    packages=['bfcm'],
+    license='MIT'
+)
