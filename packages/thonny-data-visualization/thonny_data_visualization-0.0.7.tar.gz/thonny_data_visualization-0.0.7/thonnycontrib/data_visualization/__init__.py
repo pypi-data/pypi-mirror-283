8f8da48c98572fa5b3663aece4f177b487351583
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-10 03:07:13 +0000

    Add package thonny-data-visualization/0.0.7/thonny_data_visualization-0.0.7.tar.gz

---
+++ packages/thonny-data-visualization/thonny_data_visualization-0.0.7.tar.gz/thonny_data_visualization-0.0.7/thonnycontrib/data_visualization/__init__.py
@@ -0,0 +1,13 @@
+# package marker
+from thonny import get_workbench
+from thonny.languages import tr
+from hierarchic_view import HierarchicView
+from Network_view import NetworkXView
+
+'''Premet de charger les plug-ins au lancement de Thonny'''
+
+def load_plugin() -> None:
+    get_workbench().add_view(HierarchicView, tr("Hierarchic view"), "s")
+
+def load_plugin() -> None:
+    get_workbench().add_view(NetworkXView, tr("NetworkX view"), "s")
