8ca07bf2bdc9001538f7c6927c72d82ee5c65aed
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-10 07:05:07 +0000

    Add package alertbox/0.0.1/alertbox-0.0.1.tar.gz

---
+++ packages/alertbox/alertbox-0.0.1.tar.gz/alertbox-0.0.1/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup, find_packages
+from os import path
+working_directory = path.abspath(path.dirname(__file__))
+
+with open(path.join(working_directory, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+
+setup(
+    name='alertbox', 
+    version='0.0.1',
+    url='https://github.com/HajMasterX/alertbox',
+    author='HajMasterX',
+    author_email='masterxq22@gmail.com',
+    description='Simple MessageBox Builder',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    packages=find_packages(),
+    install_requires=['os'],
+)
