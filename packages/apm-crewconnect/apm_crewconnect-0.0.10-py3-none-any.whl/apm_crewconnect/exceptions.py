6a354c580a2d0f1d39086f84e2b94a20299db136
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-10 07:05:02 +0000

    Add package apm-crewconnect/0.0.10/apm_crewconnect-0.0.10-py3-none-any.whl

---
+++ packages/apm-crewconnect/apm_crewconnect-0.0.10-py3-none-any.whl/apm_crewconnect/exceptions.py
@@ -0,0 +1,18 @@
+class ApmClientException(Exception):
+    pass
+
+
+class OktaClientException(Exception):
+    pass
+
+
+class InvalidAuthRedirectException(OktaClientException):
+    pass
+
+
+class InvalidTokenException(ApmClientException):
+    pass
+
+
+class UnhandledAircraftTypeException(Exception):
+    pass
