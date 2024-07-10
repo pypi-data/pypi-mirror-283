f70e91fcc85462e92b2657cfac4b84d851b6b748
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-10 06:04:45 +0000

    Add package pyside-app-core/0.5.3/pyside_app_core-0.5.3-py3-none-any.whl

---
+++ packages/pyside-app-core/pyside_app_core-0.5.3-py3-none-any.whl/pyside_app_core/ui/widgets/connection_manager.py
@@ -0,0 +1,160 @@
+from typing import Protocol, cast
+
+from PySide6.QtCore import QSize, Qt, QTimer, Signal, Slot
+from PySide6.QtSerialPort import QSerialPort, QSerialPortInfo
+from PySide6.QtWidgets import QComboBox, QHBoxLayout, QPushButton, QVBoxLayout, QWidget
+
+from pyside_app_core import log
+from pyside_app_core.ui.widgets.core_icon import CoreIcon
+from pyside_app_core.ui.widgets.settings_mixin import SettingsMixin
+from pyside_app_core.utils.time_ms import SECONDS
+
+
+class DisplayNameMapper(Protocol):
+    def __call__(self, port_info: QSerialPortInfo) -> str: ...
+
+
+def _default_display_name_mapper(port_info: QSerialPortInfo) -> str:
+    return port_info.portName()
+
+
+class ConnectionManager(SettingsMixin, QWidget):
+    refresh_ports = Signal()
+    request_connect = Signal(QSerialPortInfo)
+    request_disconnect = Signal()
+
+    SIZE = QSize(30, 30)
+    ICON_SIZE = QSize(20, 20)
+
+    def __init__(
+        self,
+        *,
+        display_name_mapper: DisplayNameMapper = _default_display_name_mapper,
+        remember_last_connection: bool = False,
+        parent: QWidget | None = None,
+    ):
+        super().__init__(parent=parent)
+
+        self._display_name_mapper = display_name_mapper
+        self._remember_last_connection = remember_last_connection
+
+        _ly = QVBoxLayout()
+        _ly.setSpacing(0)
+        _ly.setContentsMargins(5, 5, 5, 5)
+        self.setLayout(_ly)
+
+        _ly_list = QHBoxLayout()
+        _ly_list.setSpacing(5)
+        _ly.addLayout(_ly_list)
+
+        _ly_actions = QHBoxLayout()
+        _ly.addLayout(_ly_actions)
+
+        # -------
+        # refresh button
+        self._refresh_btn = QPushButton(
+            icon=CoreIcon(":/core/iconoir/refresh-circle.svg"),
+            text="",
+            parent=self,
+        )
+        self._refresh_btn.setFixedSize(self.SIZE)
+        self._refresh_btn.setIconSize(self.ICON_SIZE)
+        self._refresh_btn.setContentsMargins(0, 0, 0, 0)
+        self._refresh_btn.setToolTip("Refresh Device List")
+        _ly_list.addWidget(self._refresh_btn)
+
+        # -------
+        # port selection list
+        self._port_list = QComboBox(self)
+        self._port_list.setPlaceholderText("Choose A Device")
+        self._port_list.setFixedHeight(self.SIZE.height())
+        _ly_list.addWidget(self._port_list, stretch=9)
+
+        # --------
+        # connect/disconnect button
+        self._connect_btn = QPushButton(
+            icon=CoreIcon(
+                ":/core/iconoir/ev-plug-charging.svg",
+                ":/core/iconoir/ev-plug-xmark.svg",
+            ),
+            text="",
+            parent=self,
+        )
+        self._connect_btn.setCheckable(True)
+        self._connect_btn.setFixedSize(self.SIZE)
+        self._connect_btn.setIconSize(self.ICON_SIZE)
+        self._connect_btn.setContentsMargins(0, 0, 0, 0)
+        self._connect_btn.setToolTip("Refresh Device List")
+        _ly_list.addWidget(self._connect_btn)
+
+        # ------------------------------------------------------------------------------
+        # signals
+        self._refresh_btn.clicked.connect(self.request_port_refresh)
+        self._port_list.currentIndexChanged.connect(self._on_port_selected)
+
+    @property
+    def current_port(self) -> QSerialPortInfo | None:
+        return cast(
+            QSerialPortInfo | None,
+            self._port_list.currentData(Qt.ItemDataRole.UserRole),
+        )
+
+    @Slot()
+    def handle_serial_connect(self, com: QSerialPort) -> None:
+        log.debug(f"Connected to serial port {com.portName()}")
+
+    @Slot()
+    def handle_serial_disconnect(self) -> None:
+        log.debug("Com port disconnected")
+        self.request_port_refresh()
+
+    @Slot()
+    def handle_serial_ports(self, ports: list[QSerialPortInfo]) -> None:
+        self.setEnabled(True)
+        self._port_list.clear()
+
+        for port in ports:
+            log.debug(f"Adding port {port.systemLocation()}")
+
+            self._port_list.addItem(
+                self._display_name_mapper(port),
+                port,
+            )
+
+    @Slot()
+    def handle_serial_data(self, data: object) -> None:
+        log.debug(f"Received data: {data}")
+
+    @Slot()
+    def handle_serial_error(self, error: Exception) -> None:
+        pass
+
+    def request_port_refresh(self) -> None:
+        log.debug("Requesting Port Refresh")
+        self.setDisabled(True)
+        QTimer.singleShot(1 * SECONDS, self.refresh_ports.emit)
+
+    def _on_port_selected(self, index: int) -> None:
+        port = self._port_list.itemData(index, Qt.ItemDataRole.UserRole)
+        log.debug(f"Port Selected: {port}")
+        self.request_connect.emit(port)
+
+    def _format_port_name(self, port: QSerialPortInfo) -> str:
+        name: str = port.portName()
+        extra = ""
+        if mfc := port.manufacturer():
+            prod_id = port.productIdentifier()
+            extra = f"{mfc}/{prod_id}" if prod_id else mfc
+
+        return f"{name} ({extra})" if extra else name
+
+    def _store_state(self) -> None:
+        if not self._remember_last_connection:
+            return
+
+        if self.current_port and self.current_port.serialNumber():
+            self._settings.setValue("last_port_serial", self.current_port.serialNumber())
+
+    def _restore_state(self) -> None:
+        if not self._remember_last_connection:
+            return None
---
+++ packages/pyside-app-core/pyside_app_core-0.5.3-py3-none-any.whl/pyside_app_core/ui/widgets/settings_mixin.py
@@ -0,0 +1,36 @@
+from typing import cast
+
+from PySide6.QtCore import QCoreApplication, QObject, QSettings
+from PySide6.QtGui import QShowEvent
+from PySide6.QtWidgets import QWidget
+
+from pyside_app_core.ui.application_service import AppMetadata
+
+
+class SettingsMixin:
+    def __init__(self, parent: QObject | None = None, *args: object, **kwargs: object):
+        super().__init__(*args, **kwargs)
+
+        self._settings = QSettings(
+            AppMetadata.id,
+            AppMetadata.name,
+            parent,
+        )
+
+        self._restored = False
+
+        cast(QCoreApplication, QCoreApplication.instance()).aboutToQuit.connect(self._store_state)
+
+    def showEvent(self, event: QShowEvent) -> None:  # noqa: N802
+        if not self._restored:
+            self._restore_state()
+            self._restored = True
+
+        if isinstance(self, QWidget):
+            cast(QWidget, super()).showEvent(event)
+
+    def _restore_state(self) -> None:
+        pass
+
+    def _store_state(self) -> None:
+        pass
