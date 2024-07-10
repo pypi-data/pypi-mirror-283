a036f1f0c12ac129df9ba99198616e6d4cdf33a4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-10 03:07:03 +0000

    Add package aiokdb/0.1.7/aiokdb-0.1.7.tar.gz

---
+++ packages/aiokdb/aiokdb-0.1.7.tar.gz/aiokdb-0.1.7/aiokdb/extras.py
@@ -0,0 +1,37 @@
+import uuid
+
+from aiokdb import KIntArray, KLongArray, KObj, TypeEnum, ktn, tn
+
+# these are python friendly constructors for vector types, incomplete
+# not sure if there is a better way
+
+
+def ktni(t: TypeEnum, *ints: int) -> KObj:
+    v = ktn(t)
+    if t == TypeEnum.KG:
+        v.kG().extend(ints)
+    elif t == TypeEnum.KH:
+        v.kH().extend(ints)
+    elif isinstance(v, KIntArray):
+        v.kI().extend(ints)
+    elif isinstance(v, KLongArray):
+        v.kJ().extend(ints)
+    else:
+        raise ValueError(f"No int array initialiser for {tn(t)}")
+    return v
+
+
+def ktns(t: TypeEnum, *ss: str) -> KObj:
+    v = ktn(t)
+    if t == TypeEnum.KS:
+        for s in ss:
+            v.appendS(s)
+    else:
+        raise ValueError(f"No str array initialiser for {tn(t)}")
+    return v
+
+
+def ktnu(*uuids: uuid.UUID) -> KObj:
+    v = ktn(TypeEnum.UU)
+    v.kU().extend(uuids)
+    return v
