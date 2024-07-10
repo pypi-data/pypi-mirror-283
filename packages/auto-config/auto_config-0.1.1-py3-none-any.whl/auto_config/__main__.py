a67f6ef6db3f3d85f605b07867fbb682289b3516
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-10 04:04:51 +0000

    Add package auto-config/0.1.1/auto_config-0.1.1-py3-none-any.whl

---
+++ packages/auto-config/auto_config-0.1.1-py3-none-any.whl/auto_config/__main__.py
@@ -0,0 +1,21 @@
+from __future__ import annotations
+
+from sys import stdout
+
+import typer
+from loguru import logger
+
+from auto_config import utils
+
+app = typer.Typer()
+
+
+@app.command()
+def generate_config(path: str = typer.Argument("~/.config/autoconfig/config.toml"), *, log_level="INFO"):
+    logger.remove()
+    logger.add(stdout, level=log_level)
+    utils.generate_config(path)
+
+
+if __name__ == "__main__":
+    app()
---
+++ packages/auto-config/auto_config-0.1.1-py3-none-any.whl/auto_config/generator/dns_config.py
@@ -0,0 +1,25 @@
+from __future__ import annotations
+
+import json
+from collections.abc import Sequence
+
+from ..device import Device
+from ..field import DefaultExtraField
+from .base import GeneratorBase
+
+
+class DNSConfigGenerator(GeneratorBase):
+    def __init__(self, devices: Sequence[Device[DefaultExtraField]]):
+        super().__init__()
+        self.devices = devices
+
+    def generate(self):
+        record_list = []
+        for device in self.devices:
+            name = device.get_domain()
+            target = device.extra.dns.public if device.extra.dns is not None else "unknown"
+            if device.extra.ansible is not None and device.extra.ansible.server:
+                record_list.append((device.group, "{:}.bone6.top".format(name)))
+            record_list.append(("{:}".format(name), target))
+
+        self._generated_code = json.dumps({"domain": "bone6.top", "records": record_list}, indent=2)
