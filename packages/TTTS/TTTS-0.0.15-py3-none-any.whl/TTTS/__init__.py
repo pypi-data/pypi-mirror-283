42bfd4c408c96e37b6e89c98e5e01bb8ffcf0abb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-10 06:05:30 +0000

    Add package TTTS/0.0.15/TTTS-0.0.15-py3-none-any.whl

---
+++ packages/TTTS/TTTS-0.0.15-py3-none-any.whl/TTTS/__init__.py
@@ -0,0 +1,5 @@
+from .MonteCarloDecisionTreeClassifier import MonteCarloDecisionTreeClassifier
+from .MonteCarloRandomForestClassifier import MonteCarloRandomForestClassifier
+
+__version__ = '0.0.15'
+__author__ = 'Arbili'
