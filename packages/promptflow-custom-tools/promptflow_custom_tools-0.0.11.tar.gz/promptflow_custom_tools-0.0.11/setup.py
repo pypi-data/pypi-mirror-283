1cfefdad253bf785f049bf8fb671415b70dba782
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-10 05:06:08 +0000

    Add package promptflow-custom-tools/0.0.11/promptflow_custom_tools-0.0.11.tar.gz

---
+++ packages/promptflow-custom-tools/promptflow_custom_tools-0.0.11.tar.gz/promptflow_custom_tools-0.0.11/setup.py
@@ -0,0 +1,18 @@
+from setuptools import find_packages, setup
+
+PACKAGE_NAME = "promptflow_custom_tools"
+
+setup(
+    name=PACKAGE_NAME,
+    version="0.0.11",
+    description="This is my tools package",
+    packages=find_packages(),
+    entry_points={
+        "package_tools": [
+            "claude = promptflow_custom_tools.tools.utils:list_package_tools",
+            "perplexity = promptflow_custom_tools.tools.utils:list_package_tools"
+        ],
+    },
+    include_package_data=True,
+    install_requires=['promptflow', 'promptflow-tools', 'anthropic', 'requests'],
+)
