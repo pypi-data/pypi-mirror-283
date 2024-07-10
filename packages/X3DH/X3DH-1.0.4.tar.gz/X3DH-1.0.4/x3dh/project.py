c8d21bef92e2eddfe2a1baeb08f2e9d7cd2ad2d5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-10 04:04:52 +0000

    Add package X3DH/1.0.4/X3DH-1.0.4.tar.gz

---
+++ packages/X3DH/X3DH-1.0.4.tar.gz/X3DH-1.0.4/x3dh/project.py
@@ -0,0 +1,13 @@
+__all__ = [ "project" ]  # pylint: disable=unused-variable
+
+project = {
+    "name"         : "X3DH",
+    "description"  : "A Python implementation of the Extended Triple Diffie-Hellman key agreement protocol.",
+    "url"          : "https://github.com/Syndace/python-x3dh",
+    "year"         : "2022",
+    "author"       : "Tim Henkes (Syndace)",
+    "author_email" : "me@syndace.dev",
+    "categories"   : [
+        "Topic :: Security :: Cryptography"
+    ]
+}
