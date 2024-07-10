d3a6b635b67ca6efa78a4287dc97482da3a31de5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-10 05:06:00 +0000

    Add package notification-schedule-library/0.4/notification_schedule_library-0.4.tar.gz

---
+++ packages/notification-schedule-library/notification_schedule_library-0.4.tar.gz/notification_schedule_library-0.4/setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="notification_schedule_library",
+    version="0.4",
+    packages=find_packages(),
+    description="A library for handling notifications.",
+    author="Gabriel",
+    author_email="gabr.fern99@gmail.com",
+    url="https://github.com/gabrfern99/notification_library",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    install_requires=[
+        'requests',  # Example dependency
+    ],
+    python_requires='>=3.6',
+
+)
