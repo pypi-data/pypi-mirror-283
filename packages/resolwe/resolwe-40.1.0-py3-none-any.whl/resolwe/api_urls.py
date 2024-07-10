1b4df808aaed486599e7f485d269463f7fcaf7e8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-10 07:05:06 +0000

    Add package resolwe/40.1.0/resolwe-40.1.0-py3-none-any.whl

---
+++ packages/resolwe/resolwe-40.1.0-py3-none-any.whl/resolwe/api_urls.py
@@ -0,0 +1,38 @@
+"""Urls for Django Rest Framework."""
+
+from django.urls import include, path
+from rest_framework import routers
+
+from .flow.views import (
+    AnnotationFieldViewSet,
+    AnnotationPresetViewSet,
+    AnnotationValueViewSet,
+    CollectionViewSet,
+    DataViewSet,
+    DescriptorSchemaViewSet,
+    EntityViewSet,
+    ProcessViewSet,
+    RelationViewSet,
+    StorageViewSet,
+)
+from .observers.views import BackgroundTaksViewSet
+from .storage.views import UploadConfig
+
+api_router = routers.DefaultRouter(trailing_slash=False)
+api_router.register(r"collection", CollectionViewSet)
+api_router.register(r"process", ProcessViewSet)
+api_router.register(r"data", DataViewSet)
+api_router.register(r"entity", EntityViewSet)
+api_router.register(r"relation", RelationViewSet)
+api_router.register(r"descriptorschema", DescriptorSchemaViewSet)
+api_router.register(r"storage", StorageViewSet)
+api_router.register(r"upload_config", UploadConfig, basename="upload_config")
+api_router.register(r"task", BackgroundTaksViewSet, basename="backgroundtask")
+api_router.register(r"annotation_field", AnnotationFieldViewSet)
+api_router.register(r"annotation_preset", AnnotationPresetViewSet)
+api_router.register(r"annotation_value", AnnotationValueViewSet)
+
+
+urlpatterns = [
+    path("api/", include((api_router.urls, "resolwe-api"), namespace="resolwe-api")),
+]
