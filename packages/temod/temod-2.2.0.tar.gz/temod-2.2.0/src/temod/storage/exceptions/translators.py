e98ca55a9c5992b7f4621a70afb97407ada553a0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-10 03:05:28 +0000

    Add package temod/2.2.0/temod-2.2.0.tar.gz

---
+++ packages/temod/temod-2.2.0.tar.gz/temod-2.2.0/src/temod/storage/exceptions/translators.py
@@ -0,0 +1,9 @@
+class AttributeTranslatorException(Exception):
+	"""docstring for AttributeTranslatorException"""
+	def __init__(self, *args, **kwargs):
+		super(AttributeTranslatorException, self).__init__(*args, **kwargs)
+
+class ConditionTranslatorException(Exception):
+	"""docstring for ConditionTranslatorException"""
+	def __init__(self, *args, **kwargs):
+		super(ConditionTranslatorException, self).__init__(*args, **kwargs)
